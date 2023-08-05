09eaec9b07bebf397e6ae87049e565f6f3ef94e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:28 +0000

    Add package http-router/3.0.0/http_router-3.0.0-cp37-cp37m-musllinux_1_1_x86_64.whl

---
+++ packages/http-router/http_router-3.0.0-cp37-cp37m-musllinux_1_1_x86_64.whl/http_router/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "3.0.0"
+__license__ = "MIT"
+
+
+from .exceptions import MethodNotAllowed, NotFound, RouterError
+from .router import Router
+from .routes import DynamicRoute, Mount, PrefixedRoute, Route
+
+__all__ = (
+    "DynamicRoute",
+    "Mount",
+    "PrefixedRoute",
+    "Route",
+    "Router",
+    "MethodNotAllowed",
+    "NotFound",
+    "RouterError",
+)
