58a2bd0967027a0094f423ea9e8c15de8adfdaff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:20 +0000

    Add package graph2topictm/1.1/graph2topictm-1.1.tar.gz

---
+++ packages/graph2topictm/graph2topictm-1.1.tar.gz/graph2topictm-1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import  setup
+import setuptools
+packages = ['graph2topictm']# 唯一的包名，自己取名
+setup(name='graph2topictm',
+	version='1.1',
+    description='Graph2Topic is a topic model based on PLMs and community detections. more details in https://github.com/lunar-moon/Graph2Topic.git',
+	author='ljp',
+    author_email='313446266@qq.com',
+    packages=setuptools.find_packages()
+    )
