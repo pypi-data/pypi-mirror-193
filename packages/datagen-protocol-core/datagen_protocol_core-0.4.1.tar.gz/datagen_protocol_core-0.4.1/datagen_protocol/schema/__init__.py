a48a0b0f5d95cd6da1312928dbd4b4c2712b0681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:49 +0000

    Add package datagen-protocol-core/0.4.1/datagen_protocol_core-0.4.1.tar.gz

---
+++ packages/datagen-protocol-core/datagen_protocol_core-0.4.1.tar.gz/datagen_protocol_core-0.4.1/datagen_protocol/schema/__init__.py
@@ -0,0 +1,37 @@
+from datagen_protocol.schema.d3 import Point, Rotation, Vector
+from datagen_protocol.schema.environment import (
+    Background,
+    Camera,
+    ExtrinsicParams,
+    IntrinsicParams,
+    Light,
+    LightType,
+    Projection,
+    Wavelength,
+)
+from datagen_protocol.schema.humans.accessories import (
+    Accessories,
+    FrameColor,
+    Glasses,
+    GlassesPosition,
+    LensColor,
+    Mask,
+    MaskColor,
+    MaskPosition,
+    MaskTexture,
+)
+from datagen_protocol.schema.humans.human import (
+    Expression,
+    ExpressionName,
+    Eyes,
+    Gaze,
+    Hair,
+    Eyebrows,
+    FacialHair,
+    HairColor,
+    Head,
+    Human,
+    HumanDatapoint,
+)
+from datagen_protocol.schema.request import DataRequest
+from datagen_protocol.schema.hic.sequence import ClutterLevel, DataSequence
