3a1ae574a29e4f0337f09606d561d7c5fa1b2e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:38 +0000

    Add package videosum/0.0.7/videosum-0.0.7.tar.gz

---
+++ packages/videosum/videosum-0.0.7.tar.gz/videosum-0.0.7/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import setuptools
+import unittest
+
+# Read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(name='videosum',
+    version='0.0.7',
+    description='Python module to summarise a video into a collage.',
+    author='Luis C. Garcia-Peraza Herrera',
+    author_email='luiscarlos.gph@gmail.com',
+    license='MIT License',
+    url='https://github.com/luiscarlosgph/endoseg',
+    packages=[
+        'videosum',
+        'videosum._methods',
+    ],
+    package_dir={
+        'videosum'          : 'src',
+        'videosum._methods' : 'src/_methods',
+    }, 
+    install_requires = [
+        'numpy', 
+        'opencv-python',
+        'numba',
+        'faiss-gpu',
+        'scikit-image',
+        'scikit-learn-extra',
+        'imageio_ffmpeg',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    test_suite='test',
+)
---
+++ packages/videosum/videosum-0.0.7.tar.gz/videosum-0.0.7/src/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.0.7'
+from .videosum import *
+from .inception import *
+from .uid import *
+from .videoreader import *
