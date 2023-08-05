aac418847eaf1d8ab700b7010e6b222a0a1cc2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:24 +0000

    Add package protmo/0.0.22/protmo-0.0.22-py3-none-any.whl

---
+++ packages/protmo/protmo-0.0.22-py3-none-any.whl/protmo/create_proto_file.py
@@ -0,0 +1,26 @@
+from protmo.pm import *
+from protmo.ast import *
+from protmo.grpc_ast import AstToGrpcAst
+from protmo.settings_loader import settings
+
+
+def create_proto_file(package: str) -> None:
+
+    content = 'syntax = "proto3";\n\n'
+    content += f'package {package};\n\n'
+
+    seen = set()
+    for model_class in (Message.__subclasses__()):
+        msg = MessageInfo(model_class)
+        proto_messages = AstToGrpcAst(msg)
+        content += f'\n// {msg.name} ------\n\n'
+        content += str(proto_messages.messages[0]) + '\n\n'
+        if proto_messages.service:
+            content += str(proto_messages.service) + '\n\n'
+        for proto_msg in proto_messages.messages[1:]:
+            if proto_msg.name not in seen:
+                content += str(proto_msg) + '\n\n'
+                seen.add(proto_msg.name)
+
+    with open(f'{settings.proto_folder}/{package}.proto', 'w') as f:
+        f.write(content)
