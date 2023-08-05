71a903e9b4d2fd4705740ee22cedd398538e2b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:36 +0000

    Add package recpack/0.3.5/recpack-0.3.5.tar.gz

---
+++ packages/recpack/recpack-0.3.5.tar.gz/recpack-0.3.5/recpack/tests/test_algorithms/test_factorization.py
@@ -0,0 +1,62 @@
+# RecPack, An Experimentation Toolkit for Top-N Recommendation
+# Copyright (C) 2020  Froomle N.V.
+# License: GNU AGPLv3 - https://gitlab.com/recpack-maintainers/recpack/-/blob/master/LICENSE
+# Author:
+#   Lien Michiels
+#   Robin Verachtert
+
+import pytest
+import sklearn
+from recpack.algorithms import NMF, SVD
+
+
+def test_nmf(pageviews):
+    a = NMF(2)
+    a.fit(pageviews)
+
+    assert a.user_embedding_ is not None
+    assert a.item_embedding_ is not None
+
+    prediction = a.predict(pageviews)
+    assert prediction.shape == pageviews.shape
+
+    # Reconstruction expected, so items with 1 should have high scores.
+    assert prediction[0, 0] > prediction[0, 1]
+    assert prediction[0, 2] > prediction[0, 1]
+    assert prediction[0, 3] > prediction[0, 1]
+
+    assert prediction[2, 1] > prediction[2, 0]
+    assert prediction[2, 1] > prediction[2, 2]
+
+
+def test_nmf_predict_no_fit(pageviews):
+    a = NMF(2)
+
+    with pytest.raises(sklearn.exceptions.NotFittedError):
+        a.predict(pageviews[2])
+
+
+def test_svd(pageviews):
+    a = SVD(2)
+    a.fit(pageviews)
+
+    assert a.user_embedding_ is not None
+    assert a.item_embedding_ is not None
+
+    prediction = a.predict(pageviews)
+    assert prediction.shape == pageviews.shape
+
+    # Reconstruction expected, so items with 1 should have high scores.
+    assert prediction[0, 0] > prediction[0, 1]
+    assert prediction[0, 2] > prediction[0, 1]
+    assert prediction[0, 3] > prediction[0, 1]
+
+    assert prediction[2, 1] > prediction[2, 0]
+    assert prediction[2, 1] > prediction[2, 2]
+
+
+def test_svd_predict_no_fit(pageviews):
+    a = SVD(2)
+
+    with pytest.raises(sklearn.exceptions.NotFittedError):
+        a.predict(pageviews[2])
