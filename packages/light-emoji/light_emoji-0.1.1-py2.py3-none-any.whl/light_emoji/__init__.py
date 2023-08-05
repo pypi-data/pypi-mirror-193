5416e8be5bff6aebe8ae8e4f7adf703f544e3908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:29 +0000

    Add package light-emoji/0.1.1/light_emoji-0.1.1-py2.py3-none-any.whl

---
+++ packages/light-emoji/light_emoji-0.1.1-py2.py3-none-any.whl/light_emoji/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+Light emoji enumeration.
+"""
+
+from ._version import __version__
+
+__short_description__ = "Light emoji enumeration."
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
+
+from . import emojis, common
---
+++ packages/light-emoji/light_emoji-0.1.1-py2.py3-none-any.whl/light_emoji/common.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+
+from . import emojis
+
+# shape
+red_circle = emojis.red_circle
+yellow_circle = emojis.yellow_circle
+green_circle = emojis.green_circle
+blue_circle = emojis.blue_circle
+orange_circle = emojis.orange_circle
+purple_circle = emojis.purple_circle
+black_circle = emojis.black_circle
+white_circle = emojis.white_circle
+brown_circle = emojis.brown_circle
+
+red_square = emojis.red_square
+yellow_square = emojis.yellow_square
+green_square = emojis.green_square
+blue_square = emojis.blue_square
+orange_square = emojis.orange_square
+purple_square = emojis.purple_square
+black_large_square = emojis.black_large_square
+white_large_square = emojis.white_large_square
+black_square_button = emojis.black_square_button
+white_square_button = emojis.white_square_button
+brown_square = emojis.brown_square
+
+red_heart = emojis.sparkling_heart
+yellow_heart = emojis.yellow_heart
+green_heart = emojis.green_heart
+blue_heart = emojis.blue_heart
+orange_heart = emojis.orange_heart
+purple_heart = emojis.purple_heart
+brown_heart = emojis.brown_heart
+black_heart = emojis.black_heart
+white_heart = emojis.white_heart
+two_hearts = emojis.two_hearts
+
+python = emojis.snake
+
+# hand gesture
+thumbs_up = emojis.thumbs_up
+thumbs_down = emojis.thumbs_down
+pointing_up = emojis.backhand_index_pointing_up
+pointing_down = emojis.backhand_index_pointing_down
+pointing_left = emojis.backhand_index_pointing_left
+pointing_right = emojis.backhand_index_pointing_right
+
+fist_raised = emojis.raised_fist
+fist_oncoming = emojis.oncoming_fist
+fist_left = emojis.left_facing_fist
+fist_right = emojis.right_facing_fist
+
+# start and end
+start_time = "⏱"
+end_time = emojis.alarm_clock
+
+# succeeded and failed
+succeeded = emojis.check_mark_button
+failed = emojis.cross_mark
+
+# flow control
+play_or_pause = emojis.play_or_pause_button
+pause = emojis.pause_button
+stop = emojis.stop_button
+sync = emojis.counterclockwise_arrows_button
+
+fast_forward_button = emojis.fast_forward_button
+fast_reverse_button = emojis.fast_reverse_button
+fast_up_button = emojis.fast_up_button
+fast_down_button = emojis.fast_down_button
+
+# flower
+cherry = emojis.cherry_blossom
+rose = emojis.rose
+sunflower = emojis.sunflower
+
+# plant
+palm_tree = emojis.palm_tree
+seedling = emojis.seedling
+herb = emojis.herb
+four_leaf_clover = emojis.four_leaf_clover
+evergreen_tree = emojis.evergreen_tree
+
+# CI/CD
+install = emojis.floppy_disk
+test = emojis.test_tube
+build = emojis.building_construction
+deploy = emojis.rocket
+delete = emojis.wastebasket
+package = emojis.package
+
+# warning and error
+fire = emojis.fire
+explode = emojis.collision
+bomb = emojis.bomb
+red_exclamation_mark = emojis.red_exclamation_mark
+red_question_mark = emojis.red_question_mark
+prohibited = emojis.prohibited
+
+# internet
+email = emojis.e_mail
+laptop = emojis.laptop
+ladder = emojis.ladder
+label = emojis.label
+speech_balloon = emojis.speech_balloon
+thought_balloon = emojis.thought_balloon
+zzz = emojis.ZZZ
+
+# body parts
+eyes = emojis.eyes
+
+# party
+party_popper = emojis.party_popper
+
+# face
+happy_face = emojis.grinning_face
+hot_face = emojis.hot_face
+
+# animal
+monkey_face = emojis.monkey_face
+dog = emojis.dog_face
+wolf = emojis.wolf
+poodle = emojis.poodle
+fox_face = emojis.fox
+cat = emojis.cat_face
+lion = emojis.lion
+tiger = emojis.tiger_face
+horse = emojis.horse
+racehorse = emojis.horse_face
+unicorn = emojis.unicorn
+deer = emojis.deer
+cow = emojis.cow_face
+pig = emojis.pig_face
