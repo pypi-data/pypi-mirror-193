ff6ba71a654e69fb7facc6bcdfb640a5a8dff957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:03 +0000

    Add package voilite/0.0.1/voilite-0.0.1.tar.gz

---
+++ packages/voilite/voilite-0.0.1.tar.gz/voilite-0.0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+__AUTHOR__ = 'QuantStack dev team'
+
+setup(
+    name='voilite',
+    version='0.0.1',
+    author=__AUTHOR__,
+    maintainer=__AUTHOR__,
+    url='https://github.com/voila-dashboards/voilite',
+    license='BSD 3-Clause',
+    python_requires='>=3.6',
+    install_requires=[
+    ],
+    extras_require={
+    },
+    platforms=['any'],
+    classifiers=[
+    ],
+)
