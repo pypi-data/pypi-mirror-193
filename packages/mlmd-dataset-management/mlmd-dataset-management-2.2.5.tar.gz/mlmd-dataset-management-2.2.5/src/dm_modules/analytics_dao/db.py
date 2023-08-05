3f27cdf39faae3cf7533200e8e64dfd5a2db3d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:32 +0000

    Add package mlmd-dataset-management/2.2.5/mlmd-dataset-management-2.2.5.tar.gz

---
+++ packages/mlmd-dataset-management/mlmd-dataset-management-2.2.5.tar.gz/mlmd-dataset-management-2.2.5/src/dm_modules/analytics_dao/db.py
@@ -0,0 +1,14 @@
+from google.cloud import firestore
+
+def get_connection(project_id="mlops-315607"):
+    db = firestore.Client(project=project_id)
+    return db
+
+def get_collection_name(context, env_name):
+    if context == "prediction":
+        if not env_name:
+            return None
+        return "{}_ds_predictions".format(env_name)
+    if context == "groundtruth":
+        return "ds_groundtruth"
+    return None
