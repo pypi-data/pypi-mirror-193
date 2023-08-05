23cf051300d4faa67d315e862e57da4d6bb7889a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:26 +0000

    Add package cocktaildbpy/0.0.2/cocktaildbpy-0.0.2-py3-none-any.whl

---
+++ packages/cocktaildbpy/cocktaildbpy-0.0.2-py3-none-any.whl/cocktaildbpy/drink.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from typing import List, Dict
+
+from cocktaildbpy.constants import Category, Ingredient, Glass
+
+
+@dataclass
+class DrinkShortcut:
+    name: str
+    thumbnail: str
+    id: str
+
+    @classmethod
+    def loads(cls, **kwargs):
+        return cls(
+            name=kwargs["strDrink"],
+            thumbnail=kwargs["strDrinkThumb"],
+            id=kwargs["idDrink"],
+        )
+
+@dataclass
+class Drink:
+    id: str
+    name: str
+    category: str
+    alcoholic: bool
+    glass: str
+    instructions: List[str]
+    ingriedients: Dict[str, str]
+
+    @classmethod
+    def loads(cls, **kwargs):
+        ingriedients = {}
+        for k, v in kwargs.items():
+            if "Ingredient" in k:
+                if v is not None:
+                    num = k.split("Ingredient")[1]
+                    ingriedients[v] = kwargs[f"strMeasure{num}"]
+
+        return cls(
+            id=kwargs["idDrink"],
+            name=kwargs["strDrink"],
+            category=Category(kwargs["strCategory"]),
+            alcoholic=True if kwargs["strAlcoholic"] == "Alcoholic" else False,
+            glass=Glass(kwargs["strGlass"]),
+            instructions=kwargs["strInstructions"].split("."),
+            ingriedients=ingriedients,
+        )
