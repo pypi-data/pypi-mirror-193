ee67fff8e8d03e105bf58c9c875ef226ce5686ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:47 +0000

    Add package monopyly/1.2.2/monopyly-1.2.2.tar.gz

---
+++ packages/monopyly/monopyly-1.2.2.tar.gz/monopyly-1.2.2/monopyly/common/transactions.py
@@ -0,0 +1,67 @@
+"""
+Tools for building a common transaction interface.
+"""
+from flask import current_app
+
+from ..database.models import (
+    BankAccountTypeView,
+    BankTransactionView,
+    CreditTransactionView,
+)
+
+
+def get_linked_transaction(transaction):
+    """
+    Find a transaction that is linked to the given transaction.
+
+    Checks all transaction databases for a transaction that matches
+    the given transaction.
+
+    Parameters
+    ----------
+    transaction : Transaction
+        The transaction for which to find a linked transaction.
+
+    Returns
+    -------
+    linked_transaction : Transaction
+        A transaction that is linked to the given transaction. If no
+        linked transaction is found, `None` is returned.
+    """
+    internal_transaction_id = transaction.internal_transaction_id
+    if not internal_transaction_id:
+        return None
+    # First, check if there is a matching bank transaction
+    linked_transaction = _get_linked_bank_transaction(
+        transaction.id, internal_transaction_id
+    )
+    if not linked_transaction:
+        # Otherwise, check if there is a matching credit transaction
+        linked_transaction = _get_linked_credit_transaction(
+            transaction.id, internal_transaction_id
+        )
+    return linked_transaction
+
+
+def _get_linked_bank_transaction(transaction_id, internal_transaction_id):
+    """Get a bank transaction linked to the given transaction."""
+    query = BankTransactionView.select_for_user().join(BankAccountTypeView)
+    criteria = [
+        BankTransactionView.id != transaction_id,
+        BankTransactionView.internal_transaction_id == internal_transaction_id,
+    ]
+    query = query.where(*criteria)
+    transaction = current_app.db.session.execute(query).scalar_one_or_none()
+    return transaction
+
+
+def _get_linked_credit_transaction(transaction_id, internal_transaction_id):
+    """Get a credit transaction linked to the given transaction."""
+    query = CreditTransactionView.select_for_user()
+    criteria = [
+        CreditTransactionView.id != transaction_id,
+        CreditTransactionView.internal_transaction_id == internal_transaction_id,
+    ]
+    query = query.where(*criteria)
+    transaction = current_app.db.session.execute(query).scalar_one_or_none()
+    return transaction
---
+++ packages/monopyly/monopyly-1.2.2.tar.gz/monopyly-1.2.2/monopyly/credit/accounts.py
@@ -0,0 +1,69 @@
+"""
+Tools for interacting with credit accounts in the database.
+"""
+from ..database.handler import DatabaseHandler
+from ..database.models import CreditAccount
+
+
+class CreditAccountHandler(DatabaseHandler):
+    """
+    A database handler for managing credit accounts.
+
+    Parameters
+    ----------
+    user_id : int
+        The ID of the user who is the subject of database access. If not
+        given, the handler defaults to using the logged-in user.
+
+    Attributes
+    ----------
+    table : str
+        The name of the database table that this handler manages.
+    db : sqlite3.Connection
+        A connection to the database for interfacing.
+    user_id : int
+        The ID of the user who is the subject of database access.
+    """
+
+    model = CreditAccount
+
+    @classmethod
+    def get_accounts(cls, bank_ids=None):
+        """
+        Get credit accounts from the database.
+
+        Query the database to select credit account fields. Accounts can
+        be filtered by the issuing bank.
+
+        Parameters
+        ----------
+        bank_ids : tuple of int, optional
+            A sequence of bank IDs for which accounts will be selected
+            (if `None`, all banks will be selected).
+
+        Returns
+        -------
+        accounts : sqlalchemy.engine.ScalarResult
+            Returns credit accounts matching the criteria.
+        """
+        criteria = [
+            cls._filter_values(cls.model.bank_id, bank_ids),
+        ]
+        accounts = super().get_entries(*criteria)
+        return accounts
+
+    @classmethod
+    def delete_entry(cls, entry_id):
+        """
+        Delete a credit card account from the database.
+
+        Given an account ID, delete the credit card account from the
+        database. Deleting an account will also delete all credit cards
+        (along with statements and transactions) for that account.
+
+        Parameters
+        ----------
+        entry_id : int
+            The ID of the account to be deleted.
+        """
+        super().delete_entry(entry_id)
