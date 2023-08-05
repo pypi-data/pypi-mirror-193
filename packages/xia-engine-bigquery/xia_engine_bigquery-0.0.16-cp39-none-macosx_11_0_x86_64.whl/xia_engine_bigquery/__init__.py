ac3bc9c0f278037a9acae3ff34334f8675bb8ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:05 +0000

    Add package xia-engine-bigquery/0.0.16/xia_engine_bigquery-0.0.16-cp39-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-engine-bigquery/xia_engine_bigquery-0.0.16-cp39-none-macosx_11_0_x86_64.whl/xia_engine_bigquery/__init__.py
@@ -0,0 +1,12 @@
+from xia_engine_bigquery.proto import DocToProto
+from xia_engine_bigquery.schema import DocToSchema
+from xia_engine_bigquery.engine import BigqueryEngine, BigqueryWriteEngine, BigqueryStreamEngine
+
+
+__all__ = [
+    "DocToProto",
+    "DocToSchema",
+    'BigqueryEngine', "BigqueryWriteEngine", "BigqueryStreamEngine"
+]
+
+__version__ = "0.0.16"
