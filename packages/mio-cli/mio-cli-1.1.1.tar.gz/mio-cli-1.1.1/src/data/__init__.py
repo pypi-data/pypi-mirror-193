719e2b60f8ef8fc5bcc623fc68d50a21be51d75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:57 +0000

    Add package mio-cli/1.1.1/mio-cli-1.1.1.tar.gz

---
+++ packages/mio-cli/mio-cli-1.1.1.tar.gz/mio-cli-1.1.1/src/data/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2021-2022 Datum Technology Corporation
+# SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+########################################################################################################################
