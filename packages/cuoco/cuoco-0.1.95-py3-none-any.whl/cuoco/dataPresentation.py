07330876e10cf83629f964b095d9726006a1bf32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:46 +0000

    Add package cuoco/0.1.95/cuoco-0.1.95-py3-none-any.whl

---
+++ packages/cuoco/cuoco-0.1.95-py3-none-any.whl/cuoco/dataPresentation.py
@@ -0,0 +1,53 @@
+# Count number of nans
+def countNans(df):
+    nan_count = df.isna().sum().sum()
+    return nan_count
+
+def summary(json_file):
+    print("*************************")
+    print("** SUMMARY             **")
+    print("")
+    print("· Name of the preprocessed data file: " + json_file['new_fileName'])
+    print("· Route of the preprocessed data file: " + json_file['new_file_route'])
+    print("· Output format: " + json_file['output_format'])
+    print("Applied normalization: " + json_file['normalize_method'])
+    print("Applied balance method: " + json_file['balance_data'])
+    print("")
+    print("")
+
+
+def showStats(original_df, actual_df):
+    # Dataset information
+    print("*************************")
+    print("** DATASET INFORMATION **")
+    print("")
+    print("- Number of rows: " + str(len(actual_df)))
+    print("")
+    print("- Columns: " + actual_df.columns)
+
+    print("")
+    print("-------------------------")
+    print("DATASET STATISTICS")
+    print("")
+
+    # NANS
+    print("· Count of NANS in the raw dataset:")
+    print(countNans(original_df))
+    print(" ")
+
+    # Removed rows
+    print("· Number of removed rows:")
+    print(len(original_df) - len(actual_df))
+    print(" ")
+
+    # Count of classes before oversampling
+    print("· Data distribution (on the y_col column)")
+
+
+def writeDF(df, route, name, output_format, index_format):
+    if output_format == "csv" or output_format == "txt":
+        df.to_csv(route + name + "." + output_format, index=index_format)
+    elif output_format == "orc":
+        df.to_orc(route + name + "." + output_format, index=index_format)
+    elif output_format == "parquet":
+        df.to_parquet(route + name + "." + output_format, index=index_format)
