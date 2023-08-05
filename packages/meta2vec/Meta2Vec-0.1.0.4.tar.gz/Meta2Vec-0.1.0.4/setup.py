4f29fb3fee7404bbad2e3f4090901761a33eff0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:41 +0000

    Add package meta2vec/0.1.0.4/Meta2Vec-0.1.0.4.tar.gz

---
+++ packages/meta2vec/Meta2Vec-0.1.0.4.tar.gz/Meta2Vec-0.1.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="Meta2Vec",
+    version="0.1.0.4",
+    author="Yuxing Lu",
+    author_email="yxlu0613@gmail.com",
+    description="Meta2Vec, an embedding for Metabolomics study",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/YuxingLu613/meta2vec",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
