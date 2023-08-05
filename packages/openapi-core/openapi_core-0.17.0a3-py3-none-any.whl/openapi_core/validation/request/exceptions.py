3948c515824b6a990e991fe9559a36f74e4cc8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:53 +0000

    Add package openapi-core/0.17.0a3/openapi_core-0.17.0a3-py3-none-any.whl

---
+++ packages/openapi-core/openapi_core-0.17.0a3-py3-none-any.whl/openapi_core/validation/request/exceptions.py
@@ -0,0 +1,91 @@
+import warnings
+from dataclasses import dataclass
+from typing import Iterable
+
+from openapi_core.datatypes import Parameters
+from openapi_core.exceptions import OpenAPIError
+from openapi_core.spec import Spec
+from openapi_core.validation.exceptions import ValidationError
+from openapi_core.validation.schemas.exceptions import ValidateError
+
+
+@dataclass
+class ParametersError(Exception):
+    parameters: Parameters
+    errors: Iterable[OpenAPIError]
+
+    @property
+    def context(self) -> Iterable[OpenAPIError]:
+        warnings.warn(
+            "context property of ParametersError is deprecated. "
+            "Use errors instead.",
+            DeprecationWarning,
+        )
+        return self.errors
+
+
+class RequestError(ValidationError):
+    """Request error"""
+
+
+class RequestBodyError(RequestError):
+    def __str__(self) -> str:
+        return "Request body error"
+
+
+class InvalidRequestBody(RequestBodyError, ValidateError):
+    """Invalid request body"""
+
+
+class MissingRequestBodyError(RequestBodyError):
+    """Missing request body error"""
+
+
+class MissingRequestBody(MissingRequestBodyError):
+    def __str__(self) -> str:
+        return "Missing request body"
+
+
+class MissingRequiredRequestBody(MissingRequestBodyError):
+    def __str__(self) -> str:
+        return "Missing required request body"
+
+
+@dataclass
+class ParameterError(RequestError):
+    name: str
+    location: str
+
+    @classmethod
+    def from_spec(cls, spec: Spec) -> "ParameterError":
+        return cls(spec["name"], spec["in"])
+
+    def __str__(self) -> str:
+        return f"{self.location.title()} parameter error: {self.name}"
+
+
+class InvalidParameter(ParameterError, ValidateError):
+    def __str__(self) -> str:
+        return f"Invalid {self.location} parameter: {self.name}"
+
+
+class MissingParameterError(ParameterError):
+    """Missing parameter error"""
+
+
+class MissingParameter(MissingParameterError):
+    def __str__(self) -> str:
+        return f"Missing {self.location} parameter: {self.name}"
+
+
+class MissingRequiredParameter(MissingParameterError):
+    def __str__(self) -> str:
+        return f"Missing required {self.location} parameter: {self.name}"
+
+
+class SecurityError(RequestError):
+    pass
+
+
+class InvalidSecurity(SecurityError, ValidateError):
+    """Invalid security"""
---
+++ packages/openapi-core/openapi_core-0.17.0a3-py3-none-any.whl/openapi_core/validation/response/exceptions.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+from typing import Any
+from typing import Dict
+from typing import Iterable
+
+from openapi_core.exceptions import OpenAPIError
+from openapi_core.validation.exceptions import ValidationError
+from openapi_core.validation.schemas.exceptions import ValidateError
+
+
+@dataclass
+class HeadersError(Exception):
+    headers: Dict[str, Any]
+    context: Iterable[OpenAPIError]
+
+
+class ResponseError(ValidationError):
+    """Response error"""
+
+
+class DataError(ResponseError):
+    """Data error"""
+
+
+class InvalidData(DataError, ValidateError):
+    """Invalid data"""
+
+
+class MissingData(DataError):
+    def __str__(self) -> str:
+        return "Missing response data"
+
+
+@dataclass
+class HeaderError(ResponseError):
+    name: str
+
+
+class InvalidHeader(HeaderError, ValidateError):
+    """Invalid header"""
+
+
+class MissingHeaderError(HeaderError):
+    """Missing header error"""
+
+
+class MissingHeader(MissingHeaderError):
+    def __str__(self) -> str:
+        return f"Missing header (without default value): {self.name}"
+
+
+class MissingRequiredHeader(MissingHeaderError):
+    def __str__(self) -> str:
+        return f"Missing required header: {self.name}"
