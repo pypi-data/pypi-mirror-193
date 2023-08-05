5ac8d73ceed5d46052f4f3aca891853604b79078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:41 +0000

    Add package fastiot/0.2.173/fastiot-0.2.173.tar.gz

---
+++ packages/fastiot/fastiot-0.2.173.tar.gz/fastiot-0.2.173/src/fastiot/cli/model/compose_info.py
@@ -0,0 +1,15 @@
+from typing import List, Dict
+
+from pydantic.main import BaseModel
+
+
+class ServiceComposeInfo(BaseModel):
+    name: str
+    image: str
+    environment: Dict[str, str]
+    ports: List[str]
+    volumes: List[str]
+    devices: List[str] = []
+    tmpfs: List[str] = []
+
+    privileged: bool = False
