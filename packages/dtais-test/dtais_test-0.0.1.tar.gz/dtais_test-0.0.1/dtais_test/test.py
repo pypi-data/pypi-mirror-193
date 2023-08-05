55ed2d90ebe27d0a3bbaeef163c2e60df3273af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:56 +0000

    Add package dtais-test/0.0.1/dtais_test-0.0.1.tar.gz

---
+++ packages/dtais-test/dtais_test-0.0.1.tar.gz/dtais_test-0.0.1/dtais_test/test.py
@@ -0,0 +1,6 @@
+def test():
+    print('dtais_test test main okok')
+
+
+if __name__ == "__main__":
+    test()
---
+++ packages/dtais-test/dtais_test-0.0.1.tar.gz/dtais_test-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="dtais_test",
+    version="0.0.1",
+    author="Lazarrt",
+    author_email="LazarrtLi@outlook.com",
+    description="dtais cmd test",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
