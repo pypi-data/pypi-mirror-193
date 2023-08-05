00c3ffa888608c71f966ac27b5683ce13aee7a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:23 +0000

    Add package volcengine-python-sdk/1.0.3/volcengine-python-sdk-1.0.3.tar.gz

---
+++ packages/volcengine-python-sdk/volcengine-python-sdk-1.0.3.tar.gz/volcengine-python-sdk-1.0.3/volcenginesdkcore/__init__.py
@@ -0,0 +1,5 @@
+# coding:utf-8
+
+from volcenginesdkcore.configuration import Configuration
+from volcenginesdkcore.api_client import ApiClient
+from volcenginesdkcore.universal import UniversalApi, UniversalInfo
