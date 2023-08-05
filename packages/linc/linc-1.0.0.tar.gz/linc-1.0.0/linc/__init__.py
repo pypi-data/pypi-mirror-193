83b623eb1c1463c32d196f32a983fdbe5e44ce34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:47 +0000

    Add package linc/1.0.0/linc-1.0.0.tar.gz

---
+++ packages/linc/linc-1.0.0.tar.gz/linc-1.0.0/linc/__init__.py
@@ -0,0 +1,12 @@
+from pdb import set_trace
+import pkg_resources
+
+from .write.writer_nc import write_nc_legacy
+from .config.read import get_config
+
+__version__ = pkg_resources.get_distribution("linc").version
+
+__all__ = [
+    "write_nc_legacy",
+    "get_config"
+]
