c5f272291c438665b3646786b49d3dfc8e379a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:59 +0000

    Add package LWD-utils/1.0.0/LWD_utils-1.0.0-py3-none-any.whl

---
+++ packages/LWD-utils/LWD_utils-1.0.0-py3-none-any.whl/LWD_utils/document_util.py
@@ -0,0 +1,6 @@
+from LWD_utils.translate_util import *
+from LWD_utils.pdf_util import *
+from LWD_utils.office_util import *
+
+if __name__ == "__main__":
+    basic_config(logs_style=LOG_STYLE_PRINT)
