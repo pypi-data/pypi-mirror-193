731d4bbd495083626b3e019d564957113e71782a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:03 +0000

    Add package extrarandom/1.0.0/extrarandom-1.0.0.tar.gz

---
+++ packages/extrarandom/extrarandom-1.0.0.tar.gz/extrarandom-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="extrarandom",
+    version="1.0.0",
+    description="More random utils",
+    url="https://github.com/tovicheung/extrarandom",
+    author="tovicheung",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="random",
+    packages=find_packages("extrarandom"),
+)
