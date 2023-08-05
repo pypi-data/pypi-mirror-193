ab5b24e630110af12316f8b6eff05779102aa791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:21 +0000

    Add package voici/0.0.1/voici-0.0.1.tar.gz

---
+++ packages/voici/voici-0.0.1.tar.gz/voici-0.0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+__AUTHOR__ = 'QuantStack dev team'
+
+setup(
+    name='voici',
+    version='0.0.1',
+    author=__AUTHOR__,
+    maintainer=__AUTHOR__,
+    url='https://github.com/voila-dashboards/voici',
+    license='BSD 3-Clause',
+    python_requires='>=3.6',
+    install_requires=[
+    ],
+    extras_require={
+    },
+    platforms=['any'],
+    classifiers=[
+    ],
+)
