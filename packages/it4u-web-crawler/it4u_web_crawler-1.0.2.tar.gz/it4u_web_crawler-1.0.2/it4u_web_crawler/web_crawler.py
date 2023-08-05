a58c5e9ec24d748cfe0ab385f63de5a007b6ce17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:00 +0000

    Add package it4u-web-crawler/1.0.2/it4u_web_crawler-1.0.2.tar.gz

---
+++ packages/it4u-web-crawler/it4u_web_crawler-1.0.2.tar.gz/it4u_web_crawler-1.0.2/it4u_web_crawler/web_crawler.py
@@ -0,0 +1,63 @@
+import threading
+import logging
+import requests
+import json
+import time
+import reactivex as rx
+from reactivex import Observable, operators as ops
+from typing import Dict, Any, Callable, List
+from it4u_http_request import HttpRequest, default_cache_key_calculator
+
+class Url:
+    def __init__(self, url: str, method: str = "GET", params: Dict[str, Any] = None, json: Dict[str, Any] = None) -> None:
+        self.url: str = url
+        self.method: str = method
+        self.params: Dict[str, Any] = params
+        self.json: Dict[str, Any] = json
+
+
+class WebCrawler:
+    def __init__(self, max_threads: int = 16, session_builder: Callable[[], requests.Session] = lambda: requests.Session(), headers: Dict[str, str] = None):
+        self.http_request = HttpRequest(
+            cache_key_calculator=default_cache_key_calculator, 
+            session_builder=session_builder, 
+            headers=headers)
+        self.max_threads: int = max_threads
+        self.responses_lock = threading.Lock()
+
+    def crawl_data(self, responses: List[Any], semaphore, url: Url, pipes: List[Observable] = [], headers: Dict[str, str] = None, allow_redirects: bool = True, timeout: int = 5, max_retries: int = 3) -> Any:
+        with semaphore:
+            try:
+                response = self.http_request \
+                    .get_response_stream(url=url,
+                                         pipes=pipes,
+                                         headers=headers,
+                                         allow_redirects=allow_redirects,
+                                         timeout=timeout,
+                                         max_retries=max_retries)
+
+                with self.responses_lock:
+                    responses.append(response)
+
+                logging.info("Crawled data from: %s", url.url)
+            except requests.exceptions.RequestException as e:
+                logging.error("Error crawling data from: %s, %s", url.url, e)
+
+    def start_crawling(self, urls: List[Url], pipes: List[Observable] = [], headers: Dict[str, str] = None, allow_redirects: bool = True, timeout: int = 5, max_retries: int = 3) -> List[Any]:
+        responses = []
+        semaphore = threading.Semaphore(self.max_threads)
+        threads = []
+        start_time = time.time()
+
+        for url in urls:
+            thread = threading.Thread(target=self.crawl_data, args=(responses, semaphore, url, pipes, headers, allow_redirects, timeout, max_retries))
+            thread.start()
+            threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+
+        end_time = time.time()
+        logging.info("All threads have finished.")
+        execution_time = end_time - start_time
+        return responses, execution_time
---
+++ packages/it4u-web-crawler/it4u_web_crawler-1.0.2.tar.gz/it4u_web_crawler-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'it4u_web_crawler',
+    packages = ['it4u_web_crawler'],
+    version = 'v1.0.2',  # Ideally should be same as your GitHub release tag varsion
+    description = 'WebCrawler will fetch all requests parallel',
+    author = 'Louis',
+    author_email = 'thonh.it@gmail.com',
+    url = 'https://github.com/louis-it4u/web-crawler',
+    download_url = 'https://github.com/louis-it4u/web-crawler/archive/refs/tags/v1.0.1.tar.gz',
+    keywords = ['requests', 'crawler'],
+    classifiers = [],
+)
