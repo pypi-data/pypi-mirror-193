c560ccfdf846a36ef01a6e114e4dd96c653203e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:59 +0000

    Add package nonebot-plugin-access-control/0.3.5/nonebot_plugin_access_control-0.3.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-access-control/nonebot_plugin_access_control-0.3.5-py3-none-any.whl/nonebot_plugin_access_control_kaiheila/subject/extractor/__init__.py
@@ -0,0 +1,36 @@
+from nonebot.adapters.kaiheila import Bot, Event
+
+from nonebot_plugin_access_control.subject import SubjectExtractor
+from nonebot_plugin_access_control.utils.superuser import is_superuser
+
+
+class KaiheilaSubjectExtractor(SubjectExtractor[Bot, Event]):
+    def get_adapter_shortname(self) -> str:
+        return 'kaiheila'
+
+    def get_adapter_fullname(self) -> str:
+        return 'Kaiheila'
+
+    def is_platform_supported(self, platform: str) -> bool:
+        return platform == 'kaiheila'
+
+    def extract(self, bot: Bot, event: Event):
+        li = []
+
+        user_id = getattr(event, "user_id", None)
+        if user_id is not None:
+            li.append(f"kaiheila:{user_id}")
+            if is_superuser(bot, event):
+                li.append("superuser")
+
+        channel_id = getattr(event, "group_id", None)
+        if channel_id is not None:
+            li.append(f"kaiheila:c{channel_id}")
+
+        if event.extra.guild_id is not None:
+            li.append(f"kaiheila:g{event.extra.guild_id}")
+
+        li.append("kaiheila")
+        li.append("all")
+
+        return li
