8d3f0efa4544eeebf02d09bcb34916981539f27b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:30 +0000

    Add package scrapautoscout/1.0.8/scrapautoscout-1.0.8.tar.gz

---
+++ packages/scrapautoscout/scrapautoscout-1.0.8.tar.gz/scrapautoscout-1.0.8/scrapautoscout/cli.py
@@ -0,0 +1,27 @@
+import argparse
+import os
+
+import scrapautoscout.config as config
+# from scrapautoscout import config
+from scrapautoscout.scrapper import get_all_article_ids_forloop, read_ids_json_files_from_cache
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir_out', default=config.DIR_CACHE,
+                        help='Where to save artifacts. Default: /cache from project root')
+    parser.add_argument('--makers', default=config.MAKERS, nargs='+', type=str, help = 'Parse a list of makers')
+
+    args = parser.parse_args()
+    # override default folder for cache
+    config.DIR_CACHE = args.dir_out
+    # override default list of makers
+    config.MAKERS = args.makers
+
+    os.makedirs(config.DIR_CACHE, exist_ok=True)
+    get_all_article_ids_forloop()
+    read_ids_json_files_from_cache()
+
+
+if __name__ == "__main__":
+    run()
