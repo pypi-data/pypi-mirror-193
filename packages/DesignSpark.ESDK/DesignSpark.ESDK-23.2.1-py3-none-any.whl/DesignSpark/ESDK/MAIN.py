a373faf6fcb07f962a1d0ef3fc515c9858f7b5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:12 +0000

    Add package DesignSpark.ESDK/23.2.1/DesignSpark.ESDK-23.2.1-py3-none-any.whl

---
+++ packages/DesignSpark.ESDK/DesignSpark.ESDK-23.2.1-py3-none-any.whl/DesignSpark/ESDK/MAIN.py
@@ -0,0 +1,406 @@
+# Copyright (c) 2021 RS Components Ltd
+# SPDX-License-Identifier: MIT License
+
+'''
+ESDK Main board interface
+'''
+
+import smbus2
+import toml
+import threading
+import re
+import subprocess
+import pkg_resources
+import imp
+import inspect
+import os
+import RPi.GPIO as GPIO
+from gpsdclient import GPSDClient
+from . import AppLogger
+from . import MAIN, THV, CO2, PM2, NO2, NRD, FDH
+
+possibleModules = {
+    "THV": 0x44, 
+    "CO2": 0x62, 
+    "PM2": 0x69,
+    "NO2": 0x40,
+    "NRD": 0x60,
+    "FDH": 0x5D
+}
+
+moduleTypeDict = {
+    'THV': THV,
+    'CO2': CO2,
+    'PM2': PM2,
+    'NO2': NO2,
+    "NRD": NRD,
+    "FDH": FDH
+}
+
+# GPIOs used for board features
+SENSOR_3V3_EN = 7
+SENSOR_5V_EN = 16
+BUZZER_PIN = 26
+GPIO_LIST = [SENSOR_3V3_EN, SENSOR_5V_EN, BUZZER_PIN]
+
+strip_unicode = re.compile("([^-_a-zA-Z0-9!@#%&=,/'\";:~`\$\^\*\(\)\+\[\]\.\{\}\|\?\<\>\\]+|[^\s]+)")
+
+class ModMAIN:
+    """ This class handles the ESDK mainboard, and it's various features.
+
+    :param config: A dictionary containing configuration data with a minimum of:
+
+    .. code-block:: text
+
+        {
+            "esdk":{
+                "GPS":False
+            }
+        }
+    
+    :type config: dict
+    :param debug: Debug logging control, defaults to False
+    :type debug: bool, optional
+    :param loggingLevel: One of 'off', 'error' or 'full' to control file logging, defaults to 'full'
+    :type loggingLevel: str, optional
+    :param pluginDir: A string value containing a file path to a plugin directory, defaults to None
+    :type pluginDir: str, optional
+    """
+    def __init__(self, config, debug=False, loggingLevel='full', pluginDir=None):
+        self.logger = AppLogger.getLogger(__name__, debug, loggingLevel)
+        try:
+            self.bus = smbus2.SMBus(1)
+
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(GPIO_LIST, GPIO.OUT)
+            self.buzzer_pwm = GPIO.PWM(BUZZER_PIN, 1000)
+        except Exception as e:
+            raise e
+
+        self.moduleNames = []
+        self.sensorModules = {}
+        self.sensorData = {}
+        self.configDict = config
+        self.location = {}
+        self.gpsStatus = {"gpsStatus": {}}
+        self.pluginDir = pluginDir
+        self.pluginsModuleList = []
+        self.plugins = []
+        self._parseConfig()
+
+    def _parseConfig(self):
+        """ Parse config when mainboard initialised """
+        if 'ESDK' in self.configDict:
+            if 'gps' in self.configDict['ESDK']:
+                if self.configDict['ESDK']['gps'] is not None:
+                    if self.configDict['ESDK']['gps'] == True:
+                        self.logger.info("GPS is enabled")
+                        self.gps = GPSDClient(host="localhost")
+                        gpsHandlerThreadHandle = threading.Thread(target=self._gpsHandlerThread, daemon=True)
+                        gpsHandlerThreadHandle.name = "gpsHandlerThread"
+                        gpsHandlerThreadHandle.start()
+
+    def _gpsHandlerThread(self):
+        """ Thread for polling GPS module. """
+        self.logger.debug("Started GPS handling thread")
+        while True:
+            try:
+                for result in self.gps.dict_stream():
+                    if result["class"] == "TPV":
+                        self.location['lat'] = result.get("lat", "n/a")
+                        self.location['lon'] = result.get("lon", "n/a")
+                        self.logger.debug("GPS location {}".format(self.location))
+                        self.gpsStatus['gpsStatus'].update({'mode': result.get("mode", 0)})
+
+                    if result["class"] == "SKY":
+                        satellitesList = result.get("satellites", "")
+                        satellitesUsedCount = 0
+
+                        for satellite in satellitesList:
+                            if satellite['used']:
+                                satellitesUsedCount = satellitesUsedCount + 1
+
+                        self.gpsStatus['gpsStatus'].update({'satellitesUsed': satellitesUsedCount})
+
+            except Exception as e:
+                self.logger.error("Error getting GPS location, reason: {}".format(e))
+
+    def _probeModules(self):
+        """ Probes I2C bus to attempt to find sensor modules. """
+        self.moduleNames.clear()
+        self.logger.debug("Starting module probe")
+        for module, addr in possibleModules.items():
+            try:
+                # ADC used on NO2 board is an annoying edge case, does not seemingly acknowledge 0x0
+                if module != "NO2":
+                    self.bus.write_byte(addr, 0)
+                    self.moduleNames.append(module)
+                else:
+                    # Instead issue reset command, and check for an acknowledgement
+                    self.bus.write_byte(addr, 0x06)
+                    self.moduleNames.append(module)
+            except Exception as e:
+                # Ignore any that fail - the modules aren't present on the bus
+                pass
+        self.logger.info("Found modules {}".format(self.moduleNames))
+
+    def getLocation(self):
+        """ Returns a dictionary containing GPS location, or configuration file location if GPS is disabled.
+
+        :return: A dictionary containing:
+
+        .. code-block:: text
+
+            {
+                "lat":0.0,
+                "lon":0.0
+            }
+
+        :rtype: dict
+
+        """
+        if self.configDict['ESDK']['gps'] == False or self.configDict['ESDK']['gps'] is None:
+            self.location['lat'] = self.configDict['ESDK']['latitude']
+            self.location['lon'] = self.configDict['ESDK']['longitude']
+            return self.location
+
+        if self.configDict['ESDK']['gps'] == True:
+            if "lat" and "lon" in self.location:
+                return self.location
+            else:
+                return {}
+
+    def getGPSStatus(self):
+        """ Returns a dictionary containing GPS status. 
+
+        :return: A dictionary containing:
+
+        .. code-block:: text
+
+            {
+                "gpsStatus":{
+                    "mode":0,
+                    "satellites":13,
+                    "satellitesUsed":5
+                }
+            }
+
+        :rtype: dict
+
+        """
+        return self.gpsStatus
+
+    def createModules(self):
+        """ Discovers and instantiates module objects for use with ``readAllModules()``. """
+        self._probeModules()
+        self.logger.debug("Creating module objects")
+        for moduleName in self.moduleNames:
+            try:
+                if moduleName == "THV":
+                    self.sensorModules[moduleName] = moduleTypeDict[moduleName].ModTHV()
+
+                if moduleName == "CO2":
+                    self.sensorModules[moduleName] = moduleTypeDict[moduleName].ModCO2()
+
+                if moduleName == "PM2":
+                    self.sensorModules[moduleName] = moduleTypeDict[moduleName].ModPM2()
+
+                if moduleName == "NO2":
+                    if "NO2" in self.configDict:
+                        sensitivityCode = self.configDict["NO2"]["sensitivity"]
+                        self.sensorModules[moduleName] = moduleTypeDict[moduleName].ModNO2(sensitivity=sensitivityCode)
+                    else:
+                        raise Exception("No NO2 module configuration provided")
+
+                if moduleName == "NRD":
+                    self.sensorModules[moduleName] = moduleTypeDict[moduleName].ModNRD()
+
+                if moduleName == "FDH":
+                    self.sensorModules[moduleName] = moduleTypeDict[moduleName].ModFDH()
+            except Exception as e:
+                self.logger.error("Could not create module {}, reason: {}".format(moduleName, e))
+
+    def readAllModules(self):
+        """ Reads all sensor modules and returns a dictionary containing sensor data. """
+        try:
+            for name, module in self.sensorModules.items():
+                self.logger.debug("Trying to read sensor {}".format(name))
+                data = module.readSensors()
+                if data != -1:
+                    self.sensorData.update(data)
+        except Exception as e:
+            self.logger.error("Could not read module {}, reason: {}".format(name, e))
+
+        # Read loaded plugins
+        try:
+            for plugin in self.plugins:
+                pluginName = plugin.__class__.__name__
+                self.logger.debug("Trying to read plugin {}".format(pluginName))
+                try:
+                    data = plugin.readSensors()
+                    if data != -1:
+                        self.sensorData.update(data)
+                except Exception as e:
+                    self.logger.error("Could not read plugin {}, reason: {}".format(pluginName, e))
+        except Exception as e:
+            self.logger.error("Error handling plugins, reason: {}".format(e))
+
+        self.logger.debug("Sensor data {}".format(self.sensorData))
+        return self.sensorData
+
+    def getSerialNumber(self):
+        """ Returns a dictionary containing the Raspberry Pi serial number.
+
+        :return: A dictionary containing:
+
+        .. code-block:: text
+
+            {
+                "serialNumber":"RPI0123456789"
+            }
+
+        :rtype: dict
+
+        """
+        try:
+            serialNumber = {}
+            with open('/sys/firmware/devicetree/base/serial-number') as f:
+                serialNumber['hardwareId'] = "RPI{}".format(strip_unicode.sub('',f.read()))
+                self.logger.info("Hardware ID is {}".format(serialNumber['hardwareId']))
+            return serialNumber
+        except Exception as e:
+            self.logger.error("Could not retrieve serial number, reason: {}".format(e))
+            return -1
+
+    def getModuleVersion(self):
+        """ Returns a dictionary containing ESDK module version.
+
+        :return: A dictionary containing:
+
+        .. code-block:: text
+
+            {
+                "moduleVerson":"0.0.1"
+            }
+
+        :rtype: dict
+
+        """
+        return {"moduleVersion": pkg_resources.get_distribution('DesignSpark.ESDK').version}
+
+    def getUndervoltageStatus(self):
+        """ Returns a dictionary containing the Raspberry Pi throttle status and code.
+
+        :return: A dictionary containing (throttle_state is optional, and only populated should a nonzero code exist)
+
+        .. code-block:: text
+
+            {
+                "throttle_state":{
+                    "code":0,
+                    "throttle_state":""
+                }
+            }
+
+        :rtype: dict
+
+        """
+        try:
+            cmdOutput = subprocess.run(["vcgencmd", "get_throttled"], capture_output=True)
+            statusData = cmdOutput.stdout.decode('ascii').strip().strip("throttled=")
+
+            code = int(statusData, 16)
+            status = {"code": code}
+            response = {"throttle_state": status}
+
+            if statusData == "0x0":
+                return response
+
+            statusBits = [[0, "Under_Voltage detected"],
+                    [1, "Arm frequency capped"],
+                    [2, "Currently throttled"],
+                    [3, "Soft temperature limit active"],
+                    [16, "Under-voltage has occurred"],
+                    [17, "Arm frequency capping has occurred"],
+                    [18, "Throttling has occurred"],
+                    [19, "Soft temperature limit has occurred"]]
+
+            statusStrings = []
+
+            for x in range(0, len(statusBits)):
+                statusBitString = statusBits[x][1]
+                if (code & (1 << statusBits[x][0])):
+                    statusStrings.append(statusBitString)
+
+            status.update({"status_strings": statusStrings})
+            response = {"throttle_state": status}
+            return response
+
+        except Exception as e:
+            self.logger.error("Could not retrieve undervoltage status, reason: {}".format(e))
+            return -1
+
+    def setPower(self, vcc3=False, vcc5=False):
+        """ Switches 3.3V and 5V sensor power supply rails according to supplied arguments.
+
+        :param vcc3: 3.3V sensor power supply status, defaults to False
+        :type vcc3: bool, optional
+        :param vcc5: 5V sensor power supply status, defaults to False
+        :type vcc5: bool, optional
+        """
+        try:
+            self.logger.debug("Setting sensor power rails, 3V3: {}, 5V: {}".format(vcc3, vcc5))
+            GPIO.output(SENSOR_3V3_EN, vcc3)
+            GPIO.output(SENSOR_5V_EN, vcc5)
+        except Exception as e:
+            raise e
+
+    def setBuzzer(self, freq=0):
+        """ Sets a PWM frequency on the buzzer output.
+
+        :param freq: Buzzer frequency, 0 stops the buzzer
+        :type freq: int, optional
+        """
+        try:
+            if freq > 0:
+                self.logger.debug("Setting buzzer frequency to {}".format(freq))
+                self.buzzer_pwm.start(50)
+                self.buzzer_pwm.ChangeFrequency(freq)
+
+            if freq == 0:
+                self.logger.debug("Stopping buzzer")
+                self.buzzer_pwm.stop()
+        except Exception as e:
+            raise e
+
+    def loadPlugins(self):
+        """ Attempts to load and instantiate plugins from a specified folder. """
+        if self.pluginDir == None:
+            cwd = os.getcwd()
+            self.pluginFullPath = cwd + "/plugins"
+            self.logger.debug("No plugin folder provided, using default")
+            self.logger.debug("Current working directory: {}, plugins path: {}".format(cwd, self.pluginFullPath))
+        else:
+            self.pluginFullPath = self.pluginDir
+
+        # Create a list of available plugin modules
+        for filename in os.listdir(self.pluginFullPath):
+            modulename, extension = os.path.splitext(filename)
+            if extension == '.py':
+                file, path, descr = imp.find_module(modulename, [self.pluginFullPath])
+                if file:
+                    try:
+                        self.logger.debug("Found plugin module: {}".format(file.name))
+                        module = imp.load_module(modulename, file, path, descr)
+                        self.pluginsModuleList.append(module)
+                    except Exception as e:
+                        self.logger.error("Could not load plugin {}! Reason {}".format(file.name, e))
+
+        # Create a list of instantiated plugin classes
+        for pluginModule in self.pluginsModuleList:
+            for name, obj in inspect.getmembers(pluginModule):
+                if inspect.isclass(obj):
+                    self.logger.debug("Created plugin class {}".format(obj))
+                    self.plugins.append(obj())
+
+        self.logger.info("Loaded {} plugin(s)".format(len(self.plugins)))
---
+++ packages/DesignSpark.ESDK/DesignSpark.ESDK-23.2.1-py3-none-any.whl/DesignSpark/ESDK/NO2.py
@@ -0,0 +1,295 @@
+# Copyright (c) 2022 RS Components Ltd
+# SPDX-License-Identifier: MIT License
+
+'''
+ESDK NO2 board interface
+'''
+
+import time
+import smbus2
+import threading
+import math
+from smbus2 import i2c_msg
+from statistics import mode
+
+moduleVersionString = "NO20.1"
+
+ADC_ADDR = 0x40
+
+ADC_REF = 3.000
+
+ADC_UPPER = 0x7FFF
+
+class ModNO2:
+	""" This is a class that handles interfacing with the ESDK-PM2 board.
+
+	:param sensitivity: Sensitivity code from barcode on sensor
+	:type sensitivity: float
+	:param tia_gain: Transimpedance amplifier gain from sensor datasheet
+	:type tia_gain: float, optional - default 499
+	:param voffset: Offset voltage used in gas calculation
+	:type voffset: float, optional - default 0
+	:param movingAverageWindow: Window size for the NO2 moving average
+	:type movingAverageWindow: int, optional - default 15
+	"""
+	def __init__(self, sensitivity=None, tia_gain=499, voffset=0, movingAverageWindow=15):
+		if sensitivity == None:
+			raise Exception("No NO2 sensitivity code provided")
+
+		try:
+			self.bus = smbus2.SMBus(1)
+
+			# Sensitivity should be read from the back of the sensing element
+			# As this varies from device to device
+			self.sensitivity = sensitivity
+
+			# TIA gain is provided by the Spec datasheet, and only changes on sensing element type
+			self.tia_gain = tia_gain
+
+			# Voffset figure used in calculation of gas concentration
+			self.voffset = voffset
+
+			self.movingAverageWindow = movingAverageWindow
+
+			self.no2AverageList = []
+			# NO2 value updated in thread
+			self.no2Value = 0
+		except Exception as e:
+			raise e
+
+		self._resetADC()
+		adcPollingThreadHandle = threading.Thread(target=self._adcPollingThread, daemon=True)
+		adcPollingThreadHandle.name = "no2_adcPollingThreadHandle"
+		adcPollingThreadHandle.start()
+
+	def _resetADC(self):
+		""" Issues reset command to ADC. """
+		try:
+			self.bus.write_byte(ADC_ADDR, 0x06)
+		except Exception as e:
+			raise e
+
+	def _isDataReady(self):
+		""" Queries ADC to see if data is available to be read.
+
+		:return: True or false if ADC data is available.
+		:rtype: bool
+
+		"""
+		try:
+			write = i2c_msg.write(ADC_ADDR, [0x24])
+			read = i2c_msg.read(ADC_ADDR, 1)
+			self.bus.i2c_rdwr(write, read)
+
+			ready = list(read)[0] >> 7
+
+			if ready == 1:
+				return True
+			else:
+				return False
+		except Exception as e:
+			raise e
+
+	def _readVrefChannel(self):
+		""" Read and convert AIN1 (sensor Vref) to a voltage. 
+
+		:return: The voltage of AIN1 (sensor Vref).
+		:rtype: float
+
+		"""
+		try:
+			# 0x81 is the config register data
+			# AINp = AIN1, AINn = GND
+			# Gain = 1
+			# 20 SPS
+			# Single conversion
+			# External reference
+			self.bus.write_byte_data(ADC_ADDR, 0x40, 0x81)
+
+			# Trigger read
+			self.bus.write_byte(ADC_ADDR, 0x08)
+
+			time.sleep(0.05)
+
+			if self._isDataReady():
+				write = i2c_msg.write(ADC_ADDR, [0x10])
+				read = i2c_msg.read(ADC_ADDR, 2)
+				self.bus.i2c_rdwr(write, read)
+
+				read = list(read)
+				voltage = ((read[0] << 8) + read[1]) * (ADC_REF / ADC_UPPER)
+				return round(voltage, 3)
+			else:
+				return -1
+		except Exception as e:
+			raise e
+
+	def _readVgasChannel(self):
+		""" Read and convert AIN0 (sensor Vgas) to a voltage. 
+
+		:return: The voltage of AIN0 (sensor Vgas).
+		:rtype: float
+
+		"""
+		try:
+			# 0x61 is the config register data
+			# AINp = AIN0, AINn = GND
+			# Gain = 1
+			# 20 SPS
+			# Single conversion
+			# External reference
+			self.bus.write_byte_data(ADC_ADDR, 0x40, 0x61)
+
+			# Trigger read
+			self.bus.write_byte(ADC_ADDR, 0x08)
+
+			time.sleep(0.05)
+
+			if self._isDataReady():
+				write = i2c_msg.write(ADC_ADDR, [0x10])
+				read = i2c_msg.read(ADC_ADDR, 2)
+				self.bus.i2c_rdwr(write, read)
+
+				read = list(read)
+				voltage = ((read[0] << 8) + read[1]) * (ADC_REF / ADC_UPPER)
+				return round(voltage, 3)
+			else:
+				return -1
+		except Exception as e:
+			raise e
+
+	def _readVtempChannel(self):
+		""" Read and convert AIN2 (sensor Vtemp) to a voltage.
+
+		:return: The voltage of AIN2 (sensor Vtemp).
+		:rtype: float
+		
+		"""
+		try:
+			# 0xA1 is the config register data
+			# AINp = AIN2, AINn = GND
+			# Gain = 1
+			# 20 SPS
+			# Single conversion
+			# External reference
+			self.bus.write_byte_data(ADC_ADDR, 0x40, 0xA1)
+
+			# Trigger read
+			self.bus.write_byte(ADC_ADDR, 0x08)
+
+			time.sleep(0.05)
+
+			if self._isDataReady():
+				write = i2c_msg.write(ADC_ADDR, [0x10])
+				read = i2c_msg.read(ADC_ADDR, 2)
+				self.bus.i2c_rdwr(write, read)
+
+				read = list(read)
+				voltage = ((read[0] << 8) + read[1]) * (ADC_REF / ADC_UPPER)
+				return round(voltage, 3)
+			else:
+				return -1
+		except Exception as e:
+			raise e
+
+	def _calculateMovingAverage(self, new_data_point, data_list, average_size):
+		""" Function to calculate a simple moving average """
+		# Add fresh data
+		data_list.insert(0, new_data_point)
+		try:
+			# Remove nth item in list
+			data_list.pop(average_size)
+		except Exception as e:
+			pass
+		total = 0
+		total = sum(data_list)
+        
+		#value = math.ceil(total / len(data_list))
+		value = round(total / len(data_list), 2)
+		if(value == None):
+			return 0
+		else:
+			return value
+
+	def _adcPollingThread(self):
+		""" Thread that polls the ADC to provid an updated NO2 value every five seconds """
+		vref = -1
+		vgasList = []
+		vgasLastMode = 0
+
+		while vref == -1:
+			vref = self._readVrefChannel()
+
+		while True:
+			try:
+				# Take 10 voltage readings from ADC
+				for i in range(1, 10):
+					vgas = -1
+					# Wait until ADC says data is available
+					while vgas == -1:
+						vgas = self._readVgasChannel()
+						time.sleep(0.1)
+
+					# Append each sample to list and wait before taking another reading
+					vgasList.append(vgas)
+					time.sleep(0.25)
+				# Take mode value for use in calculations to help reduce sensor noise (ADC and inputs seem noisy)
+				try:
+					vgasLastMode = vgas
+					vgas = mode(vgasList)
+				except Exception as e:
+					vgasList.clear()
+
+				if vgas != -1 and vref != -1:
+					vgas0 = vref + self.voffset
+					conc = (vgas - vgas0) / (self.tia_gain * 1e3) / (self.sensitivity * 1e-9)
+					conc = round(conc, 2)
+			except Exception as e:
+				pass
+
+			# Calculate moving average to use for value
+			self.no2Value = self._calculateMovingAverage(conc, self.no2AverageList, self.movingAverageWindow)
+
+			vgasList.clear()
+			time.sleep(2.5)
+
+	def readNO2(self):
+		""" Returns an NO2 reading. 
+
+		:return: The NO2 concentration.
+		:rtype: float
+
+		"""
+
+		return self.no2Value
+
+	def readSensors(self):
+		""" Reads sensor and returns a dictionary containing module version, and all readings.
+
+        :return: A dictionary containing
+
+        .. code-block:: text
+            
+            {
+                "no2":{
+                    "sensor":"NO20.1",
+                    "no2":2.1,
+                }
+            }
+            
+        Or -1 if data is unavailable
+
+        :rtype: dict, int
+
+        """
+		try:
+			sensorData = {}
+			no2 = self.readNO2()
+			sensorData['sensor'] = moduleVersionString
+			if no2 != -1:
+				sensorData['no2'] = no2
+				return {'no2': sensorData}
+			else:
+				return -1
+		except Exception as e:
+			raise e
