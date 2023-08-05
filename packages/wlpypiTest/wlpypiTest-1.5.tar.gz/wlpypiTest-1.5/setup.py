5307d51da23bfdd1bae33db63775e008400c4a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:53 +0000

    Add package wlpypiTest/1.5/wlpypiTest-1.5.tar.gz

---
+++ packages/wlpypiTest/wlpypiTest-1.5.tar.gz/wlpypiTest-1.5/setup.py
@@ -0,0 +1,30 @@
+#!python
+# -*- coding:utf-8 -*-
+from __future__ import print_function
+from setuptools import setup, find_packages
+import commons
+import core
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="wlpypiTest",
+    version=1.5,
+    author="wanglie",
+    author_email="lie_wangxy@163.com",
+    description="test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="http://www.baidu.com",
+    packages=find_packages("commons"),
+    install_requires=[
+
+        ],
+    classifiers=[
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.7",
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
