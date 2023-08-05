c0db7b51ec8a3f71139cc8a6a933f0979415fdbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:01 +0000

    Add package mxpissh/1.6.2/mxpissh-1.6.2-py2.py3-none-any.whl

---
+++ packages/mxpissh/mxpissh-1.6.2-py2.py3-none-any.whl/mxpissh/__init__.py
@@ -0,0 +1,7 @@
+import sys
+from mxpissh._version import __version__, __version_info__
+
+if sys.platform == 'win32' and sys.version_info.major == 3 and \
+        sys.version_info.minor >= 8:
+    import asyncio
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
