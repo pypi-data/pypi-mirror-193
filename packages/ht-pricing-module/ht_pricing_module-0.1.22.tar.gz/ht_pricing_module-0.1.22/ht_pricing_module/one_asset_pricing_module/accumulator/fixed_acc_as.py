25a4537ce96953d78bbc77effef5d76bd044f215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:33 +0000

    Add package ht-pricing-module/0.1.22/ht_pricing_module-0.1.22.tar.gz

---
+++ packages/ht-pricing-module/ht_pricing_module-0.1.22.tar.gz/ht_pricing_module-0.1.22/ht_pricing_module/one_asset_pricing_module/accumulator/fixed_acc_as.py
@@ -0,0 +1,45 @@
+from ..one_asset_option_base import *
+from ..vanilla import Vanilla
+from ..binary import Binary
+
+
+class FixedAcc(OneAssetOptionBase):
+
+    def __calculate_present_value__(self) -> float:
+        rst = 0
+
+        leveraged_vanilla_option_type = {OptionType.ACCUMULATOR: OptionType.PUT,
+                                         OptionType.DECUMULATOR: OptionType.CALL}[self.param.option_type]
+        binary_option_type = {OptionType.ACCUMULATOR: OptionType.CALL,
+                              OptionType.DECUMULATOR: OptionType.PUT}[self.param.option_type]
+
+        for obs in self.param.obs_date:
+            param = Struct({})
+            param['option_type'] = leveraged_vanilla_option_type
+            param['exercise_type'] = self.param.exercise_type
+            param['spot_price'] = self.param.spot_price
+            param['strike_price'] = self.param.strike_price
+            param['expiry_date'] = obs.obs_index
+            param['current_date'] = self.param.current_date
+            param['volatility'] = self.param.volatility
+            param['riskfree_rate'] = self.param.riskfree_rate
+            param['dividend'] = self.param.dividend
+            param['year_base'] = int(self.param.year_base)
+            leveraged_vanilla = Vanilla(param)
+
+            param = Struct({})
+            param['option_type'] = binary_option_type
+            param['exercise_type'] = self.param.exercise_type
+            param['spot_price'] = self.param.spot_price
+            param['strike_price'] = self.param.strike_price
+            param['expiry_date'] = obs.obs_index
+            param['current_date'] = self.param.current_date
+            param['volatility'] = self.param.volatility
+            param['riskfree_rate'] = self.param.riskfree_rate
+            param['dividend'] = self.param.dividend
+            param['payoff'] = self.param.payoff
+            param['year_base'] = int(self.param.year_base)
+            binary = Binary(param)
+            rst = rst + (-self.param.leverage * leveraged_vanilla.present_value()
+                         + binary.present_value())
+        return rst
