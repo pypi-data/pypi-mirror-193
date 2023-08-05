f590822681195ca24746a813cf859fb58cc6540d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:00 +0000

    Add package odoo12-addon-somalimentacio/12.0.0.0.1.99.dev1/odoo12_addon_somalimentacio-12.0.0.0.1.99.dev1-py3-none-any.whl

---
+++ packages/odoo12-addon-somalimentacio/odoo12_addon_somalimentacio-12.0.0.0.1.99.dev1-py3-none-any.whl/odoo/addons/somalimentacio/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Odoo Somalimentacio customizations",
+    'version': '12.0.0.0.1',
+    'depends': ['easy_my_coop_website'],
+    'author': "Coopdevs Treball SCCL",
+    'website': 'https://coopdevs.org',
+    'category': "Cooperative management",
+    'description': "Odoo Somalimentacio customizations",
+    "license": "AGPL-3",
+    'data': [
+        "views/subscription_template.xml",
+    ],
+}
