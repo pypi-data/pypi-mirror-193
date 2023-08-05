5a67fa7179f06daa4620108865deb2dbf9cdc278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:56 +0000

    Add package Hosein-Tabr/4.6.5/Hosein_Tabr-4.6.5-py3-none-any.whl

---
+++ packages/Hosein-Tabr/Hosein_Tabr-4.6.5-py3-none-any.whl/Hosein_Tabr/Copyright.py
@@ -0,0 +1,24 @@
+from time import sleep
+
+class color:
+      red = '\033[91m'
+      green = '\033[92m'
+      blue = '\033[34m'
+      yellow = '\033[93m'
+      magenta = '\033[95m'
+      cyan = '\033[96m'
+      white = '\033[97m'
+      bold = '\033[1m'
+      underline = '\033[4m'
+      black='\033[30m'
+
+class copyright:
+      CopyRight = print(f"""{color.bold}
+{color.white} [␛] - {color.cyan} 𝐇𝐎𝐒𝐄𝐈𝐍 𝐓𝐀𝐁𝐑 {color.yellow} 𝐕𝐄𝐑𝐒𝐈𝐎𝐍  {color.red}4.6.0    
+
+{color.white} [␛] - {color.cyan} 𝐇𝐎𝐒𝐄𝐈𝐍 𝐓𝐀𝐁𝐑 {color.yellow} 𝐂𝐎𝐏𝐘𝐑𝐈𝐆𝐇𝐓 (𝐂) {color.red} 2022 {color.green}𝐇𝐎𝐒𝐄𝐈𝐍 𝐓𝐀𝐁𝐑  
+
+{color.white} [␛] - {color.cyan} 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑  {color.yellow} 𝐑𝐔𝐁𝐈𝐊𝐀 {color.red} 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 : {color.green} @accant_app
+
+{color.white}╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾
+""")
---
+++ packages/Hosein-Tabr/Hosein_Tabr-4.6.5-py3-none-any.whl/Hosein_Tabr/__init__.py
@@ -0,0 +1,1 @@
+from Hosein_Tabr.Hosein_Tabr import *
