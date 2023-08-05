d236e359dbae99387814315a8abfe935711df04d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:59 +0000

    Add package NorenRestApiPytest/0.0.22/NorenRestApiPytest-0.0.22.tar.gz

---
+++ packages/NorenRestApiPytest/NorenRestApiPytest-0.0.22.tar.gz/NorenRestApiPytest-0.0.22/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from distutils.core import setup
+#python setup.py bdist_wheel --universal
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NorenRestApiPytest",
+    version="0.0.22",
+    author="KumarAnand",
+    author_email="kumar.anand@kambala.co.in",
+    description="A package for NorenOMS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+    ],
+)
+
+
+
