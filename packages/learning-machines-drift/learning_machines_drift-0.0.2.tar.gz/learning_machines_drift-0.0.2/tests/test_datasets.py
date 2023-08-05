35ad6384fd43bb7e5ba2eb2e8cc1d00b98a30e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:58 +0000

    Add package learning-machines-drift/0.0.2/learning_machines_drift-0.0.2.tar.gz

---
+++ packages/learning-machines-drift/learning_machines_drift-0.0.2.tar.gz/learning_machines_drift-0.0.2/tests/test_datasets.py
@@ -0,0 +1,19 @@
+"""Demo datasets for developing package"""
+
+from learning_machines_drift import datasets
+
+
+def test_logistic_model() -> None:
+    """Test a simple generative classification model"""
+
+    # Given the size sample we want (n)
+    n_count = 10
+
+    # When we draw from a model
+    x_coord, y_coord, latents_coord = datasets.logistic_model(size=n_count)
+
+    # Then we get n features and n labels back
+    assert x_coord.shape[0] == 10
+    assert y_coord.shape[0] == 10
+    if latents_coord is not None:
+        assert latents_coord.shape[0] == 10
