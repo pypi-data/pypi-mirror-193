de31a4ae28909f4c28459afc5c6f3cc976ca5f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:42 +0000

    Add package limepkg-getaccept/1.1.2/limepkg_getaccept-1.1.2-py3-none-any.whl

---
+++ packages/limepkg-getaccept/limepkg_getaccept-1.1.2-py3-none-any.whl/limepkg_getaccept/web_components/static/types/types/Recipient.d.ts
@@ -0,0 +1,11 @@
+export interface IRecipient {
+  name: string;
+  email: string;
+  mobile: string;
+  lime_id: string;
+  role: string;
+  limetype: string;
+  document_url?: string;
+  company?: string;
+  role_id?: string;
+}
