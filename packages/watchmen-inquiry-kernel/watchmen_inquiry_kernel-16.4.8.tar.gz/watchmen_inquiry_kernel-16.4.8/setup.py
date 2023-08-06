c73a9c743f8fd1a88131b0f9bbfb0c0b6600ac48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:30 +0000

    Add package watchmen-inquiry-kernel/16.4.8/watchmen_inquiry_kernel-16.4.8.tar.gz

---
+++ packages/watchmen-inquiry-kernel/watchmen_inquiry_kernel-16.4.8.tar.gz/watchmen_inquiry_kernel-16.4.8/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['watchmen_inquiry_kernel',
+ 'watchmen_inquiry_kernel.common',
+ 'watchmen_inquiry_kernel.meta',
+ 'watchmen_inquiry_kernel.schema',
+ 'watchmen_inquiry_kernel.storage']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['watchmen-data-kernel==16.4.8']
+
+extras_require = \
+{'mongodb': ['watchmen-storage-mongodb==16.4.8'],
+ 'mssql': ['watchmen-storage-mssql==16.4.8'],
+ 'mysql': ['watchmen-storage-mysql==16.4.8'],
+ 'oracle': ['watchmen-storage-oracle==16.4.8'],
+ 'oss': ['watchmen-storage-oss==16.4.8'],
+ 'postgresql': ['watchmen-storage-postgresql==16.4.8'],
+ 's3': ['watchmen-storage-s3==16.4.8'],
+ 'trino': ['watchmen-inquiry-trino==16.4.8']}
+
+setup_kwargs = {
+    'name': 'watchmen-inquiry-kernel',
+    'version': '16.4.8',
+    'description': '',
+    'long_description': 'None',
+    'author': 'botlikes',
+    'author_email': '75356972+botlikes456@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
