294888e3f88b8bc11536f8c18eb9dd5998130ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:47 +0000

    Add package afscgap/0.0.3/afscgap-0.0.3.tar.gz

---
+++ packages/afscgap/afscgap-0.0.3.tar.gz/afscgap-0.0.3/afscgap/test/__init__.py
@@ -0,0 +1,11 @@
+"""
+Root file for the tests directory for afscgap.
+
+(c) 2023 The Eric and Wendy Schmidt Center for Data Science and the Environment
+at UC Berkeley.
+
+This file is part of afscgap released under the BSD 3-Clause License. See
+LICENSE.txt.
+"""
+
+# pylint: disable=C0115, C0116
