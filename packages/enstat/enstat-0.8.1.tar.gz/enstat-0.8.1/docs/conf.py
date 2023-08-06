676f587970d0995f3ebd65628ca2d4e11cacfa1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:10 +0000

    Add package enstat/0.8.1/enstat-0.8.1.tar.gz

---
+++ packages/enstat/enstat-0.8.1.tar.gz/enstat-0.8.1/docs/conf.py
@@ -0,0 +1,11 @@
+project = "enstat"
+copyright = "2021, Tom de Geus"
+author = "Tom de Geus"
+autodoc_type_aliases = {"Iterable": "Iterable", "ArrayLike": "ArrayLike"}
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+html_theme = "furo"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx_mdinclude",
+]
