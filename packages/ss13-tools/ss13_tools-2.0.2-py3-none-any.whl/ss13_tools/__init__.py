770d3510146f4d41cd4a56fd04dd7eea2c2fa76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:46 +0000

    Add package ss13-tools/2.0.2/ss13_tools-2.0.2-py3-none-any.whl

---
+++ packages/ss13-tools/ss13_tools-2.0.2-py3-none-any.whl/ss13_tools/__init__.py
@@ -0,0 +1,14 @@
+from colorama import init as colorama_init
+
+from .__version__ import __version__
+
+colorama_init()
+
+VERSION = __version__
+
+__all__ = [
+    'slur_detector',
+    'centcom',
+    'byond',
+    'log_downloader'
+]
