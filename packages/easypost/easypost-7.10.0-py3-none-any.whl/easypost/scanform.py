d7e8e43194c4b28dae2a80c9a8bf63ed550b6a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:36 +0000

    Add package easypost/7.10.0/easypost-7.10.0-py3-none-any.whl

---
+++ packages/easypost/easypost-7.10.0-py3-none-any.whl/easypost/scanform.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from easypost.easypost_object import convert_to_easypost_object
+from easypost.requestor import (
+    RequestMethod,
+    Requestor,
+)
+from easypost.resource import AllResource
+
+
+class ScanForm(AllResource):
+    @classmethod
+    def create(cls, api_key: Optional[str] = None, **params):
+        """Create a scanform."""
+        requestor = Requestor(local_api_key=api_key)
+        url = cls.class_url()
+        response, api_key = requestor.request(method=RequestMethod.POST, url=url, params=params)
+        return convert_to_easypost_object(response=response, api_key=api_key)
