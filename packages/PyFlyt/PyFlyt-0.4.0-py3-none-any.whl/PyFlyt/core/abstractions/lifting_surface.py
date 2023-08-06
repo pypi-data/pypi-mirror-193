4aa712563dd2b5bbc685ac1c1afa6a060788ca87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:53 +0000

    Add package PyFlyt/0.4.0/PyFlyt-0.4.0-py3-none-any.whl

---
+++ packages/PyFlyt/PyFlyt-0.4.0-py3-none-any.whl/PyFlyt/core/abstractions/lifting_surface.py
@@ -0,0 +1,262 @@
+from __future__ import annotations
+
+import warnings
+
+import numpy as np
+from pybullet_utils import bullet_client
+
+
+class LiftingSurface:
+    """LiftingSurface."""
+
+    def __init__(
+        self,
+        p: bullet_client.BulletClient,
+        physics_period: float,
+        np_random: np.random.RandomState,
+        uav_id: int,
+        surface_id: int,
+        command_id: None | int,
+        command_sign: float,
+        lifting_vector: np.ndarray,
+        forward_vector: np.ndarray,
+        aerofoil_params: dict,
+    ):
+        """Used for simulating a single lifting surface.
+
+        Args:
+            p (bullet_client.BulletClient): p
+            physics_period (float): physics_period
+            uav_id (int): uav_id
+            surface_id (int): surface_id
+            command_id (None | int): command_id, for convenience
+            command_sign (float): command_sign, for convenience
+            lifting_axis (np.ndarray): (3,) vector representing the direction of lift
+            forward_axis (np.ndarray): (3,) vector representing the direction of travel
+            aerofoil_params (dict): aerofoil_params, see below
+
+        Aerofoil params must have these components (everything is in degrees and is a float):
+            - Cl_alpha_2D
+            - chord
+            - span
+            - flap_to_chord
+            - eta
+            - alpha_0_base
+            - alpha_stall_P_base
+            - alpha_stall_N_base
+            - Cd_0
+            - deflection_limit
+        """
+        self.p = p
+        self.physics_period = physics_period
+        self.np_random = np_random
+
+        # store IDs
+        self.uav_id = uav_id
+        self.surface_id = surface_id
+
+        # command inputs for referencing
+        self.command_id = command_id
+        self.command_sign = command_sign
+
+        # check that we have all the required params
+        params_list = [
+            "Cl_alpha_2D",
+            "chord",
+            "span",
+            "flap_to_chord",
+            "eta",
+            "alpha_0_base",
+            "alpha_stall_P_base",
+            "alpha_stall_N_base",
+            "Cd_0",
+            "deflection_limit",
+        ]
+        for key in aerofoil_params:
+            if key in params_list:
+                params_list.remove(key)
+
+        assert (
+            len(params_list) == 0
+        ), f"Missing parameters: {params_list} in aerofoil_params for component {surface_id}."
+
+        # some checks for the lifting and norm vectors
+        assert lifting_vector.shape == (3,)
+        assert forward_vector.shape == (3,)
+        if np.linalg.norm(lifting_vector) != 1.0:
+            warnings.warn(f"Norm of `{lifting_vector=}` is not 1.0, normalizing...")
+            lifting_vector /= np.linalg.norm(lifting_vector)
+        if np.linalg.norm(forward_vector) != 1.0:
+            warnings.warn(f"Norm of `{forward_vector=}` is not 1.0, normalizing...")
+            forward_vector /= np.linalg.norm(forward_vector)
+        if np.dot(lifting_vector, forward_vector) != 0.0:
+            warnings.warn(
+                f"`{forward_vector}` and `{lifting_vector}` are not orthogonal, you have been warned..."
+            )
+
+        # get the lift, drag, and torque units
+        self.lift_unit = lifting_vector
+        self.drag_unit = forward_vector
+        self.torque_unit = np.cross(lifting_vector, forward_vector)
+
+        # wing parameters
+        self.Cl_alpha_2D = float(aerofoil_params["Cl_alpha_2D"])
+        self.chord = float(aerofoil_params["chord"])
+        self.span = float(aerofoil_params["span"])
+        self.flap_to_chord = float(aerofoil_params["flap_to_chord"])
+        self.eta = float(aerofoil_params["eta"])
+        self.alpha_0_base = float(aerofoil_params["alpha_0_base"])
+        self.alpha_stall_P_base = float(aerofoil_params["alpha_stall_P_base"])
+        self.alpha_stall_N_base = float(aerofoil_params["alpha_stall_N_base"])
+        self.Cd_0 = float(aerofoil_params["Cd_0"])
+        self.deflection_limit = float(aerofoil_params["deflection_limit"])
+
+        # precompute some constants
+        self.half_rho = 0.5 * 1.225
+        self.area = self.chord * self.span
+        self.aspect = self.span / self.chord
+        self.alpha_stall_P_base = np.deg2rad(self.alpha_stall_P_base)
+        self.alpha_stall_N_base = np.deg2rad(self.alpha_stall_N_base)
+        self.alpha_0_base = np.deg2rad(self.alpha_0_base)
+        self.Cl_alpha_3D = self.Cl_alpha_2D * (
+            self.aspect
+            / (self.aspect + ((2.0 * (self.aspect + 4.0)) / (self.aspect + 2.0)))
+        )
+        self.theta_f = np.arccos(2.0 * self.flap_to_chord - 1.0)
+        self.tau = 1 - ((self.theta_f - np.sin(self.theta_f)) / np.pi)
+
+        # runtime parameters
+        self.local_surface_velocity = np.array([0.0, 0.0, 0.0])
+
+    def update_local_surface_velocity(
+        self, rotation_matrix: np.ndarray, surface_velocity: np.ndarray
+    ):
+        """update_local_surface_velocity.
+
+        Args:
+            rotation_matrix (np.ndarray): rotation_matrix
+            surface_velocity (np.ndarray): surface_velocity
+        """
+        self.local_surface_velocity = np.matmul(rotation_matrix, surface_velocity)
+
+    def pwm2forces(self, actuation: float):
+        """compute_force_torque.
+
+        Args:
+            actuation (float): normalized actuation in [-1, 1]
+
+        Returns:
+            tuple[np.ndarray, np.ndarray]: vec3 force, vec3 torque
+        """
+        freestream_speed = np.linalg.norm(self.local_surface_velocity)
+        lifting_airspeed = np.dot(self.local_surface_velocity, self.lift_unit)
+        forward_airspeed = np.dot(self.local_surface_velocity, self.drag_unit)
+        alpha = np.arctan2(-lifting_airspeed, forward_airspeed)
+
+        deflection = self.deflection_limit * actuation
+        [Cl, Cd, CM] = self._compute_aero_data(deflection, alpha)
+
+        Q = self.half_rho * np.square(freestream_speed)  # Dynamic pressure
+        Q_area = Q * self.area
+
+        lift = Cl * Q_area
+        drag = Cd * Q_area
+        force_normal = (lift * np.cos(alpha)) + (drag * np.sin(alpha))
+        force_parallel = (lift * np.sin(alpha)) - (drag * np.cos(alpha))
+
+        force = self.lift_unit * force_normal + self.drag_unit * force_parallel
+        torque = Q_area * CM * self.chord * self.torque_unit
+
+        self.p.applyExternalForce(
+            self.uav_id,
+            self.surface_id,
+            force,
+            [0.0, 0.0, 0.0],
+            self.p.LINK_FRAME,
+        )
+        self.p.applyExternalTorque(
+            self.uav_id, self.surface_id, torque, self.p.LINK_FRAME
+        )
+
+    def _compute_aero_data(
+        self, deflection: float, alpha: float
+    ) -> tuple[float, float, float]:
+        """_compute_aero_data.
+
+        Args:
+            deflection (float): deflection of the lifting surface in degrees
+            alpha (float): angle of attack in degrees
+
+        Returns:
+            tuple[float, float, float]: Cl, Cd, CM
+        """
+        # deflection must be in degrees because engineering uses degrees
+        deflection = np.deg2rad(deflection)
+
+        delta_Cl = self.Cl_alpha_3D * self.tau * self.eta * deflection
+        delta_Cl_max = self.flap_to_chord * delta_Cl
+        Cl_max_P = (
+            self.Cl_alpha_3D * (self.alpha_stall_P_base - self.alpha_0_base)
+            + delta_Cl_max
+        )
+        Cl_max_N = (
+            self.Cl_alpha_3D * (self.alpha_stall_N_base - self.alpha_0_base)
+            + delta_Cl_max
+        )
+        alpha_0 = self.alpha_0_base - (delta_Cl / self.Cl_alpha_3D)
+        alpha_stall_P = alpha_0 + (Cl_max_P / self.Cl_alpha_3D)
+        alpha_stall_N = alpha_0 + (Cl_max_N / self.Cl_alpha_3D)
+
+        # no stall condition
+        if alpha_stall_N < alpha and alpha < alpha_stall_P:
+            Cl = self.Cl_alpha_3D * (alpha - alpha_0)
+            alpha_i = Cl / (np.pi * self.aspect)
+            alpha_eff = alpha - alpha_0 - alpha_i
+            CT = self.Cd_0 * np.cos(alpha_eff)
+            CN = (Cl + (CT * np.sin(alpha_eff))) / np.cos(alpha_eff)
+            Cd = (CN * np.sin(alpha_eff)) + (CT * np.cos(alpha_eff))
+            CM = -CN * (0.25 - (0.175 * (1.0 - ((2.0 * alpha_eff) / np.pi))))
+
+            return Cl, Cd, CM
+
+        # positive stall
+        if alpha > 0.0:
+            # Stall calculations to find alpha_i at stall
+            Cl_stall = self.Cl_alpha_3D * (alpha_stall_P - alpha_0)
+            alpha_i_at_stall = Cl_stall / (np.pi * self.aspect)
+            # alpha_i post-stall Pos
+            alpha_i = np.interp(
+                alpha, [alpha_stall_P, np.pi / 2.0], [alpha_i_at_stall, 0.0]
+            )
+        # negative stall
+        else:
+            # Stall calculations to find alpha_i at stall
+            Cl_stall = self.Cl_alpha_3D * (alpha_stall_N - alpha_0)
+            alpha_i_at_stall = Cl_stall / (np.pi * self.aspect)
+            # alpha_i post-stall Neg
+            alpha_i = np.interp(
+                alpha, [-np.pi / 2.0, alpha_stall_N], [0.0, alpha_i_at_stall]
+            )
+
+        alpha_eff = alpha - alpha_0 - alpha_i
+
+        # Drag coefficient at 90 deg dependent on deflection angle
+        Cd_90 = (
+            ((-4.26 * (10**-2)) * (deflection**2))
+            + ((2.1 * (10**-1)) * deflection)
+            + 1.98
+        )
+        CN = (
+            Cd_90
+            * np.sin(alpha_eff)
+            * (
+                1.0 / (0.56 + 0.44 * abs(np.sin(alpha_eff)))
+                - 0.41 * (1.0 - np.exp(-17.0 / self.aspect))
+            )
+        )
+        CT = 0.5 * self.Cd_0 * np.cos(alpha_eff)
+        Cl = (CN * np.cos(alpha_eff)) - (CT * np.sin(alpha_eff))
+        Cd = (CN * np.sin(alpha_eff)) + (CT * np.cos(alpha_eff))
+        CM = -CN * (0.25 - (0.175 * (1.0 - ((2.0 * abs(alpha_eff)) / np.pi))))
+
+        return Cl, Cd, CM
---
+++ packages/PyFlyt/PyFlyt-0.4.0-py3-none-any.whl/PyFlyt/core/drones/rocket.py
@@ -0,0 +1,237 @@
+from __future__ import annotations
+
+import numpy as np
+import yaml
+from pybullet_utils import bullet_client
+
+from ..abstractions.base_drone import DroneClass
+from ..abstractions.boosters import Boosters
+from ..abstractions.camera import Camera
+from ..abstractions.lifting_surface import LiftingSurface
+
+
+class Rocket(DroneClass):
+    """Rocket instance that handles everything about a thrust vectored rocket with mildly throttleable boosters."""
+
+    def __init__(
+        self,
+        p: bullet_client.BulletClient,
+        start_pos: np.ndarray,
+        start_orn: np.ndarray,
+        ctrl_hz: int,
+        physics_hz: int,
+        drone_model: str = "rocket",
+        model_dir: None | str = None,
+        use_camera: bool = False,
+        use_gimbal: bool = False,
+        camera_angle_degrees: int = 20,
+        camera_FOV_degrees: int = 90,
+        camera_resolution: tuple[int, int] = (128, 128),
+        np_random: None | np.random.RandomState = None,
+    ):
+        """Creates a drone in the QuadX configuration and handles all relevant control and physics.
+
+        Args:
+            p (bullet_client.BulletClient): p
+            start_pos (np.ndarray): start_pos
+            start_orn (np.ndarray): start_orn
+            ctrl_hz (int): ctrl_hz
+            physics_hz (int): physics_hz
+            model_dir (None | str): model_dir
+            drone_model (str): drone_model
+            use_camera (bool): use_camera
+            use_gimbal (bool): use_gimbal
+            camera_angle_degrees (int): camera_angle_degrees
+            camera_FOV_degrees (int): camera_FOV_degrees
+            camera_resolution (tuple[int, int]): camera_resolution
+            np_random (None | np.random.RandomState): np_random
+        """
+        super().__init__(
+            p=p,
+            start_pos=start_pos,
+            start_orn=start_orn,
+            ctrl_hz=ctrl_hz,
+            physics_hz=physics_hz,
+            model_dir=model_dir,
+            drone_model=drone_model,
+            np_random=np_random,
+        )
+
+        """Reads fixedwing.yaml file and load UAV parameters"""
+        with open(self.param_path, "rb") as f:
+            # load all params from yaml
+            all_params = yaml.safe_load(f)
+            booster_params = all_params["booster_params"]
+
+            # add all finlets
+            self.lifting_surfaces: list[LiftingSurface] = []
+            for finlet_id in [2, 3]:
+                # pitching fins
+                self.lifting_surfaces.append(
+                    LiftingSurface(
+                        p=self.p,
+                        physics_period=self.physics_period,
+                        np_random=self.np_random,
+                        uav_id=self.Id,
+                        surface_id=finlet_id,
+                        command_id=0,
+                        command_sign=+1.0,
+                        lifting_vector=np.array([0.0, 0.0, 1.0]),
+                        forward_vector=np.array([0.0, -1.0, 0.0]),
+                        aerofoil_params=all_params["finlet_params"],
+                    )
+                )
+            for finlet_id in [4, 5]:
+                # rolling fins
+                self.lifting_surfaces.append(
+                    LiftingSurface(
+                        p=self.p,
+                        physics_period=self.physics_period,
+                        np_random=self.np_random,
+                        uav_id=self.Id,
+                        surface_id=finlet_id,
+                        command_id=1,
+                        command_sign=+1.0,
+                        lifting_vector=np.array([1.0, 0.0, 0.0]),
+                        forward_vector=np.array([0.0, -1.0, 0.0]),
+                        aerofoil_params=all_params["finlet_params"],
+                    )
+                )
+
+            # add the booster
+            self.boosters = Boosters(
+                p=self.p,
+                physics_period=self.physics_period,
+                np_random=self.np_random,
+                uav_id=self.Id,
+                booster_ids=np.array([1], dtype=int),
+                fueltank_ids=np.array([0], dtype=int),
+                total_fuel_mass=np.array([booster_params["total_fuel"]]),
+                max_fuel_rate=np.array([booster_params["max_fuel_rate"]]),
+                max_inertia=np.array(
+                    [
+                        [
+                            booster_params["inertia_ixx"],
+                            booster_params["inertia_iyy"],
+                            booster_params["inertia_izz"],
+                        ]
+                    ]
+                ),
+                min_thrust=np.array([booster_params["min_thrust"]]),
+                max_thrust=np.array([booster_params["max_thrust"]]),
+                thrust_unit=np.array([[0.0, 1.0, 0.0]]),
+                reignitable=np.array([booster_params["reignitable"]], dtype=bool),
+                booster_tau=np.array([booster_params["booster_tau"]]),
+                gimbal_tau=np.array([booster_params["gimbal_tau"]]),
+                gimbal_range_degrees=np.array([booster_params["gimbal_range_degrees"]]),
+            )
+
+        """ CAMERA """
+        self.use_camera = use_camera
+        if self.use_camera:
+            self.camera = Camera(
+                p=self.p,
+                uav_id=self.Id,
+                camera_id=0,
+                use_gimbal=use_gimbal,
+                camera_FOV_degrees=camera_FOV_degrees,
+                camera_angle_degrees=camera_angle_degrees,
+                camera_resolution=camera_resolution,
+            )
+
+        """ CUSTOM CONTROLLERS """
+        # dictionary mapping of controller_id to controller objects
+        self.registered_controllers = dict()
+        self.instanced_controllers = dict()
+        self.registered_base_modes = dict()
+
+        self.reset()
+
+    def reset(self):
+        """reset."""
+        self.set_mode(0)
+        self.setpoint = np.zeros((4))
+        self.pwm = np.zeros((4))
+
+        self.p.resetBasePositionAndOrientation(self.Id, self.start_pos, self.start_orn)
+        self.boosters.reset()
+        self.update_state()
+
+        if self.use_camera:
+            self.rgbaImg, self.depthImg, self.segImg = self.camera.capture_image()
+
+    def update_state(self):
+        """ang_vel, ang_pos, lin_vel, lin_pos"""
+        lin_pos, ang_pos = self.p.getBasePositionAndOrientation(self.Id)
+        lin_vel, ang_vel = self.p.getBaseVelocity(self.Id)
+
+        # express vels in local frame
+        rotation = np.array(self.p.getMatrixFromQuaternion(ang_pos)).reshape(3, 3).T
+        lin_vel = np.matmul(rotation, lin_vel)
+        ang_vel = np.matmul(rotation, ang_vel)
+
+        # ang_pos in euler form
+        ang_pos = self.p.getEulerFromQuaternion(ang_pos)
+
+        # create the state
+        self.state = np.stack([ang_vel, ang_pos, lin_vel, lin_pos], axis=0)
+
+        # update all lifting surface velocities
+        for surface in self.lifting_surfaces:
+            surface_velocity = self.p.getLinkState(
+                self.Id, surface.surface_id, computeLinkVelocity=True
+            )[-2]
+            surface.update_local_surface_velocity(rotation, surface_velocity)
+
+        # update fuel state
+        self.aux_state = self.boosters.ratio_fuel_remaining
+
+    def update_control(self):
+        """runs through controllers"""
+        # the default mode
+        if self.mode == 0:
+            self.cmd = self.setpoint
+            return
+
+        # otherwise, check that we have a custom controller
+        if self.mode not in self.registered_controllers.keys():
+            raise ValueError(
+                f"Don't have other modes aside from 0, received {self.mode}."
+            )
+
+        # custom controllers run if any
+        self.cmd = self.instanced_controllers[self.mode].step(self.state, self.setpoint)
+
+    def update_forces(self):
+        """Calculates and applies forces acting on Rocket"""
+        # update all finlets
+        for surface in self.lifting_surfaces:
+            actuation = (
+                0.0
+                if surface.command_id is None
+                else float(self.cmd[surface.command_id] * surface.command_sign)
+            )
+
+            surface.pwm2forces(actuation)
+
+        # update booster
+        self.boosters.settings2forces(
+            ignition=self.cmd[[2]],
+            pwm=self.cmd[[3]],
+            gimbal_x=self.cmd[[4]],
+            gimbal_y=self.cmd[[5]],
+        )
+
+    def update_physics(self):
+        """update_physics."""
+        self.update_state()
+        self.update_forces()
+
+    def update_avionics(self):
+        """
+        updates state and control
+        """
+        self.update_control()
+
+        if self.use_camera:
+            self.rgbaImg, self.depthImg, self.segImg = self.camera.capture_image()
