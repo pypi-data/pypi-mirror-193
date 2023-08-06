c72b489e620589455d1ade1066403dc16cc1a76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:29 +0000

    Add package ilus/1.3.0/ilus-1.3.0.tar.gz

---
+++ packages/ilus/ilus-1.3.0.tar.gz/ilus-1.3.0/ilus/log.py
@@ -0,0 +1,15 @@
+import sys
+import logbook
+from logbook import Logger, StreamHandler
+
+LOG_NAME = "ilus"
+
+logbook.set_datetime_format("local")
+log_handler = StreamHandler(sys.stderr)
+log_handler.push_application()
+Log = Logger(LOG_NAME)
+
+# other log
+logger = Logger(LOG_NAME)
+logger_cmd = Logger(LOG_NAME + "-commands")
+logger_stdout = Logger(LOG_NAME + "-stdout")
