d80d0e1f2437b3e77c9aba98d4873d18901232f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:00 +0000

    Add package fullwait/0.0.1/fullwait-0.0.1-py3-none-any.whl

---
+++ packages/fullwait/fullwait-0.0.1-py3-none-any.whl/fullwait/lib.py
@@ -0,0 +1,31 @@
+import logging
+import re
+
+import bs4
+import requests
+
+_logger = logging.getLogger(__name__)
+
+pat = re.compile(
+    r"https://efa-installer.amazonaws.com/aws-efa-installer-(?P<version>[\.\d]+).tar.gz"
+)
+
+
+def get_efa_version(url: str) -> str:
+    _logger.debug(f"fetching {url}")
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    _logger.debug(f"parsing html from {url}")
+    text = soup.get_text()
+
+    mo = pat.search(text, re.IGNORECASE)
+
+    version = None
+    if mo:
+        version = mo.group("version")
+        _logger.debug(f"found version {version}")
+
+    if not mo:
+        _logger.fatal(f"couldn't find version number in html page {url}")
+
+    return version
