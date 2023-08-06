ce990076e650f9d63776a1a399a957a1bde78725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:34 +0000

    Add package playgroundrl/0.0.4/playgroundrl-0.0.4.tar.gz

---
+++ packages/playgroundrl/playgroundrl-0.0.4.tar.gz/playgroundrl-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+   name='playgroundrl',
+   version='0.0.4',
+   author='Rayan Krishnan',
+   packages=['playgroundrl'],
+   package_dir={'':'src'},
+   scripts=[],
+   url='http://pypi.python.org/pypi/playgroundrl/',
+   # license='LICENSE.txt',
+   description='Python SDK for Playground RL',
+   # long_description=open('README.txt').read(),
+   install_requires=[
+        'python-socketio==5.6.0',
+        'attrs==22.2.0',
+        'stockfish==3.28' 
+   ],
+)
