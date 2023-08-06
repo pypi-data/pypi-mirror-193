c8b001c7b68a5b505db46dd0f95d4241330d68ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:28 +0000

    Add package ocrmymanga/0.15/ocrmymanga-0.15.tar.gz

---
+++ packages/ocrmymanga/ocrmymanga-0.15.tar.gz/ocrmymanga-0.15/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='ocrmymanga',
+    version='0.15',
+    description='A handy tool for OCRing manga',
+    packages=['OCRMyManga'],
+    install_requires=[
+        # List any dependencies your package requires
+        'requests',
+        'pillow',
+        'reportlab',
+        'PyPDF2',
+        'google-cloud-vision',
+    ],
+    entry_points={
+        'console_scripts': [
+            # If your package includes a command-line interface, you can define it here
+            'ocrmymanga = OCRMyManga.__init__:main',
+        ],
+    },
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+)
