fb400d0976c4bd58597b8c3898de546ea00d6c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:36 +0000

    Add package mob-tool/1.0.21/mob-tool-1.0.21.tar.gz

---
+++ packages/mob-tool/mob-tool-1.0.21.tar.gz/mob-tool-1.0.21/src/mobt/GitCli/GitPython/GitActions/Push.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+
+from git import GitCommandError, Repo
+
+from mobt.GitCli.BranchName import BranchName
+from mobt.GitCli.GitPython.GitActions.Exceptions import NonFastForwardPush
+from mobt.GitCli.GitPython.GitActions.GitAction import GitAction
+
+
+@dataclass(frozen=False)
+class _PullRequestData:
+    term: str
+    create_url: str
+    view_url: str
+
+    def __str__(self):
+        return f'Create new {self.term}: {self.create_url}'
+
+
+@dataclass(frozen=False)
+class _PushUndoContext:
+    unset_upstream: bool = False
+    delete_remote_branch: bool = False
+
+
+@dataclass()
+class Push(GitAction):
+    repo: Repo
+    branch_to_push: BranchName
+    force: bool = False
+
+    def __post_init__(self):
+        self.__context: _PushUndoContext = _PushUndoContext()
+        super().__post_init__()
+
+    def _execute(self) -> None:
+        branch = self.repo.branches[self.branch_to_push]
+        if not branch.tracking_branch():
+            self._create_upstream()
+            self.__context.unset_upstream = True
+            self.__context.delete_remote_branch = True
+        else:
+            self._push_existing_branch()
+
+    def _undo(self):
+        if self.__context.delete_remote_branch:
+            self.repo.git.push("origin", "--delete", self.branch_to_push)
+
+        if self.__context.unset_upstream:
+            self.repo.git.branch("--unset-upstream", self.branch_to_push)
+
+    def _push_existing_branch(self):
+        try:
+            if self.force:
+                self.repo.git.push("origin", self.branch_to_push, "--force")
+            else:
+                self.repo.git.push("origin", self.branch_to_push)
+        except GitCommandError as e:
+            if self._is_non_fast_forward_push(str(e)):
+                raise NonFastForwardPush.create()
+            raise e
+
+    def _create_upstream(self):
+        self.repo.git.push("origin", self.branch_to_push, "--set-upstream")
+
+    @staticmethod
+    def _is_non_fast_forward_push(error_message: str):
+        return "failed to push some refs" in error_message and "non-fast-forward" in error_message
---
+++ packages/mob-tool/mob-tool-1.0.21.tar.gz/mob-tool-1.0.21/src/mobt/MobApp/StartMobbing.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from git import GitError
+from injector import inject
+
+from mobt.GitCli.BranchName import BranchName
+from mobt.GitCli.GitCliWithAutoRollback import GitCliWithAutoRollback
+from mobt.GitCli.GitPython import log_undoing_all_git_commands
+from mobt.MobApp.Exceptions import BranchAlreadyExistsAndIsNotMobBranch
+from mobt.MobException import MobException
+from mobt.SessionSettings import SessionSettings
+from mobt.SessionSettings.RotationSettings import RotationSettings
+from mobt.SessionSettings.SessionSettings import TeamMembers
+from mobt.SessionSettings.SessionSettingsService import SessionSettingsService
+from mobt.Timer.TimerService import TimerService
+
+
+@inject
+@dataclass
+class StartMobbing:
+    git: GitCliWithAutoRollback
+
+    session_settings_services: SessionSettingsService
+
+    timer: TimerService
+
+    def start(self, branch_name: Optional[BranchName], team: TeamMembers,
+              force_if_non_mob_branch: bool = False) -> SessionSettings:
+        self.git.fail_if_dirty()
+
+        branch_name = branch_name or self.git.current_branch()
+
+        try:
+            if self.git.branch_exists(branch_name):
+                self.git.checkout(branch_name)
+                session_settings = self.session_settings_services.find()
+                if not session_settings:
+                    if force_if_non_mob_branch:
+                        self.git.pull_with_rebase()
+                        session_settings = self.session_settings_services.find()
+                        if not session_settings:
+                            self._create_session_settings_commit_and_push(team)
+                            session_settings = self.session_settings_services.find()
+                    else:
+                        raise BranchAlreadyExistsAndIsNotMobBranch.create(branch_name)
+
+                if session_settings.team != team:
+                    self.session_settings_services.update_members(team)
+                    self.git.add_undo_callable(
+                        lambda: self.session_settings_services.update_members(session_settings.team)
+                    )
+                    self.git.commit_all_and_push("WIP: mob start", skip_hooks=True)
+            else:
+                self.git.create_new_branch_from_main_and_checkout(branch_name)
+                self._create_session_settings_commit_and_push(team)
+
+            return self.session_settings_services.get()
+        except Exception as e:
+            if self.git.undo_commands.has_commands:
+                log_undoing_all_git_commands()
+                self.git.undo()
+
+            if isinstance(e, GitError):
+                e = MobException(str(e))
+
+            raise e
+
+    def _create_session_settings_commit_and_push(self, team: TeamMembers):
+        self.session_settings_services.create(team, RotationSettings())
+        self.git.add_undo_callable(lambda: self.session_settings_services.delete())
+        self.git.commit_all_and_push("WIP: mob start", skip_hooks=True)
