ed7b8b71d4bb11ebe7bca37da79db33a57ca71aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:02 +0000

    Add package keraspoj/0.3.3/keraspoj-0.3.3.tar.gz

---
+++ packages/keraspoj/keraspoj-0.3.3.tar.gz/keraspoj-0.3.3/keraspoj/src/backend/base.py
@@ -0,0 +1,47 @@
+from keraspoj.src.util.tools import generate_uuid
+
+
+class TikzElement:
+    def __init__(self, internal_name, depends_on=None):
+        self.internal_name = internal_name
+        self.depends_on = depends_on if depends_on is not None else []
+
+    def to_code(self):
+        raise NotImplementedError()
+
+
+class TikzArg(TikzElement):
+    def __init__(self, key, value=None):
+        self.key = key
+        self.value = value
+        super().__init__(generate_uuid(), depends_on=None)
+
+    def to_code(self):
+        return str(self)
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        if self.value is None:
+            return str(self.key)
+        return f"{self.key}={self.value}"
+
+
+class TikzOptions(TikzElement):
+    def __init__(self, *args, **kwargs):
+        self.options = [TikzArg(arg) for arg in args]
+        self.options.extend([TikzArg(key, value) for key, value in kwargs.items()])
+        super().__init__(generate_uuid(), depends_on=None)
+
+    def add_option(self, key, value):
+        self.options.append(TikzArg(key, value))
+
+    def add_flag(self, flag):
+        self.options.append(TikzArg(flag))
+
+    def to_code(self):
+        return str(self)
+
+    def __str__(self):
+        return ",".join([str(option) for option in self.options]) if self.options else ""
---
+++ packages/keraspoj/keraspoj-0.3.3.tar.gz/keraspoj-0.3.3/keraspoj/src/backend/misc/grid.py
@@ -0,0 +1,19 @@
+from keraspoj.src.backend.base import TikzElement
+from keraspoj.src.util.tools import generate_uuid
+
+
+class Grid(TikzElement):
+    def __init__(self, to_x, to_y, from_x=0, from_y=0, grid_style_name="grid"):
+        super().__init__("grid_" + generate_uuid())
+        self.from_x = from_x
+        self.from_y = from_y
+        self.to_x = to_x
+        self.to_y = to_y
+        self.grid_style_name = grid_style_name
+
+    def draw(self):
+        return self.to_code()
+
+    def to_code(self):
+        # \draw[step=20pt, black, very thin] (0,0) grid (60,60);
+        return rf"\draw[{self.grid_style_name}] ({self.from_x}, {self.from_y}) grid ({self.to_x}, {self.to_y});" + "\n"
