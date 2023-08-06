76da1e4c100191cb0fc9c248cb429bee17bb078f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:54 +0000

    Add package ncbiutils/0.7.1/ncbiutils-0.7.1-py3-none-any.whl

---
+++ packages/ncbiutils/ncbiutils-0.7.1-py3-none-any.whl/ncbiutils/ncbiutils.py
@@ -0,0 +1,221 @@
+from pydantic import BaseModel, validator
+from typing import ClassVar, Any, Optional, Tuple, List, Dict, Generator, Union, NamedTuple
+from ncbiutils.types import HttpMethodEnum, DbEnum, RetModeEnum, RetTypeEnum, DownloadPathEnum, DocTypeEnum
+from ncbiutils.http import safe_requests
+from ncbiutils.pubmedxmlparser import PubmedXmlParser
+from ncbiutils.pmcxmlparser import PmcXmlParser
+from ncbiutils.pubmed import Citation
+from ncbiutils.xml import _from_raw
+import gzip
+
+
+class Chunk(NamedTuple):
+    """Article citations are delivered in multiple Chunks"""
+
+    error: Optional[Exception]
+    citations: Optional[List[Citation]]
+    ids: Optional[List[str]]
+
+
+class Eutil(BaseModel):
+    """
+    A base class for other NCBI E-Utilities
+
+    Class attributes
+    ----------
+    base_url : str
+        Base URL for the various NCBI E-Utilities
+    retmax_limit : int
+        Maximum number of records that can be returned
+
+    Attributes
+    ----------
+    retstart : int
+        Index before the first record to return (default 0)
+    retmax : int
+        Maximum number of records to return (default 10000)
+    api_key : str
+        Key for NCBI E-Utilities
+
+    Methods
+    ----------
+    request(url: str, **opts)
+        Make request with appropriate body form parameters
+
+    """
+
+    base_url: ClassVar[str] = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
+    retmax_limit: ClassVar[int] = 10000
+
+    retstart: int = 0
+    retmax: int = retmax_limit
+    api_key: Optional[str] = None
+
+    @validator('retmax')
+    def retmax_is_nonneg_lt_limit(cls, v):
+        if v < 0 or v > cls.retmax_limit:
+            raise ValueError(f'Must be positive number less than {cls.retmax_limit}')
+        return v
+
+    def request(self, url: str, **opts) -> Tuple[Optional[Exception], Any]:
+        """Call one of the NCBI E-Utilities and return (error, requests.Response)"""
+        params: Dict[str, Union[str, int]] = {'retstart': self.retstart, 'retmax': self.retmax}
+        params.update(opts)
+        if self.api_key:
+            params.update({'api_key': self.api_key})
+        err, response = safe_requests(url, method=HttpMethodEnum.POST, files=params, stream=True)
+        return err, response
+
+
+class Efetch(Eutil):
+    """
+    A class tailored for the EFETCH E-Utility
+
+    Class attributes
+    ----------
+    efetch_url : str
+        The E-Utilities URL for EFETCH
+
+     Methods
+    ----------
+    request(db: DbEnum, id: str, **opts)
+        Make request to EFETCH URL with constrained body form parameters
+
+    """
+
+    url: ClassVar[str] = f'{Eutil.base_url}efetch.fcgi'
+
+    def _fetch(self, db: DbEnum, id: str, **opts) -> Tuple[Optional[Exception], Any]:
+        """Call EFETCH E-Utility for the given id and db"""
+        params: Dict[str, Union[str, int]] = {'db': db, 'id': id}
+        params.update(opts)
+        err, response = self.request(self.url, **params)
+        return err, response
+
+
+class PubMedFetch(Efetch):
+    """
+    A class that retrieves article information from PubMed
+
+    Attributes
+    ----------
+    db : DbEnum
+        The pubmed database
+
+    Methods
+    -------
+    fetch(uids: List[str])
+        Retrieve text records given the list of uids
+
+    """
+
+    db: DbEnum = DbEnum.pubmed
+
+    retmode: RetModeEnum = RetModeEnum.xml
+    rettype: Optional[RetTypeEnum]
+
+    def fetch(self, ids: List[str]) -> Tuple[Optional[Exception], Any]:
+        """Return id, and text (i.e. title + abstract) given a PubMed id"""
+        id = ','.join(ids)
+        params: Dict[str, Union[str, int, RetModeEnum, Optional[RetTypeEnum]]] = {
+            'retmode': self.retmode,
+            'rettype': self.rettype,
+        }
+        err, response = self._fetch(db=self.db, id=id, **params)
+        return err, response
+
+    def _parse_xml(self, data: bytes) -> List[Citation]:
+        """Return a list of Citations given the server response"""
+        xml_tree = _from_raw(data)
+        root = xml_tree.docinfo.root_name
+        if root == DocTypeEnum.pubmedArticleSet:
+            parser = PubmedXmlParser()
+        elif root == DocTypeEnum.pmcArticleset:
+            parser = PmcXmlParser()
+        else:
+            raise ValueError(f'Unsupported DOCTYPE root: {root}')
+        records = parser.parse(xml_tree)
+        return list(records)
+
+    def _parse_response(self, data: bytes) -> List[Citation]:
+        """Delegate to an implementation or raise ValueError."""
+        if self.retmode == RetModeEnum.xml and self.rettype is None:
+            return self._parse_xml(data)
+        else:
+            raise ValueError(f'Unsupported retmode: {self.retmode}')
+
+    def _chunks(self, lst: List[str], n: int) -> Generator[List[str], None, None]:
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i : i + n]
+
+    def get_citations(self, uids: List[str]) -> Generator[Chunk, None, None]:
+        """Yields Chunk error, citations (possibly empty) and PubMed uids"""
+        for ids in self._chunks(uids, self.retmax):
+            citations = None
+            error, response = self.fetch(ids)
+            if not error and response:
+                citations = self._parse_response(response.content)
+            yield Chunk(error, citations, ids)
+
+
+class PubMedDownload(BaseModel):
+    """
+    A class that retrieves article information from PubMed FTP file server
+
+    Class attributes
+    ----------
+    base_url : str
+        Base URL for the FTP download site
+    updatefiles_path: str
+        Base path for Daily update files
+    baselinefiles_path: str
+        Base path for Annual baseline files
+
+    Attributes
+    ----------
+    retmax : int
+        Maximum number of records to return (default 10000)
+
+    Methods
+    ----------
+    fetch(url: str)
+        Make request with appropriate body form parameters
+    get_citations(files: List[str],
+      download_path: DownloadPathEnum = DownloadPathEnum.updateFiles) -> Generator[Chunk, None, None]
+        Retrieve the Citations from the indicated files, optional path from DownloadPathEnum
+    """
+
+    base_url: ClassVar[str] = 'https://ftp.ncbi.nlm.nih.gov/pubmed/'
+    updatefiles_path: ClassVar[str] = 'updatefiles'
+    baselinefiles_path: ClassVar[str] = 'baseline'
+
+    def _request(self, url: str) -> Tuple[Optional[Exception], Any]:
+        """Retrieve from NCBI PubMed download sites and return (error, requests.Response)"""
+        err, response = safe_requests(url, method=HttpMethodEnum.GET, stream=True)
+        return err, response
+
+    def _parse_xml(self, data: bytes) -> List[Citation]:
+        """Return a list of Citations given the server response"""
+        parser = PubmedXmlParser()
+        xml_tree = _from_raw(data)
+        records = parser.parse(xml_tree)
+        return list(records)
+
+    def _parse_response(self, data: bytes) -> List[Citation]:
+        """Delegate to an implementation or raise ValueError."""
+        decompressed = gzip.decompress(data)
+        return self._parse_xml(decompressed)
+
+    def get_citations(
+        self, files: List[str], download_path: DownloadPathEnum = DownloadPathEnum.updatefiles
+    ) -> Generator[Chunk, None, None]:
+        url = f'{self.base_url}{download_path}'
+        for file in files:
+            ids = [file]
+            file_url = f'{url}/{file}'
+            citations = None
+            error, response = self._request(file_url)
+            if not error and response:
+                citations = self._parse_response(response.content)
+            yield Chunk(error, citations, ids)
---
+++ packages/ncbiutils/ncbiutils-0.7.1-py3-none-any.whl/ncbiutils/pubmedxmlparser.py
@@ -0,0 +1,170 @@
+from pydantic import BaseModel
+from typing import Optional, List, Generator, Dict, Any
+import re
+from typing_extensions import TypeAlias
+from ncbiutils.pubmed import Author, Journal, Citation
+from ncbiutils.xml import (
+    Element,
+    XmlTree,
+    _find_safe,
+    _text_safe,
+    _find_all,
+    _collect_element_text_with_prefix,
+    _collect_element_text,
+)
+
+#############################
+#   Aliases
+#############################
+
+PubmedArticle: TypeAlias = Element
+PubmedArticleSet: TypeAlias = Element
+
+
+class PubmedXmlParser(BaseModel):
+    """
+    Capabilities to parse PubMed XML.
+    See DTD http://dtd.nlm.nih.gov/ncbi/pubmed/out/pubmed_190101.dtd
+
+    Methods
+    ----------
+    parse(data: bytes) -> Generator[Citation, None, None]
+        Return a list of article data given PubMed XML bytes
+
+    """
+
+    def _get_pmid(self, pubmed_article: PubmedArticle) -> Optional[str]:
+        pmid = _text_safe(pubmed_article, './/MedlineCitation/PMID')
+        return pmid
+
+    def _get_doi(self, pubmed_article: PubmedArticle) -> Optional[str]:
+        doi = _text_safe(pubmed_article, './/PubmedData/ArticleIdList/ArticleId[@IdType="doi"]')
+        return doi
+
+    def _get_pmc(self, pubmed_article: PubmedArticle) -> Optional[str]:
+        pmc = _text_safe(pubmed_article, './/PubmedData/ArticleIdList/ArticleId[@IdType="pmc"]')
+        return pmc
+
+    def _get_abstract(self, pubmed_article: PubmedArticle) -> Optional[str]:
+        abstract = []
+        AbstractTexts = _find_all(pubmed_article, './/MedlineCitation/Article/Abstract/AbstractText')
+        for AbstractText in AbstractTexts:
+            abstract.append(_collect_element_text_with_prefix(AbstractText, 'Label'))
+        return ' '.join(abstract) if len(abstract) > 0 else None
+
+    def _get_title(self, pubmed_article: PubmedArticle) -> str:
+        title = pubmed_article.find('.//MedlineCitation/Article/ArticleTitle')
+        text = _collect_element_text(title)
+        return text
+
+    def _get_affiliations(self, author: Element) -> Optional[List[str]]:
+        affiliations = _find_all(author, './/AffiliationInfo/Affiliation')
+        alist = [_collect_element_text(affiliation) for affiliation in affiliations]
+        return alist if len(alist) > 0 else None
+
+    def _get_emails(self, author: Element) -> Optional[List[str]]:
+        emails: List[str] = []
+        email_regex = re.compile(r'[\w.+-]+@[\w-]+\.[\w.-]+')
+        affiliations = _find_all(author, './/AffiliationInfo/Affiliation')
+        for affiliation in affiliations:
+            line = _collect_element_text(affiliation)
+            sanitized = line.strip('.')
+            matches = re.findall(email_regex, sanitized)
+            emails = emails + matches
+        return emails if len(emails) > 0 else None
+
+    def _get_author(self, author: Element) -> Author:
+        return Author(
+            fore_name=_text_safe(author, './/ForeName'),
+            last_name=_text_safe(author, './/LastName'),
+            initials=_text_safe(author, './/Initials'),
+            collective_name=_text_safe(author, './/CollectiveName'),
+            orcid=_text_safe(author, './/Identifier[@Source="ORCID"]'),
+            affiliations=self._get_affiliations(author),
+            emails=self._get_emails(author),
+        )
+
+    def _get_author_list(self, pubmed_article: PubmedArticle) -> Optional[List[Author]]:
+        authors = _find_all(pubmed_article, './/MedlineCitation/Article/AuthorList/Author')
+        author_list = [self._get_author(author) for author in authors]
+        return author_list if len(author_list) > 0 else None
+
+    def _get_PubmedArticleSet(self, xml_tree: XmlTree) -> PubmedArticleSet:
+        pubmed_article_set = xml_tree.getroot()
+        if pubmed_article_set.tag != 'PubmedArticleSet':
+            raise ValueError('XML document does not contain a PubmedArticleSet')
+        return pubmed_article_set
+
+    def _get_journal(self, pubmed_article: PubmedArticle) -> Journal:
+        journal = _find_safe(pubmed_article, './/MedlineCitation/Article/Journal')
+        issn = [_collect_element_text(issn) for issn in _find_all(journal, './/ISSN')]
+        title = _text_safe(journal, './/Title')
+        iso_abbreviation = _text_safe(journal, './/ISOAbbreviation')
+        volume = _text_safe(journal, './/JournalIssue/Volume')
+        issue = _text_safe(journal, './/JournalIssue/Issue')
+        pub_year = _text_safe(journal, './/JournalIssue/PubDate/Year')
+        pub_month = _text_safe(journal, './/JournalIssue/PubDate/Month')
+        pub_day = _text_safe(journal, './/JournalIssue/PubDate/Day')
+        return Journal(
+            issn=issn,
+            title=title,
+            iso_abbreviation=iso_abbreviation,
+            volume=volume,
+            issue=issue,
+            pub_year=pub_year,
+            pub_month=pub_month,
+            pub_day=pub_day,
+        )
+
+    def _get_pubtypes(self, pubmed_article: PubmedArticle) -> List[str]:
+        publication_types = _find_all(pubmed_article, './/MedlineCitation/Article/PublicationTypeList/PublicationType')
+        uids = [element.get('UI') for element in publication_types]
+        return uids
+
+    def _get_ui(self, element: Element) -> Dict[str, str]:
+        ui = element.get('UI')
+        value = _collect_element_text(element)
+        return {'ui': ui, 'value': value}
+
+    def _get_mesh_heading(self, mesh_heading: Element) -> Dict[str, Any]:
+        heading: Dict[str, Any] = {}
+        descriptor_name = _find_safe(mesh_heading, './/DescriptorName')
+        heading['descriptor_name'] = self._get_ui(descriptor_name)
+        qualifier_name_list = _find_all(mesh_heading, './/QualifierName')
+        if len(qualifier_name_list) > 0:
+            heading['qualifier_name'] = [self._get_ui(qualifier_name) for qualifier_name in qualifier_name_list]
+        return heading
+
+    def _get_mesh_list(self, pubmed_article: PubmedArticle) -> Optional[List[Dict[str, Any]]]:
+        mesh_heading_list = _find_all(pubmed_article, './/MedlineCitation/MeshHeadingList/MeshHeading')
+        mesh_list = [self._get_mesh_heading(mesh_heading) for mesh_heading in mesh_heading_list]
+        return mesh_list if len(mesh_list) > 0 else None
+
+    def parse(self, xml_tree: XmlTree) -> Generator[Citation, None, None]:
+        """Parse an XML document to a list of custom citations"""
+        pubmed_article_set = self._get_PubmedArticleSet(xml_tree)
+        pubmed_articles = _find_all(pubmed_article_set, './/PubmedArticle')
+
+        for pubmed_article in pubmed_articles:
+            pmid = self._get_pmid(pubmed_article)
+            pmc = self._get_pmc(pubmed_article)
+            doi = self._get_doi(pubmed_article)
+            title = self._get_title(pubmed_article)
+            abstract = self._get_abstract(pubmed_article)
+            author_list = self._get_author_list(pubmed_article)
+            journal = self._get_journal(pubmed_article)
+            publication_type_list = self._get_pubtypes(pubmed_article)
+            mesh_list = self._get_mesh_list(pubmed_article)
+            citation = Citation(
+                pmid=pmid,
+                pmc=pmc,
+                title=title,
+                doi=doi,
+                abstract=abstract,
+                author_list=author_list,
+                journal=journal,
+                publication_type_list=publication_type_list,
+                correspondence=[],
+                mesh_list=mesh_list,
+            )
+            yield citation
