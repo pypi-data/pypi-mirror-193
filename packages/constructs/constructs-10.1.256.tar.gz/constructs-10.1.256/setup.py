1bc4e77a375013dce0533bcba2683e83cf3a1411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:14 +0000

    Add package constructs/10.1.256/constructs-10.1.256.tar.gz

---
+++ packages/constructs/constructs-10.1.256.tar.gz/constructs-10.1.256/setup.py
@@ -0,0 +1,63 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "constructs",
+    "version": "10.1.256",
+    "description": "A programming model for software-defined state",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws/constructs",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/aws/constructs.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "constructs",
+        "constructs._jsii"
+    ],
+    "package_data": {
+        "constructs._jsii": [
+            "constructs@10.1.256.jsii.tgz"
+        ],
+        "constructs": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.7",
+    "install_requires": [
+        "jsii>=1.75.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard~=2.13.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Typing :: Typed",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
