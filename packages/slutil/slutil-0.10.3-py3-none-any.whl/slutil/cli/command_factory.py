c3b4ce721f96b57a2e9d0523cf116113e58754c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:50 +0000

    Add package slutil/0.10.3/slutil-0.10.3-py3-none-any.whl

---
+++ packages/slutil/slutil-0.10.3-py3-none-any.whl/slutil/cli/command_factory.py
@@ -0,0 +1,180 @@
+import click
+from slutil.adapters.abstract_slurm_service import AbstractSlurmService
+from slutil.adapters.abstract_vcs import AbstractVCS
+from slutil.cli.cmd_delete import cmd_delete
+from slutil.cli.cmd_filter import cmd_filter
+from slutil.cli.cmd_undelete import cmd_undelete
+from slutil.services.abstract_uow import AbstractUnitOfWork
+from slutil.cli.cmd_submit import cmd_submit
+from slutil.cli.cmd_status import cmd_status
+from slutil.cli.cmd_report import cmd_report
+from slutil.cli.cmd_edit import cmd_edit
+import re
+from typing import Optional
+
+
+def command_factory(
+    uow: AbstractUnitOfWork, slurm: AbstractSlurmService, vcs: AbstractVCS
+) -> click.Group:
+    parent_cmd = click.Group()
+
+    def validate_dependency_str(ctx, param, value) -> tuple[Optional[str], list[int]]:
+        if value:
+            if re.match(
+                r"^(((after|afterany|afternotok|afterok)(:([\d])*)+)|singleton)$", value
+            ):
+                dep_type, *numbers = value.split(":")
+                return (dep_type, numbers)
+            raise click.BadParameter("not a supported dependency option")
+        return (None, [])
+
+    parent_cmd.add_command(
+        click.Command(
+            name="submit",
+            context_settings=None,
+            callback=lambda sbatch_file, description, dependency: cmd_submit(
+                uow, slurm, vcs, sbatch_file, description, dependency
+            ),
+            params=[
+                click.Argument(
+                    ["sbatch_file"], required=True, type=click.Path(exists=True)
+                ),
+                click.Argument(["description"], required=True, type=str),
+                click.Option(
+                    ["-dp", "--dependency"],
+                    required=False,
+                    type=str,
+                    callback=validate_dependency_str,
+                    help="The job's dependencies. In the form ('<type>(:[dependent_id])+' type:=after|afterany|afternotok|afterok) or 'singleton' e.g. 'afterok:123456:345678'"
+                ),
+            ],
+            help=cmd_submit.__doc__,
+        )
+    )
+
+    parent_cmd.add_command(
+        click.Command(
+            name="report",
+            context_settings=None,
+            callback=lambda count, verbose: cmd_report(uow, slurm, count, verbose),
+            params=[
+                click.Option(["-c", "--count"], default=10),
+                click.Option(["-v", "--verbose"], is_flag=True, default=False),
+            ],
+            help=cmd_report.__doc__,
+        )
+    )
+
+    parent_cmd.add_command(
+        click.Command(
+            name="status",
+            context_settings=None,
+            callback=lambda slurm_id, verbose: cmd_status(
+                uow, slurm, slurm_id, verbose
+            ),
+            params=[
+                click.Argument(["slurm_id"], type=int),
+                click.Option(["-v", "--verbose"], is_flag=True, default=False),
+            ],
+            help=cmd_status.__doc__,
+        )
+    )
+
+    parent_cmd.add_command(
+        click.Command(
+            name="delete",
+            context_settings=None,
+            callback=lambda slurm_id, verbose: cmd_delete(
+                uow, slurm, slurm_id, verbose
+            ),
+            params=[
+                click.Argument(["slurm_id"], type=int),
+                click.Option(["-v", "--verbose"], is_flag=True, default=False),
+            ],
+            help=cmd_delete.__doc__,
+        )
+    )
+
+    parent_cmd.add_command(
+        click.Command(
+            name="restore",
+            context_settings=None,
+            callback=lambda slurm_id: cmd_undelete(uow, slurm, slurm_id),
+            params=[
+                click.Argument(["slurm_id"], type=int),
+            ],
+            help=cmd_undelete.__doc__,
+        )
+    )
+
+    parent_cmd.add_command(
+        click.Command(
+            name="edit",
+            context_settings=None,
+            callback=lambda slurm_id: cmd_edit(uow, slurm, slurm_id),
+            params=[
+                click.Argument(["slurm_id"], type=int),
+            ],
+            help=cmd_edit.__doc__,
+        )
+    )
+
+    parent_cmd.add_command(
+        click.Command(
+            name="filter",
+            context_settings=None,
+            callback=lambda job_id, status, description, submit_time, commit, sbatch, verbose: cmd_filter(
+                uow,
+                slurm,
+                job_id,
+                status,
+                description,
+                submit_time,
+                commit,
+                sbatch,
+                verbose,
+            ),
+            params=[
+                click.Option(
+                    ["-j", "--job-id"],
+                    help="regex to match against job id",
+                    type=str,
+                    default=None,
+                ),
+                click.Option(
+                    ["-s", "--status"],
+                    help="regex to match against job status",
+                    type=str,
+                    default=None,
+                ),
+                click.Option(
+                    ["-d", "--description"],
+                    help="regex to match against job description",
+                    type=str,
+                    default=None,
+                ),
+                click.Option(
+                    ["-t", "--submit-time"],
+                    help="regex to match against string representation of submit timestamp (e.g. '2023-02-06 14:32:38')",
+                    type=str,
+                    default=None,
+                ),
+                click.Option(
+                    ["-c", "--commit"],
+                    help="regex to match against commit tag",
+                    type=str,
+                    default=None,
+                ),
+                click.Option(
+                    ["-sb", "--sbatch"],
+                    help="regex to match against sbatch file used",
+                    type=str,
+                    default=None,
+                ),
+                click.Option(["-v", "--verbose"], is_flag=True, default=False),
+            ],
+            help=cmd_filter.__doc__,
+        )
+    )
+
+    return parent_cmd
---
+++ packages/slutil/slutil-0.10.3-py3-none-any.whl/slutil/main.py
@@ -0,0 +1,49 @@
+from rich.console import Console
+from slutil.services.csv_uow import CsvUnitOfWork
+from slutil.services.abstract_uow import AbstractUnitOfWork
+from slutil.adapters.slurm import SlurmService
+from slutil.adapters.abstract_slurm_service import AbstractSlurmService
+from slutil.adapters.abstract_vcs import AbstractVCS
+from slutil.adapters.git import Git
+from slutil.cli.command_factory import command_factory
+from dataclasses import dataclass
+
+
+@dataclass
+class Dependencies:
+    uow: AbstractUnitOfWork
+    slurm: AbstractSlurmService
+    vcs: AbstractVCS
+
+
+def build_dependencies(debug: bool) -> Dependencies:
+    # if debug:
+    #     uow = CsvUnitOfWork("")
+    # from tests.conftest import FakeSlurm, FakeVCS
+
+    #     slurm = FakeSlurm()
+    #     vcs = FakeVCS()
+    # else:
+    uow = CsvUnitOfWork("")
+    slurm = SlurmService()
+    vcs = Git()
+    return Dependencies(uow, slurm, vcs)
+
+
+def start_cli():
+    debug = True
+    dependencies = build_dependencies(debug=debug)
+    c = command_factory(dependencies.uow, dependencies.slurm, dependencies.vcs)
+    
+    try:
+        c()
+    except Exception as e:
+        if debug:
+            raise e
+        else:
+            console = Console()
+            console.print(f"[red]Error: {str(e)}[/red]")
+
+
+if __name__ == "__main__":
+    start_cli()
