62ddcdf2420403ea1eb29425e382fbf941f7709d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:04 +0000

    Add package wjec-utils-package/1.0/wjec_utils_package-1.0.tar.gz

---
+++ packages/wjec-utils-package/wjec_utils_package-1.0.tar.gz/wjec_utils_package-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+from wjec_utils_package import __version__
+
+setup(
+    name='wjec_utils_package',
+    version=__version__,
+
+    url='',
+    author='Cathal D',
+    author_email='cathald@weir-jones.com',
+    install_requires=[],
+    packages=find_packages()
+)
