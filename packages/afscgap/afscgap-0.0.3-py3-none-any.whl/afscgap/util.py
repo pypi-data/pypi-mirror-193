66a5cabccc67cca00cfe3c71ea87ab21f2401538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:47 +0000

    Add package afscgap/0.0.3/afscgap-0.0.3-py3-none-any.whl

---
+++ packages/afscgap/afscgap-0.0.3-py3-none-any.whl/afscgap/util.py
@@ -0,0 +1,15 @@
+"""
+Convienence functions and definitions.
+
+(c) 2023 The Eric and Wendy Schmidt Center for Data Science and the Environment
+at UC Berkeley.
+
+This file is part of afscgap released under the BSD 3-Clause License. See
+LICENSE.txt.
+"""
+
+import typing
+
+OPT_FLOAT = typing.Optional[float]
+OPT_INT = typing.Optional[int]
+OPT_STR = typing.Optional[str]
