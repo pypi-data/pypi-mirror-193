5ddfca9d567bc263c028433b3f42e03b5083ee97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:23 +0000

    Add package vitalpy/0.7/vitalpy-0.7.tar.gz

---
+++ packages/vitalpy/vitalpy-0.7.tar.gz/vitalpy-0.7/v/file.py
@@ -0,0 +1,45 @@
+import os, shutil
+
+def ls(path):
+	return os.listdir(path)
+
+def create(file):
+	try:
+		return open(file, "x")
+	except:
+		return None
+
+def write(file, content):
+	try:
+		with open(file, "w") as f:
+			return f.write(content)
+	except:
+		return None
+
+def read(file):
+	try:
+		with open(file):
+			return file.read()
+	except:
+		return None
+
+def rm(path):
+	try:
+		os.remove(path)
+		return True
+	except:
+		return None
+
+def rmdir(path):
+	try:
+		os.rmdir(path)
+		return True
+	except:
+		return None
+
+def mkdir(folder):
+	try:
+		os.mkdir(folder)
+		return True
+	except:
+		return None
