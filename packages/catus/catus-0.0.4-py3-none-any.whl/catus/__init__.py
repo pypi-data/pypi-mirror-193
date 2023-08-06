f8fe364657a55d9c1a60f3c323432691210977d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:52 +0000

    Add package catus/0.0.4/catus-0.0.4-py3-none-any.whl

---
+++ packages/catus/catus-0.0.4-py3-none-any.whl/catus/__init__.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+
+plt.rcParams.update({
+    'text.usetex': True,
+    'font.family': 'serif',
+    'font.size': 12,
+    'figure.figsize': (5, 5),
+    'figure.titlesize': 'large',
+    'axes.titlesize': 'large',
+    'axes.labelsize': 'large',
+    'xtick.top': True,
+    'xtick.major.size': 5,
+    'xtick.minor.size': 3,
+    'xtick.minor.visible': True,
+    'xtick.direction': 'out',
+    'xtick.labelsize': 'medium',
+    'ytick.right': True,
+    'ytick.major.size': 5,
+    'ytick.minor.size': 3,
+    'ytick.minor.visible': True,
+    'ytick.direction': 'out',
+    'ytick.labelsize': 'medium',
+    'legend.frameon': True,
+    'legend.framealpha': 1,
+    'legend.fontsize': 'medium',
+    'lines.linewidth': 2,
+    'patch.linewidth': 2,
+    'hatch.linewidth': 2,
+    })
+
+cmap = plt.get_cmap('tab10')
+
