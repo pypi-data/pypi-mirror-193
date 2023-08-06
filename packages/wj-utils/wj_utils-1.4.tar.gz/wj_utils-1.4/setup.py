98ff54798101205ec1bb77665025fc962e5e5756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:53 +0000

    Add package wj-utils/1.4/wj_utils-1.4.tar.gz

---
+++ packages/wj-utils/wj_utils-1.4.tar.gz/wj_utils-1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+from wj_utils import __version__
+
+setup(
+    name='wj_utils',
+    version=__version__,
+    copyright='Weir-Jones Ltd',
+    author='Cathal D',
+    packages=find_packages()
+)
