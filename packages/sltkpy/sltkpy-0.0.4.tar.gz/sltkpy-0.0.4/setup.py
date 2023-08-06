c323eefbd99c35bffd310b9abee10dbb7efdbf97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:32 +0000

    Add package sltkpy/0.0.4/sltkpy-0.0.4.tar.gz

---
+++ packages/sltkpy/sltkpy-0.0.4.tar.gz/sltkpy-0.0.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4' 
+DESCRIPTION = 'Sinhala Language Tool Kit'
+LONG_DESCRIPTION = 'Sinhala Language Tool Kit'
+
+# Setting up
+
+# python setup.py sdist bdist_wheel
+# python -m twine upload dist/*
+
+setup(
+       # the name must match the folder name 'sltk'
+        name="sltkpy", 
+        version=VERSION,
+        author="Buddhi Kavindra Ranasinghe",
+        author_email="info@buddhilive.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages
+        package_data={"my_package": ["*.txt", "*.json"]},
+        include_package_data=True,
+        data_files=[('shared', ['sltkpy/shared/pre.txt', 'sltkpy/shared/abbr.json', 'sltkpy/shared/dict.json'])],
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
