16d89ad08fb388a6c2b134a85b175e8806931267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:06 +0000

    Add package softsh/0.1.1/softsh-0.1.1.tar.gz

---
+++ packages/softsh/softsh-0.1.1.tar.gz/softsh-0.1.1/src/lib.rs
@@ -0,0 +1,24 @@
+use pyo3::prelude::*;
+use sys_info;
+
+/// Returns the distribution name along with package manager
+#[pyfunction]
+fn get_dist_info() -> PyResult<String> {
+    let dist_release_info = sys_info::linux_os_release().unwrap();
+    let dist_id = dist_release_info.id.unwrap(); 
+    println!("os release is :{dist_id}");
+    if dist_id.eq("fedora"){
+        return Ok(dist_id + ":dnf");
+    }
+    if dist_id.eq("ubuntu"){
+        return Ok(dist_id + ":apt");
+    }
+    Ok(dist_id)
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn softsh(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(get_dist_info, m)?)?;
+    Ok(())
+}
