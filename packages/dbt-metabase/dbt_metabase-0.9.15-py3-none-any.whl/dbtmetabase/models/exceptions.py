9acaa627cbaaa17d0d3fa3ccf8c775ef192afd11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:29 +0000

    Add package dbt-metabase/0.9.15/dbt_metabase-0.9.15-py3-none-any.whl

---
+++ packages/dbt-metabase/dbt_metabase-0.9.15-py3-none-any.whl/dbtmetabase/models/exceptions.py
@@ -0,0 +1,14 @@
+class NoDbtPathSupplied(Exception):
+    """Thrown when no argument for dbt path has been supplied"""
+
+
+class NoDbtSchemaSupplied(Exception):
+    """Thrown when using folder parser without supplying a schema"""
+
+
+class MetabaseUnableToSync(Exception):
+    """Thrown when Metabase cannot sync / align models with dbt model"""
+
+
+class MetabaseRuntimeError(Exception):
+    """Thrown when Metabase execution failed."""
