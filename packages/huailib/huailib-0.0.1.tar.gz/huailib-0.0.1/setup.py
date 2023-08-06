d0ee79fb518d484763ea3b28c66e78e4f7fb3658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:41 +0000

    Add package huailib/0.0.1/huailib-0.0.1.tar.gz

---
+++ packages/huailib/huailib-0.0.1.tar.gz/huailib-0.0.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @File     : setup
+# @Author   : LiuYan
+# @Time     : 2021/4/16 10:07
+
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='huailib',
+    version='0.0.1',
+    author='huai guo',
+    author_email='guohuai@csu.edu.cn',
+    description='a helpful python library',
+    long_description=long_description,
+    url='https://github.com/wsmgh/hpl.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
