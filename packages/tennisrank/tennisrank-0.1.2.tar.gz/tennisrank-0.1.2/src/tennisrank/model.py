a701f7b1681c84fb97ad63a115076a5b940ffdae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:27 +0000

    Add package tennisrank/0.1.2/tennisrank-0.1.2.tar.gz

---
+++ packages/tennisrank/tennisrank-0.1.2.tar.gz/tennisrank-0.1.2/src/tennisrank/model.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Player:
+    id: int
+    name: str
+
+
+@dataclass(frozen=True)
+class Match:
+    winner: Player = None
+    loser: Player = None
+    point_diff: int = None
+    surface: str = None
+
+
+@dataclass(frozen=True)
+class PlayerRank:
+    player: Player = None
+    rank: float = None
+    surface: str = None
