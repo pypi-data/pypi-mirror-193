ac3cc0964f15c7a8554f9fb491a96c1c07a71d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:55 +0000

    Add package halring/2.0.3/halring-2.0.3-py3-none-any.whl

---
+++ packages/halring/halring-2.0.3-py3-none-any.whl/halring/windows_exec/halring_exec.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+import os
+import sys
+import subprocess
+
+nga_root = os.path.abspath(__file__).split("halring")[0]
+if nga_root not in nga_root:
+    sys.path.append(nga_root)
+
+
+class ExecUtil(object):
+    """
+    Exeute a program from python in Windows OS
+    Author: xinwu
+    """
+    def __init__(self, cmd):
+        self._cmd = cmd
+        self._exe_path = nga_root + "exe\\"
+
+    def block_execute(self):
+        """
+        Exeute a program from python in Windows OS by blocking mode
+        :return:
+            console standard out
+        """
+        execute_cmd = self._exe_path + self._cmd
+        print(execute_cmd)
+        with os.popen(execute_cmd, "r") as execute_cmd_hndl:
+            execute_rtn = execute_cmd_hndl.read()
+            print(execute_rtn)
+            execute_cmd_hndl.close()
+        return execute_rtn
+
+    def non_block_execute(self):
+        """
+        Exeute a program from python in Windows OS by non-blocking mode
+        :return:
+        """
+        execute_cmd = self._exe_path + self._cmd
+        p = subprocess.Popen(execute_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        '''while p.poll()==None:
+            print(p.stdout.readline())
+        print(p.returncode)'''
+
+
+if __name__ == '__main__':
+    exe = ExecUtil("startEzSTEP E:\FEAProduct\EzStepDTP\EzSTEP.exe")
+    exe.block_execute()
