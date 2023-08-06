3736272fbe02e37be0b704870f2cbf8de5bc77d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:36 +0000

    Add package pyttb/1.3.9/pyttb-1.3.9-py3-none-any.whl

---
+++ packages/pyttb/pyttb-1.3.9-py3-none-any.whl/pyttb/ttensor.py
@@ -0,0 +1,16 @@
+# Copyright 2022 National Technology & Engineering Solutions of Sandia,
+# LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the
+# U.S. Government retains certain rights in this software.
+
+import pyttb as ttb
+from .pyttb_utils import *
+import numpy as np
+
+class ttensor(object):
+    """
+    TTENSOR Class for Tucker tensors (decomposed).
+
+    """
+
+    def __init__(self, *args):  # pragma:no cover
+        assert False, "TTENSOR class not yet implemented"
