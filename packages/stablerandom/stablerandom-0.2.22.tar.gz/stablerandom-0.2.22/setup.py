1882398f10f766498766997ff49333e8a2c9fbde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:46 +0000

    Add package stablerandom/0.2.22/stablerandom-0.2.22.tar.gz

---
+++ packages/stablerandom/stablerandom-0.2.22.tar.gz/stablerandom-0.2.22/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['stablerandom']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.22,<2.0']
+
+setup_kwargs = {
+    'name': 'stablerandom',
+    'version': '0.2.22',
+    'description': 'Create stable/repeatable numpy.random applications',
+    'long_description': 'None',
+    'author': 'John Heintz',
+    'author_email': 'john@gistlabs.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
