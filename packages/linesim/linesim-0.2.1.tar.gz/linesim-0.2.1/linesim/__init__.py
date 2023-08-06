e07cde4a2487d93348d0050b6f251b159414b71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:07 +0000

    Add package linesim/0.2.1/linesim-0.2.1.tar.gz

---
+++ packages/linesim/linesim-0.2.1.tar.gz/linesim-0.2.1/linesim/__init__.py
@@ -0,0 +1,8 @@
+"""
+Line simulation made with pygame for testing
+a robot's pathfinding abilities.
+"""
+
+from .beacons import Magnet
+from .sensors import Line, Ultrasonic, Hall
+from .linesim import LineSimulation, Robot
