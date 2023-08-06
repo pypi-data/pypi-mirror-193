3e4e4caf4a7852a520918ddd918e7c7434c7bbfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:52 +0000

    Add package wdtea/0.0.2/wdtea-0.0.2.tar.gz

---
+++ packages/wdtea/wdtea-0.0.2.tar.gz/wdtea-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wdtea",
+    version="0.0.2",
+    author="jaxbax",
+    author_email="labbolabyt@gmail.com",
+    description="Basically a module with stuff that I find helpful",
+    long_description=long_description, # don't touch this, this is your README.md
+    long_description_content_type="text/markdown",
+    url="https://replit.com/@LabboLab/wdtea?v=1",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
