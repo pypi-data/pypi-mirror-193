5c5b9503b23bcefef22b2a30b4c22fae7c63da36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:26 +0000

    Add package django-migration-dependencies/0.1.4/django_migration_dependencies-0.1.4.tar.gz

---
+++ packages/django-migration-dependencies/django_migration_dependencies-0.1.4.tar.gz/django_migration_dependencies-0.1.4/dmdg/migrations_graph/views.py
@@ -0,0 +1,123 @@
+from collections import namedtuple
+
+from django.apps import apps
+from django.conf import settings
+from django.db.migrations.graph import Node, MigrationGraph
+from django.db.migrations.loader import MigrationLoader
+from django.db.migrations.operations.special import SeparateDatabaseAndState, RunPython, RunSQL
+from django.shortcuts import render
+
+
+Migration = namedtuple("Migration", ("id", "name", "parent", "runs_special"))
+App = namedtuple("App", ("id", "name"))
+Edge = namedtuple("Edge", ("id", "source", "target"))
+
+
+def make_migration(node: Node, graph: MigrationGraph) -> Migration:
+    mig = graph.nodes[node.key]
+    return Migration(
+        id=f"{node.key[0]}-{node.key[1]}",
+        name=node.key[1],
+        parent=node.key[0],
+        runs_special=any(isinstance(op, (SeparateDatabaseAndState, RunSQL, RunPython)) for op in mig.operations)
+    )
+
+
+def make_app(node: Node) -> App:
+    return App(id=node.key[0], name=node.key[0])
+
+
+def make_edge(migration_a: Migration, migration_b: Migration) -> Edge:
+    edge = Edge(
+        id=f"{migration_a.id}-{migration_b.id}",
+        source=migration_a.id,
+        target=migration_b.id
+    )
+
+    return edge
+
+
+def index(request):
+    include = request.GET.get('include', None)
+    exclude = request.GET.get('exclude', "")
+    layout_name = request.GET.get('layout', 'dagre')
+
+    loader = MigrationLoader(None)
+
+    if include:
+        include = include.split(',')
+    else:
+        include = [app.label for app in apps.get_app_configs()]
+
+    root_keys = [
+        root_keys
+        for app in include if app not in exclude
+        for root_keys in loader.graph.root_nodes(app)
+    ]
+
+    visited = set()
+    apps_set = set()
+    migrations = set()
+    edges = set()
+    for root_key in root_keys:
+        root_node = loader.graph.node_map[root_key]
+
+        nodes_to_visit = [root_node]
+        while nodes_to_visit:
+            node = nodes_to_visit.pop(0)
+
+            if node in visited:
+                continue
+
+            migration = make_migration(node, loader.graph)
+
+            visited.add(node)
+            migrations.add(migration)
+            apps_set.add(make_app(node))
+
+            for parent in node.parents:
+                parent_migration = make_migration(parent, loader.graph)
+
+                migrations.add(parent_migration)
+                apps_set.add(make_app(parent))
+                edges.add(make_edge(parent_migration, migration))
+
+            if root_node.key[0] != node.key[0]:
+                # It's coming from a different app, we won't look into their downstream tree
+                continue
+
+            for child in node.children:
+                if child not in nodes_to_visit and child not in visited:
+                    nodes_to_visit.append(child)
+
+    nodes = [
+        dict(id=app.id, name=app.name, type="app")
+        for app in apps_set
+    ] + [ # noqa
+        dict(
+            id=migration.id,
+            name=migration.name,
+            parent=migration.parent,
+            runs_special=migration.runs_special,
+            type="migration",
+        )
+        for migration in migrations
+    ]
+
+    edges = [
+        dict(id=edge.id, source=edge.source, target=edge.target)
+        for edge in edges
+    ]
+
+    special_icon = getattr(
+        settings,
+        'MIGRATIONS_GRAPH_SPECIAL_ICON',
+        'https://raw.githubusercontent.com/edg956/django-migration-dependencies/main/public/img/python-logo.svg'
+    )
+
+    return render(request, 'migrations_graph/index.html', {
+        "nodes": nodes,
+        "edges": edges,
+        "layout": layout_name,
+        "special_icon": special_icon
+    })
---
+++ packages/django-migration-dependencies/django_migration_dependencies-0.1.4.tar.gz/django_migration_dependencies-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'dmdg'}
+
+packages = \
+['migrations_graph']
+
+package_data = \
+{'': ['*'], 'migrations_graph': ['templates/migrations_graph/*']}
+
+setup_kwargs = {
+    'name': 'django-migration-dependencies',
+    'version': '0.1.4',
+    'description': 'A Django app to visualise the migrations graph',
+    'long_description': '# Django Migration Dependency Graph\nThis is an app that helps developers understand how their migrations are interconnected across their apps, serving as a tool to identify possible circular dependencies when squashing them.\n\n## Installation\n\n    pip install django-migration-dependencies\n\n## Quick start\n\n1. Add "migrations-graph" to your INSTALLED_APPS setting like this:\n\n\n    INSTALLED_APPS = [\n        ...\n        \'migrations-graph\',\n    ]\n\n\n2. Include the polls URLconf in your project urls.py like this::\n\n\n    import migrations_graph.urls\n    path(\'migrations-graph/\', include(\'migrations_graph.urls\')),\n\n\n3. Start the development server.\n\n4. Visit http://127.0.0.1:8000/migrations_graph/ to see the graph.\n<img width="689" alt="image" src="https://user-images.githubusercontent.com/36116126/220771061-ecf9812b-afe8-459b-927b-58025225dfd8.png">\n\n## Caveats\n- Some times you won\'t get a graph for some reason and find an error in the browser\'s console `Cannot set property \'order\' of undefined...`. Change the layout by passing the query param `?layout=breadthfirst`. For more info see https://github.com/dagrejs/dagre/issues/234\n\n# Disclaimer\nThis is a real quick hack to help a one-time need (for now). You won\'t find tests here (...that is, for now).\n',
+    'author': 'Eugenio Doñaque',
+    'author_email': 'eugenio@donaque.xyz',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/edg956/django-migration-dependencies',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
