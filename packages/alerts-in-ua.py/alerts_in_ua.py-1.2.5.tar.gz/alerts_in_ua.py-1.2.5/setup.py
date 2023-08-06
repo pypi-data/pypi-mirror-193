7f45e92400817a99b953b394e7943bba49b1de5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:00 +0000

    Add package alerts-in-ua.py/1.2.5/alerts_in_ua.py-1.2.5.tar.gz

---
+++ packages/alerts-in-ua.py/alerts_in_ua.py-1.2.5.tar.gz/alerts_in_ua.py-1.2.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+requires = [
+    "aiohttp~=3.8.3",
+    "requests~=2.28.1",
+    "svglib~=1.5.1",
+    "reportlab~=3.6.12"
+]
+
+
+setup(
+    name="alerts_in_ua.py",
+    version="1.2.5",
+    description="Бібліотека для використання API сайту alerts.in.ua",
+    long_description=long_description,
+    author='FOUREX, SladkayaDoza',
+    author_email="Foxtrotserega@gmail.com",
+    url="https://github.com/FOUREX/alerts_in_ua.py",
+    license="MIT",
+    install_requires=requires,
+)
