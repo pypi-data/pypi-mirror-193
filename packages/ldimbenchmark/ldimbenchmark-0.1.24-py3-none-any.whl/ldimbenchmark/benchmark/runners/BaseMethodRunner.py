cb3e20466453688dd7253d14d6089f36eaf2b989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:43 +0000

    Add package ldimbenchmark/0.1.24/ldimbenchmark-0.1.24-py3-none-any.whl

---
+++ packages/ldimbenchmark/ldimbenchmark-0.1.24-py3-none-any.whl/ldimbenchmark/benchmark/runners/BaseMethodRunner.py
@@ -0,0 +1,88 @@
+from abc import ABC, abstractmethod
+import hashlib
+import json
+import os
+from typing import Literal, Union
+
+from ldimbenchmark.datasets.classes import Dataset
+
+
+class MethodRunner(ABC):
+    """
+    Runner for a single method and dataset.
+    """
+
+    def __init__(
+        self,
+        runner_base_name: str,
+        dataset: Dataset,
+        hyperparameters: dict,
+        goal: Literal[
+            "assessment", "detection", "identification", "localization", "control"
+        ] = "detection",
+        stage: Literal["train", "detect"] = "detect",
+        method: Literal["offline", "online"] = "offline",
+        debug: bool = False,
+        resultsFolder: Union[str, None] = None,
+    ):
+        """
+        Base Class for a Method Runner.
+
+
+        Parameters
+        ----------
+        detection_method : LDIMMethodBase
+            The LDIM method object.
+
+        dataset : Union[Dataset, str]
+            The dataset object or the path to the dataset.
+
+        hyperparameters : dict, optional
+            The hyperparameters for the LDIM object, by default None
+
+        goal : Literal[
+            "assessment", "detection", "identification", "localization", "control"
+        ], optional
+            Goal of the benchmark. Possible goals:
+            "assessment" - Asses if there are any leaks
+            "detection" - Detect leaks and their onset
+            "localization" - Detect leaks and their location
+            "control" - Detect leaks and fomulate a control strategy
+            by default "detection"
+
+        stage : Literal["train", "detect"], optional
+            List of stages that should be executed. Possible stages: "train", "detect"
+
+        method : Literal["offline", "online"], optional
+            The method of the LDIM object, by default "offline"
+
+        debug : bool, optional
+            Whether to print debug information, by default False
+
+        resultsFolder : None, optional
+            The path to the results folder, by default None
+
+        """
+        if type(dataset) is str:
+            self.dataset = Dataset(dataset)
+        else:
+            self.dataset = dataset
+
+        self.hyperparameters = hyperparameters
+        if self.hyperparameters is None:
+            self.hyperparameters = {}
+        hyperparameter_hash = hashlib.md5(
+            json.dumps(hyperparameters, sort_keys=True).encode("utf-8")
+        ).hexdigest()
+
+        self.id = f"{runner_base_name}_{dataset.id}_{hyperparameter_hash}"
+
+        self.goal = goal
+        self.stage = stage
+        self.method = method
+        self.debug = debug
+        self.resultsFolder = resultsFolder
+
+    @abstractmethod
+    def run(self) -> dict:
+        pass
---
+++ packages/ldimbenchmark/ldimbenchmark-0.1.24-py3-none-any.whl/ldimbenchmark/benchmark/runners/DockerMethodRunner.py
@@ -0,0 +1,134 @@
+from datetime import time
+import hashlib
+import json
+import logging
+import os
+import tempfile
+from typing import Literal, Union
+import pandas as pd
+
+import docker
+import yaml
+from ldimbenchmark.benchmark.runners.BaseMethodRunner import MethodRunner
+from ldimbenchmark.classes import BenchmarkLeakageResult, LDIMMethodBase
+from ldimbenchmark.datasets.classes import Dataset
+
+
+# TODO: Probably merge some functionality with LocalMethodRunner as parent class
+class DockerMethodRunner(MethodRunner):
+    """
+    Runs a leakage detection method in a docker container.
+    """
+
+    # TODO: add support for bind mount parameters? or just define as standard?
+    def __init__(
+        self,
+        image: str,
+        dataset: Union[Dataset, str],
+        hyperparameters: dict = None,
+        goal: Literal[
+            "assessment", "detection", "identification", "localization", "control"
+        ] = "detection",
+        stage: Literal["train", "detect"] = "detect",
+        method: Literal["offline", "online"] = "offline",
+        debug=False,
+        resultsFolder=None,
+        docker_base_url="unix://var/run/docker.sock",
+    ):
+        super().__init__(
+            runner_base_name=image,
+            dataset=dataset,
+            hyperparameters=hyperparameters,
+            goal=goal,
+            stage=stage,
+            method=method,
+            resultsFolder=resultsFolder,
+            debug=debug,
+        )
+
+        self.image = image
+        self.docker_base_url = docker_base_url
+        # Overwrite resultsFolder
+        if resultsFolder == None:
+            self.resultsFolder = None
+        else:
+            self.resultsFolder = os.path.join(resultsFolder, self.id)
+
+    def run(self):
+        folder_parameters = tempfile.TemporaryDirectory()
+        path_options = os.path.join(folder_parameters.name, "options.yaml")
+        with open(path_options, "w") as f:
+            yaml.dump(
+                {
+                    "hyperparameters": self.hyperparameters,
+                    "goal": self.goal,
+                    "stage": self.stage,
+                    "method": self.method,
+                },
+                f,
+            )
+
+        outputFolder = self.resultsFolder
+        if outputFolder is None:
+            tempfolder = tempfile.TemporaryDirectory()
+            outputFolder = tempfolder.name
+
+        # Remove Colons from path
+        outputFolder = outputFolder.replace(":", "_")
+        # download image
+        # test compatibility (stages)
+
+        client = docker.from_env()
+        if self.docker_base_url != "unix://var/run/docker.sock":
+            client = docker.DockerClient(base_url=self.docker_base_url)
+        # run docker container
+        try:
+            client.containers.run(
+                self.image,
+                # ["echo", "hello", "world"],
+                volumes={
+                    os.path.abspath(self.dataset.path): {
+                        "bind": "/input/",
+                        "mode": "rw",
+                    },
+                    path_options: {"bind": "/input/options.yml", "mode": "rw"},
+                    os.path.abspath(outputFolder): {"bind": "/output/", "mode": "rw"},
+                },
+                mem_limit="12g",
+                cpu_count=4,
+            )
+        except docker.errors.ContainerError as e:
+            logging.error(f"Method with image {self.image} errored:")
+            for line in e.container.logs().decode().split("\n"):
+                logging.error(f"Container[{self.image}]: " + line)
+            logging.error(
+                f"If not error message is contained it might be a memory issue"
+            )
+            return None
+        # mount folder in docker container
+        # name, dst = dst.split(':')
+        #     container = client.containers.get(name)
+
+        #     os.chdir(os.path.dirname(src))
+        #     srcname = os.path.basename(src)
+        #     tar = tarfile.open(src + '.tar', mode='w')
+        #     try:
+        #         tar.add(srcname)
+        #     finally:
+        #         tar.close()
+
+        #     data = open(src + '.tar', 'rb').read()
+        #     container.put_archive(os.path.dirname(dst), data)
+        # TODO: Read results from output folder
+
+        detected_leaks = pd.read_csv(
+            os.path.join(outputFolder, "detected_leaks.csv"),
+            parse_dates=True,
+            date_parser=lambda x: pd.to_datetime(x, utc=True),
+        ).to_dict("records")
+        # if tempfolder:
+        #     tempfolder.cleanup()
+        # if folder_parameters:
+        #     tempfolder.cleanup()
+        print(outputFolder)
+        return detected_leaks
