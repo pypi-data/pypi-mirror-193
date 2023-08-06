51cf32f5e8b020ac520d345e97cedc300a055618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:06 +0000

    Add package biobricks/0.1.40/biobricks-0.1.40-py3-none-any.whl

---
+++ packages/biobricks/biobricks-0.1.40-py3-none-any.whl/biobricks/__init__.py
@@ -0,0 +1,4 @@
+from .bblib import *
+from .pull import pull, uninstall
+from .load import load
+from .bb_import import bb_import, bb_init
---
+++ packages/biobricks/biobricks-0.1.40-py3-none-any.whl/biobricks/tests/test_init.py
@@ -0,0 +1,72 @@
+import pytest
+import os
+import pathlib
+import tempfile
+
+import biobricks as bb
+from ..pull import check_url_available
+from unittest.mock import Mock
+
+@pytest.fixture
+def BBLIB():
+    oldbb = os.getenv('BBLIB')
+    tdir  = tempfile.TemporaryDirectory()
+    os.environ['BBLIB'] = tdir.name
+    yield pathlib.Path(tdir.name)
+    if oldbb: os.environ['BBLIB'] = oldbb
+    tdir.cleanup()
+
+@pytest.fixture
+def TOKEN():
+    "this is the admin+free@insilica.co test token"
+    return "VQF6Q2U-NKktZ31ioVYa9w"
+
+@pytest.fixture
+def EMPTY_BBLIB():
+    oldbb = os.getenv('BBLIB')
+    del os.environ['BBLIB']
+    yield 
+    if oldbb: os.environ['BBLIB'] = oldbb
+
+@pytest.fixture
+def local_bblib(TOKEN):
+    oldbb = os.getenv('BBLIB')
+    tdir  = tempfile.TemporaryDirectory()
+    os.environ['BBLIB'] = tdir.name
+    bb.initialize(TOKEN)
+    yield pathlib.Path(tdir.name)
+    if oldbb: os.environ['BBLIB'] = oldbb
+    tdir.cleanup()
+
+# def test_initialize(BBLIB,TOKEN):
+#     errmsg = "must use token from https://biobricks.ai/token"
+#     with pytest.raises(Exception,match=errmsg) as e:
+#         bb.initialize()
+
+#     res = bb.initialize(TOKEN)
+#     assert(res == BBLIB)
+#     assert(bb.token() == TOKEN)
+
+# def test_bblib(EMPTY_BBLIB):
+#     errmsg = "'BBLIB' env not available.*"
+#     with pytest.raises(Exception,match=errmsg):
+#         bb.bblib()
+
+# def test_load(local_bblib):
+#     brick="lkjasdfkjasdklfj"
+#     with pytest.raises(Exception,match=".*not available.*"):
+#         bb.pull(brick)
+
+#     brick = "hello-brick"
+#     org = "biobricks-ai"
+#     bb.pull(brick,org)
+#     assert bb.bblib(f'{org}/{brick}/data/mtcars.parquet').exists()
+
+#     tbls = bb.load(brick,org)
+#     assert tbls.mtcars.shape == (32,11)
+
+#     with pytest.raises(Exception,match="no path.*"):
+#         bb.load("a-brick-that-doesn't-exist")
+    
+#     with pytest.raises(Exception,match=".* not available"):
+#         check_url_available("http://the-internet.herokuapp.com/status_codes/301")
