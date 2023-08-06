9c046f49105a2b4aef64af33e066cdbd35aa2fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:32 +0000

    Add package spei/0.3.0b0/spei-0.3.0b0.tar.gz

---
+++ packages/spei/spei-0.3.0b0.tar.gz/spei-0.3.0b0/src/spei/_typing.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+from typing import Any, Union
+
+from matplotlib.axes import Axes
+from numpy import float64
+from numpy.typing import NDArray
+from scipy.stats._continuous_distns import rv_continuous
+
+ContinuousDist = Union[Any, rv_continuous]
+Axes = Union[Any, Axes]
