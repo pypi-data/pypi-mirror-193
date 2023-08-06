1cf5f851d3510ec9bde4daab10f7a7e7efb642e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:25 +0000

    Add package mir-bootleg-score/0.1.0/mir_bootleg_score-0.1.0.tar.gz

---
+++ packages/mir-bootleg-score/mir_bootleg_score-0.1.0.tar.gz/mir_bootleg_score-0.1.0/mir_bootleg_score/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.1.0"
+__author__ = 'Arjun Taneja'
+__credits__ = 'Harvey Mudd College'
---
+++ packages/mir-bootleg-score/mir_bootleg_score-0.1.0.tar.gz/mir_bootleg_score-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='mir_bootleg_score',
+    version='0.1.0',    
+    description='Built for MIR Lab. Tools for converting png images into bootleg score features.',
+    url='https://github.com/shuds13/pyexample',
+    author='Arjun Taneja',
+    author_email='ataneja@g.hmc.edu',
+    license='BSD 2-clause',
+    packages=['mir_bootleg_score'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
