b7ab518e6a4d7ac31eba3826685e19eb67cbe517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:00 +0000

    Add package alerts-in-ua.py/1.2.5/alerts_in_ua.py-1.2.5-py3-none-any.whl

---
+++ packages/alerts-in-ua.py/alerts_in_ua.py-1.2.5-py3-none-any.whl/alerts_in_ua/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "1.2.5"
+
+from alerts_in_ua.alerts_client import AlertsClient as AlertsClient
+from alerts_in_ua.async_alerts_client import AsyncAlertsClient as AsyncAlertsClient
+from alerts_in_ua.location import Location
+from alerts_in_ua.locations import Locations
+from alerts_in_ua.map_style import MapStyle
+
+from alerts_in_ua.exceptions import (
+    InvalidToken as InvalidToken,
+    TooManyRequests as TooManyRequests,
+    UnknownError as UnknownError
+)
+
+__all__: tuple[str] = (
+    "__version__",
+    "AlertsClient",
+    "AsyncAlertsClient",
+    "Location",
+    "Locations",
+    "MapStyle",
+    "InvalidToken",
+    "TooManyRequests",
+    "UnknownError"
+)
