794a984a8dce780436fe37ac05b530233ea6dd7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:54 +0000

    Add package num-system/0.0.1/num_system-0.0.1.tar.gz

---
+++ packages/num-system/num_system-0.0.1.tar.gz/num_system-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='num_system',
+    version='0.0.1',
+    description='a',
+    license='MIT',
+    packages=['num_system'],
+    author='Dimitry K',
+    author_email='dime_kozlov@mail.ru',
+    keywords=['sis'],
+    url='https://github.com/TFL-group/num_sys'
+)
