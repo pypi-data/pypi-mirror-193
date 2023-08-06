8e920feec8654179e5dfe3025bead115bef64ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:15 +0000

    Add package ccnmtlsettings/1.9.5/ccnmtlsettings-1.9.5.tar.gz

---
+++ packages/ccnmtlsettings/ccnmtlsettings-1.9.5.tar.gz/ccnmtlsettings-1.9.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ccnmtlsettings",
+    version="1.9.5",
+    author="Anders Pearson",
+    author_email="ctl-dev@columbia.edu",
+    url="https://github.com/ccnmtl/ccnmtlsettings",
+    description="Columbia CTL common Django base settings",
+    long_description="common settings we use across all our projects",
+    install_requires = [
+        "django_compressor",
+        "django-debug-toolbar",
+        "coverage",
+        "django-smoketest",
+        "django-extensions",
+        "django-statsd-mozilla",
+        "sentry-sdk",
+        "django-markwhat",
+        "django-storages",
+        "boto3",
+        "statsd",
+        "gunicorn",
+        "djangowind",
+        "django-impersonate",
+    ],
+    scripts = [],
+    license = "BSD",
+    platforms = ["any"],
+    zip_safe=False,
+    package_data = {'' : ['*.*']},
+    packages=['ccnmtlsettings'],
+)
