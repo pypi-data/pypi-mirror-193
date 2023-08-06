292911e0ac6b59540d2a203fde88aba5b229a913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:48 +0000

    Add package reisinger-drive/0.2.3/reisinger_drive-0.2.3.tar.gz

---
+++ packages/reisinger-drive/reisinger_drive-0.2.3.tar.gz/reisinger_drive-0.2.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="reisinger_drive",
+    packages=["reisingerdrive"],
+    install_requires=["aiohttp>=3.0.6", "async_timeout>=3.0.0"],
+    version="0.2.3",
+    description="A python3 library to communicate with a Reisinger Drive System",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires=">=3.5.3",
+    author="sreisi",
+    author_email="sell@reisinger.de",
+    url="https://github.com/sreisi/reisinger-drive-api",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Home Automation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
