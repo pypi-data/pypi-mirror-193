57d36f08ea026dfda2a14e99383c3d218fa01d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:10 +0000

    Add package frccontrol/2023.16/frccontrol-2023.16-py3-none-any.whl

---
+++ packages/frccontrol/frccontrol-2023.16-py3-none-any.whl/frccontrol/discretization.py
@@ -0,0 +1,24 @@
+"""Functions for discretizing state-space systems."""
+
+import numpy as np
+from scipy.linalg import expm
+
+
+def discretize_ab(A, B, T):
+    """Discretizes the given continuous A and B matrices.
+
+    Keyword arguments:
+    A -- continuous system matrix
+    B -- continuous input matrix
+    T -- discretization timestep
+
+    Returns:
+    discrete system matrix, discrete input matrix
+    """
+    states = A.shape[0]
+    inputs = B.shape[1]
+
+    M = expm(
+        np.block([[A, B], [np.zeros((inputs, states)), np.zeros((inputs, inputs))]]) * T
+    )
+    return M[:states, :states], M[:states, states:]
