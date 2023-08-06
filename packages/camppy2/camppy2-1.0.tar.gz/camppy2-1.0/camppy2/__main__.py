259c51ce9de66f35d34ae4e4718837c37cc26aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:23 +0000

    Add package camppy2/1.0/camppy2-1.0.tar.gz

---
+++ packages/camppy2/camppy2-1.0.tar.gz/camppy2-1.0/camppy2/__main__.py
@@ -0,0 +1,5 @@
+import requests
+import user-agent
+
+def camp1():
+ print('Working1')
---
+++ packages/camppy2/camppy2-1.0.tar.gz/camppy2-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import camppy2
+setup(
+    name="camppy2",
+    version=camppy2.__version__,
+    author=camppy2.__author__,
+    author_email="contactcampsmm@gmail.com",
+    description="CXMP MODULE",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "requests",
+        "user-agent",
+    ],
+)
