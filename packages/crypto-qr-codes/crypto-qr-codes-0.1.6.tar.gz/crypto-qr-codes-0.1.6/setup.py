b148d6a5f55da29fde2840bea084bd28cbcee33c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:58 +0000

    Add package crypto-qr-codes/0.1.6/crypto-qr-codes-0.1.6.tar.gz

---
+++ packages/crypto-qr-codes/crypto-qr-codes-0.1.6.tar.gz/crypto-qr-codes-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="crypto-qr-codes",
+    version="0.1.6",
+    author="Robert Capla",
+    author_email="robert.capla@icloud.com",
+    description="A package for generating QR codes for various cryptocurrencies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RandoS-svk/CryptoQRGenerator",
+    packages=["crypto_qr_codes"],
+    install_requires=["qrcode"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
