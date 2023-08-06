2e4ea697e2ba3aa5fd73847b5a00c1d00f74256d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:20 +0000

    Add package acc_provision/5.2.3.6rc2/acc_provision-5.2.3.6rc2.tar.gz

---
+++ packages/acc_provision/acc_provision-5.2.3.6rc2.tar.gz/acc_provision-5.2.3.6rc2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os, sys
+file_dir = os.path.dirname(__file__)
+sys.path.append(file_dir)
+from gitversion.gitversion import get_git_version
+os.chdir(os.path.abspath(file_dir))
+
+setup(
+    name='acc_provision',
+    version='5.2.3.6.rc2',
+    description='Tool to provision ACI for ACI Containers Controller  Build info: ' + get_git_version(),
+    author="Cisco Systems, Inc.",
+    author_email="apicapi@noironetworks.com",
+    url='http://github.com/noironetworks/acc-provision/',
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    data_files = [('bin', ['bin/acikubectl'])],
+    entry_points={
+        'console_scripts': [
+            'acc-provision=acc_provision.acc_provision:main',
+            'acc-retrieve-cert=acc_provision.acc_retrieve_cert:main',
+        ]
+    },
+    install_requires=[
+          'requests',
+          'pyyaml',
+          'jinja2',
+          'pyopenssl',
+          'MarkupSafe',
+          'boto3',
+    ],
+)
