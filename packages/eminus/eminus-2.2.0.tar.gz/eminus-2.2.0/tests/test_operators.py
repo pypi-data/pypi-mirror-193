8f577690addf2a4323a2f0187d5cfe7328d24bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:46 +0000

    Add package eminus/2.2.0/eminus-2.2.0.tar.gz

---
+++ packages/eminus/eminus-2.2.0.tar.gz/eminus-2.2.0/tests/test_operators.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+'''Test operator identities.'''
+import numpy as np
+from numpy.random import default_rng
+from numpy.testing import assert_allclose
+
+from eminus import Atoms
+
+# Create an Atoms object to build mock wave functions
+atoms = Atoms('Ne', [0, 0, 0], ecut=1).build()
+rng = default_rng()
+W_tests = {
+    'full': rng.standard_normal((len(atoms.G2), atoms.Nstate)),
+    'active': rng.standard_normal((len(atoms.G2c), atoms.Nstate)),
+    'full_single': rng.standard_normal((len(atoms.G2))),
+    'active_single': rng.standard_normal((len(atoms.G2c))),
+    'full_spin': rng.standard_normal((atoms.Nspin, len(atoms.G2), atoms.Nstate)),
+    'active_spin': rng.standard_normal((atoms.Nspin, len(atoms.G2c), atoms.Nstate))
+}
+
+
+def test_LinvL():
+    for i in ['full', 'full_spin']:
+        out = atoms.Linv(atoms.L(W_tests[i]))
+        test = np.copy(W_tests[i])
+        if test.ndim == 3:
+            test[:, 0, :] = 0
+        else:
+            test[0, :] = 0
+        assert_allclose(out, test)
+
+
+def test_LLinv():
+    for i in ['full', 'full_spin']:
+        out = atoms.L(atoms.Linv(W_tests[i]))
+        test = np.copy(W_tests[i])
+        if test.ndim == 3:
+            test[:, 0, :] = 0
+        else:
+            test[0, :] = 0
+        assert_allclose(out, test)
+
+
+def test_IJ():
+    for i in ['full', 'full_single', 'full_spin']:
+        out = atoms.I(atoms.J(W_tests[i]))
+        test = W_tests[i]
+        assert_allclose(out, test)
+
+
+def test_JI():
+    for i in ['full', 'active', 'full_single', 'active_single', 'full_spin', 'active_spin']:
+        if 'active' in i:
+            out = atoms.J(atoms.I(W_tests[i]), False)
+        else:
+            out = atoms.J(atoms.I(W_tests[i]))
+        test = W_tests[i]
+        assert_allclose(out, test)
+
+
+def test_IdagJdag():
+    for i in ['active', 'active_single', 'active_spin']:
+        out = atoms.Idag(atoms.Jdag(W_tests[i]))
+        test = W_tests[i]
+        assert_allclose(out, test)
+
+
+def test_JdagIdag():
+    for i in ['full', 'full_single', 'full_spin']:
+        out = atoms.Jdag(atoms.Idag(W_tests[i], True))
+        test = W_tests[i]
+        assert_allclose(out, test)
+
+
+def test_TT():
+    dr = rng.standard_normal(3)
+    for i in ['active', 'active_single', 'active_spin']:
+        out = atoms.T(atoms.T(W_tests[i], dr), -dr)
+        test = W_tests[i]
+        assert_allclose(out, test)
+
+
+if __name__ == '__main__':
+    import inspect
+    import pathlib
+    import pytest
+    file_path = pathlib.Path(inspect.getfile(inspect.currentframe()))
+    pytest.main(file_path)
---
+++ packages/eminus/eminus-2.2.0.tar.gz/eminus-2.2.0/tests/test_xc.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+'''Test exchange-correlation functionals.'''
+import numpy as np
+from numpy.random import default_rng
+from numpy.testing import assert_allclose
+import pytest
+
+from eminus.xc import get_exc, get_vxc, get_xc, parse_functionals, XC_MAP
+
+# Create random mock densities
+# Use absolute values since eminus' functionals have no safety checks for simplicity and performance
+rng = default_rng()
+n_tests = {
+    1: np.abs(rng.standard_normal((1, 10000))),
+    2: np.abs(rng.standard_normal((2, 10000)))
+}
+functionals = [xc for xc in XC_MAP if xc.isdigit()]
+
+
+@pytest.mark.extras
+@pytest.mark.parametrize('xc', functionals)
+@pytest.mark.parametrize('Nspin', [1, 2])
+def test_get_exc(xc, Nspin):
+    '''Compare internal functional energy densities to Libxc.'''
+    from eminus.extras import libxc_functional
+    n_spin = n_tests[Nspin]
+    e_out = get_exc(xc, n_spin, Nspin)
+    e_test, _ = libxc_functional(xc, n_spin, Nspin)
+    assert_allclose(e_out, e_test)
+
+
+@pytest.mark.extras
+@pytest.mark.parametrize('xc', functionals)
+@pytest.mark.parametrize('Nspin', [1, 2])
+def test_get_vxc(xc, Nspin):
+    '''Compare internal functional potentials to Libxc.'''
+    from eminus.extras import libxc_functional
+    n_spin = n_tests[Nspin]
+    v_out = get_vxc(xc, n_spin, Nspin)
+    _, v_test = libxc_functional(xc, n_spin, Nspin)
+    assert_allclose(v_out, v_test)
+
+
+@pytest.mark.parametrize('xc', functionals)
+@pytest.mark.parametrize('Nspin', [1, 2])
+def test_exc_only(xc, Nspin):
+    '''Test the function to only get the exc part.'''
+    n_spin = n_tests[Nspin]
+    e_out, _ = get_xc(xc, n_spin, Nspin)
+    e_test, v_test = get_xc(xc, n_spin, Nspin, exc_only=True)
+    assert_allclose(e_out, e_test)
+    assert_allclose(v_test, 0)
+
+
+@pytest.mark.parametrize('xc,ref', [('svwn', ['lda_x', 'lda_c_vwn']),
+                                    ('lda_x', ['lda_x', 'mock_xc']),
+                                    ('s,pw', ['lda_x', 'lda_c_pw']),
+                                    ('s', ['lda_x', 'mock_xc']),
+                                    ('s,', ['lda_x', 'mock_xc']),
+                                    ('pw', ['lda_c_pw', 'mock_xc']),
+                                    (',pw', ['mock_xc', 'lda_c_pw']),
+                                    ('', ['mock_xc', 'mock_xc']),
+                                    (',', ['mock_xc', 'mock_xc']),
+                                    ('libxc:1,libxc:7', ['libxc:1', 'libxc:7']),
+                                    ('libxc:1,', ['libxc:1', 'mock_xc']),
+                                    (',libxc:7', ['mock_xc', 'libxc:7']),
+                                    ('s,libxc:7', ['lda_x', 'libxc:7'])])
+def test_parse_functionals(xc, ref):
+    '''Test the xc string parsing.'''
+    f_x, f_c = parse_functionals(xc)
+    assert f_x == ref[0]
+    assert f_c == ref[1]
+
+
+if __name__ == '__main__':
+    import inspect
+    import pathlib
+    import pytest
+    file_path = pathlib.Path(inspect.getfile(inspect.currentframe()))
+    pytest.main(file_path)
