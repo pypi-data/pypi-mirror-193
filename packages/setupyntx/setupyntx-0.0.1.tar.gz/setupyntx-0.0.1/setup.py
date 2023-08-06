83191310652272abda101234b2982c066ba5d73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:05 +0000

    Add package setupyntx/0.0.1/setupyntx-0.0.1.tar.gz

---
+++ packages/setupyntx/setupyntx-0.0.1.tar.gz/setupyntx-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+
+setup(
+    name="setupyntx",
+    version='0.0.1',
+    license='Eclipse Public License 2.0',
+    authors=["setupyntx"],
+    author_email="<setupyntx@gmail.com>",
+    description="by setupyntx",
+    long_description='Documentation: https://github.com/setupyntx/setupyntx',
+    keywords=['setupyntx', 'pystyle'],
+    packages=['setupyntx']
+)
+
+
