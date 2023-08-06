5274858a09e63984d0117b459d31716902d6a0a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:08 +0000

    Add package conjur-api/0.1.0/conjur_api-0.1.0.tar.gz

---
+++ packages/conjur-api/conjur_api-0.1.0.tar.gz/conjur_api-0.1.0/conjur_api/models/general/conjur_connection_info.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+ConjurConnectionInfo module
+
+This module represents an object that holds the connection data
+"""
+
+
+# pylint: disable=too-few-public-methods
+
+
+class ConjurConnectionInfo:
+    """
+    Used for setting user input data
+    """
+
+    def __init__(self, conjur_url: str = None, account: str = None, cert_file: str = None, service_id: str = None):
+        self.conjur_url = conjur_url
+        self.conjur_account = account
+        self.cert_file = cert_file
+        self.service_id = service_id
+
+    def __repr__(self) -> str:
+        return f"{self.__dict__}"
