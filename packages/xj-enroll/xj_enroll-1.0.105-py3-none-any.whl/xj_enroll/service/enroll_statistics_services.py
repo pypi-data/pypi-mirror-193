c364ec38d155d55a64a79154a899383dd01a5e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:40 +0000

    Add package xj-enroll/1.0.105/xj_enroll-1.0.105-py3-none-any.whl

---
+++ packages/xj-enroll/xj_enroll-1.0.105-py3-none-any.whl/xj_enroll/service/enroll_statistics_services.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+"""
+@project: djangoModel->enroll_statistics
+@author: 孙楷炎
+@Email: sky4834@163.com
+@synopsis: 报名统计服务
+@created_time: 2023/2/22 14:31
+"""
+
+
+class EnrollStatistics(object):
+    """报名主项统计服务(发布人统计)"""
+    pass
+
+
+class EnrollRecordStatistics(object):
+    """报名记录统计（报名人统计）"""
+    pass
+
+
+class EnrollSubitemRecordStatistics(object):
+    """报名分你想记录统计（任务统计）"""
+    pass
