1b6d1c557ab754d5a694e3bc4f60706edbbe3176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:50 +0000

    Add package politicsnlp/0.0.6/politicsnlp-0.0.6-py3-none-any.whl

---
+++ packages/politicsnlp/politicsnlp-0.0.6-py3-none-any.whl/politicsnlp/__init__.py
@@ -0,0 +1,45 @@
+from politicsnlp.government import *
+# ----------------------------------- Individual --------------------------------------------------- #
+from politicsnlp.individual.individual import *
+from politicsnlp.individual.strongman import *
+from politicsnlp.individual.monarch import *
+from politicsnlp.individual.tyrant import *
+# ----------------------------------- People --------------------------------------------------- #
+from politicsnlp.people.person import *
+from politicsnlp.people.masses import *
+
+
+from politicsnlp.limited_power import *
+# ----------------------------------- Core --------------------------------------------------- #
+from politicsnlp.core.justice import *
+from politicsnlp.core.freedom import *
+from politicsnlp.core.democracy import *
+from politicsnlp.core.spirit_of_the_law import *
+
+from politicsnlp.philosophy.idealstate import *
+from politicsnlp.people_democracy import *
+from politicsnlp.hegemony import *
+from politicsnlp.protection import *
+from politicsnlp.social_contract import *
+# ----------------------------------- Ideology --------------------------------------------------- #
+from politicsnlp.ideology.ideology import *
+from politicsnlp.ideology.nationalism import *
+from politicsnlp.ideology.confucianism import *
+# ----------------------------------- Form Of Government --------------------------------------------------- #
+from politicsnlp.form_of_government.aristocracy import *
+from politicsnlp.form_of_government.autocracy import *
+from politicsnlp.form_of_government.form_of_government import *
+from politicsnlp.form_of_government.socialism import *
+from politicsnlp.form_of_government.colonialism import *
+from politicsnlp.form_of_government.communism import *
+from politicsnlp.form_of_government.direct_democracy import *
+from politicsnlp.form_of_government.representative_democracy import *
+from politicsnlp.form_of_government.direct_democracy import *
+from politicsnlp.form_of_government.oligarchy import *
+from politicsnlp.form_of_government.monarchy import *
+from politicsnlp.form_of_government.theocracy import *
+# ----------------------------------- Marxism --------------------------------------------------- #
+from politicsnlp.marxism.marxism import *
+from politicsnlp.marxism.mao_ze_dong_thought import *
+from politicsnlp.marxism.deng_xiao_ping_theory import *
+from politicsnlp.marxism.xi_jin_ping_thought import *
