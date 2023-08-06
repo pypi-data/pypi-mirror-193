dcff0a0320e6a4a615216464c43848f5e7e0f53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:12 +0000

    Add package PyFibreBundle/1.1.0/PyFibreBundle-1.1.0.tar.gz

---
+++ packages/PyFibreBundle/PyFibreBundle-1.1.0.tar.gz/PyFibreBundle-1.1.0/test/test_find_cores.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Tests the find_cores function of pybundle.
+
+@author: Mike Hughes
+Applied Optics Group
+University of Kent
+"""
+
+from matplotlib import pyplot as plt
+import numpy as np
+import sys
+import cv2 as cv
+
+import context    # For paths to library
+
+from PIL import Image
+
+import pybundle
+
+
+img = np.array(Image.open("data/bundle1.tif"))
+imgMasked = pybundle.auto_mask(img)         # Remove anything outside bundle
+cx,cy = pybundle.find_cores(imgMasked, 6)
+plt.figure(dpi=600)
+plt.imshow(imgMasked, cmap='gray')
+plt.plot(cx,cy,'.', markersize = .1)
+print("Found " + str(len(cx)) + " cores.")
+
+
+img = np.array(Image.open("data/bundle2.tif"))
+img = img[:,:,0]
+imgMasked = pybundle.auto_mask(img)
+cx,cy = pybundle.find_cores(imgMasked, 8)
+plt.figure(dpi=600)
+plt.imshow(imgMasked, cmap='gray')
+plt.plot(cx,cy,'.', markersize = .1)
+print("Found " + str(len(cx)) + " cores.")
+
+
+img = np.array(Image.open("data/bundle3.tif"))
+img = img[:,:,0]
+imgMasked = pybundle.auto_mask(img)
+cx,cy = pybundle.find_cores(imgMasked, 6)
+plt.figure(dpi=600)
+plt.imshow(imgMasked, cmap='gray')
+plt.plot(cx,cy,'.', markersize = .1)
+print("Found " + str(len(cx)) + " cores.")
+
+
