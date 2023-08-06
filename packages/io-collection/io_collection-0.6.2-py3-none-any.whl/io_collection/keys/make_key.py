60be3f2c7576aae361c486771f94cd35ee2ac7f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:45 +0000

    Add package io-collection/0.6.2/io_collection-0.6.2-py3-none-any.whl

---
+++ packages/io-collection/io_collection-0.6.2-py3-none-any.whl/io_collection/keys/make_key.py
@@ -0,0 +1,17 @@
+import os
+from datetime import datetime
+
+from prefect import task
+
+
+@task
+def make_key(*subkeys: str) -> str:
+    key = os.path.join(*subkeys)
+
+    timestamp = datetime.now().strftime("%Y-%m-%d")
+    key = key.replace("{{timestamp}}", timestamp)
+
+    key = key.replace("__", "_")
+    key = key.replace("_.", ".")
+
+    return key
