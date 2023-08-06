d79b1afbc13c1567aa99e078e3fe1df9831c6b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:06 +0000

    Add package streamlit-antd/0.2.4/streamlit-antd-0.2.4.tar.gz

---
+++ packages/streamlit-antd/streamlit-antd-0.2.4.tar.gz/streamlit-antd-0.2.4/streamlit_antd/steps/frontend/build/precache-manifest.3a82ef6e24cfef143c7bc354c871c28b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583906160fb1bd778cd9cdd327d869ea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fe9e40c8eeb7ec68aece",
+    "url": "./static/css/2.15aa6b1e.chunk.css"
+  },
+  {
+    "revision": "fe9e40c8eeb7ec68aece",
+    "url": "./static/js/2.4bc8b482.chunk.js"
+  },
+  {
+    "revision": "617a910d62891f85c9de31fc517c5164",
+    "url": "./static/js/2.4bc8b482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00938d5a76e7000b2f91",
+    "url": "./static/js/main.650c4fa4.chunk.js"
+  },
+  {
+    "revision": "5da51559676de863d294",
+    "url": "./static/js/runtime-main.7cc49c53.js"
+  }
+]);
