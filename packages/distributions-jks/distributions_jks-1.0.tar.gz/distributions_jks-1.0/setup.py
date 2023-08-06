01fffb30b8c0576bfe37aab4be70ce20f7415df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:02 +0000

    Add package distributions-jks/1.0/distributions_jks-1.0.tar.gz

---
+++ packages/distributions-jks/distributions_jks-1.0.tar.gz/distributions_jks-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='distributions_jks',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributions_jks'],
+      author='Jaya Krishna with Udacity',
+      author_email='jayakrishnas.work@gmail.com',
+      requires=['matplotlib', 'math'],
+      zip_safe=False)
