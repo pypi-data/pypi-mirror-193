5eec4dcaacefe56d7eacb4a063b11dffa5858c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:14 +0000

    Add package musktest/1.0.0/musktest-1.0.0-py3-none-any.whl

---
+++ packages/musktest/musktest-1.0.0-py3-none-any.whl/tests/funcTools.py
@@ -0,0 +1,13 @@
+# Author:码思客-木森
+# WeChart:musen9111
+from musktest.core.tools import *
+from hooks.db_check_hook import *
+from hooks.fixture_hook import *
+
+
+def rand_phone():
+    return random_mobile()
+
+
+def get_sign(a,b):
+    return get_timestamp()
