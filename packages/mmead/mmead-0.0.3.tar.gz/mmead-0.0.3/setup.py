837e76e73f2265fbe715298736060237c87a5517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:49 +0000

    Add package mmead/0.0.3/mmead-0.0.3.tar.gz

---
+++ packages/mmead/mmead-0.0.3.tar.gz/mmead-0.0.3/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="mmead",
+    version="0.0.3",
+    author="Chris Kamphuis",
+    author_email="chris@cs.ru.nl",
+    description="MS MARCO entity annotations and disambiguations",
+    url="https://github.com/informagi/mmead",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6"
+)
