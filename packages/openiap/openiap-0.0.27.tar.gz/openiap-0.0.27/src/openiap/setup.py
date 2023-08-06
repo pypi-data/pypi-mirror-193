680434b8c9c0225e0647ced58bc0aa64cce4842f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:22 +0000

    Add package openiap/0.0.27/openiap-0.0.27.tar.gz

---
+++ packages/openiap/openiap-0.0.27.tar.gz/openiap-0.0.27/src/openiap/setup.py
@@ -0,0 +1,15 @@
+# from setuptools import setup
+
+# setup(
+#     name='openiap',
+#     version='0.0.26',
+#     description='Simple openiap api wrapper using proto',
+#     author='OpenIAP ApS / Allan Zimmerman',
+#     author_email='info@openiap.io',
+#     packages=['openiap'],
+#     install_requires=[
+#         'protobuf',
+#         'grpcio',
+#         'websockets'
+#     ],
+# )
