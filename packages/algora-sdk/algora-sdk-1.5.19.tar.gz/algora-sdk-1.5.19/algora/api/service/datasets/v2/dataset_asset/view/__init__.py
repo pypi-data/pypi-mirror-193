2cc019b7ac987fbf587cc8ca6a831ff20c6faaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:09 +0000

    Add package algora-sdk/1.5.19/algora-sdk-1.5.19.tar.gz

---
+++ packages/algora-sdk/algora-sdk-1.5.19.tar.gz/algora-sdk-1.5.19/algora/api/service/datasets/v2/dataset_asset/view/__init__.py
@@ -0,0 +1,10 @@
+from algora.api.service.datasets.v2.dataset_asset.view.synchronous import (
+    get_dataset_asset_view,
+    get_dataset_asset_views,
+    search_dataset_asset_views,
+)
+from algora.api.service.datasets.v2.dataset_asset.view.asynchronous import (
+    async_get_dataset_asset_view,
+    async_get_dataset_asset_views,
+    async_search_dataset_asset_views,
+)
