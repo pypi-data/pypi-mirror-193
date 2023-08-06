0d1be126ecef9a4cee2ba2c9e380bae10d091332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:56 +0000

    Add package kitsec/0.1.0/kitsec-0.1.0.tar.gz

---
+++ packages/kitsec/kitsec-0.1.0.tar.gz/kitsec-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kitsec',
+    version='0.1.0',
+    description='A package to streamline Ethical hackers workflows',
+    author='Idriss CHEBAK',
+    author_email='idrisschebak@me.com',
+    packages=find_packages(),
+    install_requires=[
+        'Pillow',
+    ],
+)
