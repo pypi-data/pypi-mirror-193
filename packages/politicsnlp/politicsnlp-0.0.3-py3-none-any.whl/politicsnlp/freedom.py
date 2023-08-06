2d04ba3c180fd765615e62affe4b9246ec4adf72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:14 +0000

    Add package politicsnlp/0.0.3/politicsnlp-0.0.3-py3-none-any.whl

---
+++ packages/politicsnlp/politicsnlp-0.0.3-py3-none-any.whl/politicsnlp/freedom.py
@@ -0,0 +1,7 @@
+class Freedom:
+    def __init__(self, speech, religion, want, fear):
+        self.speech = speech
+        self.religion = religion
+        self.want = want
+        self.fear = fear
+
