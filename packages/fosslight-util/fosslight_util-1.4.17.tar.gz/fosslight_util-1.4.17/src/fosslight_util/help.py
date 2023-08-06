74719bde15afc6c7e4e8ad168fea6cb4ad696465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:33 +0000

    Add package fosslight-util/1.4.17/fosslight_util-1.4.17.tar.gz

---
+++ packages/fosslight-util/fosslight_util-1.4.17.tar.gz/fosslight_util-1.4.17/src/fosslight_util/help.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021 LG Electronics Inc.
+# SPDX-License-Identifier: Apache-2.0
+import sys
+import pkg_resources
+
+_HELP_MESSAGE_COMMON = """
+         _______  _______  _______  _______  ___      ___           __
+        |       ||       ||       ||       ||   |    |___|         |  |         _
+        |    ___||   _   ||  _____||  _____||   |     ___          |  |____  __| |__
+        |   |___ |  | |  || |_____ | |_____ |   |    |   | _______ |   _   ||__   __|
+        |    ___||  |_|  ||_____  ||_____  ||   |___ |   ||   _   ||  | |  |   | |
+        |   |    |       | _____| | _____| ||       ||   ||  |_|  ||  | |  |   | |__
+        |___|    |_______||_______||_______||_______||___||____   ||__| |__|   |____|
+                                                               |  |
+                                                           ____|  |
+                                                          |_______|
+"""
+
+
+class PrintHelpMsg():
+    message_suffix = ""
+
+    def __init__(self, value):
+        self.message_suffix = value
+
+    def print_help_msg(self, exitopt):
+        print(_HELP_MESSAGE_COMMON)
+        print(self.message_suffix)
+
+        if exitopt:
+            sys.exit()
+
+
+def print_package_version(pkg_name, msg="", exitopt=True):
+    if msg == "":
+        msg = f"{pkg_name} Version:"
+    cur_version = pkg_resources.get_distribution(pkg_name).version
+    print(f'{msg} {cur_version}')
+
+    if exitopt:
+        sys.exit(0)
