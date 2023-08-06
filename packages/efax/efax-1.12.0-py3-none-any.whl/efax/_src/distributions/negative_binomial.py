33f3a9e9cd7f4af91a2466f5c07a6ac77acc07a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:31 +0000

    Add package efax/1.12.0/efax-1.12.0-py3-none-any.whl

---
+++ packages/efax/efax-1.12.0-py3-none-any.whl/efax/_src/distributions/negative_binomial.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import jax.numpy as jnp
+from tjax import IntegralArray, RealArray
+from tjax.dataclasses import dataclass
+
+from ..parameter import ScalarSupport, distribution_parameter
+from .negative_binomial_common import NBCommonEP, NBCommonNP
+
+__all__ = ['NegativeBinomialNP', 'NegativeBinomialEP']
+
+
+@dataclass
+class NegativeBinomialNP(NBCommonNP['NegativeBinomialEP']):
+    failures: IntegralArray = distribution_parameter(ScalarSupport(), fixed=True)
+    log_not_p: RealArray = distribution_parameter(ScalarSupport())
+
+    # Implemented methods --------------------------------------------------------------------------
+    def to_exp(self) -> NegativeBinomialEP:
+        return NegativeBinomialEP(self.failures, self._mean())
+
+    def sufficient_statistics(self, x: RealArray) -> NegativeBinomialEP:
+        return NegativeBinomialEP(jnp.broadcast_to(self.failures, x.shape), x)
+
+
+@dataclass
+class NegativeBinomialEP(NBCommonEP[NegativeBinomialNP]):
+    failures: IntegralArray = distribution_parameter(ScalarSupport(), fixed=True)
+    mean: RealArray = distribution_parameter(ScalarSupport())
+
+    # Implemented methods --------------------------------------------------------------------------
+    @classmethod
+    def natural_parametrization_cls(cls) -> type[NegativeBinomialNP]:
+        return NegativeBinomialNP
+
+    def to_nat(self) -> NegativeBinomialNP:
+        return NegativeBinomialNP(self.failures, self._log_not_p())
---
+++ packages/efax/efax-1.12.0-py3-none-any.whl/efax/_src/distributions/negative_binomial_common.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from typing import Any, Generic, TypeVar
+
+import jax.numpy as jnp
+from jax.scipy.special import gammaln
+from tjax import IntegralNumeric, RealArray, Shape
+
+from ..expectation_parametrization import ExpectationParametrization
+from ..natural_parametrization import EP, NaturalParametrization
+
+__all__: list[str] = []
+
+
+class NBCommonNP(NaturalParametrization[EP, RealArray], Generic[EP]):
+    failures: IntegralNumeric
+    log_not_p: RealArray
+
+    # Implemented methods --------------------------------------------------------------------------
+    @property
+    def shape(self) -> Shape:
+        return self.log_not_p.shape
+
+    def log_normalizer(self) -> RealArray:
+        return -self.failures * jnp.log1p(-jnp.exp(self.log_not_p))  # type: ignore[return-value]
+
+    def carrier_measure(self, x: RealArray) -> RealArray:
+        a = x + self.failures - 1
+        # Return log(a choose x).
+        return gammaln(a + 1) - gammaln(x + 1) - gammaln(a - x + 1)
+
+    # Private methods ------------------------------------------------------------------------------
+    def _mean(self) -> RealArray:
+        return self.failures / jnp.expm1(-self.log_not_p)
+
+
+NP = TypeVar('NP', bound=NBCommonNP[Any])
+
+
+class NBCommonEP(ExpectationParametrization[NP], Generic[NP]):
+    failures: IntegralNumeric
+    mean: RealArray
+
+    # Implemented methods --------------------------------------------------------------------------
+    @property
+    def shape(self) -> Shape:
+        return self.mean.shape
+
+    # def conjugate_prior_distribution(self, n: RealArray) -> BetaPrimeNP:
+    #     return BetaPrimeNP(n * self.failures * (self.mean, jnp.ones_like(self.mean)))
+
+    # Private methods ------------------------------------------------------------------------------
+    def _log_not_p(self) -> RealArray:
+        return -jnp.log1p(self.failures / self.mean)
