f5cf6c49bfda97396f3f1955b9c3de839bf414ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:49 +0000

    Add package SERVIR-Template-CLI/0.0.4/SERVIR_Template_CLI-0.0.4.tar.gz

---
+++ packages/SERVIR-Template-CLI/SERVIR_Template_CLI-0.0.4.tar.gz/SERVIR_Template_CLI-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+setup(
+    name='SERVIR_Template_CLI',
+    version='0.0.4',
+    author='Billy Ashmall',
+    author_email='billy.ashmall@nasa.gov',
+    license='MIT License',
+    description='Installer for the SERVIR App Template',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/SERVIR/SERVIR_Template_CLI',
+    py_modules=['servir_template', 'app', 'support', 'default_files'],
+    packages=find_packages(),
+    install_requires=[requirements],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    entry_points='''
+        [console_scripts]
+        servir_template=servir_template:cli
+    '''
+)
