7fd3b986e3c78c765e403beb3529b59a71c6fd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:11 +0000

    Add package slogs/0.1.4/slogs-0.1.4-py3-none-any.whl

---
+++ packages/slogs/slogs-0.1.4-py3-none-any.whl/slogs/__init__.py
@@ -0,0 +1,7 @@
+__authors__ = "deepcaps"
+__contact__ = "deepcaps@outlook.com"
+__version__ = "1.0"
+__copyright__ = "deepcaps"
+
+from .slogs import slogs
+from .templates import parents
---
+++ packages/slogs/slogs-0.1.4-py3-none-any.whl/slogs/templates.py
@@ -0,0 +1,8 @@
+from colorama import Fore
+
+
+class parents():
+    SYSTEM = {"color" : Fore.YELLOW, "label" : "system"}
+    DEBUG = {"color" : Fore.MAGENTA, "label" : "debug"}
+
+parents = parents()
