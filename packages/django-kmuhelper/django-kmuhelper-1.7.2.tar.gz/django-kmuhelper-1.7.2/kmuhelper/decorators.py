65d56c0e3994660bb10cbb428bbd328e81dd2ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:51 +0000

    Add package django-kmuhelper/1.7.2/django-kmuhelper-1.7.2.tar.gz

---
+++ packages/django-kmuhelper/django-kmuhelper-1.7.2.tar.gz/django-kmuhelper-1.7.2/kmuhelper/decorators.py
@@ -0,0 +1,52 @@
+from functools import wraps
+
+from django.contrib import messages
+from django.http import Http404
+from django.urls import reverse
+from django.shortcuts import redirect, render
+from django.utils.translation import gettext
+
+from kmuhelper.utils import render_error
+
+_ = gettext
+
+def confirm_action(action_message):
+    """Decorator to show a confirm page where the user has to 
+    confirm an action before executing it."""
+
+    def decorator(function):
+        @wraps(function)
+        def wrap(request, *args, **kwargs):
+            if request.method == "POST":
+                return function(request, *args, **kwargs)
+            return render(request, "admin/kmuhelper/_confirm.html", {"action": action_message})
+        return wrap
+    return decorator
+
+
+def require_object(model, redirect_url=None, raise_404=False, show_errorpage=False, custom_response=None):
+    """Decorator to only call the view if an object with the given id exists
+    and automatically pass it instead of the id."""
+
+    def decorator(function):
+        @wraps(function)
+        def wrap(request, object_id, *args, **kwargs):
+            if model.objects.filter(pk=int(object_id)).exists():
+                obj = model.objects.get(pk=int(object_id))
+                return function(request, obj, *args, **kwargs)
+
+            if custom_response:
+                return custom_response
+
+            messages.warning(
+                request, _("%(name)s with ID %(id)s was not found!") % {"name": model._meta.verbose_name, "id": object_id})
+
+            if raise_404:
+                raise Http404
+
+            if show_errorpage:
+                return render_error(request)
+
+            return redirect(redirect_url or reverse(f"admin:{model._meta.app_label}_{model._meta.model_name}_changelist"))
+        return wrap
+    return decorator
---
+++ packages/django-kmuhelper/django-kmuhelper-1.7.2.tar.gz/django-kmuhelper-1.7.2/kmuhelper/views.py
@@ -0,0 +1,95 @@
+from django.contrib.auth.decorators import login_required, permission_required
+from django.urls import NoReverseMatch, reverse_lazy, reverse
+from django.shortcuts import render
+from django.template import TemplateDoesNotExist, TemplateSyntaxError
+from django.template.loader import get_template
+from django.utils.translation import gettext
+
+from kmuhelper.utils import render_error
+from kmuhelper.constants import URL_MANUAL
+
+_ = gettext
+
+# Create your views here.
+
+
+@login_required(login_url=reverse_lazy("admin:login"))
+def home(request):
+    grid = [
+        [
+            {
+                "title": _("Admin"),
+                "subtitle": _("Volle Kontrolle über die Hauptfunktionen"),
+                "url": reverse("admin:app_list", kwargs={"app_label": "kmuhelper"}),
+                "icon": "fas fa-key",
+            },
+            {
+                "title": _("E-Mails"),
+                "subtitle": _("Verwaltung und Erstellung von E-Mails"),
+                "url": reverse("kmuhelper:email-index"),
+                "icon": "fas fa-envelope-open",
+            },
+        ],
+        [
+            {
+                "title": _("App (Mobile)"),
+                "subtitle": _("Eingeschränkte Verwaltung von Daten in verschiedenen Arbeitsschritten"),
+                "url": reverse("kmuhelper:app-mobile"),
+                "icon": "fas fa-mobile-alt",
+            },
+            {
+                "title": _("App (Desktop)"),
+                "subtitle": _("Eingeschränkte Verwaltung von Daten in verschiedenen Arbeitsschritten"),
+                "url": reverse("kmuhelper:app-desktop"),
+                "icon": "fas fa-desktop",
+            },
+        ],
+        [
+            {
+                "title": _("Statistiken"),
+                "subtitle": _("Diagramme und co."),
+                "url": reverse("kmuhelper:stats"),
+                "icon": "fas fa-chart-pie",
+            },
+            {
+                "title": _("Einstellungen"),
+                "subtitle": _("Einstellungen für den KMUHelper"),
+                "url": reverse("kmuhelper:settings"),
+                "icon": "fas fa-cog",
+            },
+        ],
+        [
+            {
+                "title": _("Handbuch"),
+                "subtitle": _("Dokumentation zum KMUHelper"),
+                "url": URL_MANUAL,
+                "icon": "fas fa-book",
+            },
+        ]
+    ]
+    return render(request, "kmuhelper/home.html", {"has_permission": True, "grid": grid})
+
+
+def error(request):
+    return render_error(request)
+
+
+@permission_required("is_superuser")
+def _templatetest(request, templatename):
+    """DEBUG: Test a template with given parameters"""
+
+    try:
+        get_template(templatename)
+        return render(request, templatename, request.GET.dict())
+    except TemplateDoesNotExist:
+        return render_error(request, status=404, 
+                            message=_("Vorlage %s wurde nicht gefunden.") % templatename)
+    except TemplateSyntaxError as error:
+        return render_error(request, status=400, 
+                            message=_("Vorlage %(name)s enthält einen Syntaxfehler: %(error)s") % {
+                                "name": templatename,
+                                "error": error
+                            })
+    except (NoReverseMatch) as error:
+        return render_error(request, status=400, 
+                            message=_("Es ist ein Fehler aufgetreten: %s") % error)
