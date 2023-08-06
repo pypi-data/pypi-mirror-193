01882f8bb6326358c0c837dbad45487adf51b5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:36 +0000

    Add package isbntools/4.3.29/isbntools-4.3.29-py2.py3-none-any.whl

---
+++ packages/isbntools/isbntools-4.3.29-py2.py3-none-any.whl/isbntools/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Define isbntools version and support IDs."""
+
+__version__ = '4.3.29'
+__support__ = 'py27, py35, py36, py37, py38, py39, py310'
---
+++ packages/isbntools/isbntools-4.3.29-py2.py3-none-any.whl/isbntools/contrib/modules/rename/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Write here what you want to expose from this module."""
+
+from ._rename import (
+    checkpattern,
+    cleannewname,
+    get_isbn,
+    newfilename,
+    rencwdfiles,
+    renfile,
+)
