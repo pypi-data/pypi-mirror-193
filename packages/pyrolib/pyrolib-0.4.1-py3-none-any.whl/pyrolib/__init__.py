1671897c48a02291bf5a2ed46e6dc8af63a092c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:11 +0000

    Add package pyrolib/0.4.1/pyrolib-0.4.1-py3-none-any.whl

---
+++ packages/pyrolib/pyrolib-0.4.1-py3-none-any.whl/pyrolib/__init__.py
@@ -0,0 +1,10 @@
+__author__ = "Aurélien Costes"
+__credits__ = ["Aurélien Costes"]
+__license__ = "MIT"
+__version__ = "0.4.1"
+__maintainer__ = "Aurélien Costes"
+__email__ = "aurelien.costes31@gmail.com"
+
+from . import fuelmap
+from . import data_processing
+from . import blaze
---
+++ packages/pyrolib/pyrolib-0.4.1-py3-none-any.whl/pyrolib/fuelmap/__init__.py
@@ -0,0 +1,14 @@
+from .fuels import (
+    BalbiFuel,
+    show_fuel_classes,
+)
+from .fuelmap import (
+    FuelMap,
+)
+from .utility import (
+    fire_array_2d_to_3d,
+    fire_array_3d_to_2d,
+)
+from .fuel_database import (
+    FuelDatabase,
+)
