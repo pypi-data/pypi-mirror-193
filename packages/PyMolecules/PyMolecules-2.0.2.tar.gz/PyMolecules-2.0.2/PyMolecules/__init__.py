250a98ec3214b47e00daeb2d333c3a64fd801059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:42 +0000

    Add package PyMolecules/2.0.2/PyMolecules-2.0.2.tar.gz

---
+++ packages/PyMolecules/PyMolecules-2.0.2.tar.gz/PyMolecules-2.0.2/PyMolecules/__init__.py
@@ -0,0 +1,6 @@
+from PyMolecules import SingleLinkedList, DoubleLinkedList,SingleCircularLinkedList
+
+
+__author__ = "Yokesh Bollineni"
+__version__ = "2.0.2"
+__what_is_new__ = "Removal of junk code in the version 2.0.1"
