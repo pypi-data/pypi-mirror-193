9ab4c70ea089477759a5b1fdfc2549df591e3d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:03 +0000

    Add package verb-conjugator/0.0.4/verb_conjugator-0.0.4-py3-none-any.whl

---
+++ packages/verb-conjugator/verb_conjugator-0.0.4-py3-none-any.whl/verb_conjugator/__main__.py
@@ -0,0 +1,26 @@
+import sys
+
+from .data import COMMON_VERBS
+from .verb_conjugator import VerbConjugator
+
+
+def main():
+    vc = VerbConjugator()
+
+    while True:
+        user_selection = vc.display_menu()
+        if user_selection == "1":
+            vc.setup()
+            vc.display_verb_conjugation()
+        elif user_selection == "2":
+            vc.setup()
+            vc.drill_and_practice()
+        elif user_selection == "3":
+            vc.common_verbs_quiz(COMMON_VERBS)
+        else:
+            print("\nGoodbye!")
+            sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
