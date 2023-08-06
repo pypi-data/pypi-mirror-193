93acd8a64f397d58e0c4c29d42e636a25fd784c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:11 +0000

    Add package strbuilder/1.0.5/strbuilder-1.0.5.tar.gz

---
+++ packages/strbuilder/strbuilder-1.0.5.tar.gz/strbuilder-1.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', mode='r', encoding='utf-8') as f:
+    long_description = f.read()
+
+author = 'am230'
+name = 'strbuilder'
+
+setup(
+    name=name,
+    version="1.0.5",
+    keywords=("builder"),
+    description="A simple string builder",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="MIT Licence",
+    url=f"https://github.com/{author}/{name}",
+    author=author,
+    author_email="am.230@outlook.jp",
+    py_modules=['strbuilder'],
+    platforms="any",
+    packages=find_packages()
+)
