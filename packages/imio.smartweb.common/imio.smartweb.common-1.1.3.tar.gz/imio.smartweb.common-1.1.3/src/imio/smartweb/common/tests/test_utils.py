bb5e807839fe4d8f6d25ce22cdff7542b41320b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:41 +0000

    Add package imio.smartweb.common/1.1.3/imio.smartweb.common-1.1.3.tar.gz

---
+++ packages/imio.smartweb.common/imio.smartweb.common-1.1.3.tar.gz/imio.smartweb.common-1.1.3/src/imio/smartweb/common/tests/test_utils.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.common.interfaces import IAddress
+from imio.smartweb.common.testing import IMIO_SMARTWEB_COMMON_FUNCTIONAL_TESTING
+from imio.smartweb.common.utils import geocode_object
+from imio.smartweb.common.utils import get_term_from_vocabulary
+from imio.smartweb.common.utils import translate_vocabulary_term
+from plone.formwidget.geolocation.geolocation import Geolocation
+from unittest import mock
+from unittest.mock import patch
+from zope.interface import implementer
+
+import geopy
+import unittest
+
+
+@implementer(IAddress)
+class GeolocatedObject(object):
+    """Dummy class for geolocation tests"""
+
+    street = None
+    number = None
+    complement = None
+    zipcode = None
+    city = None
+    country = None
+
+    def reindexObject(self, idxs):
+        return
+
+
+class TestUtils(unittest.TestCase):
+    layer = IMIO_SMARTWEB_COMMON_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests"""
+        self.request = self.layer["request"]
+        self.portal = self.layer["portal"]
+
+    def test_get_term_from_vocabulary(self):
+        term = get_term_from_vocabulary("imio.smartweb.vocabulary.Topics", "culture")
+        self.assertEqual(term.title, "Culture")
+
+        term = get_term_from_vocabulary("imio.smartweb.vocabulary.IAm", "parent")
+        self.assertEqual(term.title, "Parent")
+
+        term = get_term_from_vocabulary("imio.smartweb.vocabulary.IAm", "non_existing")
+        self.assertEqual(term.title, "non_existing")
+
+    def test_translate_vocabulary_term(self):
+        self.assertEqual(
+            translate_vocabulary_term("imio.smartweb.vocabulary.Countries", None),
+            "",
+        )
+        self.assertEqual(
+            translate_vocabulary_term("imio.smartweb.vocabulary.Countries", "be"),
+            "Belgium",
+        )
+        self.assertEqual(
+            translate_vocabulary_term("imio.smartweb.vocabulary.Countries", "be", "fr"),
+            "Belgique",
+        )
+        self.assertEqual(
+            translate_vocabulary_term("imio.smartweb.vocabulary.Countries", "be", "nl"),
+            "België",
+        )
+        with patch("plone.api.portal.get_current_language", return_value="fr"):
+            self.assertEqual(
+                translate_vocabulary_term("imio.smartweb.vocabulary.Countries", "be"),
+                "Belgique",
+            )
+
+    def test_geolocation(self):
+        attr = {"geocode.return_value": mock.Mock(latitude=1, longitude=2)}
+        geopy.geocoders.Nominatim = mock.Mock(return_value=mock.Mock(**attr))
+
+        obj = GeolocatedObject()
+        obj.geolocation = Geolocation(0, 0)
+
+        geocoded = geocode_object(obj)
+        self.assertFalse(geocoded)
+        self.assertEqual(obj.geolocation.latitude, 0)
+        self.assertEqual(obj.geolocation.longitude, 0)
+
+        obj.street = "My beautiful street"
+        geocoded = geocode_object(obj)
+        self.assertTrue(geocoded)
+        self.assertEqual(obj.geolocation.latitude, 1)
+        self.assertEqual(obj.geolocation.longitude, 2)
---
+++ packages/imio.smartweb.common/imio.smartweb.common-1.1.3.tar.gz/imio.smartweb.common-1.1.3/src/imio/smartweb/common/utils.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.common.config import TRANSLATED_VOCABULARIES
+from plone import api
+from plone.formwidget.geolocation.geolocation import Geolocation
+from zope.component import getUtility
+from zope.i18n import translate
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+
+import geopy
+import re
+
+
+def get_vocabulary(voc_name):
+    factory = getUtility(IVocabularyFactory, voc_name)
+    vocabulary = factory(api.portal.get())
+    return vocabulary
+
+
+def get_term_from_vocabulary(vocabulary, value):
+    portal = api.portal.get()
+    factory = getUtility(IVocabularyFactory, vocabulary)
+    vocabulary = factory(portal)
+    try:
+        term = vocabulary.getTerm(value)
+    except LookupError:
+        return SimpleTerm(value=value, title=value)
+    return term
+
+
+def translate_vocabulary_term(vocabulary, term, lang=None):
+    if term is None:
+        return ""
+    portal = api.portal.get()
+    factory = getUtility(IVocabularyFactory, vocabulary)
+    if vocabulary in TRANSLATED_VOCABULARIES:
+        vocabulary = factory(portal, lang=lang)
+    else:
+        vocabulary = factory(portal)
+    term = vocabulary.getTerm(term)
+    if term is None:
+        return ""
+    if lang is None:
+        lang = api.portal.get_current_language()[:2]
+    return translate(term.title, target_language=lang)
+
+
+def geocode_object(obj):
+    street_parts = [
+        obj.number and str(obj.number) or "",
+        obj.street,
+        obj.complement,
+    ]
+    street = " ".join(filter(None, street_parts))
+    entity_parts = [
+        obj.zipcode and str(obj.zipcode) or "",
+        obj.city,
+    ]
+    entity = " ".join(filter(None, entity_parts))
+    country = translate_vocabulary_term(
+        "imio.smartweb.vocabulary.Countries", obj.country
+    )
+    address = " ".join(filter(None, [street, entity, country]))
+    if not address:
+        return
+    geolocator = geopy.geocoders.Nominatim(user_agent="contact@imio.be", timeout=3)
+    location = geolocator.geocode(address)
+    if location:
+        obj.geolocation = Geolocation(
+            latitude=location.latitude, longitude=location.longitude
+        )
+        obj.reindexObject(idxs=["longitude", "latitude"])
+        return True
+    return False
+
+
+def rich_description(description):
+    # **strong**
+    description = re.sub(r"\*\*([^\*\*]*)\*\*", r"<strong>\1</strong>", description)
+    # <br/>
+    description = "<br/>".join(description.split("\r\n"))
+    return description
