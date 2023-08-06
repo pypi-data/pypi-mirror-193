0102742cf675e1cde952bfd0f0dc607bf010786e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:14 +0000

    Add package 256-encrypt/0.0.1/256-encrypt-0.0.1.tar.gz

---
+++ packages/256-encrypt/256-encrypt-0.0.1.tar.gz/256-encrypt-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='256-encrypt',
+  version='0.0.1',
+  description='SHA256 Encryption Algorithm.',
+  long_description='SHA256 is Encryption Algorithm for encrypting certain values. It was created by The National Security Agency and this is my implementation for the Python programming language.',
+  url='',  
+  author='64biit',
+  author_email='sixtyfourblit@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calculator', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
