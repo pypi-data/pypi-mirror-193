2992ffa8b81bf9cf98838f90573a30ff28d4053d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:47 +0000

    Add package oidcish/0.2.0/oidcish-0.2.0-py3-none-any.whl

---
+++ packages/oidcish/oidcish-0.2.0-py3-none-any.whl/oidcish/__init__.py
@@ -0,0 +1,6 @@
+"""oidcish is a library that obtains claims from an identity provider via OIDC.
+
+Device and code flows are supported.
+"""
+from oidcish.device import DeviceFlow
+from oidcish.code import CodeFlow
