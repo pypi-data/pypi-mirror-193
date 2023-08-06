89d8bbf478f328630bcb70d89c3047de250f9417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:52 +0000

    Add package resourcesing/0.1.5/resourcesing-0.1.5.tar.gz

---
+++ packages/resourcesing/resourcesing-0.1.5.tar.gz/resourcesing-0.1.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['resourcesing']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['daemonprocessing>=0.1.1,<0.2.0']
+
+setup_kwargs = {
+    'name': 'resourcesing',
+    'version': '0.1.5',
+    'description': '',
+    'long_description': '',
+    'author': 'jawide',
+    'author_email': '596929059@qq.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
