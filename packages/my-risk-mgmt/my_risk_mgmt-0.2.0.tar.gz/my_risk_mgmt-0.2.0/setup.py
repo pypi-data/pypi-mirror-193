67fbafb86c0cb5cfe766a3cfc98cf9ca4811109f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:04 +0000

    Add package my-risk-mgmt/0.2.0/my_risk_mgmt-0.2.0.tar.gz

---
+++ packages/my-risk-mgmt/my_risk_mgmt-0.2.0.tar.gz/my_risk_mgmt-0.2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='my_risk_mgmt',
+    version='0.2.0',
+    description='using for quatitative risk management',
+    author='Qihang Ma',
+    packages=['my_risk_mgmt'],
+    install_requires=['pandas','numpy'],
+)
