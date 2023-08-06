a16904894f8de8ddfb0732c6da6256cca3e054c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:55 +0000

    Add package helix.fhir.client.sdk/1.0.31a2/helix.fhir.client.sdk-1.0.31a2-py3-none-any.whl

---
+++ packages/helix.fhir.client.sdk/helix.fhir.client.sdk-1.0.31a2-py3-none-any.whl/helix_fhir_client_sdk/responses/fhir_get_response.py
@@ -0,0 +1,47 @@
+import json
+from typing import Optional, Dict, Any, List
+
+
+class FhirGetResponse:
+    def __init__(
+        self,
+        *,
+        request_id: Optional[str],
+        url: str,
+        responses: str,
+        error: Optional[str],
+        access_token: Optional[str],
+        total_count: Optional[int],
+        status: int,
+        next_url: Optional[str] = None,
+        extra_context_to_return: Optional[Dict[str, Any]]
+    ) -> None:
+        """
+        Class that encapsulates the response from FHIR server
+
+        :param request_id: request id
+        :param url: url that was being accessed
+        :param responses: response text
+        :param error: Any error returned by FHIR server
+        :param access_token: access token that was used
+        :param total_count: count of total records that match the provided query.
+                            Only set if include_total_count was set to avoid expensive operation by server.
+        :param extra_context_to_return: a dict to return with every row (separate_bundle_resources is set)
+                                        or with FhirGetResponse
+        """
+        self.request_id: Optional[str] = request_id
+        self.url: str = url
+        self.responses: str = responses
+        self.error: Optional[str] = error
+        self.access_token: Optional[str] = access_token
+        self.total_count: Optional[int] = total_count
+        self.status: int = status
+        self.next_url: Optional[str] = next_url
+        self.extra_context_to_return: Optional[Dict[str, Any]] = extra_context_to_return
+
+    def append(self, other: List["FhirGetResponse"]) -> "FhirGetResponse":
+        response_json = json.loads(self.responses)
+        for other_response in other:
+            if other_response.responses:
+                other_response_json = json.loads(other_response.responses)
+        return self
