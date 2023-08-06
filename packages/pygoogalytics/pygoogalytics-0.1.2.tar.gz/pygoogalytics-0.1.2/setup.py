7fb93a8b7dca4a5b1f3a8be0bcbde36ec130a0b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:00 +0000

    Add package pygoogalytics/0.1.2/pygoogalytics-0.1.2.tar.gz

---
+++ packages/pygoogalytics/pygoogalytics-0.1.2.tar.gz/pygoogalytics-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='pygoogalytics',
+    version='0.1.2',
+    description='',
+    url='https://github.com/Blink-SEO/pygoogalytics',
+    author='Joshua Prettyman',
+    author_email='joshua@blinkseo.co.uk',
+    license='MIT',
+    packages=['pygoogalytics'],
+    install_requires=[
+        'pandas',
+        'google-api-python-client',
+        'google-analytics-data'
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
+
+# python setup.py check
+# python setup.py sdist
+# python setup.py bdist_wheel --universal
+# twine upload dist/*
