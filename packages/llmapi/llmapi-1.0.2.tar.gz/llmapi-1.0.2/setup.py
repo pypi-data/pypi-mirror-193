c8d00349dba1c5ba48abdaebb7b7407df46c83ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:33 +0000

    Add package llmapi/1.0.2/llmapi-1.0.2.tar.gz

---
+++ packages/llmapi/llmapi-1.0.2.tar.gz/llmapi-1.0.2/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+
+    LLMAapi
+    
+    unified openapi for large language models
+
+    :author:    llmapi <llmapi@163.com>
+    :homepage:  https://github.com/whlook/
+    :license:   MIT, see LICENSE for more details.
+    :copyright: Copyright (c) 2023 llmapi. All rights reserved
+"""
+import codecs
+import llmapi
+import setuptools.command.test
+
+
+# -*- Long Description -*-
+
+def long_description():
+    try:
+        return codecs.open('README.md', 'r', 'utf-8').read()
+    except IOError:
+        return 'Long description error: Missing README.rst file'
+
+
+setuptools.setup(
+    name=llmapi.__name__,
+    version=llmapi.__version__,
+    description=llmapi.__description__,
+    long_description=long_description(),
+    keywords=llmapi.__keywords__,
+    author=llmapi.__author__,
+    author_email=llmapi.__contact__,
+    url=llmapi.__url__,
+    license=llmapi.__license__,
+    platforms=['any'],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: English',
+        'Topic :: Utilities',
+        'Topic :: Terminals',
+        "Topic :: System :: Distributed Computing",
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=setuptools.find_packages(exclude=['tests']),
+    include_package_data=True,
+    package_data={
+        '': ['*.py']
+    },
+    entry_points={
+        'console_scripts': [
+            'llmapi = llmapi:main',
+        ],
+    },
+)
