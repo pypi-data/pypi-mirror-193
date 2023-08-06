63e0a81047a5ce7648ac4fbb130e42ad42ebfa91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:47 +0000

    Add package v4l2py/1.3.0/v4l2py-1.3.0.tar.gz

---
+++ packages/v4l2py/v4l2py-1.3.0.tar.gz/v4l2py-1.3.0/v4l2py/__init__.py
@@ -0,0 +1,9 @@
+#
+# This file is part of the v4l2py project
+#
+# Copyright (c) 2021 Tiago Coutinho
+# Distributed under the GPLv3 license. See LICENSE for more info.
+
+from .device import Device, VideoCapture, iter_devices, iter_video_capture_devices
+
+__version__ = "1.3.0"
