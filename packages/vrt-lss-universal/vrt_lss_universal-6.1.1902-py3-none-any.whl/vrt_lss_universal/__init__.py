4f1b0869f3be0ab9a19b9d03c496d651dadd8b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:44 +0000

    Add package vrt-lss-universal/6.1.1902/vrt_lss_universal-6.1.1902-py3-none-any.whl

---
+++ packages/vrt-lss-universal/vrt_lss_universal-6.1.1902-py3-none-any.whl/vrt_lss_universal/__init__.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+
+"""
+    VRt.Universal [UV]
+
+    The version of the OpenAPI document: 6.1.1902
+    Contact: servicedesk@veeroute.com
+    Auto Generated
+"""
+
+
+__version__ = "6.1.1902"
+
+# import ApiClient
+from vrt_lss_universal.api_client import ApiClient
+
+# import Configuration
+from vrt_lss_universal.configuration import Configuration
+
+# import exceptions
+from vrt_lss_universal.exceptions import OpenApiException
+from vrt_lss_universal.exceptions import ApiAttributeError
+from vrt_lss_universal.exceptions import ApiTypeError
+from vrt_lss_universal.exceptions import ApiValueError
+from vrt_lss_universal.exceptions import ApiKeyError
+from vrt_lss_universal.exceptions import ApiException
