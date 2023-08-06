14ea73b0d63eba93802f0c7764dbd8f90e49be6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:22 +0000

    Add package PyComplexHeatmap/1.2.9/PyComplexHeatmap-1.2.9.tar.gz

---
+++ packages/PyComplexHeatmap/PyComplexHeatmap-1.2.9.tar.gz/PyComplexHeatmap-1.2.9/PyComplexHeatmap/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from .clustermap import *
+from .colors import *
+from .example import *
+from .bioinformatics_plot import *
+
+#__all__=['*']
+__version__='1.2.9'
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
