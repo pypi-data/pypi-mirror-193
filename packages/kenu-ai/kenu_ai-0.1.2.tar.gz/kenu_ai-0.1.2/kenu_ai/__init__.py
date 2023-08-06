83aab56646dc945e242813b8a74d7b16f7049e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:05 +0000

    Add package kenu-ai/0.1.2/kenu_ai-0.1.2.tar.gz

---
+++ packages/kenu-ai/kenu_ai-0.1.2.tar.gz/kenu_ai-0.1.2/kenu_ai/__init__.py
@@ -0,0 +1,18 @@
+from googletrans import Translator
+import requests
+
+def chat(value):
+	if("sahibin kim" in value):
+		return "Sahibim namıdeğer r4isy."
+	else:
+		translator = Translator()
+		t = translator.translate(value, dest="en")
+		response = requests.get("http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=1&msg=" + t.text)
+		veri = response.text.split(':"')[-1].split('"}')[0]
+		translated = translator.translate(veri, dest="tr")
+		if("zackybot" in translated.text):
+			new_string = string.replace("ZackyBot", "kenu-ai")
+			return new_string
+		else:
+			return translated.text
+
