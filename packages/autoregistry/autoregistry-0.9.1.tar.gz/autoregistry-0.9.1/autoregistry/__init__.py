cc9f1fb528c9b1e54bc3be31d9f05fca28c88736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:37 +0000

    Add package autoregistry/0.9.1/autoregistry-0.9.1.tar.gz

---
+++ packages/autoregistry/autoregistry-0.9.1.tar.gz/autoregistry-0.9.1/autoregistry/__init__.py
@@ -0,0 +1,13 @@
+# Don't manually change, let poetry-dynamic-versioning-plugin handle it.
+__version__ = "0.9.1"
+
+from .config import RegistryConfig
+from .exceptions import (
+    CannotDeriveNameError,
+    CannotRegisterPythonBuiltInError,
+    InvalidNameError,
+    KeyCollisionError,
+    ModuleAliasError,
+    RegistryError,
+)
+from .registry import Registry, RegistryDecorator, RegistryMeta
