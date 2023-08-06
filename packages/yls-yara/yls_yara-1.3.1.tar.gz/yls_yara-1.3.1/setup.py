66f619a41a0c7c5852035faab1043c1deef440bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:57 +0000

    Add package yls-yara/1.3.1/yls_yara-1.3.1.tar.gz

---
+++ packages/yls-yara/yls_yara-1.3.1.tar.gz/yls_yara-1.3.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['yls_yara']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['yara-python>=4.2.0,<5.0.0', 'yls>=1.3.1,<2.0.0']
+
+entry_points = \
+{'yls': ['yara = yls_yara']}
+
+setup_kwargs = {
+    'name': 'yls-yara',
+    'version': '1.3.1',
+    'description': 'YLS plugin adding linting using yara-python.',
+    'long_description': '# yls-yara\n\n![PyPI](https://img.shields.io/pypi/v/yls-yara)\n\nAn [YLS](https://www.github.com/avast/yls) plugin adding\n[YARA](https://github.com/VirusTotal/yara) linting capabilities.\n\nThis plugin runs `yara.compile` on every save, parses the errors, and returns\nlist of diagnostic messages.\n\n## License\n\nCopyright (c) 2022 Avast Software, licensed under the MIT license. See the\n[`LICENSE`](https://github.com/avast/yls/blob/master/plugins/yls-yara/LICENSE)\nfile for more details.\n',
+    'author': 'Matej Kastak',
+    'author_email': 'matej.kastak@avast.com',
+    'maintainer': 'Matej Kašťák',
+    'maintainer_email': 'matej.kastak@avast.com',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<3.12',
+}
+
+
+setup(**setup_kwargs)
