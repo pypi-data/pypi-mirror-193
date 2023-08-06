311eedea0587e57e678143fccc482619a37a954d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:38 +0000

    Add package viam-sdk/0.2.10/viam_sdk-0.2.10-py3-none-macosx_10_16_x86_64.whl

---
+++ packages/viam-sdk/viam_sdk-0.2.10-py3-none-macosx_10_16_x86_64.whl/viam/components/sensor/service.py
@@ -0,0 +1,33 @@
+from grpclib.server import Stream
+
+from viam.components.service_base import ComponentServiceBase
+from viam.errors import ResourceNotFoundError
+from viam.proto.component.sensor import (
+    GetReadingsRequest,
+    GetReadingsResponse,
+    SensorServiceBase,
+)
+from viam.utils import sensor_readings_native_to_value, struct_to_dict
+
+from .sensor import Sensor
+
+
+class SensorService(SensorServiceBase, ComponentServiceBase[Sensor]):
+    """
+    gRPC Service for a generic Sensor
+    """
+
+    RESOURCE_TYPE = Sensor
+
+    async def GetReadings(self, stream: Stream[GetReadingsRequest, GetReadingsResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        name = request.name
+        try:
+            sensor = self.get_component(name)
+        except ResourceNotFoundError as e:
+            raise e.grpc_error
+        timeout = stream.deadline.time_remaining() if stream.deadline else None
+        readings = await sensor.get_readings(extra=struct_to_dict(request.extra), timeout=timeout, metadata=stream.metadata)
+        response = GetReadingsResponse(readings=sensor_readings_native_to_value(readings))
+        await stream.send_message(response)
---
+++ packages/viam-sdk/viam_sdk-0.2.10-py3-none-macosx_10_16_x86_64.whl/viam/logging.py
@@ -0,0 +1,61 @@
+from copy import copy
+import logging
+from logging import DEBUG, INFO, WARN, WARNING, ERROR, FATAL  # noqa: F401
+from typing import Dict
+import sys
+
+LOG_LEVEL = INFO
+LOGGERS: Dict[str, logging.Logger] = {}
+
+
+class ColorFormatter(logging.Formatter):
+
+    MAPPING = {
+        "DEBUG": 37,  # white
+        "INFO": 36,  # cyan
+        "WARNING": 33,  # yellow
+        "ERROR": 31,  # red
+        "CRITICAL": 41,  # white on red bg
+    }
+
+    def __init__(self, patern):
+        logging.Formatter.__init__(self, patern)
+
+    def format(self, record):
+        colored_record = copy(record)
+        levelname = colored_record.levelname
+        seq = self.MAPPING.get(levelname, 37)  # default white
+        colored_levelname = f"\x1b[33;{seq}m{levelname}\x1b[0m"
+        colored_record.levelname = colored_levelname
+        return super().format(colored_record)
+
+
+def getLogger(name: str) -> logging.Logger:
+    logger = LOGGERS.get(name)
+    if logger:
+        return logger
+
+    logger = logging.getLogger(name)
+    logger.setLevel(LOG_LEVEL)
+    format = ColorFormatter("%(asctime)s\t\t" + "%(levelname)s\t" + "%(name)s (%(filename)s:%(lineno)d)\t" + "%(message)s\t")
+
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(format)
+    # filter out logs at error level or above
+    handler.addFilter(filter=lambda record: (record.levelno < ERROR))
+    logger.addHandler(handler)
+
+    err_handler = logging.StreamHandler(stream=sys.stderr)
+    err_handler.setFormatter(format)
+    # filter out logs below error level
+    handler.addFilter(filter=lambda record: (record.levelno >= ERROR))
+    logger.addHandler(err_handler)
+
+    LOGGERS[name] = logger
+    return logger
+
+
+def setLevel(level: int):
+    LOG_LEVEL = level
+    for logger in LOGGERS.values():
+        logger.setLevel(LOG_LEVEL)
