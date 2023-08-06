3aee6f6273f880cdf3f39f74641e9fb043986ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:14 +0000

    Add package nlecloud-framework/0.0.5/nlecloud_framework-0.0.5.tar.gz

---
+++ packages/nlecloud-framework/nlecloud_framework-0.0.5.tar.gz/nlecloud_framework-0.0.5/nlecloud_framework/demo.py
@@ -0,0 +1,41 @@
+# _*_ coding:utf-8 _*_
+"""
+@File: demo.py
+@Author: cfp
+@Date: 2020-08-21 14:07:08
+@LastEditTime: 2023/2/22 15:46
+@LastEditors: cfp
+@LastModifyTime @Version  @Desciption
+@Description:
+"""
+
+def sayhello():
+    print("111")
+
+def sayhello2():
+    print("111")
+    print("111")
+
+def func(a,b):
+    """
+    :description
+    :param a:
+    :param b:
+    :return:
+    """
+
+def FuncName():
+    """
+    @description:
+    @param args:
+    @return
+    @last_editors: cfp
+    """
+    pass
+
+
+
+if __name__ == '__main__':
+    print("hello1111222")
+
+
