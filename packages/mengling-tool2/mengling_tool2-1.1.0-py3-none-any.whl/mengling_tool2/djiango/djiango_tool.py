26972351124fa97a5ca01f1936551a5d34192c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:49 +0000

    Add package mengling-tool2/1.1.0/mengling_tool2-1.1.0-py3-none-any.whl

---
+++ packages/mengling-tool2/mengling_tool2-1.1.0-py3-none-any.whl/mengling_tool2/djiango/djiango_tool.py
@@ -0,0 +1,39 @@
+from django.core.handlers.wsgi import WSGIRequest
+
+
+def _saveFile(file, filepath):
+    with open(filepath, mode='wb+') as f:
+        for chunk in file.chunks():
+            f.write(chunk)
+    return filepath
+
+
+def getFile(request: WSGIRequest, file_key, path='', filename=None, if_save_file=True):
+    file = request.FILES.get(file_key)
+    filepath = f'{path}/{filename if filename else file.name}'
+    if if_save_file:
+        return _saveFile(file, filepath)
+    else:
+        return file
+
+
+def getFiles(request: WSGIRequest, files_key, path='', filename_qz=None, if_save_file=True):
+    files = request.FILES.getlist(files_key)
+    res = list()
+    for file in files:
+        filepath = f'{path}/{filename_qz if filename_qz else ""}{file.name}'
+        if if_save_file:
+            with open(filepath, mode='wb+') as f:
+                for chunk in file.chunks():
+                    f.write(chunk)
+            res.append(filepath)
+        else:
+            res.append(file)
+
+
+def getIp(request: WSGIRequest):
+    if request.META.get('HTTP_X_FORWARDED_FOR', None) is not None:
+        ip = request.META['HTTP_X_FORWARDED_FOR']
+    else:
+        ip = request.META['REMOTE_ADDR']
+    return ip
