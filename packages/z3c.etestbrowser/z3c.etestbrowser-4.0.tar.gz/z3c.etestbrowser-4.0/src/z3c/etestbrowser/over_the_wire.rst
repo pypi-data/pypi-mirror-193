9cb367eb003b239e1e2da4c36c9f1412f4272063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:45 +0000

    Add package z3c.etestbrowser/4.0/z3c.etestbrowser-4.0.tar.gz

---
+++ packages/z3c.etestbrowser/z3c.etestbrowser-4.0.tar.gz/z3c.etestbrowser-4.0/src/z3c/etestbrowser/over_the_wire.rst
@@ -0,0 +1,21 @@
+Using testbrowser on the internet
+---------------------------------
+
+The ``z3c.etestbrowser.browser`` module exposes an ``ExtendedTestBrowser``
+class that simulates a web browser similar to Mozilla Firefox or IE.
+
+    >>> from z3c.etestbrowser.browser import ExtendedTestBrowser
+    >>> browser = ExtendedTestBrowser()
+
+It can send arbitrary headers; this is helpful for setting the language value,
+so that your tests format values the way you expect in your tests, if you rely
+on zope.i18n locale-based formatting or a similar approach.
+
+    >>> browser.addHeader('Accept-Language', 'en-US')
+
+The browser can `open` web pages:
+
+    >>> browser.open('https://www.w3.org')
+    >>> print(browser.contents)
+    <!DOCTYPE html ...
+    ...The World Wide Web Consortium (W3C) is an international community...
---
+++ packages/z3c.etestbrowser/z3c.etestbrowser-4.0.tar.gz/z3c.etestbrowser-4.0/src/z3c/etestbrowser/testing.py
@@ -0,0 +1,32 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Extensions for z3c.etestbrowser
+
+$Id$
+"""
+
+import zope.testbrowser.wsgi
+
+import z3c.etestbrowser.browser
+
+
+class ExtendedTestBrowser(zope.testbrowser.wsgi.Browser,
+                          z3c.etestbrowser.browser.ExtendedTestBrowser):
+    """An extended testbrowser implementation.
+
+    Features:
+
+        - offers the content also as parsed etree
+
+    """
