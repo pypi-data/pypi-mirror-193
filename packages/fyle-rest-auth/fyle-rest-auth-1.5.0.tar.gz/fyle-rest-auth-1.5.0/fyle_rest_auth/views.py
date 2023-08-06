9340f2e52bd4942d5262b9384334b00cee6f7df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:49 +0000

    Add package fyle-rest-auth/1.5.0/fyle-rest-auth-1.5.0.tar.gz

---
+++ packages/fyle-rest-auth/fyle-rest-auth-1.5.0.tar.gz/fyle-rest-auth-1.5.0/fyle_rest_auth/views.py
@@ -0,0 +1,65 @@
+"""
+Fyle Authentication views
+"""
+from rest_framework.views import APIView, status
+from rest_framework.response import Response
+
+from .helpers import (
+    validate_code_and_login,
+    validate_and_refresh_token,
+    validate_refresh_token_and_login
+)
+
+
+class LoginView(APIView):
+    """
+    Login Using Fyle Account
+    """
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        """
+        Login using authorization code
+        """
+        tokens = validate_code_and_login(request)
+
+        return Response(
+            data=tokens,
+            status=status.HTTP_200_OK,
+        )
+
+
+class LoginWithRefreshTokenView(APIView):
+    """
+    Login Using Fyle Account
+    """
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        """
+        Login using refresh token
+        """
+        tokens = validate_refresh_token_and_login(request)
+
+        return Response(
+            data=tokens,
+            status=status.HTTP_200_OK,
+        )
+
+
+class RefreshView(APIView):
+    """
+    Refresh Access Token
+    """
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        tokens = validate_and_refresh_token(request)
+
+        return Response(
+            data=tokens,
+            status=status.HTTP_200_OK
+        )
