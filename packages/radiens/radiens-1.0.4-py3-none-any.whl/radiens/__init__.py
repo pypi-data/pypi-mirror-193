a6ff056ccc4bf75dfce252d22f41112b4b385ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:52 +0000

    Add package radiens/1.0.4/radiens-1.0.4-py3-none-any.whl

---
+++ packages/radiens/radiens-1.0.4-py3-none-any.whl/radiens/__init__.py
@@ -0,0 +1,7 @@
+name = "radiens"
+
+
+from .allego_client import AllegoClient
+from .videre_client import VidereClient
+from .signal_group.SignalGroup import SignalGroup
+from .version import __version__
