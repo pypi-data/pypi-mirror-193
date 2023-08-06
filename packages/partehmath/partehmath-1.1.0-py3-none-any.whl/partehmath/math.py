e39ed960cade896a9c1245d9ba6b232081ef6df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:05 +0000

    Add package partehmath/1.1.0/partehmath-1.1.0-py3-none-any.whl

---
+++ packages/partehmath/partehmath-1.1.0-py3-none-any.whl/partehmath/math.py
@@ -0,0 +1,31 @@
+import random
+def rand(min,max):
+    return random.randrange(min,max)
+def add(addend1,addend2):
+    return addend1 + addend2
+def sub(x,y):
+    return x - y
+def mul(factor1,factor2):
+    return factor1 * factor2
+def div(x,y):
+    return x / y
+def isEven(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+def dot(arr1, arr2): 
+    return sum(x*y for x, y in zip(arr1, arr2)) 
+def square_root(num):
+    if num < 0:
+        return None
+    else:
+        return num**0.5
+def Power(k, n):
+    return k**n
+def DoubleNum(n):
+    return n + n
+def modulo(spam, eggs): 
+    print(str(spam % eggs))
+def percent(spam, eggs): 
+    return str((spam * eggs) / 100.0)
