b6aaca5cccc26013c4dfa685b431ba3a997ed4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:41 +0000

    Add package f4/0.1.7/f4-0.1.7.tar.gz

---
+++ packages/f4/f4-0.1.7.tar.gz/f4-0.1.7/src/f4/__init__.py
@@ -0,0 +1,38 @@
+#TODO:
+#  https://python101.pythonlibrary.org/chapter36_creating_modules_and_packages.html
+#  https://medium.com/@skylar.kerzner/publish-your-first-python-package-to-pypi-8d08d6399c2f
+#TODO
+#import asyncio
+import datetime
+import fastnumbers
+import glob
+import gzip
+from itertools import chain
+#TODO
+#from joblib import Parallel, delayed
+import math
+import mmap
+import msgspec
+# #TODO:
+#import multiprocessing
+import operator
+from operator import itemgetter
+import os
+import re
+import shutil
+import sys
+import tempfile
+import zstandard
+
+print("got here init")
+
+#from . Builder import testme
+
+#print("got here2")
+
+from f4.Utilities import *
+from f4.Builder import *
+from f4.Filters import *
+from f4.IndexBuilder import *
+from f4.IndexSearcher import *
+from f4.Parser import *
