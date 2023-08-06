fd2bc4fb3ecdcb9c3b784e7761f481dba8a29cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:18 +0000

    Add package kirlent/0.4/kirlent-0.4.tar.gz

---
+++ packages/kirlent/kirlent-0.4.tar.gz/kirlent-0.4/kirlent/tasks.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2022-2023 H. Turgut Uyar <uyar@tekir.org>
+#
+# Kırlent is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Kırlent is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Kırlent.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+
+from invoke import Collection, Context, task
+
+from . import slides
+from .utils import COPY
+
+
+@task
+def setup(c: Context) -> None:
+    c.run("python -m pip --require-virtualenv install -U kirlent_docutils")
+    c.run("npm install decktape")
+
+    c.run(COPY % {
+        "src": Path(__file__).parent / "env",
+        "dst": Path(".env"),
+    })
+
+    c.run(COPY % {
+        "src": Path(__file__).parent / "kirlent.json",
+        "dst": Path("kirlent.json"),
+    })
+
+
+namespace = Collection(setup, slides)
