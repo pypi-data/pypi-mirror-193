aa6fbbfa8145ae7c28c75b39504a9c3fd058f36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:36 +0000

    Add package samplenanogpt/1.1.0/samplenanogpt-1.1.0.tar.gz

---
+++ packages/samplenanogpt/samplenanogpt-1.1.0.tar.gz/samplenanogpt-1.1.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="samplenanogpt",
+    version="1.1.0",
+    description="My sample nanogpt Python library",
+    packages=setuptools.find_packages(),
+)
