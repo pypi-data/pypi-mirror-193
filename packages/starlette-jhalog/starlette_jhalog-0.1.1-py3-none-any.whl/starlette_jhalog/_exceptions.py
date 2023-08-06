c427d809ed565b9a0a0a0681979f32ab2c33265a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:11 +0000

    Add package starlette-jhalog/0.1.1/starlette_jhalog-0.1.1-py3-none-any.whl

---
+++ packages/starlette-jhalog/starlette_jhalog-0.1.1-py3-none-any.whl/starlette_jhalog/_exceptions.py
@@ -0,0 +1,19 @@
+"""Exceptions."""
+from typing import Any, Optional, Dict
+from jhalog import LogEvent
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+
+class HTTPException(StarletteHTTPException):
+    """Starlette "HTTPException" with extra "error_detail" field."""
+
+    def __init__(
+        self,
+        status_code: int,
+        detail: Any = None,
+        headers: Optional[Dict[str, Any]] = None,
+        *,
+        error_detail: Optional[Any] = None,
+    ) -> None:
+        super().__init__(status_code=status_code, detail=detail, headers=headers)
+        LogEvent.set_to_context(error_detail=str(error_detail or detail))
