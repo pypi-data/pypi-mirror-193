a919837779281c730d13b68691c89ad269b201f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:31 +0000

    Add package coco-types/0.0.1/coco_types-0.0.1-py3-none-any.whl

---
+++ packages/coco-types/coco_types-0.0.1-py3-none-any.whl/coco_types/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.1"
+
+import coco_types.coco_dict_types as dict_types  # pyright: ignore[reportUnusedImport]
+
+from .coco_keypoints import AnnotationKP, CategoryKP, DatasetKP
+from .coco_object_detection import (Annotation, BaseModel, Category, Dataset, EncodedRLE, Image, Info, Licence, RLE,
+                                    TPolygon_segmentation)
+
+__all__ = ["Annotation", "Licence", "BaseModel", "Category", "Dataset", "EncodedRLE", "Image", "RLE", "Info",
+           "TPolygon_segmentation",
+           "AnnotationKP", "CategoryKP", "DatasetKP"]
