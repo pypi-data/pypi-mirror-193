785c106331e71b350053a2cd36a090e86cde1385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:51 +0000

    Add package py-arkworks-bls12381/0.1.4/py_arkworks_bls12381-0.1.4.tar.gz

---
+++ packages/py-arkworks-bls12381/py_arkworks_bls12381-0.1.4.tar.gz/py_arkworks_bls12381-0.1.4/examples/bls.py
@@ -0,0 +1,10 @@
+from py_arkworks_bls12381 import G1Point, G2Point, Scalar
+
+# Copies the interface for py-ecc
+
+
+# This is the identity point or point at infinity
+Z1 = G1Point.identity()
+
+def KeyValidate(b) -> bool:
+    pass
