0bb2553d7b12973dad76548014db7f23a52c847c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:21 +0000

    Add package py416/0.59/py416-0.59.tar.gz

---
+++ packages/py416/py416-0.59.tar.gz/py416-0.59/src/py416/__init__.py
@@ -0,0 +1,12 @@
+'''
+Name:    py416
+Author:  Ezio416
+Created: 2022-08-15
+Updated: 2023-02-22
+Version: 0.59
+
+A collection of various functions
+'''
+from .general import *
+__version__ = 0, 59
+v = __version__
