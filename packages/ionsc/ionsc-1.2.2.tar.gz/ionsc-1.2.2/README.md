d663f7fcc168b2cf1f7d75866fcda2a78115f47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:10 +0000

    Add package ionsc/1.2.2/ionsc-1.2.2.tar.gz

---
+++ packages/ionsc/ionsc-1.2.2.tar.gz/ionsc-1.2.2/README.md
@@ -0,0 +1,11 @@
+# This is a cryptografy module that encrypt any string that you put ou decrypt it on my own cryptografy.
+
+# You can use this for anything you want
+
+# The name of this cryptografy is 'ions'.
+
+# Made in Brazil, 2023
+# Version: 1.1
+# Day: 02/18/2023
+# Made in python for PyPI
+# import ionsc
---
+++ packages/ionsc/ionsc-1.2.2.tar.gz/ionsc-1.2.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='ionsc',
+    version='1.2.2',
+    license='MIT License',
+    author='RojanWARE',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='streetyyt@gmail.com',
+    keywords='encrypt,decrypt,cryptografy,code,encode,decode,coding',
+    description=u'A cool cryptography? No? Ok..',
+    packages=['ionsc'],
+    install_requires=[],)
