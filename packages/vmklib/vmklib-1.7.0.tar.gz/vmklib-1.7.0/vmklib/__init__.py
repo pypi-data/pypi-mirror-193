7638a8d4880b463f57081bfd5482835d7fc2a427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:27 +0000

    Add package vmklib/1.7.0/vmklib-1.7.0.tar.gz

---
+++ packages/vmklib/vmklib-1.7.0.tar.gz/vmklib-1.7.0/vmklib/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.0
+# hash=c34f9e26ea692f0a0a46478534d10085
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
+PKG_NAME = "vmklib"
+VERSION = "1.7.0"
---
+++ packages/vmklib/vmklib-1.7.0.tar.gz/vmklib-1.7.0/vmklib/tasks/python/docs.py
@@ -0,0 +1,62 @@
+"""
+A module for Python documentation tasks.
+"""
+
+# built-in
+from pathlib import Path
+from typing import Dict
+
+# third-party
+from vcorelib.paths.context import in_dir
+from vcorelib.task import Inbox, Outbox
+from vcorelib.task.manager import TaskManager
+from vcorelib.task.subprocess.run import SubprocessLogMixin
+
+# internal
+from vmklib.tasks.args import environ_fallback, environ_fallback_split
+from vmklib.tasks.python import PREFIX
+
+
+class PydepsTask(SubprocessLogMixin):
+    """A task for running pydeps."""
+
+    default_requirements = {"venv", PREFIX + "install-pydeps"}
+
+    async def run(self, inbox: Inbox, outbox: Outbox, *args, **kwargs) -> bool:
+        """Create or update a project's virtual environment."""
+
+        cwd: Path = args[0]
+        project: str = args[1]
+        self.stack.enter_context(in_dir(cwd))
+
+        # Create 'im'.
+        images = Path("im")
+        images.mkdir(parents=True, exist_ok=True)
+
+        return await self.exec(
+            str(inbox["venv"]["venv{python_version}"]["python"]),
+            "-m",
+            "pydeps",
+            "--no-show",
+            "-T",
+            "svg",
+            "-o",
+            environ_fallback(
+                "PY_DEPS_OUT", str(images.joinpath("pydeps.svg")), **kwargs
+            ),
+            *environ_fallback_split("PY_DEPS_EXTRA_ARGS", **kwargs),
+            project,
+        )
+
+
+def register(
+    manager: TaskManager,
+    project: str,
+    cwd: Path,
+    substitutions: Dict[str, str],
+) -> bool:
+    """Register documentation tasks to the manager."""
+
+    manager.register(PydepsTask(PREFIX + "deps", cwd, project), [])
+    del substitutions
+    return True
