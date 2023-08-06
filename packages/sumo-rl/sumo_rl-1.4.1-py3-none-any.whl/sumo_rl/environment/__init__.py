c40267796dd7b63f078c4d8afae795dd61dcff8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:27 +0000

    Add package sumo-rl/1.4.1/sumo_rl-1.4.1-py3-none-any.whl

---
+++ packages/sumo-rl/sumo_rl-1.4.1-py3-none-any.whl/sumo_rl/environment/__init__.py
@@ -0,0 +1,8 @@
+from gymnasium.envs.registration import register
+
+
+register(
+    id='sumo-rl-v0',
+    entry_point='sumo_rl.environment.env:SumoEnvironment',
+    kwargs={'single_agent': True},
+)
