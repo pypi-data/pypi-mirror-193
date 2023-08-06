fbb95fda35685d19ef715c733a28ae69aa2a7e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:44 +0000

    Add package lamindb/0.28rc1/lamindb-0.28rc1.tar.gz

---
+++ packages/lamindb/lamindb-0.28rc1.tar.gz/lamindb-0.28rc1/noxfile.py
@@ -0,0 +1,19 @@
+import nox
+from lndb.test.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
+
+nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+def lint(session: nox.Session) -> None:
+    run_pre_commit(session)
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+def build(session):
+    login_testuser1(session)
+    login_user_2 = "lndb login testuser2@lamin.ai --password goeoNJKE61ygbz1vhaCVynGERaRrlviPBVQsjkhz"  # noqa
+    session.run(*(login_user_2.split(" ")), external=True)
+    session.install(".[dev,test]")
+    run_pytest(session)
+    build_docs(session)
