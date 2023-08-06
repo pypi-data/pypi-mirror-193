99db2b5d18ff796a22a97200cba57c45be6f1810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:18 +0000

    Add package cuoco/1.0.0/cuoco-1.0.0-py3-none-any.whl

---
+++ packages/cuoco/cuoco-1.0.0-py3-none-any.whl/cuoco/mainTest.py
@@ -0,0 +1,8 @@
+import dataPipeline
+import pandas as pd
+def main():
+    dataPipeline.readJson("C:/Users/borja/OneDrive/Documents/proyects/biostats.csv", '../jsonTESTFILE.json')
+
+if __name__ == '__main__':
+    main()
+
