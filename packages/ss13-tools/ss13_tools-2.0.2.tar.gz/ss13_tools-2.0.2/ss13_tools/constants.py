322b1c34f3e45e6cda419aa76e3718aada62f489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:46 +0000

    Add package ss13-tools/2.0.2/ss13_tools-2.0.2.tar.gz

---
+++ packages/ss13-tools/ss13_tools-2.0.2.tar.gz/ss13_tools-2.0.2/ss13_tools/constants.py
@@ -0,0 +1,5 @@
+import sys
+
+from .__version__ import __version__
+
+USER_AGENT = f"ss13tools/{__version__} ({sys.platform}) Python {sys.version}"
---
+++ packages/ss13-tools/ss13_tools-2.0.2.tar.gz/ss13_tools-2.0.2/ss13_tools/slur_detector/__init__.py
@@ -0,0 +1,10 @@
+from colorama import init as colorama_init
+
+from .slur_detector import SlurDetector
+from .__main__ import main  # noqa: F401
+
+colorama_init()
+
+__all__ = [
+    'SlurDetector'
+]
