b49160883fd406184da29acb7b35ef9a6d98c41d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:39 +0000

    Add package nlecloud-framework/0.0.2/nlecloud_framework-0.0.2.tar.gz

---
+++ packages/nlecloud-framework/nlecloud_framework-0.0.2.tar.gz/nlecloud_framework-0.0.2/nlecloud_framework/other.py
@@ -0,0 +1,20 @@
+# _*_ coding:utf-8 _*_
+"""
+@File: other.py
+@Author: cfp
+@Date: 2020-08-21 14:07:08
+@LastEditTime: 2023/2/22 16:18
+@LastEditors: cfp
+@LastModifyTime @Version  @Desciption
+@Description: 
+"""
+
+def other():
+    """
+    @description:
+    @param args:
+    @return
+    @last_editors: cfp
+    """
+    print("other")
+
