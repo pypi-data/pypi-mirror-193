b940d8b5c4c6380ac246d2d916f4cde78cea5432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:16 +0000

    Add package distributions-jks/0.1/distributions_jks-0.1.tar.gz

---
+++ packages/distributions-jks/distributions_jks-0.1.tar.gz/distributions_jks-0.1/distributions_jks/__init__.py
@@ -0,0 +1,6 @@
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
+
+__version__ = '0.1'
+__author_ = 'Jaya Krishna with Udacity'
+__author__email_ = 'jayakrishnas.work@gmail.com'
