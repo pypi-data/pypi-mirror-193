2a01744c93ae55974d260bbb02ad08cf9d946069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:43 +0000

    Add package ark-api/1.0.1/ark-api-1.0.1.tar.gz

---
+++ packages/ark-api/ark-api-1.0.1.tar.gz/ark-api-1.0.1/src/ark/buffs/buffs.py
@@ -0,0 +1,6 @@
+from .buff import Buff
+
+HUNGRY = Buff("Hungry", "templates/hungry.png")
+THIRSTY = Buff("Thirsty", "templates/thirsty.png")
+POD_XP = Buff("XP Buff", "templates/pod_buff.png")
+BROKEN_BONES = Buff("Broken Bones", "templates/broken_bones.png")
