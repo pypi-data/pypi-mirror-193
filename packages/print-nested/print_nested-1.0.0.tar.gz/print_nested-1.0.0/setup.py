d688d03ae9714d1b45e9a247736c66b84ac352f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:01 +0000

    Add package print-nested/1.0.0/print_nested-1.0.0.tar.gz

---
+++ packages/print-nested/print_nested-1.0.0.tar.gz/print_nested-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+#\Python_coding\nester>python3 setup.py sdist
+setup(
+        name = 'print_nested',
+        version = '1.0.0',
+        py_modules = ['print_nested'],
+        author = 'Amit Kumar',
+        author_email = 'amit_roy@hotmail.com',
+        description = 'A simple list iterartor that iterates over a list of lists.',
+    )
