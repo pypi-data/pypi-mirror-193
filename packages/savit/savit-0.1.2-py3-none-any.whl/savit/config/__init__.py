cc8d2644e15339c243a63ca56e20ec51f0bc090b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:27 +0000

    Add package savit/0.1.2/savit-0.1.2-py3-none-any.whl

---
+++ packages/savit/savit-0.1.2-py3-none-any.whl/savit/config/__init__.py
@@ -0,0 +1,66 @@
+import os
+import tomllib
+from pathlib import Path
+
+import typer
+from click import Choice
+
+HISTORY_PATH = ""
+DOT_CONFIG_FILE = Path(typer.get_app_dir("savit"))
+CONFIG_FILE: Path = Path(DOT_CONFIG_FILE) / "config.toml"
+
+
+def _get_shell_history(home: Path, config_file_name: str) -> str:
+    return str(home) + config_file_name
+
+
+def set_env() -> None:
+    DOT_CONFIG_FILE.mkdir(parents=True, exist_ok=True)
+    CONFIG_FILE.touch(exist_ok=True)
+
+    shell = str(os.getenv("SHELL"))
+    home = Path.home()
+    if "zsh" in shell:
+        HISTORY_PATH = _get_shell_history(home, "/.zhistory")
+    elif "bash" in shell:
+        HISTORY_PATH = _get_shell_history(home, "/.bash_history")
+    elif "fish" in shell:
+        HISTORY_PATH = _get_shell_history(home, "/.local/share/fish/fish_history")
+
+    print(HISTORY_PATH)
+
+    if confirmation_prompt := typer.confirm("is this your history file?", default=True):
+        config_toml_string = f"""[savit]
+history_path = "{HISTORY_PATH}"
+"""
+        with CONFIG_FILE.open("w") as f:
+            f.write(config_toml_string)
+
+        print("Config file saved")
+
+    else:
+        HISTORY_PATH = input("Enter the path of your history file: ")
+        config_toml_string = f"""[savit]
+history_path = "{HISTORY_PATH}"
+"""
+        with CONFIG_FILE.open("w") as f:
+            f.write(config_toml_string)
+
+        print(f"{HISTORY_PATH} saved")
+
+    output_format = typer.prompt(
+        "Choose an output format", type=Choice(["txt", "md"]), show_choices=True
+    )
+    output_folder = input(
+        "Type the path to the folder where commands should be store (leave blank to save on current usage directory): "
+    )
+
+    with CONFIG_FILE.open("a") as f:
+        f.write(f'output_format = "{output_format}"\n')
+        if output_folder:
+            f.write(f'output_folder = "{output_folder}"\n')
+        else:
+            f.write('output_folder = "./"')
+
+    print(f"Options saved on {CONFIG_FILE}")
+    print("You can edit the config file later if you want to change the options")
---
+++ packages/savit/savit-0.1.2-py3-none-any.whl/savit/main.py
@@ -0,0 +1,118 @@
+import tomllib
+from pathlib import Path
+from typing import Optional
+
+import typer
+
+from savit.config import CONFIG_FILE, set_env
+
+app = typer.Typer(help="Helping you to write docs by saving your commands")
+
+
+@app.command()
+def config(
+    open_file: bool = typer.Option(False, "--open-file", help="Opens the config file")
+) -> None:
+    """
+    Saves your configurations to ~/.config/savit/config.toml
+    """
+    if open_file:
+        typer.launch(str(CONFIG_FILE), locate=True)
+        raise typer.Exit()
+    if CONFIG_FILE.exists():
+        print("Config file already exists")
+        raise typer.Exit()
+
+    set_env()
+
+
+@app.command()
+def start() -> None:
+    """
+    Start saving your commands
+    """
+
+    if not CONFIG_FILE.exists():
+        set_env()
+
+    saving = typer.style("saving", fg=typer.colors.GREEN)
+    savit_stop = typer.style("savit stop", fg=typer.colors.BLUE)
+    typer.echo(
+        f"""
+               Started {saving}!
+               Use your commands and when you are done use {savit_stop} 
+               Don't erase your shell history file!
+               """
+    )
+
+
+@app.command()
+def stop(
+    txt: bool = typer.Option(False, "--txt", help="Saves your commands to a .txt file"),
+    md: bool = typer.Option(False, "--md", help="Saves your commands to a .md file"),
+    file: Optional[Path] = typer.Option(
+        None, help="File (may include path) to save your commands"
+    ),
+) -> None:
+    """
+    Stop saving your commands and writes them to a file
+    """
+    try:
+        with CONFIG_FILE.open("rb") as f:
+            config_toml = tomllib.load(f)
+            hist_path = config_toml["savit"]["history_path"]
+
+        with open(hist_path, "r") as hist:
+            hist_list = hist.readlines()
+            start_index = len(hist_list) - 1 - hist_list[::-1].index("savit start\n")
+            saved_commands = hist_list[start_index + 1 : -1]
+
+        output_format = config_toml["savit"]["output_format"]
+        if file is None:
+            if txt:
+                output_format = "txt"
+            elif md:
+                output_format = "md"
+            else:
+                output_format = config_toml["savit"]["output_format"]
+
+            output_folder = config_toml["savit"]["output_folder"]
+            output_file = str(Path(output_folder) / f"commands.{output_format}")
+        elif txt or md:
+            typer.secho(
+                "You can't use --txt or --md with --file. Use only one of them",
+                fg=typer.colors.RED,
+                err=True,
+            )
+            raise typer.Exit()
+
+        elif not file.is_absolute():
+            output_folder = config_toml["savit"]["output_folder"]
+            output_file = str(Path(output_folder) / file)
+
+        else:
+            output_file = str(file)
+
+        with open(output_file, "w") as commands:
+            for command in saved_commands:
+                if output_format == "txt" or output_file.endswith(".txt"):
+                    commands.write(f"{command}")
+                elif output_format == "md" or output_file.endswith(".md"):
+                    commands.write(f"```console\n{command}```\n\n")
+            print("Commands saved")
+    except FileNotFoundError as e:
+        typer.echo(f"File not found: {e}", err=True)
+    except Exception as e:
+        typer.echo(f"Error: {e}", err=True)
+
+
+@app.callback()
+def main() -> None:
+    """
+    Save your commands
+    """
+    ...
+
+
+if __name__ == "__main__":
+    app()
