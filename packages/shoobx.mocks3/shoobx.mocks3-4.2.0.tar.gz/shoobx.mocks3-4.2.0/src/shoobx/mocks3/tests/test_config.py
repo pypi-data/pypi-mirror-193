b91429f5eab2934d6ede3725d86275de0c28e7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:40 +0000

    Add package shoobx.mocks3/4.2.0/shoobx.mocks3-4.2.0.tar.gz

---
+++ packages/shoobx.mocks3/shoobx.mocks3-4.2.0.tar.gz/shoobx.mocks3-4.2.0/src/shoobx/mocks3/tests/test_config.py
@@ -0,0 +1,36 @@
+###############################################################################
+#
+# Copyright 2018 by Shoobx, Inc.
+#
+###############################################################################
+"""Shoobx S3 Config Test
+"""
+
+import os
+import shutil
+import tempfile
+import unittest
+
+from shoobx.mocks3 import config
+
+TEST_CONFIG = """
+[shoobx:mocks3]
+log-level = INFO
+directory = %s
+hostname = localhost
+"""
+
+
+class MockS3ConfigTests(unittest.TestCase):
+    def setUp(self):
+        self._dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self._dir)
+
+    def test_configure(self):
+        config_path = os.path.join(self._dir, "config.ini")
+        with open(config_path, "w") as file:
+            file.write(TEST_CONFIG % self._dir)
+        app = config.configure(config_path)
+        self.assertEqual("s3-sbx", app.service)
