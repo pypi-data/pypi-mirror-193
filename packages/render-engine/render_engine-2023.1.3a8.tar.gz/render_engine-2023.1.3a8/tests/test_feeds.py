5521f330a99902f92f062b6531c1830b0eec055c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:39 +0000

    Add package render-engine/2023.1.3a8/render_engine-2023.1.3a8.tar.gz

---
+++ packages/render-engine/render_engine-2023.1.3a8.tar.gz/render_engine-2023.1.3a8/tests/test_feeds.py
@@ -0,0 +1,50 @@
+import pluggy
+import pytest
+
+from render_engine.collection import Collection
+from render_engine.feeds import RSSFeed
+from render_engine.page import Page
+from render_engine.parsers.markdown import MarkdownPageParser
+
+pm = pluggy.PluginManager("fake_test")
+
+
+def test_can_manually_set_slug():
+    """Test that the feed slug can be set manually"""
+
+    class feed(RSSFeed):
+        pages = []
+        slug = "test-feed-slug"
+
+    assert feed().slug == "test-feed-slug"
+
+
+def test_rss_feed_title_from_collection():
+    """Test that the feed title is set from the collection"""
+
+    class TestCollection(Collection):
+        feed_title = "Test Feed Title"
+        Feed = RSSFeed
+        pages = [Page()]
+
+    collection = TestCollection()
+
+    assert collection._feed.title == "Test Feed Title"
+
+
+def test_rss_feed_inherites_from_collection(tmp_path):
+    """Test that the feed title is set from the collection"""
+
+    tmp_dir = tmp_path / "content"
+    tmp_dir.mkdir()
+    file = tmp_dir / "test.md"
+    file.write_text("test")
+
+    class BasicCollection(Collection):
+        content_path = tmp_dir.absolute()
+        archive_template = None
+        Feed = RSSFeed
+
+    collection = BasicCollection()
+
+    assert collection._feed.title == "BasicCollection"
