affc300c08fa3e97e1fab7dd083b5cf108a83ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:34 +0000

    Add package psi4-step/2023.2.21/psi4_step-2023.2.21-py2.py3-none-any.whl

---
+++ packages/psi4-step/psi4_step-2023.2.21-py2.py3-none-any.whl/psi4_step/__init__.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+"""
+psi4_step
+A step for Psi4 in a SEAMM flowchart
+"""
+
+# Bring up the classes so that they appear to be directly in
+# the psi4_step package.
+
+from psi4_step.psi4_metadata import methods  # noqa: F401
+from psi4_step.psi4_metadata import dft_functionals  # noqa: F401
+from psi4_step.psi4_metadata import optimization_methods  # noqa: F401
+from psi4_step.psi4_metadata import optimization_convergence  # noqa: F401
+from psi4_step.psi4_metadata import metadata  # noqa: F401
+
+from psi4_step.psi4 import Psi4  # noqa: F401
+from psi4_step.psi4_parameters import Psi4Parameters  # noqa: F401
+from psi4_step.psi4_step import Psi4Step  # noqa: F401
+from psi4_step.tk_psi4 import TkPsi4  # noqa: F401
+
+from psi4_step.initialization import Initialization  # noqa: F401
+from psi4_step.initialization_parameters import InitializationParameters  # noqa: F401
+from psi4_step.initialization_step import InitializationStep  # noqa: F401
+from psi4_step.tk_initialization import TkInitialization  # noqa: F401
+
+from psi4_step.energy import Energy  # noqa: F401
+from psi4_step.energy_parameters import EnergyParameters  # noqa: F401
+from psi4_step.energy_step import EnergyStep  # noqa: F401
+from psi4_step.tk_energy import TkEnergy  # noqa: F401
+
+from psi4_step.optimization import Optimization  # noqa: F401
+from psi4_step.optimization_parameters import OptimizationParameters  # noqa: F401
+from psi4_step.optimization_step import OptimizationStep  # noqa: F401
+from psi4_step.tk_optimization import TkOptimization  # noqa: F401
+
+# from psi4_step.accelerated_optimization import AcceleratedOptimization  # noqa: F401
+# from psi4_step.accelerated_optimization_parameters import (  # noqa: F401
+#     AcceleratedOptimizationParameters,
+# )
+# from psi4_step.accelerated_optimization_step import (  # noqa: F401
+#     AcceleratedOptimizationStep,
+# )
+# from psi4_step.tk_accelerated_optimization import (  # noqa: F401
+#     TkAcceleratedOptimization,
+# )
+
+# Handle versioneer
+from ._version import get_versions
+
+__author__ = """Paul Saxe"""
+__email__ = "psaxe@molssi.org"
+versions = get_versions()
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
+del get_versions, versions
