8d91c1d689fbf2d6a87d0007efaaa632f75a3700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:09 +0000

    Add package log21/2.4.2/log21-2.4.2.tar.gz

---
+++ packages/log21/log21-2.4.2.tar.gz/log21-2.4.2/log21/Formatters.py
@@ -0,0 +1,232 @@
+# log21.Formatters.py
+# CodeWriter21
+
+import time as _time
+from logging import Formatter as __Formatter
+from typing import Dict as _Dict, Tuple as _Tuple
+from log21.Colors import get_colors as _gc, ansi_escape
+from log21.Levels import INPUT, CRITICAL, ERROR, WARNING, INFO, DEBUG, PRINT
+
+__all__ = ['ColorizingFormatter', 'DecolorizingFormatter']
+
+
+class _Formatter(__Formatter):
+    level_names: _Dict[int, str] = {
+        DEBUG: 'DEBUG',
+        INFO: 'INFO',
+        WARNING: 'WARNING',
+        ERROR: 'ERROR',
+        CRITICAL: 'CRITICAL',
+        PRINT: 'PRINT',
+        INPUT: 'INPUT'
+    }
+
+    def __init__(self, fmt: str = None, datefmt: str = None, style: str = '%', level_names: _Dict[int, str] = None):
+        """
+        `level_names` usage:
+        >>> import log21
+        >>> logger = log21.Logger('MyLogger', log21.DEBUG)
+        >>> stream_handler = log21.ColorizingStreamHandler()
+        >>> formatter = log21.ColorizingFormatter(fmt='[%(levelname)s] %(message)s',
+        ...             level_names={log21.DEBUG: ' ', log21.INFO: '+', log21.WARNING: '-', log21.ERROR: '!',
+        ...                          log21.CRITICAL: 'X'})
+        >>> stream_handler.setFormatter(formatter)
+        >>> logger.addHandler(stream_handler)
+        >>>
+        >>> logger.debug('Just wanna see if this works...')
+        [ ] Just wanna see if this works...
+        >>> logger.info("FYI: I'm glad somebody read this 8)")
+        [+] FYI: I'm glad somebody read this 8)
+        >>> logger.warning("Oh no! Something's gonna happen!")
+        [-] Oh no! something's gonna happen!
+        >>> logger.error('AN ERROR OCCURRED! (told ya ;))')
+        [!] AN ERROR OCCURRED! (told ya ;))
+        >>> logger.critical('Crashed....')
+        [X] Crashed....
+        >>>
+        >>> # Hope you've enjoyed
+        >>>
+        """
+        super().__init__(fmt=fmt, datefmt=datefmt, style=style)
+        if level_names:
+            for level, name in level_names.items():
+                self.level_names[level] = name
+
+    def format(self, record) -> str:
+        record.message = record.getMessage()
+        if self.usesTime():
+            record.asctime = self.formatTime(record, self.datefmt)
+
+        record.levelname = self.level_names.get(record.levelno, 'NOTSET')
+
+        s = self.formatMessage(record)
+        if record.exc_info:
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            s = s + record.exc_text
+        if record.stack_info:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            s = s + self.formatStack(record.stack_info)
+        return s
+
+
+class ColorizingFormatter(_Formatter):
+    # Default color values
+    level_colors: _Dict[int, _Tuple[str, ...]] = {
+        DEBUG: ('lightblue',),
+        INFO: ('green',),
+        WARNING: ('lightyellow',),
+        ERROR: ('light red',),
+        CRITICAL: ('background red', 'white'),
+        PRINT: ('Cyan',),
+        INPUT: ('Magenta',)
+    }
+    time_color: _Tuple[str, ...] = ('lightblue',)
+    name_color = pathname_color = filename_color = module_color = func_name_color = thread_name_color = \
+        message_color = tuple()
+
+    def __init__(self, fmt: str = None, datefmt: str = None, style: str = '%', level_names: _Dict[int, str] = None,
+                 level_colors: _Dict[int, _Tuple[str]] = None,
+                 time_color: _Tuple[str, ...] = None, name_color: _Tuple[str, ...] = None,
+                 pathname_color: _Tuple[str, ...] = None, filename_color: _Tuple[str, ...] = None,
+                 module_color: _Tuple[str, ...] = None, func_name_color: _Tuple[str, ...] = None,
+                 thread_name_color: _Tuple[str, ...] = None, message_color: _Tuple[str, ...] = None):
+        super().__init__(fmt=fmt, datefmt=datefmt, style=style, level_names=level_names)
+        # Checks and sets colors
+        if level_colors:
+            if type(level_colors) is not dict:
+                raise TypeError('`level_colors` must be a dictionary!')
+            for level, color in level_colors.items():
+                self.level_colors[level] = (_gc(*color),)
+        if time_color:
+            if type(time_color) is not tuple:
+                raise TypeError('`time_color` must be a tuple!')
+            self.time_color = time_color
+        if name_color:
+            if type(name_color) is not tuple:
+                raise TypeError('`name_color` must be a tuple!')
+            self.name_color = name_color
+        if pathname_color:
+            if type(pathname_color) is not tuple:
+                raise TypeError('`pathname_color` must be a tuple!')
+            self.pathname_color = pathname_color
+        if filename_color:
+            if type(filename_color) is not tuple:
+                raise TypeError('`filename_color` must be a tuple!')
+            self.filename_color = filename_color
+        if module_color:
+            if type(module_color) is not tuple:
+                raise TypeError('`module_color` must be a tuple!')
+            self.module_color = module_color
+        if func_name_color:
+            if type(func_name_color) is not tuple:
+                raise TypeError('`func_name_color` must be a tuple!')
+            self.func_name_color = func_name_color
+        if thread_name_color:
+            if type(thread_name_color) is not tuple:
+                raise TypeError('`thread_name_color` must be a tuple!')
+            self.thread_name_color = thread_name_color
+        if message_color:
+            if type(message_color) is not tuple:
+                raise TypeError('`message_color` must be a tuple!')
+            self.message_color = message_color
+
+    def format(self, record) -> str:
+        """
+        Colorizes the record and returns the formatted message.
+
+        :param record:
+        :return: str
+        """
+        record.message = record.getMessage()
+        if self.usesTime():
+            record.asctime = self.formatTime(record, self.datefmt)
+
+        record.levelname = self.level_names.get(record.levelno, 'NOTSET')
+
+        record = self.colorize(record)
+
+        s = self.formatMessage(record)
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            s = s + record.exc_text
+        if record.stack_info:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            s = s + self.formatStack(record.stack_info)
+        return s
+
+    def colorize(self, record):
+        """
+        Colorizes the record attributes.
+
+        :param record:
+        :return: colorized record
+        """
+        reset = '\033[0m'
+
+        if hasattr(record, 'asctime'):
+            record.asctime = _gc(*self.time_color) + record.asctime + reset
+        if hasattr(record, 'levelno'):
+            record.levelname = _gc(*self.level_colors.get(int(record.levelno), ('lw',))) + \
+                               getattr(record, 'levelname', 'NOTSET') + reset
+        if hasattr(record, 'name'):
+            record.name = _gc(*self.name_color) + str(record.name) + reset
+        if hasattr(record, 'pathname'):
+            record.pathname = _gc(*self.pathname_color) + record.pathname + reset
+        if hasattr(record, 'filename'):
+            record.filename = _gc(*self.filename_color) + record.filename + reset
+        if hasattr(record, 'module'):
+            record.module = _gc(*self.module_color) + record.module + reset
+        if hasattr(record, 'funcName'):
+            record.funcName = _gc(*self.func_name_color) + record.funcName + reset
+        if hasattr(record, 'threadName'):
+            record.threadName = _gc(*self.thread_name_color) + record.threadName + reset
+        if hasattr(record, 'message'):
+            record.message = _gc(*self.message_color) + record.message
+
+        return record
+
+
+class DecolorizingFormatter(_Formatter):
+    def formatTime(self, record, datefmt=None):
+        """
+        Returns the creation time of the specified LogRecord as formatted text.
+        """
+        ct = self.converter(int(record.created))
+        if datefmt:
+            s = _time.strftime(datefmt, ct)
+        else:
+            t = _time.strftime(self.default_time_format, ct)
+            s = self.default_msec_format % (t, record.msecs)
+        return s
+
+    def format(self, record) -> str:
+        """
+        Decolorizes the record and returns the formatted message.
+
+        :param record:
+        :return: str
+        """
+        return self.decolorize(super().format(record))
+
+    @staticmethod
+    def decolorize(text: str):
+        """
+        Removes all ansi colors in the text.
+
+        :param text: str: Input text
+        :return: str: decolorized text
+        """
+
+        return ansi_escape.sub('', text)
---
+++ packages/log21/log21-2.4.2.tar.gz/log21-2.4.2/log21/Logger.py
@@ -0,0 +1,204 @@
+# log21.Logger.py
+# CodeWriter21
+
+import logging as _logging
+
+from getpass import getpass as _getpass
+from typing import Sequence as _Sequence, Union as _Union
+from logging import raiseExceptions as _raiseExceptions
+
+import log21 as _log21
+from log21.Levels import CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET, PRINT, INPUT
+
+__all__ = ['Logger']
+
+
+class Logger(_logging.Logger):
+    def __init__(self, name, level=NOTSET, handlers: _Union[_Sequence[_logging.Handler], _logging.Handler] = None):
+        super().__init__(name, level)
+        self.setLevel(level)
+        self._progress_bar = None
+        if handlers:
+            if not isinstance(handlers, _Sequence):
+                if isinstance(handlers, _logging.Handler):
+                    handlers = [handlers]
+                else:
+                    raise TypeError("handlers must be a list of logging.Handler objects")
+            for handler in handlers:
+                self.addHandler(handler)
+
+    def isEnabledFor(self, level):
+        """
+        Is this logger enabled for level 'level'?
+        """
+
+        return (self.level <= level) or (level in (PRINT, INPUT))
+
+    def log(self, level: int, *msg, args: tuple = (), end='\n', **kwargs):
+        """
+        Log 'msg % args' with the integer severity 'level'.
+
+        To pass exception information, use the keyword argument exc_info with
+        a true value, e.g.
+
+        logger.log(level, "We have a %s", args=("mysterious problem",), exc_info=1)
+        """
+        msg = ' '.join([str(m) for m in msg]) + end
+        if not isinstance(level, int):
+            if _raiseExceptions:
+                raise TypeError("level must be an integer")
+            else:
+                return
+        if self.isEnabledFor(level):
+            self._log(level, msg, args, **kwargs)
+
+    def debug(self, *msg, args: tuple = (), end='\n', **kwargs):
+        """
+        Log 'msg % args' with severity 'DEBUG'.
+
+        To pass exception information, use the keyword argument exc_info with
+        a true value, e.g.
+
+        logger.debug("Houston, we have a %s", args=("thorny problem",), exc_info=1)
+        """
+        if self.isEnabledFor(DEBUG):
+            msg = ' '.join([str(m) for m in msg]) + end
+            self._log(DEBUG, msg, args, **kwargs)
+
+    def info(self, *msg, args: tuple = (), end='\n', **kwargs):
+        """
+        Log 'msg % args' with severity 'INFO'.
+
+        To pass exception information, use the keyword argument exc_info with
+        a true value, e.g.
+
+        logger.info("Houston, we have an %s", args=("interesting problem",), exc_info=1)
+        """
+        if self.isEnabledFor(INFO):
+            msg = ' '.join([str(m) for m in msg]) + end
+            self._log(INFO, msg, args, **kwargs)
+
+    def warning(self, *msg, args: tuple = (), end='\n', **kwargs):
+        """
+        Log 'msg % args' with severity 'WARNING'.
+
+        To pass exception information, use the keyword argument exc_info with
+        a true value, e.g.
+
+        logger.warning("Houston, we have a %s", args=("bit of a problem",), exc_info=1)
+        """
+        if self.isEnabledFor(WARNING):
+            msg = ' '.join([str(m) for m in msg]) + end
+            self._log(WARNING, msg, args, **kwargs)
+
+    warn = warning
+
+    def write(self, *msg, args: tuple = (), end='', **kwargs):
+        """
+        Log 'msg % args' with severity 'WARNING'.
+
+        To pass exception information, use the keyword argument exc_info with
+        a true value, e.g.
+
+        logger.write("Houston, we have a %s", args=("bit of a problem",), exc_info=1)
+        """
+        if self.isEnabledFor(WARNING):
+            msg = ' '.join([str(m) for m in msg]) + end
+            self._log(WARNING, msg, args, **kwargs)
+
+    def error(self, *msg, args: tuple = (), end='\n', **kwargs):
+        """
+        Log 'msg % args' with severity 'ERROR'.
+
+        To pass exception information, use the keyword argument exc_info with
+        a true value, e.g.
+
+        logger.error("Houston, we have a %s", args=("major problem",), exc_info=1)
+        """
+        if self.isEnabledFor(ERROR):
+            msg = ' '.join([str(m) for m in msg]) + end
+            self._log(ERROR, msg, args, **kwargs)
+
+    def exception(self, *msg, args: tuple = (), exc_info=True, **kwargs):
+        """
+        Convenience method for logging an ERROR with exception information.
+        """
+        self.error(*msg, args=args, exc_info=exc_info, **kwargs)
+
+    def critical(self, *msg, args: tuple = (), end='\n', **kwargs):
+        """
+        Log 'msg % args' with severity 'CRITICAL'.
+
+        To pass exception information, use the keyword argument exc_info with
+        a true value, e.g.
+
+        logger.critical("Houston, we have a %s", args=("major disaster",), exc_info=1)
+        """
+        if self.isEnabledFor(CRITICAL):
+            msg = ' '.join([str(m) for m in msg]) + end
+            self._log(CRITICAL, msg, args, **kwargs)
+
+    fatal = critical
+
+    def print(self, *msg, args: tuple = (), end='\n', **kwargs):
+        """
+        Log 'msg % args'.
+
+        To pass exception information, use the keyword argument exc_info with
+        a true value, e.g.
+
+        logger.print("Houston, we have a %s", args=("major disaster",), exc_info=1)
+        """
+        msg = ' '.join([str(m) for m in msg]) + end
+        self._log(PRINT, msg, args, **kwargs)
+
+    def input(self, *msg, args: tuple = (), end='', **kwargs):
+        """
+        Log 'msg % args'.
+
+        To pass exception information, use the keyword argument exc_info with
+        a true value, e.g.
+
+        age = logger.input("Enter your age: ")
+        """
+        msg = ' '.join([str(m) for m in msg]) + end
+        self._log(INPUT, msg, args, **kwargs)
+        return input()
+
+    def getpass(self, *msg, args: tuple = (), end='', **kwargs):
+        """
+
+
+        :return:
+        """
+        msg = ' '.join([str(m) for m in msg]) + end
+        self._log(self.level if self.level >= NOTSET else NOTSET, msg, args, **kwargs)
+        return _getpass('')
+
+    def print_progress(self, progress: float, total: float, **kwargs):
+        """
+        Log progress.
+        """
+        self.progress_bar(progress, total, **kwargs)
+
+    @property
+    def progress_bar(self):
+        if not self._progress_bar:
+            from log21.ProgressBar import ProgressBar
+            self._progress_bar = ProgressBar(logger=self)
+        return self._progress_bar
+
+    @progress_bar.setter
+    def progress_bar(self, value: '_log21.ProgressBar'):
+        self._progress_bar = value
+
+    def clear_line(self, length: int = None):
+        """
+        Clear the current line.
+
+        :param length: The length of the line to clear.
+        :return:
+        """
+        for handler in self.handlers:
+            if hasattr(handler, 'clear_line'):
+                handler.clear_line(length)
