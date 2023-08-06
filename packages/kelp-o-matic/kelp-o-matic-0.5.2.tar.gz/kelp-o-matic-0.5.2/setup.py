15d5b768b54aa2203ae2acfdb6746525ca40d339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:29 +0000

    Add package kelp-o-matic/0.5.2/kelp-o-matic-0.5.2.tar.gz

---
+++ packages/kelp-o-matic/kelp-o-matic-0.5.2.tar.gz/kelp-o-matic-0.5.2/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+PACKAGE_NAME = 'kelp-o-matic'
+VERSION = '0.5.2'
+
+packages = [
+    'kelp_o_matic',
+    'kelp_o_matic.data',
+    'kelp_o_matic.geotiff_io'
+]
+
+package_data = {
+    '': ['*']
+}
+
+install_requires = [
+    'typer~=0.4',
+    'numpy~=1.16',
+    'torch~=1.10',
+    'rasterio~=1.2',
+    'torchvision~=0.11',
+    'tqdm~=4.62'
+]
+
+entry_points = {
+    'console_scripts': ['kom = kelp_o_matic.cli:cli']
+}
+
+setup_kwargs = {
+    'name': PACKAGE_NAME,
+    'version': VERSION,
+    'description': 'Segmentation Tools for Remotely Sensed RPAS Imagery',
+    'author': 'Taylor Denouden',
+    'author_email': 'taylordenouden@gmail.com',
+    'url': 'https://github.com/HakaiInstitute/kelp-o-matic',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '~=3.7,<3.11',
+}
+
+setup(**setup_kwargs)
