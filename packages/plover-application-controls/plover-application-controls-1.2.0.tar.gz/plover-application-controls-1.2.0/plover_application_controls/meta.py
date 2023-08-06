e15e7ca62fd8b74c697f8960bc12bc48813b2ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:25 +0000

    Add package plover-application-controls/1.2.0/plover-application-controls-1.2.0.tar.gz

---
+++ packages/plover-application-controls/plover-application-controls-1.2.0.tar.gz/plover-application-controls-1.2.0/plover_application_controls/meta.py
@@ -0,0 +1,13 @@
+from plover.formatting import _Context, _Action
+
+from plover_application_controls.window.tracker import WindowTracker
+
+
+def meta(ctx: _Context, cmdline: str) -> _Action:
+    action = ctx.new_action()
+    action.text = ":".join(map(get_property, cmdline.split(":")))
+    return action
+
+
+def get_property(prop: str) -> str:
+    return WindowTracker.current_window_details.get_property(prop)
