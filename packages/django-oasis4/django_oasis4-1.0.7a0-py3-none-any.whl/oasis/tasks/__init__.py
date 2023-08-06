44c210a4da78bfb3c979483e03c98399dc908148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:12 +0000

    Add package django-oasis4/1.0.7a0/django_oasis4-1.0.7a0-py3-none-any.whl

---
+++ packages/django-oasis4/django_oasis4-1.0.7a0-py3-none-any.whl/oasis/tasks/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+#  Developed by CQ Inversiones SAS. Copyright ©. 2019 - 2022. All rights reserved.
+#  Desarrollado por CQ Inversiones SAS. Copyright ©. 2019 - 2022. Todos los derechos reservado
+
+# ****************************************************************
+# IDE:          PyCharm
+# Developed by: macercha
+# Date:         10/12/22 3:52 PM
+# Project:      CFHL Transactional Backend
+# Module Name:  __init__.py
+# Description:
+# ****************************************************************
+from .sync_company import sync_company
+from .sync_document_types import sync_document_types
+from .sync_products import sync_products
+
+__all__ = [
+    "sync_products",
+    "sync_company",
+    "sync_document_types"
+]
