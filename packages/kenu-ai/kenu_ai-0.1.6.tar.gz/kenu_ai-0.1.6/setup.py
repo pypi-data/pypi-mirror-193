43217b6d40fa6ddd73cb55a635282af789e057ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:32 +0000

    Add package kenu-ai/0.1.6/kenu_ai-0.1.6.tar.gz

---
+++ packages/kenu-ai/kenu_ai-0.1.6.tar.gz/kenu_ai-0.1.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name="kenu_ai",
+	version="0.1.6",
+	description="Python Chat AI",
+	author="r4isy",
+	author_email="r4isy@kenucheck.xyz",
+	packages=["kenu_ai"],
+	install_requires=[
+	'googletrans==4.0.0-rc1',
+	'requests'
+	]
+	)
