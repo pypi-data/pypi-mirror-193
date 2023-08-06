a74f98534e90bb768d0af987fe7fcd036c2b3af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:05 +0000

    Add package mkdocs-obsidian-support-plugin/0.3.5/mkdocs-obsidian-support-plugin-0.3.5.tar.gz

---
+++ packages/mkdocs-obsidian-support-plugin/mkdocs-obsidian-support-plugin-0.3.5.tar.gz/mkdocs-obsidian-support-plugin-0.3.5/obsidian_support/plugin.py
@@ -0,0 +1,25 @@
+from mkdocs.plugins import BasePlugin
+
+from obsidian_support.conversion.admonition import AdmonitionConvert
+from obsidian_support.conversion.image_link import ImageLinkConvert
+from obsidian_support.markdown_code_extract import get_code_indices, EXCLUDE_RANGES
+from obsidian_support.markdown_convert import markdown_convert
+
+"""
+A mkdocs plugin that support conversion from 
+'obsidian syntax' to 'mkdocs-material syntax'
+"""
+
+
+class ObsidianSupportPlugin(BasePlugin):
+
+    def on_page_markdown(self, markdown, page, config, files):
+        ## apply conversions
+        markdown = markdown_convert(markdown, AdmonitionConvert(), get_excluded_indices(markdown))
+        markdown = markdown_convert(markdown, ImageLinkConvert(), get_excluded_indices(markdown))
+
+        return markdown
+
+
+def get_excluded_indices(markdown: str) -> EXCLUDE_RANGES:
+    return get_code_indices(markdown)
