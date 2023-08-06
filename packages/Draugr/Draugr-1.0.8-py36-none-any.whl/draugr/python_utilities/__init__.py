f2a0693946710d41b96ac458da09f16af84d0a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:47 +0000

    Add package Draugr/1.0.8/Draugr-1.0.8-py36-none-any.whl

---
+++ packages/Draugr/Draugr-1.0.8-py36-none-any.whl/draugr/python_utilities/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Christian Heider Nielsen"
+__doc__ = r"""
+
+           Created on 14/01/2020
+           """
+
+# with open(Path(__file__).parent / "README.md", "r") as this_init_file:
+#    __doc__ += this_init_file.read()  # .replace("#", "")  # .encode("ascii", "ignore")
+
+
+from .function_wrappers import *
+from .generators import *
+from .http import *
+from .sockets import *
+from .torch_like_channel_transformation import *
+from .counter_filter import *
+from .matrix import *
+from .iterators import *
+from .display import *
+from .platform_context import *
---
+++ packages/Draugr/Draugr-1.0.8-py36-none-any.whl/draugr/tensorboard_utilities/exporting/file_export.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Christian Heider Nielsen"
+__doc__ = r"""
+
+            TODO: Finish this off, only a placeholder for now
+
+           Created on 17-03-2021
+           """
+
+from draugr.tensorboard_utilities.exporting.event_export import TensorboardEventExporter
+from typing import Sequence, MutableMapping
+
+
+class TensorboardEventExporterFile(TensorboardEventExporter):
+    def __init__(self, *args: Sequence, **kwargs: MutableMapping):
+        super().__init__(*args, **kwargs)
+        self.file = kwargs.get("file")
+        if self.file is not None:
+            self.file.write("")
+        else:
+            raise ValueError("No file provided")
+
+    def export(self):
+        self.file.write("")
+        super().export()
+
+    def close(self):
+        self.file.close()
