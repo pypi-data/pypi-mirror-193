0b54e75e583b73bd8555f6501f13ba3c97bc745e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:54 +0000

    Add package mojito2/0.1.6/mojito2-0.1.6.tar.gz

---
+++ packages/mojito2/mojito2-0.1.6.tar.gz/mojito2-0.1.6/setup.py
@@ -0,0 +1,31 @@
+"""setup file for pypi
+"""
+import setuptools
+
+install_requires = [
+    'requests',
+    'websockets',
+    'pycryptodome'
+]
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='mojito2',
+    version='0.1.6',
+    author='Jonghun Yoo, Brayden Jo',
+    author_email='jonghun.yoo@pyquant.co.kr, brayden.jo@pyquant.co.kr',
+    description="python wrapper for korea broker's REST API services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/sharebook-kr/mojito',
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
