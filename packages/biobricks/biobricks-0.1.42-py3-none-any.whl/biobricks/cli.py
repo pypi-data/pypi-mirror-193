c9101d37cc2b9936a4a40f27d91c8221c375035d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:24 +0000

    Add package biobricks/0.1.42/biobricks-0.1.42-py3-none-any.whl

---
+++ packages/biobricks/biobricks-0.1.42-py3-none-any.whl/biobricks/cli.py
@@ -0,0 +1,73 @@
+import sys
+import os
+import biobricks as bb
+import click
+from logger import logger
+from pathlib import Path
+from .config import read_config, write_config, init_bblib
+from .checks import check_token
+from .brick import Brick
+
+@click.group()
+def cli():
+    pass
+
+@cli.command(help="configure biobricks with a token and filesystem path")
+def configure():
+
+    path = Path.home().joinpath(".biobricks")
+    config = {}
+
+    if path.exists():
+        config = read_config()
+        if not click.confirm(click.style("WARNING: overwrite existing config?", fg="red")): sys.exit(0)
+    
+    # get bblib (default to existing bblib)
+    if config.keys() >= {"BBLIB"} and click.confirm(f"use current BBLIB '{config['BBLIB']}'?", default=True):
+        bblib = config["BBLIB"]
+    else:
+        bblib = click.prompt("Choose path to store bricks", type=click.Path())
+
+    # initialize credentials (default to existing token)
+    deftoken = "skip to use free token"
+    if config.keys() >= {"TOKEN"} and click.confirm(f"use current token (see {path})?"):
+        token = config["TOKEN"]
+    else:
+        token = click.prompt("what is your biobricks.ai/token?", hide_input=True, default = deftoken)
+    
+    while token != deftoken and not check_token(token, silent=True):
+        click.echo(click.style("invalid token. check your token at https://biobricks.ai/token", fg="red"))
+        token = click.prompt("what is your biobricks.ai token?",hide_input=True, default=deftoken)
+    token = "VQF6Q2U-NKktZ31ioVYa9w" if token == deftoken else token
+    click.echo()
+
+    # write configuration
+    config = { "BBLIB": bblib, "TOKEN": token }
+    write_config(config)
+
+    # initialize bblib
+    bblib = init_bblib()
+
+    msg = f"Done! BioBricks has BBLIB {bblib} and config {path}"
+    click.echo(click.style(msg, fg="green"))
+
+
+@cli.command(help="Initialize a .bb directory for data dependencies")
+def init():
+    bb.bb_init()
+
+@cli.command(name="import",help="Import a data dependency into the .bb directory")
+def import_(brick,org="biobricks-ai",location=".bb"):
+    bb.bb_import(brick,org,location)
+
+@cli.command(help="Install a data dependency into $BBLIB")
+@click.argument("ref",type=str)
+def install(ref):
+    return Brick.Resolve(ref, force_remote=True).install()
+
+@cli.command(help="Show the status of BBLIB")
+def status():
+    print("BBLIB: " + str(bb.bblib()))
+
+if __name__ == "__main__":
+    cli()
