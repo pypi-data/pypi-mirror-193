a11e14c96b0bc3cf0d2a6424ea2e94e35d9baca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:21 +0000

    Add package lxlog/0.10.0/lxlog-0.10.0.tar.gz

---
+++ packages/lxlog/lxlog-0.10.0.tar.gz/lxlog-0.10.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="lxlog",
+    version="0.10.0",
+    author="selvaguru",
+    author_email="your_email@example.com",
+    long_description=long_description,
+    description="A package for analyzing system log files",
+    long_description_content_type="text/markdown",
+    url="https://selvaguru-s.web.app",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
