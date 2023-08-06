bae71c8c09a10ba58c02e093be298015cbb44b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:34 +0000

    Add package watchmen-indicator-surface/16.4.8/watchmen_indicator_surface-16.4.8.tar.gz

---
+++ packages/watchmen-indicator-surface/watchmen_indicator_surface-16.4.8.tar.gz/watchmen_indicator_surface-16.4.8/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['watchmen_indicator_surface',
+ 'watchmen_indicator_surface.data',
+ 'watchmen_indicator_surface.meta',
+ 'watchmen_indicator_surface.util']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['watchmen-indicator-kernel==16.4.8', 'watchmen-rest==16.4.8']
+
+extras_require = \
+{'mongodb': ['watchmen-storage-mongodb==16.4.8'],
+ 'mssql': ['watchmen-storage-mssql==16.4.8'],
+ 'mysql': ['watchmen-storage-mysql==16.4.8'],
+ 'oracle': ['watchmen-storage-oracle==16.4.8'],
+ 'postgresql': ['watchmen-storage-postgresql==16.4.8']}
+
+setup_kwargs = {
+    'name': 'watchmen-indicator-surface',
+    'version': '16.4.8',
+    'description': '',
+    'long_description': 'None',
+    'author': 'botlikes',
+    'author_email': '75356972+botlikes456@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
