342863b544369e92bf6a263a065de1d1cb7a288f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:22 +0000

    Add package xelpaste/3.3.3/xelpaste-3.3.3-py2.py3-none-any.whl

---
+++ packages/xelpaste/xelpaste-3.3.3-py2.py3-none-any.whl/libpaste/context_processors.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from libpaste.conf import settings
+
+
+def libpaste_settings(request):
+    return {
+        'LIBPASTE_SLUG_LENGTH': settings.LIBPASTE_SLUG_LENGTH,
+        'LIBPASTE_BASE_URL': settings.LIBPASTE_BASE_URL,
+        'LIBPASTE_SITENAME': settings.LIBPASTE_SITENAME,
+    }
---
+++ packages/xelpaste/xelpaste-3.3.3-py2.py3-none-any.whl/libpaste/management/commands/cleanup_snippets.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from django.utils import timezone
+from django.core.management.base import BaseCommand
+
+from ...models import Snippet
+
+class Command(BaseCommand):
+    help = "Purges snippets that are expired"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--dry-run', '-n',
+            action='store_true',
+            dest='dry_run',
+            help="Don't do anything",
+        )
+
+    def handle(self, *args, **options):
+        deleteable_snippets = Snippet.objects.filter(
+            expires__isnull=False,
+            expire_type=Snippet.EXPIRE_TIME,
+            expires__lte=timezone.now()
+        )
+        self.stdout.write(u"%s snippets gets deleted:\n" % deleteable_snippets.count())
+        for d in deleteable_snippets:
+            self.stdout.write(u"- %s (%s)\n" % (d.secret_id, d.expires))
+        if options.get('dry_run'):
+            self.stdout.write('Dry run - Not actually deleting snippets!\n')
+        else:
+            deleteable_snippets.delete()
