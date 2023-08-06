4b618d36e3ca225123c6597af3ec9888907a7716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:15 +0000

    Add package FImdlp/0.9.3/FImdlp-0.9.3-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/FImdlp/FImdlp-0.9.3-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/fimdlp/cfimdlp.pyx
@@ -0,0 +1,33 @@
+# distutils: language = c++
+# cython: language_level = 3
+from libcpp.vector cimport vector
+from libcpp.string cimport string
+
+cdef extern from "../cppmdlp/CPPFImdlp.h" namespace "mdlp":
+    ctypedef float precision_t
+    cdef cppclass CPPFImdlp:
+        CPPFImdlp() except + 
+        CPPFImdlp& fit(vector[precision_t]&, vector[int]&)
+        vector[precision_t] getCutPoints()
+        string version()
+        
+cdef class CFImdlp:
+    cdef CPPFImdlp *thisptr
+    def __cinit__(self):
+        self.thisptr = new CPPFImdlp()
+    def __dealloc__(self):
+        del self.thisptr
+    def fit(self, X, y):
+        self.thisptr.fit(X, y)
+        return self
+    def get_cut_points(self):
+        return self.thisptr.getCutPoints()
+    def get_version(self):
+        return self.thisptr.version()
+    def __reduce__(self):
+        return (CFImdlp, ())
+
+cdef extern from "Factorize.h" namespace "utils":
+    vector[int] cppFactorize(vector[string] &input_vector)
+def factorize(input_vector):
+    return cppFactorize(input_vector)
