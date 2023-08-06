172d0294ee82eed4ecebc3e832d90b671339babd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:31 +0000

    Add package gloo-client/0.10.3/gloo_client-0.10.3.tar.gz

---
+++ packages/gloo-client/gloo_client-0.10.3.tar.gz/gloo_client-0.10.3/gloo_client/app_client.py
@@ -0,0 +1,59 @@
+import typing
+from urllib.parse import urljoin
+from gloo_client.model import (
+    CreateAppRequest,
+    EmbeddingType,
+    AppResponse,
+    UpdateAppRequest,
+    SearchResponse,
+    SearchRequest,
+)
+from gloo_client.base_client import GlooBaseClient
+
+
+class AppClient(GlooBaseClient):
+    def __init__(self, *, base: GlooBaseClient):
+        super().__init__(
+            origin=urljoin(base.origin, f"app"), app_secret=base.app_secret
+        )
+
+    def update(
+        self,
+        app_id: str,
+        name: typing.Optional[str] = None,
+        embeddings: typing.Optional[typing.List[EmbeddingType]] = None,
+    ) -> AppResponse:
+        return AppResponse.parse_raw(
+            self._post(
+                f"{app_id}", data=UpdateAppRequest(name=name, embeddings=embeddings)
+            ).text
+        )
+
+    def get(self, app_id: str) -> AppResponse:
+        return AppResponse.parse_raw(self._get(f"{app_id}").text)
+
+    def search(
+        self,
+        app_id: str,
+        query: str,
+        *,
+        max_content_size: typing.Optional[int] = None,
+        tags: typing.Optional[typing.List[str]] = None,
+    ) -> AppResponse:
+        return AppResponse.parse_raw(
+            self._post(
+                f"{app_id}/search",
+                data=SearchRequest(
+                    query=query,
+                    max_content_size=max_content_size,
+                    included_tags=tags,
+                    excluded_tags=None,
+                    embedding=EmbeddingType.SBERT_MPNET_BASE_V_2,
+                ),
+            ).text
+        )
+
+    def create(self, name: str, embeddings: typing.List[EmbeddingType]) -> AppResponse:
+        return AppResponse.parse_raw(
+            self._post("", data=CreateAppRequest(name=name, embeddings=embeddings)).text
+        )
