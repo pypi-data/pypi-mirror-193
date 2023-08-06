a95b82e0c6da8c7bf829ea74a29c9bc7351a7fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:34 +0000

    Add package apf-base/2.0.0/apf_base-2.0.0-py3-none-any.whl

---
+++ packages/apf-base/apf_base-2.0.0-py3-none-any.whl/apf/core/templates/step/settings.py
@@ -0,0 +1,17 @@
+##################################################
+#       {{step_name}}   Settings File
+##################################################
+
+## Set the global logging level to debug
+# LOGGING_DEBUG = True
+
+CONSUMER_CONFIG = {}
+PRODUCER_CONFIG = {}
+METRICS_CONFIG = {}
+
+## Step Configuration
+STEP_CONFIG = {
+    "CONSUMER_CONFIG": CONSUMER_CONFIG,
+    "PRODUCER_CONFIG": PRODUCER_CONFIG,
+    "METRICS_CONFIG": METRICS_CONFIG,
+}
