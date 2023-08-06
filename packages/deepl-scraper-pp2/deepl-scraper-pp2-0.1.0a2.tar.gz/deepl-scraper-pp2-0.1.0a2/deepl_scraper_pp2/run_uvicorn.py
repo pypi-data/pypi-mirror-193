2cd698ca9c65b5cedb6411a1cfef0d1a75ce5f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:14 +0000

    Add package deepl-scraper-pp2/0.1.0a2/deepl-scraper-pp2-0.1.0a2.tar.gz

---
+++ packages/deepl-scraper-pp2/deepl-scraper-pp2-0.1.0a2.tar.gz/deepl-scraper-pp2-0.1.0a2/deepl_scraper_pp2/run_uvicorn.py
@@ -0,0 +1,74 @@
+r"""Run uvicorn with deepl_fastapi.deepl_server:app.
+
+uvicorn deepl_fastapi.deepl_server:app --reload
+
+from pypi deepl-fastapi\deepl_fastapi run_uvicorn.py
+"""
+# pylint: disable=invalid-name, duplicate-code
+# from pathlib import Path
+# import portalocker
+from typing import Optional
+from signal import SIG_DFL, SIGINT, signal
+
+import uvicorn
+from logzero import logger
+
+
+def run_uvicorn(
+    host: Optional[str] = None,  # default to "127.0.0.1",
+    port=8000,
+    debug=False,
+    reload=False,
+):
+    """Run uvicorn."""
+    if debug:
+        log_level = "debug"
+    else:
+        log_level = None
+    uvicorn.run(
+        # app="deepl_fastapi.deepl_server:app",
+        app="deepl_scraper_pp2.deepl_server:app",
+        host=host,
+        port=port,
+        reload=reload,
+        log_level=log_level,
+        # workers=2,
+        # loop="asyncio",  # default "auto"
+        # loop="uvloop",  # posix (linux and mac) only
+    )
+
+
+def main():
+    """Run main."""
+    signal(SIGINT, SIG_DFL)
+    print("ctrl-C to interrupt")
+
+    _ = """
+    file_ = Path(__file__).parent / "deepl_server.py"
+    lockfile = Path(f"{file_}.portalocker.lock")
+    if not Path(lockfile).exists():
+        Path(lockfile).touch()
+    try:
+        file = open(lockfile, "r+")
+        # portalocker.lock(file, portalocker.constants.LOCK_EX)
+        portalocker.lock(file, portalocker.LOCK_EX | portalocker.LOCK_NB)
+        ...
+    except Exception as exc:
+        logger.debug(exc)
+        logger.error("Another copy is running, exiting...")
+        raise SystemExit(1) from exc
+        # raise
+    finally:
+        # LOOP.close()
+        ...
+    # """
+
+    try:
+        run_uvicorn()
+    except Exception as exc:
+        logger.exception(exc)
+        raise SystemExit(1) from exc
+
+
+if __name__ == "__main__":
+    main()
