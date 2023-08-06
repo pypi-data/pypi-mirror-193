22f01780f882d568e5e31877150d160b772675c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:02 +0000

    Add package vantage6-common/3.7.3/vantage6_common-3.7.3-py3-none-any.whl

---
+++ packages/vantage6-common/vantage6_common-3.7.3-py3-none-any.whl/vantage6/common/globals.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+#
+#   PACKAGE GLOBALS
+#
+STRING_ENCODING = "utf-8"
+
+DEFAULT_ENVIRONMENT = "application"
+
+APPNAME = "vantage6"
+
+MAIN_VERSION_NAME = "petronas"
+
+DEFAULT_DOCKER_REGISTRY = "harbor2.vantage6.ai"
+
+DEFAULT_NODE_IMAGE = f"infrastructure/node:{MAIN_VERSION_NAME}"
+
+DEFAULT_SERVER_IMAGE = f"infrastructure/server:{MAIN_VERSION_NAME}"
+
+
+#
+#   COMMON GLOBALS
+#
+PACKAGE_FOLDER = Path(__file__).parent.parent.parent
+
+VPN_CONFIG_FILE = 'vpn-config.ovpn.conf'
