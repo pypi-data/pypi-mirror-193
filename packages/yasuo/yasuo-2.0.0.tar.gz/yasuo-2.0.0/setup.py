0661345eea0e3e01ba7280dfcad81c508eb5701d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:28 +0000

    Add package yasuo/2.0.0/yasuo-2.0.0.tar.gz

---
+++ packages/yasuo/yasuo-2.0.0.tar.gz/yasuo-2.0.0/setup.py
@@ -0,0 +1,26 @@
+"""
+This file provider the config for PYPI.org
+"""
+
+import os
+
+from setuptools import setup
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(HERE, 'README.md'), 'r', encoding='utf8') as f:
+    long_description = f.read()
+
+setup(
+    name='yasuo',
+    version='2.0.0',
+    description='A Yasuo Python package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Dung BV',
+    author_email='bvdzung@gmail.com',
+    packages=['yasuo'],
+    install_requires=[
+        'pytz', 'slugify', 'unidecode'
+    ],
+)
