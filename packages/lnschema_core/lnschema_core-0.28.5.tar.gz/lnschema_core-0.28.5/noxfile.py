47a0543971bf3edb9ed32948d946df303fceb670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:25 +0000

    Add package lnschema_core/0.28.5/lnschema_core-0.28.5.tar.gz

---
+++ packages/lnschema_core/lnschema_core-0.28.5.tar.gz/lnschema_core-0.28.5/noxfile.py
@@ -0,0 +1,26 @@
+import nox
+from lndb.test.nox import (
+    build_docs,
+    login_testuser1,
+    run_pre_commit,
+    run_pytest,
+    setup_test_instances_from_main_branch,
+    upload_docs_dir,
+)
+
+nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+def lint(session: nox.Session) -> None:
+    run_pre_commit(session)
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+def build(session):
+    login_testuser1(session)
+    setup_test_instances_from_main_branch(session)
+    session.install(".[dev,test]")
+    run_pytest(session)
+    build_docs(session)
+    upload_docs_dir()
