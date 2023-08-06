af75a7726240de0a5a80d6e25bdf6db62e12c2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:10 +0000

    Add package ASAPy/1.1.2/ASAPy-1.1.2.tar.gz

---
+++ packages/ASAPy/ASAPy-1.1.2.tar.gz/ASAPy-1.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ASAPy',
+    version='1.1.2',
+    author='Steven Wilson',
+    author_email='sawilso6@asu.edu',
+    url='https://github.com/MuhichLab/ASAP',
+    packages=["ASAP",],
+   )
+
+
+'''
+add this later
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[ 
+        "sympy",
+        "pymatgen>=2022.0.17"
+    ],
+'''
