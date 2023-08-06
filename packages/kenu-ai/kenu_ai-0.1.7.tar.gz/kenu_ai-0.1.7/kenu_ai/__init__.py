a1dd406d8334e614138fc5ede6d8b5f97e0b5459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:44 +0000

    Add package kenu-ai/0.1.7/kenu_ai-0.1.7.tar.gz

---
+++ packages/kenu-ai/kenu_ai-0.1.7.tar.gz/kenu_ai-0.1.7/kenu_ai/__init__.py
@@ -0,0 +1,20 @@
+from googletrans import Translator
+import requests
+
+def chat(value):
+	if("sahibin kim" in value):
+		print("kenan")
+		return "Sahibim namıdeğer r4isy."
+	else:
+		translator = Translator()
+		t = translator.translate(value, dest="en")
+		response = requests.get("http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=1&msg=" + t.text)
+		veri = response.text.split(':"')[-1].split('"}')[0]
+		translated = translator.translate(veri, dest="tr")
+		if("zackybot" in translated.text):
+			new_string = "Adım, kenu-ai. r4isy tarafından yazıldım."
+			return new_string
+		elif("Acobot" in translated.text):
+			return "Sahibim namıdeğer r4isy."
+		else:
+			return translated.text
