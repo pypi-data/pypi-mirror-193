f9c4f120f7a7c8e1721307a90fe42ef5ed6c2de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:30 +0000

    Add package CodonU/0.0.1/CodonU-0.0.1.tar.gz

---
+++ packages/CodonU/CodonU-0.0.1.tar.gz/CodonU-0.0.1/CodonU/cua_errors/__init__.py
@@ -0,0 +1,5 @@
+from .file_not_empty_err import FileNotEmptyError
+from .no_prot_err import NoProteinError
+from .nucleotide_err import NucleotideError
+from .internal_stop_codon_err import InternalStopCodonError
+from .ter_seq_err import TerSeqError
