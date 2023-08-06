7cef12b7697add8ee0f134bc91035b61400cfd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:02 +0000

    Add package osu.py/0.5.1/osu.py-0.5.1.tar.gz

---
+++ packages/osu.py/osu.py-0.5.1.tar.gz/osu.py-0.5.1/osu/__init__.py
@@ -0,0 +1,11 @@
+from .auth import AuthHandler, LazerAuthHandler
+from .client import Client
+from .exceptions import *
+from .notification import NotificationWebsocket
+from .asyncio.client import AsynchronousClient
+from .enums import *
+from .objects import *
+from .util import BeatmapsetSearchFilter, PlaylistItemUtil
+
+
+__version__ = '0.5.1'
