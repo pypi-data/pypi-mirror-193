f18b9c15cf6a2878c08af176b0b3e330b8d231a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:20 +0000

    Add package canvasdl/1.4.9/canvasdl-1.4.9.tar.gz

---
+++ packages/canvasdl/canvasdl-1.4.9.tar.gz/canvasdl-1.4.9/README.md
@@ -0,0 +1,61 @@
+# Canvasdl
+
+Tired of managing your course content on 5-10 different platforms? 
+Canvasdl comes to the rescue and saves you hours of work when studying and completing assignments.
+This convenient package:
+- Synchronizes all your course content to local files
+- Synchronizes your assignment dates to google calendar
+- Shows you what items are new
+- Remembers your progress through course materials
+- Keeps you up to date without overwhelming you with useless notifications
+- Synchronizes content available on:
+   - Canvas:
+     - Announcements
+     - Assignments
+     - Course files
+     - Video recordings
+   - Course websites
+   - Ed Discussion
+   - Google Drive
+   - Piazza
+   - Gradescope
+- Developed for students at Columbia University 
+- Useful for any school with (partly) the same platforms
+- Developed for Linux OS. 
+- Contact developer for other platforms support.
+
+## Installation
+
+Make sure you are using python3.9+
+
+1) Install the package
+    ```shell
+    pip install canvasdl
+    ```
+2) Obtain Canvas API key
+    - Columbia University students
+      - Go to Courseworks > Account > Settings
+      - Click on "New Access Token"
+
+3) [Optional] Obtain Google Calendar [API key](https://developers.google.com/calendar/api/quickstart/python#authorize_credentials_for_a_desktop_application)
+4) [Optional] Specify custom [Calendar ID](https://xfanatical.com/blog/how-to-find-your-google-calendar-id/)
+5) Configure your settings:
+   ```shell
+   canvasdl --configure
+   ```
+6) Install package for local file management
+   - python-xattr
+   - [mediainfo](https://manpages.ubuntu.com/manpages/bionic/man1/mediainfo.1.html)
+7) [Optional] Install package for UI progress during synchronization
+   - python-pyqt6
+   - pip install PyQt6
+
+## Usage
+Run command to synchronize all content and check for updates
+```shell
+canvasdl
+```
+
+## Quickstart
+To quickly experiment with the package you can run the [quickstart](https://github.com/quintenroets/canvasdl/blob/main/examples/quickstart.ipynb) in Google Collab.
+The downloaded content will be available in the folder /root/Documents in the Google Collab session.
---
+++ packages/canvasdl/canvasdl-1.4.9.tar.gz/canvasdl-1.4.9/canvasdl/core/coursemaker.py
@@ -0,0 +1,132 @@
+import cli
+from libs.shortcutmaker import ShortcutMaker
+from rich.prompt import Confirm, Prompt
+
+from canvasdl import client
+from canvasdl.asset_types import Course
+from canvasdl.checkers.canvas import videos
+from canvasdl.utils import Path, config
+
+
+def make_courses():
+    courses = client.get_courses()
+    courses = ask_courses(courses)
+    courses = save_courses(courses)
+    return courses
+
+
+def save_courses(courses: list[Course]):
+    make_shortcuts(courses)
+    for course in courses:
+        (Path.school / course.name).mkdir(parents=True, exist_ok=True)
+
+    courses_dict = [c.dict() for c in courses]
+    if config.update_content:
+        Path.courses.yaml = courses_dict
+
+    return courses_dict
+
+
+# no longer used
+def parse_courses(courses):
+    def parse_name(name: str):
+        delim = " - "
+        return name.split(delim)[1] if delim in name else name
+
+    def include_course(course):
+        return hasattr(course, "original_name")  # check if nickname given
+
+    courses = [c for c in courses if include_course(c)]
+    parsed_courses = []
+
+    for c in courses:
+        course = Course(name=parse_name(c.name), id=c.id)
+        video_id = videos.Checker(course).get_folder_id(c.original_name)
+        course.video_id = video_id
+        parsed_courses.append(course)
+
+    courses = sorted(parsed_courses, key=lambda item: item.name.lower())
+    return courses
+
+
+def make_shortcuts(courses: list[Course]):
+    try:
+        _make_shortcuts(courses)
+    except (ImportError, FileNotFoundError):
+        # disable on non-personal machines
+        pass
+
+
+def _make_shortcuts(courses: list[Course]):
+    shortcut_maker = ShortcutMaker()
+    for i, course in enumerate(courses):
+        shortcut_name = course.name.replace(" ", "_").replace("'", "")
+        url = f"{config.API_URL}/courses/{course.id}"
+
+        shortcuts = {
+            f"checkpoint {shortcut_name}": f'control "c:{10 + i}"',
+            # 10, 11, 12 -> 1, 2, 3, ..
+            f"checkpoint choose {shortcut_name}": f'control shift "c:{10 + i}"',
+            # 67, 68, 69 -> F1, F2, F3, ..
+            f"xdg-open {url}": f'control "c:{67 + i}"',
+            "": "",
+        }
+
+        for target, hotkey in shortcuts.items():
+            shortcut_maker.add_shortcut(hotkey, target)
+
+    shortcut_maker.save_shortcuts()
+
+
+def ask_courses(courses):
+    synced_courses = []
+
+    for course in courses:
+        name = get_course_name(course)
+        question = f"Do you want to synchronize {name}?"
+        do_sync = Confirm().ask(question, default=True)
+        if do_sync:
+            course = Course(name=name, id=course.id)
+            original_name = (
+                course.original_name
+                if hasattr(course, "original_name")
+                else course.name
+            )
+
+            question = "Do you have an rclone source you want to synchronize with?"
+            if Confirm().ask(question):
+                rclone_name = Prompt().ask("Rclone source name")
+                course.drive_name = rclone_name
+
+            question = "Do you want to synchronize the Piazza posts in this course?"
+            if Confirm().ask(question):
+                url = Prompt().ask("Piazza url for the course")
+                course.piazza_id = url.split("/")[-1]
+
+            question = "Does the course have a custom website you want to synchronize?"
+            if Confirm().ask(question):
+                root_url = Prompt().ask("Course webpage root url")
+                question = (
+                    "Does the course have any additional specific subpages you want to"
+                    " synchronize?"
+                )
+                sub_pages = []
+                while Confirm().ask(question):
+                    sub_pages.append(Prompt.ask("Course subpage sub url"))
+                if not sub_pages:
+                    sub_pages = [""]
+                course.websites = {root_url: sub_pages}
+
+            with cli.status("Saving course"):
+                course.video_id = videos.Checker(course).get_folder_id(original_name)
+            synced_courses.append(course)
+
+    return synced_courses
+
+
+def get_course_name(course_info):
+    delim = " - "
+    name = course_info.name
+    if delim in name:
+        name = name.split(delim)[1]
+    return name
