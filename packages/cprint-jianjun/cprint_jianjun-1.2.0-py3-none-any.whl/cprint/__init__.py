a2900142911651ca05e0bf089be62fe013f73007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:08 +0000

    Add package cprint-jianjun/1.2.0/cprint_jianjun-1.2.0-py3-none-any.whl

---
+++ packages/cprint-jianjun/cprint_jianjun-1.2.0-py3-none-any.whl/cprint/__init__.py
@@ -0,0 +1,10 @@
+from cprint.main import cp, cf
+from cprint.tools import demo as _demo
+from cprint.tools import demoid as _demoid
+from cprint.tools import getshow_config as _showconfig
+
+_cpshow = lambda : _showconfig(2)
+_cpshoww = lambda : _showconfig(3)
+
+config = tools = main = None
+del config, tools, main
