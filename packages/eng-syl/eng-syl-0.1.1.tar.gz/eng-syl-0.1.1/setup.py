d13de94f1641454de825697c18711a693ff54158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:53 +0000

    Add package eng-syl/0.1.1/eng-syl-0.1.1.tar.gz

---
+++ packages/eng-syl/eng-syl-0.1.1.tar.gz/eng-syl-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='eng-syl',
+      version='0.1.1',
+      description='English word syllabifier',
+      url='https://github.com/ellipse-liu/eng-syl',
+      author='ellipse-liu',
+      author_email='timothys.new.email@gmail.comm',
+      license='MIT',
+      install_requires=[
+          'tensorflow==2.6.0',
+          'nltk',
+          'numpy',
+      ],
+      keywords = ['Syllable', 'NLP', 'psycholinguistics'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3.9',
+    ],
+      zip_safe=False)
