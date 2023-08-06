2d2f7db90c8624fb8dd82a45c707044b2b44fd93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:09 +0000

    Add package rubbrband/0.1.0/rubbrband-0.1.0.tar.gz

---
+++ packages/rubbrband/rubbrband-0.1.0.tar.gz/rubbrband-0.1.0/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "rubbrband",
+    version = "0.1.0",
+    author = "Rubbrband Inc",
+    author_email = "contact@rubbrband.com",
+    description = "A library to train ML models.",
+    long_description="A library to train ML models",
+    license = "BSD",
+    keywords = "rubbrband ml-models ml",
+    packages=['rubbrband'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
