e0444ba231e0b89169999a3887875235f5db72c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:23 +0000

    Add package economynlp/0.0.2/economynlp-0.0.2.tar.gz

---
+++ packages/economynlp/economynlp-0.0.2.tar.gz/economynlp-0.0.2/economynlp/__init__.py
@@ -0,0 +1,18 @@
+# ----------------------------------- Abnormal --------------------------------------------------- #
+from economynlp.abnormal.winner_curse import *
+from economynlp.abnormal.bubble import *
+# ----------------------------------- System --------------------------------------------------- #
+from economynlp.system.economic_system import *
+from economynlp.system.financial_system import *
+from economynlp.system.physical_economy import *
+from economynlp.system.digital_economy import *
+from economynlp.system.market_economy import *
+from economynlp.system.planned_economy import *
+# ----------------------------------- Environment --------------------------------------------------- #
+from economynlp.environment.economic_environment.economic_environment import *
+from economynlp.environment.economic_environment.inflation import *
+from economynlp.environment.economic_environment.deflation import *
+from economynlp.environment.environment import *
+from economynlp.environment.market_environment import *
+from economynlp.environment.industry_environment import *
+from economynlp.environment.credit_environment import *
