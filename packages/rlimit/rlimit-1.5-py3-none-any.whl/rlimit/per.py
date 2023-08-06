ff4c7434c5094ff1d894c197a7bda8da35cb10b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:50 +0000

    Add package rlimit/1.5/rlimit-1.5-py3-none-any.whl

---
+++ packages/rlimit/rlimit-1.5-py3-none-any.whl/rlimit/per.py
@@ -0,0 +1,9 @@
+from typing import ClassVar
+
+
+class Per:
+    MILLISECOND: ClassVar[float] = .001
+    SECOND: ClassVar[int] = 1
+    MINUTE: ClassVar[int] = 60
+    HOUR: ClassVar[int] = 60 * 60
+    DAY: ClassVar[int] = 24 * 60 * 60
