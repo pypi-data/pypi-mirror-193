24456c4f31212d08281b9c59ea5f2e7dc3de6b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:42 +0000

    Add package viam-sdk/0.2.10/viam_sdk-0.2.10-py3-none-manylinux2014_x86_64.whl

---
+++ packages/viam-sdk/viam_sdk-0.2.10-py3-none-manylinux2014_x86_64.whl/viam/components/gripper/client.py
@@ -0,0 +1,57 @@
+from typing import Any, Dict, Optional
+
+from grpclib.client import Channel
+
+from viam.components.generic.client import do_command
+from viam.proto.component.gripper import (
+    GrabRequest,
+    GrabResponse,
+    GripperServiceStub,
+    IsMovingRequest,
+    IsMovingResponse,
+    OpenRequest,
+    StopRequest,
+)
+from viam.utils import dict_to_struct
+
+from .gripper import Gripper
+
+
+class GripperClient(Gripper):
+    """
+    gRPC client for the Gripper component
+    """
+
+    def __init__(self, name: str, channel: Channel):
+        self.channel = channel
+        self.client = GripperServiceStub(channel)
+        super().__init__(name)
+
+    async def open(self, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None):
+        if extra is None:
+            extra = {}
+        request = OpenRequest(name=self.name, extra=dict_to_struct(extra))
+        await self.client.Open(request, timeout=timeout)
+
+    async def grab(self, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None) -> bool:
+        if extra is None:
+            extra = {}
+        request = GrabRequest(name=self.name, extra=dict_to_struct(extra))
+        response: GrabResponse = await self.client.Grab(request, timeout=timeout)
+        return response.success
+
+    async def stop(self, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None):
+        if extra is None:
+            extra = {}
+        request = StopRequest(name=self.name, extra=dict_to_struct(extra))
+        await self.client.Stop(request, timeout=timeout)
+
+    async def is_moving(self, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None) -> bool:
+        if extra is None:
+            extra = {}
+        request = IsMovingRequest(name=self.name)
+        response: IsMovingResponse = await self.client.IsMoving(request, timeout=timeout)
+        return response.is_moving
+
+    async def do_command(self, command: Dict[str, Any], *, timeout: Optional[float] = None) -> Dict[str, Any]:
+        return await do_command(self.channel, self.name, command, timeout=timeout)
---
+++ packages/viam-sdk/viam_sdk-0.2.10-py3-none-manylinux2014_x86_64.whl/viam/components/pose_tracker/service.py
@@ -0,0 +1,34 @@
+from grpclib.server import Stream
+
+from viam.components.service_base import ComponentServiceBase
+from viam.errors import ResourceNotFoundError
+from viam.proto.component.posetracker import (
+    GetPosesRequest,
+    GetPosesResponse,
+    PoseTrackerServiceBase,
+)
+from viam.utils import struct_to_dict
+
+from .pose_tracker import PoseTracker
+
+
+class PoseTrackerService(PoseTrackerServiceBase, ComponentServiceBase[PoseTracker]):
+    """
+    gRPC service for a pose tracker
+    """
+
+    RESOURCE_TYPE = PoseTracker
+
+    async def GetPoses(self, stream: Stream[GetPosesRequest, GetPosesResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        name = request.name
+        try:
+            pose_tracker = self.get_component(name)
+        except ResourceNotFoundError as e:
+            raise e.grpc_error
+        timeout = stream.deadline.time_remaining() if stream.deadline else None
+        poses = await pose_tracker.get_poses(
+            list(request.body_names), extra=struct_to_dict(request.extra), timeout=timeout, metadata=stream.metadata
+        )
+        await stream.send_message(GetPosesResponse(body_poses=poses))
