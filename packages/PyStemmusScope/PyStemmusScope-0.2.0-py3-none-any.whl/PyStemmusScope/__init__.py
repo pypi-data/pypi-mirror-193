a26b5674ffb762452625d3145957536486fbb0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:00 +0000

    Add package PyStemmusScope/0.2.0/PyStemmusScope-0.2.0-py3-none-any.whl

---
+++ packages/PyStemmusScope/PyStemmusScope-0.2.0-py3-none-any.whl/PyStemmusScope/__init__.py
@@ -0,0 +1,10 @@
+"""Documentation about PyStemmusScope"""
+import logging
+from .stemmus_scope import StemmusScope
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Sarah Alidoost"
+__email__ = "f.alidoost@esciencecenter.nl"
+__version__ = "0.2.0"
