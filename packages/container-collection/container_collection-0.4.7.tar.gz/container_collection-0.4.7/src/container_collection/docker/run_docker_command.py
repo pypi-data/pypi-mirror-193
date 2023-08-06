22e6544412e993cfae09b5846f716bbfdb4c4c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:05 +0000

    Add package container-collection/0.4.7/container_collection-0.4.7.tar.gz

---
+++ packages/container-collection/container_collection-0.4.7.tar.gz/container_collection-0.4.7/src/container_collection/docker/run_docker_command.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+import docker
+from prefect import task
+
+
+@task
+def run_docker_command(
+    image: str,
+    command: list[str],
+    volume: Optional[docker.models.volumes.Volume] = None,
+    environment: Optional[list] = None,
+    detach: bool = False,
+) -> None:
+    environment = [] if environment is None else environment
+    volumes = {} if volume is None else {volume.name: {"bind": "/mnt", "mode": "rw"}}
+
+    client = docker.DockerClient(base_url="unix://var/run/docker.sock")
+    client.containers.run(
+        image,
+        command,
+        environment=environment,
+        volumes=volumes,
+        auto_remove=True,
+        detach=detach,
+    )
