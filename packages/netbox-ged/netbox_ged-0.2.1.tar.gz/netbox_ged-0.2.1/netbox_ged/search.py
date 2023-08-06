0d69e9039c125c0f3c842fe409108fba88125ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:39 +0000

    Add package netbox-ged/0.2.1/netbox_ged-0.2.1.tar.gz

---
+++ packages/netbox-ged/netbox_ged-0.2.1.tar.gz/netbox_ged-0.2.1/netbox_ged/search.py
@@ -0,0 +1,15 @@
+from netbox.search import SearchIndex
+from .models import Document
+from django.conf import settings
+
+# If we run NB 3.4+ register search indexes 
+if settings.VERSION >= '3.4.0':
+    class DocumentIndex(SearchIndex):
+        model = Document
+        fields = (
+            ("name", 100),
+        )
+
+
+    # Register indexes
+    indexes = [DocumentIndex]
