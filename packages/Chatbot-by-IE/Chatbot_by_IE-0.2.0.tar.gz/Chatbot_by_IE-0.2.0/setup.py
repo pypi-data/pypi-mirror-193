fdc259a46fc777d7e13cd803acd6d8b1a12a4bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:56 +0000

    Add package Chatbot-by-IE/0.2.0/Chatbot_by_IE-0.2.0.tar.gz

---
+++ packages/Chatbot-by-IE/Chatbot_by_IE-0.2.0.tar.gz/Chatbot_by_IE-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_namespace_packages
+
+
+setup(name='Chatbot_by_IE',
+      version='0.2.0',
+      description='Chatbot: Addressbook, NoteBook, Sort file, Games',
+      url='https://github.com/Sanyavas/chatbot-team-project.git',
+      author='Oleksandr Vasylyna, Oleh Vakulchyk, Nataleia Orlovska, Anton Sokhnenko, Polina Dyka',
+      author_email='vasilinaoleksanrd@gmail.com',
+      license='MIT',
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"],
+      packages=find_namespace_packages(),
+      install_requires=["prompt_toolkit", "rarfile", "prettytable", "rich"],
+      entry_points={"console_scripts": [
+            "chatbot=app.menu:main"]}
+      )
