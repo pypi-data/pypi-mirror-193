75dc1b244c0de9caf31f1d3574c194dc8c1e6b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:37 +0000

    Add package yabte/0.3.1/yabte-0.3.1.tar.gz

---
+++ packages/yabte/yabte-0.3.1.tar.gz/yabte-0.3.1/yabte/backtest/asset.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+__all__ = ["Asset"]
+
+
+class AssetName(str):
+    """Asset name string."""
+
+
+@dataclass(kw_only=True)
+class Asset:
+    """Anything that has a price.
+
+    The price currency is `denom` and default rounding controlled by
+    `price_round_dp`. The quantity rounding is controled by
+    `quantity_round_dp`.
+    """
+
+    name: AssetName
+    """Name string."""
+
+    denom: str
+    """Denominated currency."""
+
+    price_round_dp: int = 2
+    """Number of decimal places to round prices to."""
+
+    quantity_round_dp: int = 2
+    """Number of decimal places to round quantities to."""
