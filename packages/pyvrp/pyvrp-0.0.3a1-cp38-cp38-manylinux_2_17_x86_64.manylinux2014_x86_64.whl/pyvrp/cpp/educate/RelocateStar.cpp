3c8e8f0379ac236a1a9059ffbede7be0c2fa3b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:59 +0000

    Add package pyvrp/0.0.3a1/pyvrp-0.0.3a1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyvrp/pyvrp-0.0.3a1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyvrp/cpp/educate/RelocateStar.cpp
@@ -0,0 +1,40 @@
+#include "RelocateStar.h"
+
+void RelocateStar::init(Individual const &indiv)
+{
+    LocalSearchOperator<Route>::init(indiv);
+    relocate.init(indiv);
+}
+
+int RelocateStar::evaluate(Route *U, Route *V)
+{
+    move = {};
+
+    for (auto *nodeU = n(U->depot); !nodeU->isDepot(); nodeU = n(nodeU))
+    {
+        int deltaCost = relocate.evaluate(nodeU, V->depot);  // test after depot
+
+        if (deltaCost < move.deltaCost)
+            move = {deltaCost, nodeU, V->depot};
+
+        for (auto *nodeV = n(V->depot); !nodeV->isDepot(); nodeV = n(nodeV))
+        {
+            deltaCost = relocate.evaluate(nodeU, nodeV);  // test U after V
+
+            if (deltaCost < move.deltaCost)
+                move = {deltaCost, nodeU, nodeV};
+
+            deltaCost = relocate.evaluate(nodeV, nodeU);  // test V after U
+
+            if (deltaCost < move.deltaCost)
+                move = {deltaCost, nodeV, nodeU};
+        }
+    }
+
+    return move.deltaCost;
+}
+
+void RelocateStar::apply(Route *U, Route *V)
+{
+    move.from->insertAfter(move.to);
+}
