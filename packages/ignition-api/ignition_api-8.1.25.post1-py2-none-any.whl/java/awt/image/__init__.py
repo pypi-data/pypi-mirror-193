c84bb39ddda04ec99de4a30bb7598a6d3d773ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:58 +0000

    Add package ignition-api/8.1.25.post1/ignition_api-8.1.25.post1-py2-none-any.whl

---
+++ packages/ignition-api/ignition_api-8.1.25.post1-py2-none-any.whl/java/awt/image/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+__all__ = ["BufferedImage"]
+
+from typing import Any
+
+from java.awt import Image
+
+
+class BufferedImage(Image):
+    def __init__(self, *args):
+        # type: (*Any) -> None
+        print(args)
