79afb73f752d30fb060d1554ae8c8014009ebc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:10 +0000

    Add package tonkin/0.0.3/tonkin-0.0.3-py3-none-any.whl

---
+++ packages/tonkin/tonkin-0.0.3-py3-none-any.whl/tonkin/__init__.py
@@ -0,0 +1,1 @@
+from tonkin.main import *
---
+++ packages/tonkin/tonkin-0.0.3-py3-none-any.whl/tonkin/dataStream.py
@@ -0,0 +1,13 @@
+import tonkin.arrays
+
+def _calculatePacketLength(vars):
+	length = 0
+	for var in vars:
+		length += var["Length"]
+	return length
+
+# Given a data stream and the list of variable types,
+# get a list of packets from the data stream
+def splitStreamToPackets(stream, vars):
+	packetLength = _calculatePacketLength(vars)
+	return tonkin.arrays.splitArrayIntoLengths(stream, packetLength)
