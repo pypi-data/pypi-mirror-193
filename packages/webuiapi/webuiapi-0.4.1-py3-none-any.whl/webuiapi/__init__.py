bc0392bbde5c93a19a2c35449c36269a25e12059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:29 +0000

    Add package webuiapi/0.4.1/webuiapi-0.4.1-py3-none-any.whl

---
+++ packages/webuiapi/webuiapi-0.4.1-py3-none-any.whl/webuiapi/__init__.py
@@ -0,0 +1,17 @@
+from .webuiapi import WebUIApi, WebUIApiResult, Upscaler, HiResUpscaler, b64_img, raw_b64_img, ModelKeywordResult, ModelKeywordInterface, InstructPix2PixInterface, ControlNetInterface
+
+__version__ = "0.4.1"
+
+__all__ = [
+    "__version__",
+    "WebUIApi",
+    "WebUIApiResult",
+    "Upscaler",
+    "HiResUpscaler",
+    "b64_img",
+    "ModelKeywordResult",
+    "ModelKeywordInterface",
+    "InstructPix2PixInterface",
+    "ControlNetInterface",
+]
+
