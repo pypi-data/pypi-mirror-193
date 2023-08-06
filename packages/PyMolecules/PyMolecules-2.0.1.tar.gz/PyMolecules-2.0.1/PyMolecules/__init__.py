af3b287e905d1af6aca5ee0add01507905c3c0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:40 +0000

    Add package PyMolecules/2.0.1/PyMolecules-2.0.1.tar.gz

---
+++ packages/PyMolecules/PyMolecules-2.0.1.tar.gz/PyMolecules-2.0.1/PyMolecules/__init__.py
@@ -0,0 +1,6 @@
+from PyMolecules import SingleLinkedList, DoubleLinkedList,SingleCircularLinkedList
+
+
+__author__ = "Yokesh Bollineni"
+__version__ = "2.0.1"
+__what_is_new__ = "Added SCLL class, Bug fix in size() method of DLL class"
