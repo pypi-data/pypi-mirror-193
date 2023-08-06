548ae05cb461882ffa2ee8096abaacb63d4aa747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:01 +0000

    Add package meteofrance-api/1.2.0/meteofrance_api-1.2.0.tar.gz

---
+++ packages/meteofrance-api/meteofrance_api-1.2.0.tar.gz/meteofrance_api-1.2.0/src/meteofrance_api/model/__init__.py
@@ -0,0 +1,18 @@
+"""Météo-France models for the REST API."""
+from .forecast import Forecast
+from .observation import Observation
+from .picture_of_the_day import PictureOfTheDay
+from .place import Place
+from .rain import Rain
+from .warning import CurrentPhenomenons
+from .warning import Full
+
+__all__ = [
+    "Forecast",
+    "Observation",
+    "Place",
+    "PictureOfTheDay",
+    "Rain",
+    "CurrentPhenomenons",
+    "Full",
+]
