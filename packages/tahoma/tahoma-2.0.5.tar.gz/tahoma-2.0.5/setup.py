75f5c94dd1acde8fd4582933bd67a51b295b699b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:34 +0000

    Add package tahoma/2.0.5/tahoma-2.0.5.tar.gz

---
+++ packages/tahoma/tahoma-2.0.5.tar.gz/tahoma-2.0.5/setup.py
@@ -0,0 +1,10 @@
+"""
+Minimal setup.py for compatibility with legacy builds or build tool versions.
+"""
+
+from setuptools import setup
+
+setup(name='tahoma',
+      version='"Tahoma.__version__"',
+      py_modules=['tahoma'],
+      )
