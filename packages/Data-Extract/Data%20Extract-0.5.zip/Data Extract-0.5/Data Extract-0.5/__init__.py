671ad6fad02bbe511521e03b60c946fa7e412884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:54 +0000

    Add package Data-Extract/0.5/Data%20Extract-0.5.zip

---
+++ packages/Data-Extract/Data%20Extract-0.5.zip/Data Extract-0.5/Data Extract-0.5/__init__.py
@@ -0,0 +1,7 @@
+from .randomhex import createDigest
+from .filterfile import filterFile
+from .extractlist import extractList
+from .extracttable import extractTable
+from .initialize import checks
+from .clearconsole import CC
+
