9940a88d022dd12c10f7c73e326bd7aaf9cb79e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:31 +0000

    Add package googletv/0.0.1a2/googletv-0.0.1a2.tar.gz

---
+++ packages/googletv/googletv-0.0.1a2.tar.gz/googletv-0.0.1a2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['googletv']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['adb-shell[async]>=0.4.3,<0.5.0']
+
+setup_kwargs = {
+    'name': 'googletv',
+    'version': '0.0.1a2',
+    'description': '',
+    'long_description': '# python-googletv',
+    'author': 'Sören Oldag',
+    'author_email': 'soeren_oldag@freenet.de',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
