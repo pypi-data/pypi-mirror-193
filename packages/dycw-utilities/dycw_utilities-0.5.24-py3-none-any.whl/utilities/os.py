8cb172e88e4dcdfe3b58926c1e0ae91a5753d441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:25 +0000

    Add package dycw-utilities/0.5.24/dycw_utilities-0.5.24-py3-none-any.whl

---
+++ packages/dycw-utilities/dycw_utilities-0.5.24-py3-none-any.whl/utilities/os.py
@@ -0,0 +1,36 @@
+from collections.abc import Iterable, Iterator, Mapping
+from contextlib import contextmanager, suppress
+from os import cpu_count, environ, getenv
+from typing import Optional, cast
+
+from beartype import beartype
+
+CPU_COUNT = cpu_count()
+
+
+@contextmanager
+@beartype
+def temp_environ(
+    env: Optional[Mapping[str, Optional[str]]] = None,
+    **env_kwargs: Optional[str],
+) -> Iterator[None]:
+    """Context manager with temporary environment variable set."""
+    all_env = (
+        cast(dict[str, Optional[str]], {}) if env is None else env
+    ) | env_kwargs
+    prev = list(zip(all_env, map(getenv, all_env)))
+    _apply_environment(all_env.items())
+    try:
+        yield
+    finally:
+        _apply_environment(prev)
+
+
+@beartype
+def _apply_environment(items: Iterable[tuple[str, Optional[str]]], /) -> None:
+    for key, value in items:
+        if value is None:
+            with suppress(KeyError):
+                del environ[key]
+        else:
+            environ[key] = value
