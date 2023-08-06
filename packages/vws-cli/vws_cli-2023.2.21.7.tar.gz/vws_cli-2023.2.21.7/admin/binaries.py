9c6fd434e322c5d8204f05e022834f68a8c82ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:26 +0000

    Add package vws-cli/2023.2.21.7/vws_cli-2023.2.21.7.tar.gz

---
+++ packages/vws-cli/vws_cli-2023.2.21.7.tar.gz/vws_cli-2023.2.21.7/admin/binaries.py
@@ -0,0 +1,58 @@
+"""Create binaries for the CLIs."""
+
+import logging
+import uuid
+from pathlib import Path
+
+import docker
+from docker.types import Mount
+
+LOGGER = logging.getLogger(__name__)
+
+
+def make_linux_binaries(repo_root: Path) -> None:
+    """Create binaries for Linux in a Docker container.
+
+    Args:
+        repo_root: The path to the root of the repository.
+    """
+    client = docker.from_env()
+    dist_dir = repo_root / "dist"
+    assert not dist_dir.exists() or not set(dist_dir.iterdir())
+
+    target_dir = "/" + uuid.uuid4().hex
+    code_mount = Mount(
+        source=str(repo_root.absolute()),
+        target=target_dir,
+        type="bind",
+    )
+
+    # We install in editable mode to overwrite any potential
+    # ``_setuptools_scm_version.txt`` file.
+    cmd_in_container = " ".join(
+        [
+            "pip",
+            "install",
+            "--editable",
+            ".[packaging]",
+            "&&",
+            "python",
+            "admin/create_pyinstaller_binaries.py",
+        ],
+    )
+    command = f'bash -c "{cmd_in_container}"'
+
+    container = client.containers.run(
+        image="python:3.11",
+        mounts=[code_mount],
+        command=command,
+        working_dir=target_dir,
+        remove=True,
+        detach=True,
+    )
+    for line in container.logs(stream=True):
+        line = line.decode().strip()
+        LOGGER.warning(line)
+
+    status_code = container.wait()["StatusCode"]
+    assert status_code == 0
