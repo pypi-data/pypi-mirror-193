a1768377237a2aab5c9a4940300d6c4687cb9c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:14 +0000

    Add package zcmds-win32/1.0.4/zcmds_win32-1.0.4.tar.gz

---
+++ packages/zcmds-win32/zcmds_win32-1.0.4.tar.gz/zcmds_win32-1.0.4/zcmds_win32/fixvmmem.py
@@ -0,0 +1,13 @@
+
+import os
+from zcmds_win32._exec import os_exec
+from subprocess import check_output
+
+
+def main() -> int:
+    current_user = check_output("whoami", shell=True).decode("utf-8").strip()
+    cmd = f'runas /user:"{current_user}" "taskkill /F /im wslservice.exe"'
+    return os.system(cmd)
+
+if __name__ == "__main__":
+    exit(main())
