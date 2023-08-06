cfc6c8d9995f6382c1f1779f2b980758a1f2b42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:51 +0000

    Add package politicsnlp/0.0.7/politicsnlp-0.0.7-py3-none-any.whl

---
+++ packages/politicsnlp/politicsnlp-0.0.7-py3-none-any.whl/politicsnlp/marxism/__init__.py
@@ -0,0 +1,4 @@
+from politicsnlp.marxism.marxism import *
+from politicsnlp.marxism.mao_ze_dong_thought import *
+from politicsnlp.marxism.deng_xiao_ping_theory import *
+from politicsnlp.marxism.xi_jin_ping_thought import *
