47374e6d6f83eb46e78515ef22a03b2b21cd7c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:12 +0000

    Add package psu-site/0.0.1/psu_site-0.0.1.tar.gz

---
+++ packages/psu-site/psu_site-0.0.1.tar.gz/psu_site-0.0.1/setup.py
@@ -0,0 +1,40 @@
+"""
+Python setup file for the unsupported package.
+
+For more information on creating source distributions, see
+http://docs.python.org/3/distutils/sourcedist.html
+"""
+import os
+from setuptools import find_packages, setup
+import psu_site as app
+
+
+# Function for reading the contents of a file
+def read(filename):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), filename)).read()
+    except IOError:
+        return ''
+
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='psu_site',
+    version=app.__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Unsupported',
+    long_description="Unsupported code. ",
+    long_description_content_type="text/markdown",
+    url='https://www.pdx.edu',
+    author='PSU',
+    author_email='roreply@pdx.edu',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=read('requirements.txt').splitlines()
+)
