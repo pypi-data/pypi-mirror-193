c7e35e2964725b8d5aca00a73dd8fb40c224e2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:18 +0000

    Add package nubium-utils/2.5.1/nubium_utils-2.5.1-py3-none-any.whl

---
+++ packages/nubium-utils/nubium_utils-2.5.1-py3-none-any.whl/nubium_utils/fluvii_extensions/__init__.py
@@ -0,0 +1,4 @@
+from .nubium_app import NubiumTransaction, NubiumApp
+from .nubium_table_app import NubiumTableApp
+from .eloqua_retriever import NubiumEloquaRetrieverApp
+from .topic_dumper import NubiumTopicDumperApp
