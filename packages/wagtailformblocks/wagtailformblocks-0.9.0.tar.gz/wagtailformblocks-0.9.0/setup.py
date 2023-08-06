7811d335cbf62539966dbb79b0b8a7aafed94ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:58 +0000

    Add package wagtailformblocks/0.9.0/wagtailformblocks-0.9.0.tar.gz

---
+++ packages/wagtailformblocks/wagtailformblocks-0.9.0.tar.gz/wagtailformblocks-0.9.0/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('CHANGELOG.rst') as changelog_file:
+    changelog = changelog_file.read()
+
+install_requires = [
+    'django-model-utils>=4.2,<4.4',
+    'django-recaptcha>=3.0,<3.1',
+    'wagtail>=4.0,<4.3',
+    'django>=3.2,<5'
+]
+
+test_require = [
+    'psycopg2-binary',
+    # Required for test and coverage
+    'pytest',
+    'pytest-cov',
+    'pytest-django',
+    'pytest-pythonpath',
+    'coverage',
+    'factory-boy',
+    'tox',
+    # Linting
+    'flake8',
+    'isort',
+]
+
+docs_require = [
+    'sphinx',
+    'sphinx_rtd_theme',
+]
+
+setup(
+    name='wagtailformblocks',
+    version='0.9.0',
+    description="A Wagtail module that provides content blocks to display and process user defined forms",
+    long_description=readme + '\n\n' + changelog,
+    author="Tim Leguijt",
+    author_email='info@leguijtict.nl',
+    url='https://github.com/LUKKIEN/wagtailformblocks',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    install_requires=install_requires,
+    license='BSD',
+    zip_safe=False,
+    keywords='wagtailformblocks',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Wagtail',
+        'Framework :: Wagtail :: 4',
+    ],
+    extras_require={
+        'testing': test_require,
+        'docs': docs_require,
+    },
+)
---
+++ packages/wagtailformblocks/wagtailformblocks-0.9.0.tar.gz/wagtailformblocks-0.9.0/wagtailformblocks/blocks.py
@@ -0,0 +1,56 @@
+from django.urls import reverse
+from django.utils.functional import cached_property
+from wagtail import blocks
+
+from .models import BaseForm
+
+
+class FormChooserBlock(blocks.ChooserBlock):
+    target_model = BaseForm
+
+    def value_for_form(self, value):
+        return value.pk if isinstance(value, self.target_model) else value
+
+    def to_python(self, value):
+        # the incoming serialised value should be None or an ID
+        if value is None:
+            return value
+        else:
+            try:
+                if hasattr(self.target_model.objects, 'get_subclass'):
+                    return self.target_model.objects.get_subclass(pk=value)
+                return self.target_model.objects.get(pk=value)
+            except self.target_model.DoesNotExist:
+                return None
+
+    @cached_property
+    def widget(self):
+        from wagtailformblocks.widgets import FormChooserWidget
+
+        return FormChooserWidget()
+
+
+class WagtailFormBlock(blocks.StructBlock):
+    form = FormChooserBlock()
+
+    class Meta:
+        icon = 'form'
+        template = 'wagtailformblocks/form_block.html'
+
+    def get_action_url(self, form):
+        return reverse('wagtailformblocks_process', kwargs={'pk': form.id})
+
+    def get_context(self, value, parent_context=None):
+        if not parent_context:
+            context = super().get_context(value)
+        else:
+            context = super().get_context(value, parent_context)
+
+        form = value['form']
+
+        return {
+            'form': form.get_form(),
+            'form_id': form.id,
+            'action_url': self.get_action_url(form),
+            **context,
+        }
