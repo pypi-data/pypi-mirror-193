874a5eabbb323c055d5131e61870c1ba47434b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:16 +0000

    Add package FImdlp/0.9.3/FImdlp-0.9.3.tar.gz

---
+++ packages/FImdlp/FImdlp-0.9.3.tar.gz/FImdlp-0.9.3/setup.py
@@ -0,0 +1,26 @@
+"""
+    Calling
+    $python setup.py build_ext --inplace
+    will build the extension library in the current file.
+"""
+
+from setuptools import Extension, setup
+
+setup(
+    ext_modules=[
+        Extension(
+            name="fimdlp.cppfimdlp",
+            sources=[
+                "src/fimdlp/cfimdlp.pyx",
+                "src/cppmdlp/CPPFImdlp.cpp",
+                "src/cppmdlp/Metrics.cpp",
+                "src/fimdlp/Factorize.cpp",
+            ],
+            language="c++",
+            include_dirs=["fimdlp"],
+            extra_compile_args=[
+                "-std=c++11",
+            ],
+        ),
+    ]
+)
---
+++ packages/FImdlp/FImdlp-0.9.3.tar.gz/FImdlp-0.9.3/src/fimdlp/tests/__init__.py
@@ -0,0 +1,1 @@
+from .FImdlp_test import FImdlpTest
