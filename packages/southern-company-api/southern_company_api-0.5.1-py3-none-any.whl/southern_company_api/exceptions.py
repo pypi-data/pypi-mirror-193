12aca79a2383065701d00c4bed7ae95a8f520db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:35 +0000

    Add package southern-company-api/0.5.1/southern_company_api-0.5.1-py3-none-any.whl

---
+++ packages/southern-company-api/southern_company_api-0.5.1-py3-none-any.whl/southern_company_api/exceptions.py
@@ -0,0 +1,26 @@
+class InvalidLogin(Exception):
+    pass
+
+
+class CantReachSouthernCompany(Exception):
+    pass
+
+
+class NoRequestTokenFound(Exception):
+    pass
+
+
+class NoJwtTokenFound(Exception):
+    pass
+
+
+class NoScTokenFound(Exception):
+    pass
+
+
+class AccountFailure(Exception):
+    pass
+
+
+class UsageDataFailure(Exception):
+    pass
