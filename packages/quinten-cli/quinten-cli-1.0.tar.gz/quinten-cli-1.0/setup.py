a58fab7e0708e89c2081ef19ae5b259378339226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:54 +0000

    Add package quinten-cli/1.0/quinten-cli-1.0.tar.gz

---
+++ packages/quinten-cli/quinten-cli-1.0.tar.gz/quinten-cli-1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+NAME = "quinten-cli"
+
+
+def read(filename):
+    try:
+        with open(filename) as fp:
+            content = fp.read().split("\n")
+    except FileNotFoundError:
+        content = []
+    return content
+
+
+setup(
+    author="Quinten Roets",
+    author_email="quinten.roets@gmail.com",
+    description="extended subprocess",
+    name=NAME,
+    version="1.0",
+    packages=find_packages(),
+    install_requires=read("requirements.txt"),
+    entry_points={
+        "console_scripts": [
+            "konsolerun = cli.run:main",
+        ]
+    },
+)
