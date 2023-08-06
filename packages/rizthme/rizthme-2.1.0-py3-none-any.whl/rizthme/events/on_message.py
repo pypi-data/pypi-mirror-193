fac1175042890c93f3ae823d84e71537859dff43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:08 +0000

    Add package rizthme/2.1.0/rizthme-2.1.0-py3-none-any.whl

---
+++ packages/rizthme/rizthme-2.1.0-py3-none-any.whl/rizthme/events/on_message.py
@@ -0,0 +1,35 @@
+import asyncio
+
+import discord
+import logging
+
+
+def init(client: "Client"):
+
+    @client.event
+    async def on_message(message: discord.Message):
+        """
+        This event is called when a message is sent in a channel.
+
+        :param message: The message that was sent.
+        """
+        # Ignore bot messages
+        if message.author.bot:
+            return
+
+        # Ignore messages without prefix
+        if not message.content.startswith(client.PREFIX):
+            return
+
+        # Split message into command and arguments
+        cmd_line = message.content.split(' ')
+        # Get command
+        cmd: str = cmd_line[0]
+        cmd = cmd.replace(client.PREFIX, '')
+
+        # if command is in commands list
+        if cmd in client.commands:
+            logging.info(f"command: {cmd}, by {message.author} in {message.guild} on {message.channel} channel")
+            func = client.commands[cmd]
+            # Call the appropriate for the command wanted and pass the discord.Message in parameters
+            asyncio.run_coroutine_threadsafe(func(message), client.loop)
