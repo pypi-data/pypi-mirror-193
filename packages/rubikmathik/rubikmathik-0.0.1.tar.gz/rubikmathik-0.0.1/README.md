31cace3b16e3c7065179ecd07de78b896a76f605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:56 +0000

    Add package rubikmathik/0.0.1/rubikmathik-0.0.1.tar.gz

---
+++ packages/rubikmathik/rubikmathik-0.0.1.tar.gz/rubikmathik-0.0.1/README.md
@@ -0,0 +1,2 @@
+# Paquete para calcular cubos rubiks cuadrados y rectangulares <3
+## Con la colaboración de mi primito :D
---
+++ packages/rubikmathik/rubikmathik-0.0.1.tar.gz/rubikmathik-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rubikmathik',
+    version='0.0.1',
+    description='Calculador de cubos de lados cuadrados y rectangulares',
+    author='Wladimir Urzúa Calderón',
+    license='GNU GENERAL PUBLIC LICENSE',
+    packages=find_packages(),
+    url='https://github.com/Wladimir3984/rubikmathik',
+    install_requires=['numpy']
+    )
