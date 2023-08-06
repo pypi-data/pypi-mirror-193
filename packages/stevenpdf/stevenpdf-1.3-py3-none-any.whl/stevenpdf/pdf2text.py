f43ff9558685aff7d284006724bb6a8eb136f720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:07 +0000

    Add package stevenpdf/1.3/stevenpdf-1.3-py3-none-any.whl

---
+++ packages/stevenpdf/stevenpdf-1.3-py3-none-any.whl/stevenpdf/pdf2text.py
@@ -0,0 +1,30 @@
+""" 
+    pdf2text converts a pdf into text. First line of docstring is high-level description.
+
+    It does this by using something else.  Followed by a blank line - then this - a more 
+    detailed workup describing what the code does (what - not how)
+"""
+
+# Working with PyPI.org
+
+
+class Converter:
+    """
+    A simple converter for converting PDFs.
+    """
+
+    def convert(self, path):
+        """
+        Convert the given PDF to text.
+
+        A more detailed explanation.
+        Parameters:
+        parameter1(type): first parameter
+
+        Returns:
+        str:  The contents of the PDF file as text.
+        """
+        p = path
+        print(p)
+
+        print("pdf2text")
