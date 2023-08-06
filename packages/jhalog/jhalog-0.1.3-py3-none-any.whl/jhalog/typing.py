485d6cbd7b673bfc4556563d2d8a0fa32989b158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:20 +0000

    Add package jhalog/0.1.3/jhalog-0.1.3-py3-none-any.whl

---
+++ packages/jhalog/jhalog-0.1.3-py3-none-any.whl/jhalog/typing.py
@@ -0,0 +1,35 @@
+"""Specials types."""
+from datetime import datetime as _datetime
+from typing import (
+    Literal as _Literal,
+    TypedDict as _TypedDict,
+    Any as _Any,
+    List as _List,
+)
+
+HttpMethod = _Literal["DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"]  # noqa
+EventLevel = _Literal["debug", "info", "warning", "error", "critical"]  # noqa
+
+
+class Fields(_TypedDict, total=False):
+    """Log event fields."""
+
+    client_id: str
+    client_ip: str
+    client_jti: str
+    client_type: str
+    client_user_agent: str
+    created: _List[_Any]
+    date: _datetime
+    error_detail: _Any
+    execution_time: float
+    level: EventLevel
+    method: HttpMethod
+    os_uptime: float
+    path: str
+    request_id: str
+    server_id: str
+    server_version: str
+    server_uptime: float
+    status_code: int
+    type: str
