140ab2e259c5fb49c7508d6f11b9a9405eb96132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:24 +0000

    Add package lupin-grognard/1.8.0.dev1/lupin_grognard-1.8.0.dev1-py3-none-any.whl

---
+++ packages/lupin-grognard/lupin_grognard-1.8.0.dev1-py3-none-any.whl/lupin_grognard/core/format/cmake_format.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+from typing import List
+
+from lupin_grognard.core.tools.utils import die
+
+
+class CMakeFormatter:
+    def _find_cmake_files(self) -> List[str]:
+        """Search all CMake files in the code directory and subdirectories
+        return a list of CMake files paths"""
+        cmake_files = []
+        for root, dirs, files in os.walk(os.getcwd()):
+            for file in files:
+                if file == "CMakeLists.txt" or file.endswith(".cmake"):
+                    cmake_files.append(os.path.join(root, file))
+        return cmake_files
+
+    def _format_file(self, file: str) -> None:
+        """Format the CMake files"""
+        subprocess.run(["cmake-format", "-i", file])
+
+    def format_cmake_files(self) -> None:
+        cmake_files = self._find_cmake_files()
+        if len(cmake_files) == 0:
+            die(msg="no CMake files found")
+        for file in cmake_files:
+            self._format_file(file)
