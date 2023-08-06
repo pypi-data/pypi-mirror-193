47fa3a366b11409782e09d4f69eb3e4ed58723ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:41 +0000

    Add package cpanlp/1.2.44/cpanlp-1.2.44.tar.gz

---
+++ packages/cpanlp/cpanlp-1.2.44.tar.gz/cpanlp-1.2.44/cpanlp/cognitive/__init__.py
@@ -0,0 +1,8 @@
+# ----------------------------------- Cognitive --------------------------------------------------- #
+from cpanlp.cognitive.black_swan import *
+from cpanlp.cognitive.grey_rhino import *
+from cpanlp.cognitive.herd_behavior import *
+from cpanlp.cognitive.invisible_hand import *
+from cpanlp.cognitive.revolving_door import *
+from cpanlp.cognitive.tunneling import *
+from cpanlp.cognitive.visible_hand import *  
