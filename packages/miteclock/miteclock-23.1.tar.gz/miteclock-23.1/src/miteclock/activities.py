656ab7ed6ff16407192b1c0d39bbeda219e0522a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:22 +0000

    Add package miteclock/23.1/miteclock-23.1.tar.gz

---
+++ packages/miteclock/miteclock-23.1.tar.gz/miteclock-23.1/src/miteclock/activities.py
@@ -0,0 +1,241 @@
+"""This module deals with converting activity descriptions into mite time entries.
+
+An activity description is how we specify an activity on the command line.
+It can be a combination of shortcut keys, patterns for selecting projects
+and services, notes entered verbatim.
+
+The shortcuts in this combination are expanded recursively while note entries
+and raw patterns are passed unchanged.
+
+The only restriction on the possible combinations is that they must result in
+a valid time entry specification that mite can understand.
+
+"Valid" means two things:
+- there are exactly 3 fields: project pattern, service pattern, note
+- project and service patterns match exactly one project and service each
+"""
+from abc import ABC, abstractmethod
+from functools import singledispatch
+from typing import Any, Dict, List, Mapping, Sequence, Union
+
+import attrs
+import tomlkit
+from tomlkit.api import inline_table
+
+from miteclock import mite
+
+
+class MatchingPredicate(ABC):
+    @abstractmethod
+    def __call__(self, entry) -> bool:
+        ...
+
+
+@attrs.frozen
+class Pattern:
+    matches: MatchingPredicate
+    definition: str
+
+    @classmethod
+    def parse(cls, pattern_data):
+        if isinstance(pattern_data, str):
+            return cls(_parse_simple(pattern_data), pattern_data)
+        definition = _as_toml(pattern_data)
+        customer_fieldname = "customer"
+        if "project" in pattern_data:
+            if customer_fieldname in pattern_data:
+                return cls(
+                    And(
+                        [
+                            _parse_simple(pattern_data["project"]),
+                            _parse_simple(
+                                pattern_data[customer_fieldname],
+                                fieldname="customer_name",
+                            ),
+                        ]
+                    ),
+                    definition,
+                )
+            return cls(_parse_simple(pattern_data["project"]), definition)
+        if "client" in pattern_data:
+            raise ValueError(
+                f"Problem parsing this definition: {definition}. "
+                "Cannot filter by client only, please include a 'project' key."
+            )
+        return cls(_parse_simple(pattern_data), definition)
+
+
+def _as_toml(pattern_data):
+    tbl = inline_table()
+    for k, v in pattern_data.items():
+        if isinstance(v, str):
+            tbl[k] = v
+        else:
+            sub_tbl = inline_table()
+            sub_tbl.update(v)
+            tbl[k] = sub_tbl
+    return tomlkit.dumps(tbl)
+
+
+def _parse_simple(pattern_data, fieldname="name"):
+    if isinstance(pattern_data, str):
+        return SubstringMatch(fieldname, pattern_data)
+    if "pattern" not in pattern_data:
+        raise ValueError("'pattern' key is required.")
+    if pattern_data.get("match", "substring") == "substring":
+        return SubstringMatch(fieldname, pattern_data["pattern"])
+    return StrictMatch(fieldname, pattern_data["pattern"])
+
+
+@attrs.frozen
+class StrictMatch(MatchingPredicate):
+    fieldname: str
+    pattern: str
+
+    def __call__(self, entry):
+        return entry.get(self.fieldname, "") == self.pattern
+
+
+@attrs.frozen
+class SubstringMatch(MatchingPredicate):
+    fieldname: str
+    pattern: str
+
+    def __call__(self, entry):
+        return self.pattern in entry.get(self.fieldname, "")
+
+
+@attrs.frozen
+class And(MatchingPredicate):
+    predicates: Sequence[MatchingPredicate]
+
+    def __call__(self, entry) -> bool:
+        return all(pred(entry) for pred in self.predicates)
+
+
+def find_unique(entries, entry_type, pattern_data):
+    """Tries to find a unique entry that matches the pattern.
+
+    Ensures there is exactly one match for the given pattern.
+    """
+    pattern = Pattern.parse(pattern_data)
+    matched = [e for e in entries if pattern.matches(e)]
+    if not matched:
+        raise ValueError(f"'{pattern.definition}' did not match any {entry_type}.\n")
+    if len(matched) > 1:
+        raise ValueError(
+            f"'{pattern.definition}' matched the following multiple {entry_type}:\n"
+            + "\n".join(
+                mite.display_project(m) if entry_type == "projects" else m["name"]
+                for m in matched
+            )
+            + "\n\n"
+            + "Please provide an unambiguous pattern."
+        )
+    return matched[0]
+
+
+def to_time_entry_spec(activity, shortcuts, projects, services):
+    """Turn an activity into a time entry specification."""
+    assert activity
+    if len(activity) > 3:
+        raise ValueError("Activity definition too long, please enter at most 3 items.")
+    values = []
+    for key in activity:
+        values += _expand(key, shortcuts)
+    if len(values) == 2:
+        values += [""]
+    if len(values) != 3:
+        raise ValueError(
+            f"Cannot interpret the result of expanding your input: {values},\n"
+            "The result should have the following items (order matters!): "
+            "project, service, note (optional)."
+        )
+    proj_pattern, service_pattern, note = values
+    matching_project = (
+        None
+        if proj_pattern == ""
+        else find_unique(projects, "projects", proj_pattern)["id"]
+    )
+    matching_service = (
+        None
+        if service_pattern == ""
+        else find_unique(services, "services", service_pattern)["id"]
+    )
+    return {
+        "project_id": matching_project,
+        "service_id": matching_service,
+        "note": note,
+    }
+
+
+def _expand(key, shortcuts, breadcrumbs=None):
+    """Given shortcut key and mapping of shortcuts expand the shortcut key.
+
+    Return the key itself if it is not found in the mapping.
+    Avoids expansion cycles by keeping track of expansion history in the
+    `breadcrumbs` argument to recursive calls.
+    """
+    breadcrumbs = [] if breadcrumbs is None else breadcrumbs
+    if key in breadcrumbs:
+        cyclic_path = " -> ".join(breadcrumbs)
+        raise ValueError(
+            f"Detected a cycle when expanding key '{key}': {cyclic_path}\n"
+            "Please check your shortcuts."
+        )
+    # There are two termination conditions for the recursion:
+    # - The key is in fact clearly a pattern (a dictionary for now).
+    #   This would definitely exclude it as a candidate for further expansion.
+    # - The key is a string (a note, a simple pattern) but is not found in
+    #   our shortcut mapping. This simply means it cannot be further expanded.
+    if isinstance(key, dict) or key not in shortcuts:
+        return [key]
+    expansion = shortcuts[key]
+    expansions = expansion if isinstance(expansion, list) else [expansion]
+    next_level = []
+    for sk in expansions:
+        next_level += _expand(sk, shortcuts, breadcrumbs=breadcrumbs + [key])
+    return next_level
+
+
+ShortcutData = Dict[str, Union[str, List[str], Dict[str, str]]]
+
+
+def validate_shortcuts(sc: ShortcutData) -> ShortcutData:
+    # TODO: document that extent of validation doesn't include checking for cycles.
+    if not isinstance(sc, Mapping):
+        raise TypeError(
+            f"Shortcut definition must be a dictionary or mapping, got {type(sc)}."
+        )
+    for k, v in sc.items():
+        try:
+            _validate_shortcut_expansion(v)
+        except (ValueError, TypeError) as e:
+            raise e.__class__(f"The expansion for shortcut '{k}' is invalid: {e}")
+    return sc
+
+
+@singledispatch
+def _validate_shortcut_expansion(val: Any) -> None:
+    raise TypeError(f"Unsupported expansion type: {type(val)}.")
+
+
+@_validate_shortcut_expansion.register(str)
+def _(val: str) -> None:
+    pass
+
+
+@_validate_shortcut_expansion.register(list)
+def _(val: List[str]) -> None:
+    if not val:
+        raise ValueError("List expansion cannot be empty.")
+    if len(val) > 3:
+        raise ValueError(
+            f"Shortcut expansions cannot be longer than 3 items, got {len(val)}."
+        )
+
+
+@_validate_shortcut_expansion.register(dict)
+def _(val: Dict[str, Dict[str, str]]) -> None:
+    # TODO: Include some validation here
+    pass
---
+++ packages/miteclock/miteclock-23.1.tar.gz/miteclock-23.1/src/miteclock/cli.py
@@ -0,0 +1,432 @@
+"""The main application entrypoint.
+
+This module puts all the others together and uses click to interact with
+the terminal and the user input.
+"""
+from __future__ import annotations
+
+import json
+import re
+import sys
+from datetime import datetime
+from functools import partial, singledispatch
+from itertools import chain, combinations, repeat
+from operator import attrgetter, itemgetter
+from pathlib import Path
+from typing import Any, Callable, Dict, List, Optional
+
+import attrs
+import click
+from attrs import asdict
+from click_aliases import ClickAliasedGroup
+
+from miteclock import __name__, __version__
+from miteclock.activities import to_time_entry_spec
+from miteclock.mite import display_project
+from miteclock.settings import SettingsLoadError, initialize
+
+echo_error = partial(click.secho, fg="red")
+echo_success = partial(click.secho, fg="green")
+
+
+def build_menu(key_characters, time_entries):
+    """Construct menu from which to choose one of `time_entries` using key character.
+
+    Return prompt, default key and mapping from keys to time entries.
+    """
+    menu_keys = chain(
+        key_characters,
+        (k * 2 for k in key_characters),
+        (a + b for a, b in combinations(key_characters, 2)),
+    )
+    prompt = ""
+    mapping = {}
+    for key, entry in zip(menu_keys, time_entries):
+        # Also considered presenting notes first then the key.
+        # This, however, becomes tricky to align nicely.
+        prompt += f"{key}\t{entry['note']}\n"
+        mapping[key] = entry
+    default_key = key
+    prompt += "Select an entry please"
+    return prompt, default_key, mapping
+
+
+def _select_an_entry(menu_keys, entries_today):
+    """Keep prompting in a loop until user provides a usable key.
+
+    This should be tested once configuration is easier to pass.
+    """
+    prompt, default_key, keyed_entries = build_menu(menu_keys, entries_today)
+    entry = None
+    while entry is None:
+        entered_key = click.prompt(prompt, default=default_key)
+        try:
+            entry = keyed_entries[entered_key]
+        except KeyError:
+            echo_error(
+                f"The key you entered ({entered_key}) is not in the menu. Asking again."
+            )
+    return entry
+
+
+@attrs.frozen(eq=True)
+class TimeEntrySpec:
+    """Specification to create a time entry."""
+
+    project_id: Optional[int]
+    service_id: Optional[int]
+    note: str = attrs.field(converter=str.strip)
+
+
+def _idempotent_entry(entries_today, entry_spec, api):
+    """Find time entry id given entries already present and specification for an entry.
+
+    If the entry specification does not match any existing entries, create a new
+    time entry and return it, otherwise return matched existing entry.
+    """
+    entry = TimeEntrySpec(**entry_spec)
+    existing_specs = {
+        TimeEntrySpec(e["project_id"], e["service_id"], e["note"]): e
+        for e in entries_today
+    }
+    if entry in existing_specs:
+        return existing_specs[entry]
+    return api("post", "time_entries", data=json.dumps({"time_entry": asdict(entry)}))[
+        "time_entry"
+    ]
+
+
+@click.group(cls=ClickAliasedGroup, invoke_without_command=True)
+@click.option("--version", is_flag=True, help="Show miteclock's version.")
+@click.pass_context
+def main(ctx, version):
+    """miteclock
+
+    Lets you start and stop the clock in mite quickly from your terminal.
+
+    Pass the '--help' flag to sub-commands to see how to use them.
+    """
+    if version:
+        click.echo(f"miteclock {__version__}")
+        sys.exit(0)
+    # During testing ctx.obj is constructed externally and passed in, so it's not None.
+    if ctx.obj is None or isinstance(ctx.obj, Path):
+        try:
+            ctx.obj = initialize(config_root=ctx.obj, prompt=click.prompt)
+        except SettingsLoadError as err:
+            echo_error(str(err))
+            sys.exit(1)
+
+    if ctx.invoked_subcommand is None:
+        click.echo(ctx.get_help())
+
+
+@main.command(aliases=["pause", "break"])
+@click.pass_obj
+def stop(settings):
+    """Stop current clock.
+
+    Do nothing if no clock is running.
+    """
+    stopwatch = settings.mite.stopwatch
+    tracked_entry = stopwatch.tracking_time_entry()
+    if tracked_entry is None:
+        click.echo("No clock is running, nothing to do.")
+        sys.exit()
+    stopwatch.stop(tracked_entry.id)
+    echo_success("Stopping clock!")
+
+
+@main.command()
+@click.option(
+    "-l",
+    "--last",
+    is_flag=True,
+    default=False,
+    help="Start the last clock. Ignored if an activity is specified",
+)
+@click.argument("activity", nargs=-1)
+@click.pass_obj
+def start(settings, last, activity):
+    """Start the clock for an activity.
+
+    The activity can be specified as a combination of shortcuts and selector
+    patterns as well as raw notes.
+    Expanding the shortcuts must result in this form:
+
+    <project_pattern> <service_pattern> <note>
+
+    If there are already time entries present for the day, the activity can be
+    left empty. This lets you select from a list of existing time entries the one
+    you want to start the clock for. To skip this menu and automatically restart
+    the last active clock pass the `-l` flag.
+    Note that this flag is ignored if you also specify an activity.
+    """
+    entries_today = [e["time_entry"] for e in settings.mite.get("daily")]
+    if activity:
+        projects = [p["project"] for p in settings.mite.get("projects")]
+        services = [svc["service"] for svc in settings.mite.get("services")]
+        try:
+            spec = to_time_entry_spec(activity, settings.shortcuts, projects, services)
+        except ValueError as e:
+            echo_error(str(e))
+            sys.exit(1)
+        entry = _idempotent_entry(entries_today, spec, settings.mite.api)
+    else:
+        if not entries_today:
+            click.echo("No entries found for today, please specify an activity.")
+            sys.exit()
+        entries_today = sorted(entries_today, key=itemgetter("updated_at"))
+        if last:
+            entry = entries_today[-1]
+        else:
+            entry = _select_an_entry(settings.menu_keys, entries_today)
+
+    settings.mite.stopwatch.start(entry["id"])
+    echo_success("Clock started!")
+
+
+@main.command()
+@click.pass_context
+def resume(ctx):
+    """An alias for `start -l`."""
+    ctx.invoke(start, last=True)
+
+
+@main.command(aliases=["list"])
+@click.argument(
+    "what",
+    type=click.Choice(["shortcuts", "projects", "services"]),
+    default="shortcuts",
+)
+@click.pass_obj
+def show(settings, what):
+    """Show list of requested items.
+
+    This is useful to remind you which shortcuts you defined. Listing projects
+    or services can help you develop selection patterns. Beware that lists of
+    projects and services may get too large for you to scan them by sight. In
+    that case you can pipe the results to a file or a search program like grep.
+    """
+    if what in ["projects", "services"]:
+        stuff = [
+            display_project(item[what[:-1]])
+            if what == "projects"
+            else item[what[:-1]]["name"]
+            + (
+                f" (Customer: {item[what[:-1]].get('customer_name', '')})"
+                if item[what[:-1]].get("customer_name", "")
+                else ""
+            )
+            for item in settings.mite.get(what)
+        ]
+    else:
+        stuff = [f"{k} = {v}" for k, v in settings.shortcuts.items()]
+    click.echo("\n".join(stuff))
+
+
+@main.command()
+@click.argument("shell", type=click.Choice(["bash", "zsh"]), default="bash")
+@click.pass_obj
+def completion(settings, shell):
+    """Set up shell autocompletion.
+
+    Note that this will append to your shell rc file!
+    """
+
+    from pathlib import Path
+    from shutil import copyfile
+
+    completion_src = Path(__file__).parent / "completions" / shell
+    completion_dest = settings.config_dir / f"{shell}_completion"
+    copyfile(completion_src, completion_dest)
+    # Using tilde instead of resolving the full path is more portable for users who
+    # synchronize  their rc files between different machines.
+    completion_rc_path = "~" / completion_dest.relative_to(Path.home())
+    source_completion = f"source {completion_rc_path}"
+    rc_file = Path.home() / f".{shell}rc"
+    with rc_file.open(mode="r+") as fh:
+        if source_completion in fh.read():
+            echo_success("Looks like completions are already present.")
+            return
+        comment = f"# Added by {__name__}"
+        fh.write(f"\n{comment}\n{source_completion}")
+    echo_success(f"Success! Added {shell} completion loading to {rc_file}.")
+
+
+@main.command()
+@click.option(
+    "-f/-s",
+    "--full/--short",
+    default=False,
+    help="Pass '--full' to display all entries for the day. "
+    "By default only the currently tracked entry (if any) is displayed.",
+    show_default=True,
+)
+@click.pass_obj
+def status(settings, full):
+    """Display current state of mite.
+
+    Tells you whether the clock is running or not. If it's running, shows the
+    entry that you are tracking. Can also display all entries for the day.
+    """
+    entries_today = [
+        parse_mite_entry(e["time_entry"]) for e in settings.mite.get("daily")
+    ]
+    for msg in report_status(entries_today, full):
+        click.secho(msg.content, **msg.fmt_keywords)
+
+
+@attrs.frozen
+class Entry:
+    project_name: str
+    service_name: str
+    customer_name: str
+    note: str
+    minutes: MinuteCount
+    created_at: datetime
+
+
+def _to_message(e: Entry) -> Message:
+    return Message(
+        "\n".join(
+            line
+            for line in (
+                f"Project: {e.project_name}",
+                f"Customer: {e.customer_name}" if e.customer_name else "",
+                f"Service: {e.service_name}",
+                f"Note: {e.note}",
+                f"Time spent: {e.minutes}",
+            )
+            if line
+        )
+    )
+
+
+@attrs.frozen
+class TrackedEntry(Entry):
+    pass
+
+
+def parse_mite_entry(raw: Dict[str, Any]) -> Entry:
+    if "tracking" in raw:
+        entry_type = TrackedEntry
+        minutes = raw["tracking"]["minutes"]
+    else:
+        entry_type = Entry
+        minutes = raw["minutes"]
+    return entry_type(
+        project_name=raw.get("project_name", ""),
+        service_name=raw.get("service_name", ""),
+        customer_name=raw.get("customer_name", ""),
+        note=raw.get("note", ""),
+        minutes=MinuteCount(minutes),
+        created_at=datetime.strptime(
+            re.sub(r"(\+\d\d)\:(\d\d)$", r"\1\2", raw["created_at"]), "%Y-%m-%dT%X%z"
+        ),
+    )
+
+
+@attrs.frozen
+class Message:
+    content: str
+    fmt_keywords: Dict[str, Any] = attrs.field(factory=dict)
+
+
+EmptyMessage = Message("")
+
+
+@attrs.frozen
+class MinuteCount:
+    value: int
+
+    def __str__(self) -> str:
+        return "{0}h{1}m".format(*divmod(self.value, 60))
+
+    def __add__(self, other: Any) -> MinuteCount:
+        if not isinstance(other, MinuteCount):
+            return NotImplemented
+        return attrs.evolve(self, value=self.value + other.value)
+
+
+@attrs.frozen
+class _Context:
+    order: Callable[[List[Entry]], List[Entry]]
+    interpret: Callable[[Entry], Message]
+    summarize: Callable[[List[Message]], List[Message]]
+
+
+def _by_creation_time(entries: List[Entry]) -> List[Entry]:
+    return sorted(entries, key=attrgetter("created_at"))
+
+
+def _full_summary(messages: List[Message]) -> List[Message]:
+    return (
+        [Message("Entries today:")]
+        + list(
+            chain.from_iterable(zip(repeat(EmptyMessage), messages)),
+        )
+        if messages
+        else [Message("No entries today.")]
+    )
+
+
+def _short_summary(messages: List[Message]) -> List[Message]:
+    return [Message("Below is the entry being tracked.")] + messages if messages else []
+
+
+@singledispatch
+def _short_interpret(e: Entry) -> Message:
+    return EmptyMessage
+
+
+@_short_interpret.register
+def _(e: TrackedEntry) -> Message:
+    return _to_message(e)
+
+
+@singledispatch
+def _full_intrepret(e: Entry) -> Message:
+    return _to_message(e)
+
+
+@_full_intrepret.register
+def _(e: TrackedEntry) -> Message:
+    return attrs.evolve(_to_message(e), fmt_keywords={"fg": "green", "bold": True})
+
+
+def report_status(entries: List[Entry], full: bool) -> List[Message]:
+    header = Message("The clock is not running", {"fg": "red", "bold": True})
+    total_m = MinuteCount(0)
+    ctx = (
+        _Context(
+            order=_by_creation_time,
+            interpret=_full_intrepret,
+            summarize=_full_summary,
+        )
+        if full
+        else _Context(
+            order=lambda entries: entries,
+            interpret=_short_interpret,
+            summarize=_short_summary,
+        )
+    )
+    body: List[Message] = []
+    for e in ctx.order(entries):
+        header = _update_header(e, header)
+        body.append(ctx.interpret(e))
+        total_m += e.minutes
+    body = ctx.summarize([m for m in body if m.content])
+    body_padded = [EmptyMessage, *body, EmptyMessage] if body else body
+    footer = Message(f"Total time clocked in today: {total_m}", {"bold": True})
+    return [header, *body_padded, footer]
+
+
+@singledispatch
+def _update_header(e: Entry, hdr: Message) -> Message:
+    return hdr
+
+
+@_update_header.register
+def _(e: TrackedEntry, hdr: Message) -> Message:
+    return Message("The clock is running!", {"fg": "green", "bold": True})
