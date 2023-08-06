92666be63c97d44fab5819813ec4ac1470fbc0be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:44 +0000

    Add package acryo/0.2.0/acryo-0.2.0-py3-none-any.whl

---
+++ packages/acryo/acryo-0.2.0-py3-none-any.whl/acryo/_types.py
@@ -0,0 +1,10 @@
+from typing_extensions import TypeAlias
+
+RangeLike = tuple[float, float]
+Ranges = tuple[RangeLike, RangeLike, RangeLike]
+
+# type alias
+nm: TypeAlias = float  # nm is just an example. Can be angstrom or others.
+degree: TypeAlias = float
+pixel: TypeAlias = int
+subpixel: TypeAlias = float
