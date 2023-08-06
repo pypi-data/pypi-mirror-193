aaf5cec6466bc527070bed72ed037179770feae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:39 +0000

    Add package shraddha-pkg/1.0.1/shraddha-pkg-1.0.1.tar.gz

---
+++ packages/shraddha-pkg/shraddha-pkg-1.0.1.tar.gz/shraddha-pkg-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shraddha-pkg',
+    version='1.0.1',
+    packages=find_packages(),
+    url='https://github.com/shr1911/shraddha-pkg',
+    license='MIT',
+    author='Shraddha Makwana',
+    author_email='smakwana@ualberta.ca',
+    description='Shraddhas test package',
+    python_requires='>=3.6',
+)
