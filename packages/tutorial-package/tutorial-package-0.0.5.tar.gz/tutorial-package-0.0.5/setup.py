baf3b594484e2e629999543382a81db782b214be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:26 +0000

    Add package tutorial-package/0.0.5/tutorial-package-0.0.5.tar.gz

---
+++ packages/tutorial-package/tutorial-package-0.0.5.tar.gz/tutorial-package-0.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tutorial-package", # Replace with your own username
+    packages=['tutorial_package'],
+    version="0.0.5",
+    author="Anupam Basu",
+    author_email="anupam.basu91@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anupam-basu/my-own-python-package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
---
+++ packages/tutorial-package/tutorial-package-0.0.5.tar.gz/tutorial-package-0.0.5/tutorial_package/__init__.py
@@ -0,0 +1,6 @@
+
+def convert_to_boolean(val):
+    return 1 if val else 0
+
+def add_one(number):
+    return number + 1
