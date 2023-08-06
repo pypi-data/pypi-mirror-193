b354f3fee39977b2afa140f0561ece722da8c395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:50 +0000

    Add package airphin/0.0.13/airphin-0.0.13-py3-none-any.whl

---
+++ packages/airphin/airphin-0.0.13-py3-none-any.whl/airphin/constants.py
@@ -0,0 +1,62 @@
+class TOKEN:
+    """Constants token for airphin."""
+
+    QUESTION: str = "?"
+    COMMA: str = ","
+    POINT: str = "."
+    SPACE: str = " "
+    ZERO: str = "0"
+    IMPORT: str = "import"
+    STRING: str = "str"
+    CODE: str = "code"
+    NEW_LINE: str = "\n"
+
+
+class KEYWORD:
+    """Constants keywords for airphin."""
+
+    MIGRATE_MARK: str = "-airphin"
+    WORKFLOW_SUBMIT: str = "submit"
+    AIRFLOW_DAG_SCHEDULE: str = "schedule_interval"
+    AIRFLOW_DAG: str = "airflow.DAG"
+    DEFAULT_SCHEDULE: str = "0 0 0 * * ? *"
+
+
+class REGEXP:
+    """Constants regular expression for airphin."""
+
+    PATH_YAML: str = "*.yaml"
+    PATH_PYTHON: str = "*.py"
+    PATH_ALL: str = "**/*"
+
+
+class CONFIG:
+    """Constants config file for airphin."""
+
+    NAME: str = "name"
+
+    EXAMPLE: str = "examples"
+
+    MIGRATION: str = "migration"
+    MODULE: str = "module"
+    PARAMETER: str = "parameter"
+
+    ACTION: str = "action"
+    SOURCE: str = "src"
+    DESTINATION: str = "dest"
+
+    KW_REPLACE: str = "replace"
+    KW_ADD: str = "add"
+    KW_REMOVE: str = "remove"
+
+    ARGUMENT: str = "arg"
+    DEFAULT: str = "default"
+    TYPE: str = "type"
+    VALUE: str = "value"
+
+
+class NUMBER:
+    """Constants number for airphin."""
+
+    SCHEDULE_TOTAL_NUM: int = 9
+    SCHEDULE_SPACE_NUM: int = 4
