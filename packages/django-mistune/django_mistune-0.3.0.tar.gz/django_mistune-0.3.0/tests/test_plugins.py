db901a2b921f0b61c80b682f3fa560ddf351da1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:22 +0000

    Add package django-mistune/0.3.0/django_mistune-0.3.0.tar.gz

---
+++ packages/django-mistune/django_mistune-0.3.0.tar.gz/django_mistune-0.3.0/tests/test_plugins.py
@@ -0,0 +1,38 @@
+from pytest_django.asserts import assertHTMLEqual
+
+from django_mistune.templatetags.mistune import markdown
+
+
+def test_header_level():
+    assertHTMLEqual(markdown("# Test", "headers"), "<h3>Test</h3>")
+
+
+def test_header_level_nested():
+    assertHTMLEqual(
+        markdown("- Hello\n\n - World\n\n - ## Heading", "headers"),
+        """<ul>
+            <li><p>Hello</p></li>
+            <li><p>World</p></li>
+            <li><h4>Heading</h4></li>
+        </ul>""",
+    )
+
+
+def test_classes():
+    assertHTMLEqual(
+        markdown("Hello *World*", "classes"),
+        '<p class="a b">Hello <em class="c">World</em></p>',
+    )
+
+
+def test_classes_existing_attrs():
+    assertHTMLEqual(
+        markdown("This ![image](test.jpg)", "classes"),
+        '<p class="a b">This <img src="test.jpg" alt="image" class="d"></p>',
+    )
+
+
+def test_links():
+    target = '<p><a target="_blank" href="example.com">Link</a></p>'
+    assertHTMLEqual(markdown("[Link](example.com)", "links"), target)
+    assertHTMLEqual(markdown("[Link][1]\n\n[1]: example.com", "links"), target)
