aec07ef48157b8f033f5268d7a588f459fb25053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:09 +0000

    Add package fakesky/1.0.3/fakesky-1.0.3.tar.gz

---
+++ packages/fakesky/fakesky-1.0.3.tar.gz/fakesky-1.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fakesky',
+    version='1.0.3',
+    license='CC0 1.0',
+    author="Drew Weisserman",
+    author_email='drewweis@umich.edu',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/drewweis/fakesky',
+    keywords='sky image',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'scipy',
+        'astropy'
+      ],
+
+)
