cfdc0522791b1d185a7db985301a686f3793b1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:16 +0000

    Add package types-xmltodict/0.13.0.2/types-xmltodict-0.13.0.2.tar.gz

---
+++ packages/types-xmltodict/types-xmltodict-0.13.0.2.tar.gz/types-xmltodict-0.13.0.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+name = "types-xmltodict"
+description = "Typing stubs for xmltodict"
+long_description = '''
+## Typing stubs for xmltodict
+
+This is a PEP 561 type stub package for the `xmltodict` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`xmltodict`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/xmltodict. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `880c0da4045cd5ff2c29b73429629adf27e49d50`.
+'''.lstrip()
+
+setup(name=name,
+      version="0.13.0.2",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/xmltodict.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['xmltodict-stubs'],
+      package_data={'xmltodict-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
