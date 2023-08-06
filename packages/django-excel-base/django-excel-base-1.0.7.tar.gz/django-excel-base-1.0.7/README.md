e82f74c870b531bd26ee6a077b98565ee30172ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:06 +0000

    Add package django-excel-base/1.0.7/django-excel-base-1.0.7.tar.gz

---
+++ packages/django-excel-base/django-excel-base-1.0.7.tar.gz/django-excel-base-1.0.7/README.md
@@ -0,0 +1,12 @@
+# django-excel-base
+Django Excel Base
+
+## Installation
+```shell
+pip install django-excel-base
+```
+
+## Usage
+```python
+from django_excel_base import as_csv, as_dict_row_merge_xls, as_list_row_merge_xls, as_row_merge_xls, as_xls
+```
---
+++ packages/django-excel-base/django-excel-base-1.0.7.tar.gz/django-excel-base-1.0.7/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+version = '1.0.7'
+
+
+setup(
+    name='django-excel-base',
+    version=version,
+    keywords='Django Excel Base',
+    description='Django Excel Base',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/django-xxx/django-excel-base',
+
+    author='Hackathon',
+    author_email='kimi.huang@brightcells.com',
+
+    packages=['django_excel_base'],
+    py_modules=[],
+    install_requires=['xlwt', 'pytz', 'screen'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+    ],
+)
