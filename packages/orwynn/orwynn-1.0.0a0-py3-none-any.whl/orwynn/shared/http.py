ea3d26b8faa755ce91169f3c49ec48b0cef77655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:39 +0000

    Add package orwynn/1.0.0a0/orwynn-1.0.0a0-py3-none-any.whl

---
+++ packages/orwynn/orwynn-1.0.0a0-py3-none-any.whl/orwynn/shared/http.py
@@ -0,0 +1,7 @@
+from orwynn.src.web.http.Cors import Cors
+from orwynn.src.web.http.responses import (
+    HtmlHttpResponse,
+    HttpResponse,
+    JsonHttpResponse,
+    TestHttpResponse,
+)
---
+++ packages/orwynn/orwynn-1.0.0a0-py3-none-any.whl/orwynn/src/boot/BootMode.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class BootMode(Enum):
+    TEST = "test"
+    DEV = "dev"
+    PROD = "prod"
