df88538e3c8a7835feb98b11535fe6f45a15a7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:54 +0000

    Add package PassworkCreate/0.0.1/PassworkCreate-0.0.1.tar.gz

---
+++ packages/PassworkCreate/PassworkCreate-0.0.1.tar.gz/PassworkCreate-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="PassworkCreate",
+    version="0.0.1",
+    url="https://github.com/your_username/mypackage",
+    license="MIT",
+    author="Mo Chang Woo",
+    author_email="at25time@gmail.com",
+    install_requires=["numpy>=1.24.2"],    
+    description="My package TEST"    
+#    entry_points={
+#        'console_scripts': [
+#            'mycommand=mypackage.command_line:main',
+#        ],
+#    },   
+)
