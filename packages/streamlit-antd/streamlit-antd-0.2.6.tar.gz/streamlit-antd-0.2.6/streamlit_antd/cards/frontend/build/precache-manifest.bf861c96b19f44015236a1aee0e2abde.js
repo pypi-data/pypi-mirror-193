4e9c5fee07d9a6c642a310d46147334e17627d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:03 +0000

    Add package streamlit-antd/0.2.6/streamlit-antd-0.2.6.tar.gz

---
+++ packages/streamlit-antd/streamlit-antd-0.2.6.tar.gz/streamlit-antd-0.2.6/streamlit_antd/cards/frontend/build/precache-manifest.bf861c96b19f44015236a1aee0e2abde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8180f3565259374d37761a65653c2172",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e61d09ca661cbad407a",
+    "url": "./static/css/2.57ba64fa.chunk.css"
+  },
+  {
+    "revision": "4e61d09ca661cbad407a",
+    "url": "./static/js/2.a66fd7f3.chunk.js"
+  },
+  {
+    "revision": "481ffa692589faecfc9e92cb6d7fba92",
+    "url": "./static/js/2.a66fd7f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451b7aa2c24cae139da5",
+    "url": "./static/js/main.5de6e489.chunk.js"
+  },
+  {
+    "revision": "c03a404c5543622d500d",
+    "url": "./static/js/runtime-main.5a9003ef.js"
+  }
+]);
---
+++ packages/streamlit-antd/streamlit-antd-0.2.6.tar.gz/streamlit-antd-0.2.6/streamlit_antd/steps/frontend/build/precache-manifest.263b9bdd81ea54d0b11bb628f7411364.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52adaed0e0d0ecbe4d8b289511010be4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e225f93a3bf3bcc804a2",
+    "url": "./static/css/2.15aa6b1e.chunk.css"
+  },
+  {
+    "revision": "5f057c809c3cb6962d61",
+    "url": "./static/css/main.d0208de4.chunk.css"
+  },
+  {
+    "revision": "e225f93a3bf3bcc804a2",
+    "url": "./static/js/2.0e288309.chunk.js"
+  },
+  {
+    "revision": "617a910d62891f85c9de31fc517c5164",
+    "url": "./static/js/2.0e288309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f057c809c3cb6962d61",
+    "url": "./static/js/main.67ad6c51.chunk.js"
+  },
+  {
+    "revision": "5da51559676de863d294",
+    "url": "./static/js/runtime-main.7cc49c53.js"
+  }
+]);
