39059e2e7645b338a47d67f5bbfce4eec8081f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:09 +0000

    Add package lucidoc/0.4.4/lucidoc-0.4.4-py3-none-any.whl

---
+++ packages/lucidoc/lucidoc-0.4.4-py3-none-any.whl/lucidoc/exceptions.py
@@ -0,0 +1,10 @@
+""" Custom exception types """
+
+__author__ = "Vince Reuter"
+__email__ = "vreuter@virginia.edu"
+
+
+class LucidocError(Exception):
+    """Base error type for this package"""
+
+    pass
