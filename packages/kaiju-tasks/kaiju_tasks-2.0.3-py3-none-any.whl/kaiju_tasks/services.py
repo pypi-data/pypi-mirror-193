49e28d26c67628698be6943dbec35c752e44c2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:16 +0000

    Add package kaiju-tasks/2.0.3/kaiju_tasks-2.0.3-py3-none-any.whl

---
+++ packages/kaiju-tasks/kaiju_tasks-2.0.3-py3-none-any.whl/kaiju_tasks/services.py
@@ -0,0 +1,8 @@
+"""
+All service imports (used by the class registry).
+"""
+
+from .notifications import NotificationService
+from .interface import TaskService
+from .manager import TaskManager
+from .executor import TaskExecutor
