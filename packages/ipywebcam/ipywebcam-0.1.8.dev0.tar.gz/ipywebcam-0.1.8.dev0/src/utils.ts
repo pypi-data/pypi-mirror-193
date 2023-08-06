24296b945209d2260c0ae7ae26dcb2ab02d71ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:32 +0000

    Add package ipywebcam/0.1.8.dev0/ipywebcam-0.1.8.dev0.tar.gz

---
+++ packages/ipywebcam/ipywebcam-0.1.8.dev0.tar.gz/ipywebcam-0.1.8.dev0/src/utils.ts
@@ -0,0 +1,20 @@
+export function arrayInclude<T>(arr: T[], target: T): boolean {
+  return !!~arr.indexOf(target);
+}
+
+export function arrayFind<T>(
+  arr: T[],
+  cond: (v: T, idx: number) => boolean
+): T | undefined {
+  for (let i = 0; i < arr.length; ++i) {
+    const e = arr[i];
+    if (cond(e, i)) {
+      return e;
+    }
+  }
+  return undefined;
+}
+
+export function isMac(): boolean {
+  return window.navigator.userAgent.indexOf('Mac') !== -1;
+}
