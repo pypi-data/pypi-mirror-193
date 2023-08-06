52ae43771eb848351c53e330efc911da8bf4136c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:52 +0000

    Add package labelpandas/0.1.28/labelpandas-0.1.28.tar.gz

---
+++ packages/labelpandas/labelpandas-0.1.28.tar.gz/labelpandas-0.1.28/labelpandas/__init__.py
@@ -0,0 +1,5 @@
+from .client import Client
+from labelpandas import connector
+from labelpandas import uploader
+from labelpandas.rename_columns import rename_columns
+from labelpandas.load_local_files import load_local_files
