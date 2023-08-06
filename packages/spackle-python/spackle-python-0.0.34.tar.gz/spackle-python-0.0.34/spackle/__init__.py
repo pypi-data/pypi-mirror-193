91d30568449ec7172c07bfc3c64f4ee924bdf1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:06 +0000

    Add package spackle-python/0.0.34/spackle-python-0.0.34.tar.gz

---
+++ packages/spackle-python/spackle-python-0.0.34.tar.gz/spackle-python-0.0.34/spackle/__init__.py
@@ -0,0 +1,26 @@
+import threading
+from spackle.customer import Customer
+from spackle.exceptions import SpackleException
+from spackle.stores import DynamoDBStore, MemoryStore, FileStore
+from spackle.constants import FEATURE_TYPE_FLAG, FEATURE_TYPE_LIMIT
+
+api_key = None
+api_base = "https://api.spackle.so/v1"
+schema_version = 1
+_store = None
+
+
+def get_store():
+    global _store
+    if not _store:
+        _store = DynamoDBStore()
+    return _store
+
+
+def set_store(store):
+    global _store
+    _store = store
+
+
+def bootstrap():
+    get_store()
