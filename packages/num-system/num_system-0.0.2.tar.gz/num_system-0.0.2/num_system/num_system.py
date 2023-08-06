dc6bb09ff4d930520f9fdee0a994512b4c54b886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:06 +0000

    Add package num-system/0.0.2/num_system-0.0.2.tar.gz

---
+++ packages/num-system/num_system-0.0.2.tar.gz/num_system-0.0.2/num_system/num_system.py
@@ -0,0 +1,11 @@
+def ns(num, to_base=10, from_base=10):
+    # first convert to decimal number
+    num = str(num)
+    n = int(num, from_base) if isinstance(num, str) else num
+    # now convert decimal to 'to_base' base
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    res = ""
+    while n > 0:
+        n, m = divmod(n, to_base)
+        res += alphabet[m]
+    return print(res[::-1])
---
+++ packages/num-system/num_system-0.0.2.tar.gz/num_system-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='num_system',
+    version='0.0.2',
+    description='Перевод числа а в с.с. б из с.с. с',
+    license='MIT',
+    packages=['num_system'],
+    author='Dimitry K',
+    author_email='dime_kozlov@mail.ru',
+    keywords=['sis'],
+    url='https://github.com/TFL-group/num_sys'
+)
