50fe805cec4096339714842f2845169b51b1422f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:06 +0000

    Add package grupa45/0.0.1/grupa45-0.0.1.tar.gz

---
+++ packages/grupa45/grupa45-0.0.1.tar.gz/grupa45-0.0.1/README.rst
@@ -0,0 +1,2 @@
+Aici vom scrie date despre cum folosim modulul.
+Se instaleaza cu pip install grupa45.
---
+++ packages/grupa45/grupa45-0.0.1.tar.gz/grupa45-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='grupa45',
+    version='0.0.1',
+    author='Cristian Iordache',
+    author_email='iordachecristian000@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    description='descriere'
+)
