0cf0a8f6a3028abbb9b1dae3d0ea87aa01d02cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:30 +0000

    Add package djangorestframework-jwt-5/1.13.0/djangorestframework_jwt_5-1.13.0-py3-none-any.whl

---
+++ packages/djangorestframework-jwt-5/djangorestframework_jwt_5-1.13.0-py3-none-any.whl/rest_framework_jwt/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'djangorestframework-jwt-5'
+__version__ = '1.13.0'
+__author__ = 'Jose Padilla'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014-2017 Blimp LLC'
+
+# Version synonym
+VERSION = __version__
