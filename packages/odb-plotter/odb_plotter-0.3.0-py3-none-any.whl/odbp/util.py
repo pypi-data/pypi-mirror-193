d54dd2b5b7a60befca2bf87c8e0ea3703b8f3c89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:56 +0000

    Add package odb-plotter/0.3.0/odb_plotter-0.3.0-py3-none-any.whl

---
+++ packages/odb-plotter/odb_plotter-0.3.0-py3-none-any.whl/odbp/util.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+Utility methods for odb_plotter
+"""
+
+from typing import Union
+
+DefaultType = Union[None, str]
+AnswerType = Union[tuple[str, str], tuple[str, str, str]]
+
+def confirm(message: str, confirmation: str, default: DefaultType = None) -> bool:
+    yes_vals: AnswerType = ("yes", "y")
+    no_vals: AnswerType = ("no", "n")
+    if isinstance(default, str):
+        if default.lower() in yes_vals:
+            yes_vals = ("yes", "y", "")
+            confirmation += " (Y/n)? "
+        elif default.lower() in no_vals:
+            no_vals = ("no", "n", "")
+            confirmation += " (y/N)? "
+
+    else:
+        confirmation += " (y/n)? "
+
+    while True:
+        print(message)
+        user_input: str = input(confirmation).lower()
+        if user_input in yes_vals:
+            return True
+        elif user_input in no_vals:
+            return False
+        else:
+            print("Error: invalid input")
+
+
