a8c5009136b38dcf782b5a129d9290f05b5f45e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:19 +0000

    Add package arcane-storage/0.4.3/arcane_storage-0.4.3.tar.gz

---
+++ packages/arcane-storage/arcane_storage-0.4.3.tar.gz/arcane_storage-0.4.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['arcane']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['arcane-core>=1.6,<2.0',
+ 'backoff>=1.10.0,<2.0.0',
+ 'google-cloud-storage>=2.7,<3.0']
+
+setup_kwargs = {
+    'name': 'arcane-storage',
+    'version': '0.4.3',
+    'description': '',
+    'long_description': "# Arcane Storage\n\nThis package is base on [google-cloud-storage](https://pypi.org/project/google-cloud-storage/).\n\n## Get Started\n\n```sh\npip install arcane-storage\n```\n\n## Example Usage\n\n```python\nfrom arcane import storage\nclient = storage.Client()\n\nblobs = client.list_blobs('bucket-id-here')\n```\n\nor\n\n```python\nfrom arcane import storage\n\n# Import your configs\nfrom configure import Config\n\nclient = storage.Client.from_service_account_json(Config.KEY, project=Config.GCP_PROJECT)\n\nblobs = client.list_blobs('bucket-id-here')\n```\n",
+    'author': 'Arcane',
+    'author_email': 'product@arcane.run',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
