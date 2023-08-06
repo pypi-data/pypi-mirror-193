2f28a158b2bc969369f21d8623737484febeb886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:20 +0000

    Add package matrix-http-rendezvous-synapse/0.1.12/matrix_http_rendezvous_synapse-0.1.12-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/matrix-http-rendezvous-synapse/matrix_http_rendezvous_synapse-0.1.12-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/matrix_http_rendezvous_synapse/__init__.py
@@ -0,0 +1,5 @@
+from .matrix_http_rendezvous_synapse import *
+
+__doc__ = matrix_http_rendezvous_synapse.__doc__
+if hasattr(matrix_http_rendezvous_synapse, "__all__"):
+    __all__ = matrix_http_rendezvous_synapse.__all__
