356cd25ae290b50d1bd095c6dbe7e8af8edc0e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:00 +0000

    Add package pyvrp/0.0.3a1/pyvrp-0.0.3a1-cp39-cp39-musllinux_1_1_x86_64.whl

---
+++ packages/pyvrp/pyvrp-0.0.3a1-cp39-cp39-musllinux_1_1_x86_64.whl/pyvrp/cpp/educate/LocalSearch_bindings.cpp
@@ -0,0 +1,23 @@
+#include "LocalSearch.h"
+
+#include <pybind11/pybind11.h>
+
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(LocalSearch, m)
+{
+    py::class_<LocalSearch>(m, "LocalSearch")
+        .def(py::init<ProblemData &, PenaltyManager &, XorShift128 &>(),
+             py::arg("data"),
+             py::arg("penalty_manager"),
+             py::arg("rng"))
+        .def("add_node_operator", &LocalSearch::addNodeOperator, py::arg("op"))
+        .def(
+            "add_route_operator", &LocalSearch::addRouteOperator, py::arg("op"))
+        .def("set_neighbours", &LocalSearch::setNeighbours, py::arg("neighbours"))
+        .def("get_neighbours", &LocalSearch::getNeighbours)
+        .def("search", &LocalSearch::search, py::arg("individual"))
+        .def("intensify", &LocalSearch::intensify, py::arg("individual"));
+}
