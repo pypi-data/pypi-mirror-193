334d5033494dfd0a623b99ef28542ab7a1fd9507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:47 +0000

    Add package cognite-processpool/0.7.0/cognite_processpool-0.7.0-py3-none-any.whl

---
+++ packages/cognite-processpool/cognite_processpool-0.7.0-py3-none-any.whl/cognite/processpool/__init__.py
@@ -0,0 +1,3 @@
+from cognite.processpool.processpool import ProcessPool, ProcessPoolShutDownException, WorkerDiedException  # noqa
+
+__all__ = ["ProcessPool", "ProcessPoolShutDownException", "WorkerDiedException"]
