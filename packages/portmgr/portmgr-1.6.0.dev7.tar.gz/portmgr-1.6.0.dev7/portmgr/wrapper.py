bfab028fb2a7ad795c0d0cfc18bb7172df88aff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:14 +0000

    Add package portmgr/1.6.0.dev7/portmgr-1.6.0.dev7.tar.gz

---
+++ packages/portmgr/portmgr-1.6.0.dev7.tar.gz/portmgr-1.6.0.dev7/portmgr/wrapper.py
@@ -0,0 +1,47 @@
+import json
+import os
+from subprocess import call, check_output
+
+
+def getServices(includeOnlyBuildable=False):
+    data = check_output(['docker', 'compose', 'config', '--format', 'json'])
+    config = json.loads(data)
+    services = config['services']
+    if includeOnlyBuildable:
+        services = [name for name, values in services.items() if 'build' in values.keys()]
+    return services
+
+
+def getServicesRunning():
+    data = check_output(['docker', 'compose', 'ps', '--format', 'json'])
+    container_list = json.loads(data)
+    container_names = [s['name'] for s in container_list]
+    return container_names
+
+
+def getImages():
+    data = check_output(['docker', 'compose', 'images', '--format', 'json'])
+    image_list = json.loads(data)
+    images = [
+        {'ID': image['ID'],
+         'Name': image['Repository'],
+         'ContainerName': image['ContainerName'],
+         'Tag': image['Tag']}
+        for image in image_list
+    ]
+    return images
+
+
+def getStats():
+    containers = getServicesRunning()
+    data = check_output(['docker', 'stats', '--format', 'json', '--no-stream'] + containers)
+    stats = json.loads(data)
+    return stats
+
+
+def runCompose(args, **kwargs):
+    command = ['docker', 'compose']
+    if os.environ.get("PORTMGR_IN_SCRIPT", "").lower() == "true":
+        command += ["--ansi", "never"]
+    command += list(args)
+    return call(command, **kwargs)
