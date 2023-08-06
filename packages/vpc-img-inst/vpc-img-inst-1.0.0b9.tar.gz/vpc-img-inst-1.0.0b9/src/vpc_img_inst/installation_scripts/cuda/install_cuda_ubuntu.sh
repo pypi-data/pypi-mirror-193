957054bc22c0922523d13c8871355cc13e266e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:13 +0000

    Add package vpc-img-inst/1.0.0b9/vpc-img-inst-1.0.0b9.tar.gz

---
+++ packages/vpc-img-inst/vpc-img-inst-1.0.0b9.tar.gz/vpc-img-inst-1.0.0b9/src/vpc_img_inst/installation_scripts/cuda/install_cuda_ubuntu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get install linux-headers-$(uname -r) gcc -y 
+sudo apt-key del 7fa2af80
+# extract ubuntu release version and parse the required format of: <OS><VERSION_ID_DIGITS_ONLY>, e.g. 'ubuntu2204'. 
+export distro="ubuntu$(awk -F'=' '/^VERSION_ID/ {print $2}' /etc/os-release | tr -d '[".]')"
+export arch="x86_64"
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-get update 
+sudo apt-get install cuda -y
+sudo apt-get install cuda-drivers-520 -y
+sudo systemctl enable nvidia-persistenced
+
+file="/etc/profile.d/cuda-exports.sh"
+sudo sh -c "cat<<EOF > $file
+export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+export CUDA_HOME=/usr/local/cuda
+EOF"
---
+++ packages/vpc-img-inst/vpc-img-inst-1.0.0b9.tar.gz/vpc-img-inst-1.0.0b9/src/vpc_img_inst/installation_scripts/ray/install_ray_ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update && sudo apt install python3-pip -y
+test -d ~/.venv || (pip install virtualenv && virtualenv -p /usr/bin/python3 ~/.venv && echo source ~/.venv/bin/activate >> ~/.bashrc &&  echo "source ~/.venv/bin/activate" | cat - ~/.bashrc | tee ~/.bashrc)
+source ~/.venv/bin/activate
+pip install ibm-vpc ibm_platform_services ibm_cloud_sdk_core 
+pip install -U ibm-vpc-ray-connector
+sudo apt install libgl1-mesa-glx -y && pip install pandas tabulate gym tensorboardX dm_tree opencv-python transformers torch
+echo alias python=\'python3\'>>~/.bash_aliases
+pip install "ray[default]" "ray[serve]"
