1d40accb814a9175938094406e15ad11d115c447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:07 +0000

    Add package practice-distribution-msrk/0.3/practice_distribution_msrk-0.3.tar.gz

---
+++ packages/practice-distribution-msrk/practice_distribution_msrk-0.3.tar.gz/practice_distribution_msrk-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='practice_distribution_msrk',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['practice_distribution_msrk'],
+      zip_safe=False,
+      author='Satya Rohith Kumar',
+      author_email='rohithmeduri24@gmail.com',
+      requires=['matplotlib','math'],)
