5398dc3a25614d5831fa2022e6fbbf100d2fd2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:06 +0000

    Add package solara/1.6.1/solara-1.6.1.tar.gz

---
+++ packages/solara/solara-1.6.1.tar.gz/solara-1.6.1/tests/integration/api_test.py
@@ -0,0 +1,63 @@
+# tests pages in the api docs
+import playwright.sync_api
+from playwright.sync_api import expect
+
+# def test_api_sqlcode(page: playwright.sync_api.Page, solara_server, solara_app):
+#     # with screenshot_on_error(page, 'tmp/test_docs_basics.png'):
+#     with solara_app("solara.website.pages"):
+#         page.goto(solara_server.base_url + "/api")
+#         page.locator("text=Sql Code").click()
+#         page.locator('button:has-text("Execute")').wait_for()
+
+
+# def test_api_grid_draggable(page: playwright.sync_api.Page, solara_server, solara_app):
+#     # with screenshot_on_error(page, 'tmp/test_docs_basics.png'):
+#     with solara_app("solara..website.pages"):
+#         page.goto(solara_server.base_url + "/api")
+#         page.locator("text=GridDraggable").click()
+#         page.locator('button:has-text("Reset to initial layout")').wait_for()
+
+
+def test_api_markdown_editor(page_session: playwright.sync_api.Page, solara_server, solara_app):
+    with solara_app("solara.website.pages"):
+        page_session.goto(solara_server.base_url + "/api/")
+        page_session.locator("text=Markdown Editor").first.click()
+        page_session.locator('h1:has-text("Large heading")').wait_for()
+
+
+def test_api_file_browser(page_session: playwright.sync_api.Page, solara_server, solara_app):
+    with solara_app("solara.website.pages"):
+        page_session.goto(solara_server.base_url + "/api/")
+        page_session.locator("text=File Browser").first.click()
+        page_session.locator("text=You are in directory").wait_for()
+
+
+def test_api_matplotlib(page_session: playwright.sync_api.Page, solara_server, solara_app):
+    with solara_app("solara.website.pages"):
+        page_session.goto(solara_server.base_url + "/api/matplotlib")
+        page_session.locator("text=Arguments").first.wait_for()
+
+
+def test_api_style(page_session: playwright.sync_api.Page, solara_server, solara_app):
+    with solara_app("solara.website.pages"):
+        page_session.goto(solara_server.base_url + "/api/style")
+        page_session.locator("text=Add a custom piece of CSS").first.wait_for()
+        expect(page_session.locator(".mybutton")).to_have_css("color", "rgb(76, 175, 80)")
+
+        # disable css
+        page_session.locator('_vue=v-checkbox[label="Use CSS"]').click()
+        expect(page_session.locator(".mybutton")).to_have_css("color", "rgba(0, 0, 0, 0.87)")
+
+        # enable css again
+        page_session.locator('_vue=v-checkbox[label="Use CSS"]').click()
+        expect(page_session.locator(".mybutton")).to_have_css("color", "rgb(76, 175, 80)")
+
+
+def test_api_cross_filter_select(page_session: playwright.sync_api.Page, solara_server, solara_app):
+    with solara_app("solara.website.pages"):
+        page_session.goto(solara_server.base_url + "/api/cross_filter_select")
+        page_session.locator("text=244").wait_for()
+        select_sex = page_session.locator('_vue=v-autocomplete[label="Select values in sex having values:"]')
+        select_sex.click()
+        page_session.locator("text=Female").click()
+        page_session.locator("text=87 / 244").wait_for()
