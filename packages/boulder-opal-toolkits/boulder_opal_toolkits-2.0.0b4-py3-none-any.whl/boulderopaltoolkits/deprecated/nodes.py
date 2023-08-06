6797b7e69ee67c4443d6b4668c22f9d2492f028a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:46 +0000

    Add package boulder-opal-toolkits/2.0.0b4/boulder_opal_toolkits-2.0.0b4-py3-none-any.whl

---
+++ packages/boulder-opal-toolkits/boulder_opal_toolkits-2.0.0b4-py3-none-any.whl/boulderopaltoolkits/deprecated/nodes.py
@@ -0,0 +1,87 @@
+# Copyright 2023 Q-CTRL. All rights reserved.
+#
+# Licensed under the Q-CTRL Terms of service (the "License"). Unauthorized
+# copying or use of this file, via any medium, is strictly prohibited.
+# Proprietary and confidential. You may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#    https://q-ctrl.com/terms
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS. See the
+# License for the specific language.
+"""
+Deprecated nodes.
+"""
+
+from boulderopaltoolkits.namespace import Namespace
+from boulderopaltoolkits.toolkit_utils import expose
+
+# pylint: disable=unused-argument, missing-function-docstring
+
+# Namespace.PULSES deprecated 2022/12/01
+
+
+@expose(Namespace.PULSES)
+def cosine_pulse_pwc(*args, **kwargs):
+    pass
+
+
+@expose(Namespace.PULSES)
+def hann_series_pwc(*args, **kwargs):
+    pass
+
+
+@expose(Namespace.PULSES)
+def linear_ramp_pwc(*args, **kwargs):
+    pass
+
+
+@expose(Namespace.PULSES)
+def sech_pulse_pwc(*args, **kwargs):
+    pass
+
+
+@expose(Namespace.PULSES)
+def sinusoid_pwc(*args, **kwargs):
+    pass
+
+
+@expose(Namespace.PULSES)
+def square_pulse_pwc(*args, **kwargs):
+    pass
+
+
+@expose(Namespace.PULSES)
+def tanh_ramp_pwc(*args, **kwargs):
+    pass
+
+
+@expose(Namespace.PULSES)
+def cosine_pulse_stf(*args, **kwargs):
+    pass
+
+
+@expose(Namespace.PULSES)
+def hann_series_stf(*args, **kwargs):
+    pass
+
+
+@expose(Namespace.PULSES)
+def linear_ramp_stf(*args, **kwargs):
+    pass
+
+
+@expose(Namespace.PULSES)
+def sech_pulse_stf(*args, **kwargs):
+    pass
+
+
+@expose(Namespace.PULSES)
+def sinusoid_stf(*args, **kwargs):
+    pass
+
+
+@expose(Namespace.PULSES)
+def tanh_ramp_stf(*args, **kwargs):
+    pass
