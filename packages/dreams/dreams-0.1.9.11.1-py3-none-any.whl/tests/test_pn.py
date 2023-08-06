fe2d393ac5585e6412c971665bf485ecc98eb445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:42 +0000

    Add package dreams/0.1.9.11.1/dreams-0.1.9.11.1-py3-none-any.whl

---
+++ packages/dreams/dreams-0.1.9.11.1-py3-none-any.whl/tests/test_pn.py
@@ -0,0 +1,14 @@
+from dreams import pornone as pn, __version__
+print(__version__)
+
+
+
+def test_search_porn():
+    vds = pn.search_porn('lorena aquino')
+    print(vds,'\n')
+    vds = vds.videos
+
+
+
+
+test_search_porn()
