4587531d67900ef56e5b874f78bd0a70654508d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:36 +0000

    Add package cbra/1.0.2/cbra-1.0.2.tar.gz

---
+++ packages/cbra/cbra-1.0.2.tar.gz/cbra-1.0.2/cbra/core/iam/types/iuseronboardingservice.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2023 Cochise Ruhulessin
+#
+# All rights reserved. No warranty, explicit or implicit, provided. In
+# no event shall the author(s) be liable for any claim or damages.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+from typing import Protocol
+
+from headless.ext.oauth2.models import OIDCToken
+
+from .subject import Subject
+
+
+class IUserOnboardingService(Protocol):
+    def initialize(self) -> Subject: ...
+    async def oidc(self, token: OIDCToken) -> Subject: ...
+    async def update(self, subject: Subject) -> None: ...
