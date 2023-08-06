7dc9ef8e347da5f304e5924117257fcb8486137a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:37 +0000

    Add package daorionpkg-daorion/0.0.1/daorionpkg_daorion-0.0.1-py3-none-any.whl

---
+++ packages/daorionpkg-daorion/daorionpkg_daorion-0.0.1-py3-none-any.whl/daorionpkg/daorion.py
@@ -0,0 +1,23 @@
+def add(x, y):
+    """Return the sum of x and y."""
+    return x + y
+
+
+def subtract(x, y):
+    """Return the difference between x and y."""
+    return x - y
+
+
+def multiply(x, y):
+    """Return the product of x and y."""
+    return x * y
+
+
+def divide(x, y):
+    """Return the quotient of x divided by y."""
+    return x / y
+
+
+def himangshu():
+
+    return "Hi there, This is Himangshu"
