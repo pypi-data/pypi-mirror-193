c4aa1f5837d1627a522bf0b0a8b45daca9a43748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:19 +0000

    Add package Andrej/1.0.1/Andrej-1.0.1.tar.gz

---
+++ packages/Andrej/Andrej-1.0.1.tar.gz/Andrej-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="Andrej",
+	version="1.0.1",
+	packages=find_packages(),
+	description="A colorful module",
+	url="https://github.com/AndrejLocka/coloramas",
+	author="Andrej",
+	license="WTFPL",
+	python_requires=">=2.7"
+	)
