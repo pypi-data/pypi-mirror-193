f28318676c23a31d60ebc85b141e3c6bf6d27e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:12 +0000

    Add package Ansi-Shadow/0.0.2/Ansi_Shadow-0.0.2.tar.gz

---
+++ packages/Ansi-Shadow/Ansi_Shadow-0.0.2.tar.gz/Ansi_Shadow-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+
+setup(
+    name="Ansi_Shadow",
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+    ]
+)
