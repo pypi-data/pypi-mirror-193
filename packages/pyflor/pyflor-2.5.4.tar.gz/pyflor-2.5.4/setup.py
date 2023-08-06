ec15c97fe59b520cdb82742798625b6f165d618c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:21 +0000

    Add package pyflor/2.5.4/pyflor-2.5.4.tar.gz

---
+++ packages/pyflor/pyflor-2.5.4.tar.gz/pyflor-2.5.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import io
+
+with io.open("README.md", mode="r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyflor",
+    version="2.5.4",
+    author="Rolando Garcia",
+    author_email="rogarcia@berkeley.edu",
+    description="Fast Low-Overhead Recovery",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ucbrise/flor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
