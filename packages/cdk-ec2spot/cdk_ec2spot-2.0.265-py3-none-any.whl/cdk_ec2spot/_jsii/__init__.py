6c61e926fe8c47756d18ee55528ce88177ac6409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:24 +0000

    Add package cdk-ec2spot/2.0.265/cdk_ec2spot-2.0.265-py3-none-any.whl

---
+++ packages/cdk-ec2spot/cdk_ec2spot-2.0.265-py3-none-any.whl/cdk_ec2spot/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import cdk_spot_one._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-ec2spot", "2.0.265", __name__[0:-6], "cdk-ec2spot@2.0.265.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
