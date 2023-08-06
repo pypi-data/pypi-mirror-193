659cc64e2f3d7bdc119d2ca38fbabc79c34cdae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:20 +0000

    Add package simple-cog-client/0.1.1/simple_cog_client-0.1.1-py3-none-any.whl

---
+++ packages/simple-cog-client/simple_cog_client-0.1.1-py3-none-any.whl/simple_cog_client/processors.py
@@ -0,0 +1,20 @@
+import io
+from base64 import b64encode, b64decode
+
+from PIL import Image
+
+class ImageProcessor:    
+    def encode(self, img_path):
+        buffer = io.BytesIO()
+        img = Image.open(img_path).convert("RGB")
+        img.save(buffer, format="PNG")
+        return b64encode(buffer.getvalue()).decode('utf-8')
+    def decode(self, img_str):
+        img_bytes = b64decode(img_str)
+        img = Image.open(io.BytesIO(img_bytes))
+        return img
+
+
+processors = {
+    "image": ImageProcessor(),
+}
