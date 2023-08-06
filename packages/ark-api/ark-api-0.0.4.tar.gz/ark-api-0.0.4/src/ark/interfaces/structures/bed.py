7d71d52e6da33a6efe118ee4d3b11b21a477e834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:42 +0000

    Add package ark-api/0.0.4/ark-api-0.0.4.tar.gz

---
+++ packages/ark-api/ark-api-0.0.4.tar.gz/ark-api-0.0.4/src/ark/interfaces/structures/bed.py
@@ -0,0 +1,15 @@
+from typing import final
+
+from ..spawn_screen import SpawnScreen
+from .structure import Structure
+
+
+@final
+class Bed(Structure):
+
+    def __init__(self, name: str) -> None:
+        super().__init__(name, "assets/wheels/bed.png")
+        self.interface = SpawnScreen()
+
+    def spawn(self) -> None:
+        self.interface.travel_to(self.name)
