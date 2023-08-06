5b2bb94b2b66a165f728681989cb7676e62d4e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:45 +0000

    Add package resourcesing/0.1.3/resourcesing-0.1.3.tar.gz

---
+++ packages/resourcesing/resourcesing-0.1.3.tar.gz/resourcesing-0.1.3/resourcesing/__init__.py
@@ -0,0 +1,9 @@
+import pathlib as __pathlib
+import inspect as __inspect
+from daemonprocessing import rerun as __rerun
+
+def get_exe_path() -> __pathlib.Path:
+    return __rerun.current_executable().split()[-1]
+
+def get_obj_path(object) -> __pathlib.Path:
+    return __pathlib.Path(__inspect.getfile(object))
