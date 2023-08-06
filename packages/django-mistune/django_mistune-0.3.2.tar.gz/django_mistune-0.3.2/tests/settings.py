0d777c479c1dcd625eb4389863fbd1c53c835753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:28 +0000

    Add package django-mistune/0.3.2/django_mistune-0.3.2.tar.gz

---
+++ packages/django-mistune/django_mistune-0.3.2.tar.gz/django_mistune-0.3.2/tests/settings.py
@@ -0,0 +1,22 @@
+from django_mistune.plugins import AddClasses, HeaderLevels, TargetBlankLinks
+
+USE_TZ = True  # Avoid deprecation warning
+
+INSTALLED_APPS = [
+    "django_mistune",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+    },
+]
+
+MISTUNE_STYLES = {
+    "noescape": {"escape": False},
+    "headers": {"plugins": [HeaderLevels(3)]},
+    "classes": {
+        "plugins": [AddClasses({"p": ("a", "b"), "em": "c", "img": "d"})]
+    },
+    "links": {"plugins": [TargetBlankLinks()]},
+}
