b08f37e0a746f2355a70b0bdd59da3cf64ea1964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:34 +0000

    Add package DeepSlice/1.0.1/DeepSlice-1.0.1.tar.gz

---
+++ packages/DeepSlice/DeepSlice-1.0.1.tar.gz/DeepSlice-1.0.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+    name='DeepSlice',
+    packages=['DeepSlice'],
+    version='1.0.1',
+    license='GPL-3.0',
+    description='A package to align histology to 3D brain atlases',
+    author='DeepSlice Team',
+    author_email='harry.carey@medisin.uio.no',
+    url='https://github.com/PolarBean/DeepSlice',
+    download_url='https://github.com/PolarBean/DeepSlice/archive/refs/tags/1.0.1.tar.gz',
+    keywords=['histology', 'brain', 'atlas', 'alignment'],
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'scikit-image',
+        'tensorflow==1.15.0',
+        'typing',
+        'pandas'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+)   
