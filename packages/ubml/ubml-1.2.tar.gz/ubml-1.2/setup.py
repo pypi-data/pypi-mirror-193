bc88a1422a151ad5b603cfbe225d21f9084e7508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:10 +0000

    Add package ubml/1.2/ubml-1.2.tar.gz

---
+++ packages/ubml/ubml-1.2.tar.gz/ubml-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = "ubml",
+     version="1.2",
+     description = "Automatic Machine Learning",
+     packages=["ubml"],
+     author="Ubaid Ahmad",
+     author_email="ubaidp1049@gmail.com",
+     zip_safe=False)
