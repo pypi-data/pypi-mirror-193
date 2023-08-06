04bfe1d5ce282025b6048f355f9e97cc6332a2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:02 +0000

    Add package Data-Extract/1.3/Data%20Extract-1.3.tar.gz

---
+++ packages/Data-Extract/Data%20Extract-1.3.tar.gz/Data Extract-1.3/Source/__init__.py
@@ -0,0 +1,8 @@
+from randomhex import createDigest
+from filterfile import filterFile
+from extractlist import extractList
+from extracttable import extractTable
+from initialize import checks
+from clearconsole import CC
+from randnum import randnum
+
