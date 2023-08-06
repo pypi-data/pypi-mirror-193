514f29879312ab0a95cbd395139d9905734cc58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:13 +0000

    Add package a-o-lib-001/0.0.1/a_o_lib_001-0.0.1.tar.gz

---
+++ packages/a-o-lib-001/a_o_lib_001-0.0.1.tar.gz/a_o_lib_001-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="a_o_lib_001",
+    version="0.0.1",
+    author="Muhammad Ali",
+    author_email="ali.buet106@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
