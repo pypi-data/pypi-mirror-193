d8d9bd8529d65990def3544836d7e5e6287681f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:56 +0000

    Add package mymails/0.0.1/mymails-0.0.1.tar.gz

---
+++ packages/mymails/mymails-0.0.1.tar.gz/mymails-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name="mymails",
+      version="0.0.1",
+      author="Siddharth Chandel",
+      packages=['Mails'],
+      install_requires=[],
+      author_email="siddharthchandel2004@gmail.com",
+      maintainer="Siddharth Chandel",
+      maintainer_email="siddharthchandel2004@gmail.com"
+      )
