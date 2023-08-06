4b859adefdc26a54e854d2a1833184c7d2382a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:17 +0000

    Add package python-mope/0.1.0/python_mope-0.1.0-py3-none-any.whl

---
+++ packages/python-mope/python_mope-0.1.0-py3-none-any.whl/mope/resources/shop.py
@@ -0,0 +1,43 @@
+from mope.models.payments import CreatePaymentResponse, PaymentRequest
+
+
+class ShopResource:
+    def __init__(self, client) -> None:
+        self._client = client
+
+    def create_payment_request(
+        self,
+        amount: int,
+        description: str,
+        order_id: str,
+        currency: str,
+        redirect_url: str,
+    ) -> CreatePaymentResponse:
+        """
+        makes a payment request
+        """
+
+        endpoint = "shop/payment_request"
+        response = self._client.call_api(
+            "POST",
+            endpoint=endpoint,
+            json={
+                "amount": amount,
+                "order_id": order_id,
+                "currency": currency,
+                "description": description,
+                "redirect_url": redirect_url,
+            },
+        )
+        response.raise_for_status()
+        return CreatePaymentResponse.from_json(response.json())
+
+    def get_payment_request(self, payment_id: str) -> PaymentRequest:
+        """
+        fetches a payment request given a payment ID
+        """
+
+        endpoint = "shop/payment_request/%s" % payment_id
+        response = self._client.call_api("GET", endpoint=endpoint)
+        response.raise_for_status()
+        return PaymentRequest.from_json(response.json())
