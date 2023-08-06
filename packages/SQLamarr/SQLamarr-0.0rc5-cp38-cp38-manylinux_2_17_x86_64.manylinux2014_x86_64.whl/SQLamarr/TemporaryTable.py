bf773e8d454afa0e6dc0b6539937bfbc5db90788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:01 +0000

    Add package SQLamarr/0.0rc5/SQLamarr-0.0rc5-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/SQLamarr/SQLamarr-0.0rc5-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/SQLamarr/TemporaryTable.py
@@ -0,0 +1,69 @@
+# (c) Copyright 2022 CERN for the benefit of the LHCb Collaboration. 
+#                                                                             
+# This software is distributed under the terms of the GNU General Public
+# Licence version 3 (GPL Version 3), copied verbatim in the file "LICENCE".
+#                                                                             
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization  
+# or submit itself to any jurisdiction.
+
+import ctypes
+from ctypes import POINTER 
+from SQLamarr import clib, c_TransformerPtr
+from typing import List
+
+from SQLamarr.db_functions import SQLite3DB
+
+clib.new_TemporaryTable.argtypes = (
+    ctypes.c_void_p,        # void *db,
+    ctypes.c_char_p,        # const char* output_table,
+    ctypes.c_char_p,        # const char* comma_separated_outputs,
+    ctypes.c_char_p,        # const char* query,
+    ctypes.c_bool,          # bool make_persistent
+    )
+
+clib.new_TemporaryTable.restype = c_TransformerPtr
+
+class TemporaryTable:
+  """Creates a temporary table from an SQL query. Persitency can be enabled.
+
+  Python binding of `SQLamarr::TemporaryTable`.
+  """
+
+  def __init__ (
+      self, 
+      db: SQLite3DB,
+      output_table: str,
+      outputs: str,
+      query: str,
+      make_persistent: bool = False,
+      ):
+    """
+    Acquires the reference to an open connection to the DB and configure the
+    Transformer.
+
+    @param db: An open database connection;
+    @param output_table: name of the table where the query output is stored;
+    @param outputs: list of the output column names for further reference;
+    @param query: SQL query defining the output columns;
+    @param make_persistent: mark the TABLE as persistent.
+    """
+    self._self = clib.new_TemporaryTable(
+        db.get(),
+        output_table.encode('ascii'),
+        ",".join(outputs).encode('ascii'),
+        query.encode('ascii'),
+        make_persistent,
+        )
+  
+  def __del__(self):
+    """@private: Release the bound class instance"""
+    clib.del_Transformer(self._self)
+
+  @property
+  def raw_pointer(self):
+    """@private: Return the raw pointer to the algorithm."""
+    return self._self
+
+
+
