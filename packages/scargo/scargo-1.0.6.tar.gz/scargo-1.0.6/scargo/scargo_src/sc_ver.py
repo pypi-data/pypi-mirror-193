45acf3c0f5c81dd44fa784d5dd3a47dd088359b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:33 +0000

    Add package scargo/1.0.6/scargo-1.0.6.tar.gz

---
+++ packages/scargo/scargo-1.0.6.tar.gz/scargo-1.0.6/scargo/scargo_src/sc_ver.py
@@ -0,0 +1,10 @@
+# #
+# @copyright Copyright (C) 2023 SpyroSoft Solutions S.A. All rights reserved.
+# #
+
+"""feature function for scargo"""
+from scargo import __version__ as ver
+
+
+def scargo_version():
+    print(f"scargo version: {ver}")
