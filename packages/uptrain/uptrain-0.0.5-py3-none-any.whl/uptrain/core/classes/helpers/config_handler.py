64081c7df43473721dabfec0004c929bd0a70b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:06 +0000

    Add package uptrain/0.0.5/uptrain-0.0.5-py3-none-any.whl

---
+++ packages/uptrain/uptrain-0.0.5-py3-none-any.whl/uptrain/core/classes/helpers/config_handler.py
@@ -0,0 +1,62 @@
+from pydantic import BaseModel, root_validator
+import typing
+from datetime import datetime
+from uptrain.constants import AnnotationMethod
+import numpy as np
+
+
+class AnnotationArgs(BaseModel):
+    method: AnnotationMethod
+    args: typing.Dict = {}
+
+
+class TrainingArgs(BaseModel):
+    orig_training_file: str = ""
+    annotation_method: AnnotationArgs = {}
+    training_func: typing.Callable = None
+    data_transformation_func: typing.Callable = None
+
+
+class EvaluationArgs(BaseModel):
+    inference_func: typing.Callable = None
+    golden_testing_dataset: str = None
+    metrics_to_check: typing.List[str] = []
+
+
+class Config(BaseModel):
+    training_args: TrainingArgs = TrainingArgs()
+    evaluation_args: EvaluationArgs = EvaluationArgs()
+    data_identifier: str = "id"
+    checks: typing.List[typing.Dict] = []
+    retrain: bool = True
+    retrain_after: int = 100000000000
+    retraining_folder: str = "uptrain_smart_data"
+    data_id: str = "id"
+    log_folder: str = "uptrain_logs"
+    tb_logging: bool = False
+    st_logging: bool = False
+    feat_name_list: list = None
+    cluster_visualize_func: typing.Callable = None
+    use_cache: bool = False
+
+    @root_validator()
+    def only_one_logging(cls, v):
+        if v.get("tb_logging") and v.get("st_logging"):
+            raise ValueError("Use only one logging type: Tensorboard or Streamlit")
+        return v
+
+
+class InputArgs(BaseModel):
+    data: dict
+    id: typing.Optional[typing.Union[list, np.ndarray]]
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+class GroundTruthArgs(BaseModel):
+    gt: typing.Union[np.ndarray, list]
+    id: typing.Union[np.ndarray, list]
+
+    class Config:
+        arbitrary_types_allowed = True
---
+++ packages/uptrain/uptrain-0.0.5-py3-none-any.whl/uptrain/core/classes/logging/log_streamlit.py
@@ -0,0 +1,85 @@
+import os
+import csv
+import threading
+import json
+import numpy as np
+
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+class StreamlitLogs:
+    def __init__(self, log_folder, log_every=10):
+        self.log_every = log_every
+        self.placeholders = {}
+        self.prev_values = {}
+        self.dfs = {}
+        self.counts = {}
+        self.log_folder = log_folder
+
+        remote_st_py_file = "https://raw.githubusercontent.com/uptrain-ai/uptrain/dashboard/uptrain/core/classes/logging/st_run.py"
+        # remote_st_py_file = "../../uptrain/core/classes/logging/st_run.py"
+
+        cmd = "streamlit run " + remote_st_py_file + " -- " + self.log_folder
+        launch_st = lambda: os.system(cmd)
+        t = threading.Thread(target=launch_st, args=([]))
+        t.start()
+
+    def add_scalars(self, dict, folder):
+        # CSV file that includes the data
+        for key in dict.keys():
+            if key == "count":
+                continue
+            file_name = os.path.join(folder, key + ".csv")
+            if not os.path.isfile(file_name):
+                with open(file_name, "w", newline="") as f_object:
+                    writer = csv.writer(f_object)
+                    writer.writerow([key, "count"])
+                    f_object.close()
+
+            with open(file_name, "a") as f_object:
+                writer_object = csv.writer(f_object)
+                writer_object.writerow([dict[key], dict["count"]])
+                f_object.close()
+
+    def add_histogram(self, data, folder, count=-1):
+        file_name = os.path.join(folder, str(count) + ".json")
+        with open(file_name, "w") as f:
+            json.dump(data, f, cls=NumpyEncoder)
+
+    def add_alert(self, alert_name, alert, folder):
+        file_name = os.path.join(folder, str(alert_name) + ".json")
+        with open(file_name, "w") as f:
+            json.dump(alert, f)
+
+    def add_bar_graphs(self, data, folder, count=-1):
+        file_name = os.path.join(folder, str(count) + ".json")
+        with open(file_name, "w") as f:
+            json.dump(data, f, cls=NumpyEncoder)
+
+    # def feat_slicing(self, fw):
+    #     relevant_feat_list = st.sidebar.multiselect(
+    #         "Select features", fw.feat_name_list, fw.feat_name_list[0]
+    #     )
+    #     scol1, scol2 = st.sidebar.columns(2)
+    #     limit_list = []
+    #     for feat in relevant_feat_list:
+    #         with scol1:
+    #             lower_limit = st.sidebar.slider(f"Lower limit for {feat}", value=0.5)
+    #         with scol2:
+    #             upper_limit = st.sidebar.slider(f"Upper limit for {feat}", value=0.55)
+    #         limit_list.append((lower_limit, upper_limit))
+    #     # button_callback = lambda : fw.feat_slicing(relevant_feat_list, limit_list)
+
+    #     # TODO: Function is run from top every time the button is clicked
+    #     button = st.sidebar.button(
+    #         "Check",
+    #         help="Check anomalies for this function",
+    #     )
+    #     # on_click=button_callback)
+    #     if button:
+    #         fw.feat_slicing(relevant_feat_list, limit_list)
