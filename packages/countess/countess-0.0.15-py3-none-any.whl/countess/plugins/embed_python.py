971e54131d6020c62fb049efe2ba988394d879ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:10 +0000

    Add package countess/0.0.15/countess-0.0.15-py3-none-any.whl

---
+++ packages/countess/countess-0.0.15-py3-none-any.whl/countess/plugins/embed_python.py
@@ -0,0 +1,50 @@
+import dask.dataframe as dd
+import pandas as pd  # type: ignore
+
+from countess import VERSION
+from countess.core.parameters import (
+    ArrayParam,
+    BooleanParam,
+    StringParam,
+    TextParam,
+)
+from countess.core.plugins import DaskTransformPlugin
+from countess.core.logger import Logger
+
+def process(df: pd.DataFrame, codes):
+
+    for code in codes:
+        result = df.eval(code)
+        if isinstance(result, (dd.Series, pd.Series)):
+            # this was a filter
+            df["_filter"] = result
+            df = df.query("_filter").drop(columns="_filter")
+        else:
+            # this was a column assignment
+            df = result
+
+    return df
+
+
+class EmbeddedPythonPlugin(DaskTransformPlugin):
+
+    name = "Embedded Python"
+    title = "Embedded Python"
+    description = "Embed Python code into CountESS"
+    version = VERSION
+
+    parameters = {"code": ArrayParam("Code", TextParam("Code"))}
+
+    def run_dask(self, df, logger: Logger) -> dd.DataFrame:
+        assert isinstance(self.parameters["code"], ArrayParam)
+
+        codes = [
+            p.value.replace("\n", " ")
+            for p in self.parameters["code"]
+            if isinstance(p, TextParam) and p.value.strip()
+        ]
+
+        if isinstance(df, dd.DataFrame):
+            return df.map_partitions(process, codes)
+        else:
+            return process(df, codes)
