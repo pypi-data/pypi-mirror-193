90ec326239839afdbc815c254dc5137c9617f05a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:31 +0000

    Add package mqtt-tool/0.1.0/mqtt_tool-0.1.0.tar.gz

---
+++ packages/mqtt-tool/mqtt_tool-0.1.0.tar.gz/mqtt_tool-0.1.0/README.md
@@ -0,0 +1,6 @@
+you need to download mosquitto to local machine and run it. 
+[...]
+Then you can use the following command to run the mosquitto broker on the local machine:
+    
+    
+    mosquitto -c mosquitto.conf
---
+++ packages/mqtt-tool/mqtt_tool-0.1.0.tar.gz/mqtt_tool-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mqtt_tool',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://your_package_url.com',
+    license='LICENSE.txt',
+    author='Leo',
+    author_email='52guhaha@gmail.com',
+    description='a simple mqtt tool',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'paho-mqtt',
+        'mosquitto',
+    ],
+)
