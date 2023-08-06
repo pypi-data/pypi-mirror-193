1d90c5d814af7971e5f40850ab018338c38c75c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:16 +0000

    Add package types-PyScreeze/0.1.0.7/types-PyScreeze-0.1.0.7.tar.gz

---
+++ packages/types-PyScreeze/types-PyScreeze-0.1.0.7.tar.gz/types-PyScreeze-0.1.0.7/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+name = "types-PyScreeze"
+description = "Typing stubs for PyScreeze"
+long_description = '''
+## Typing stubs for PyScreeze
+
+This is a PEP 561 type stub package for the `PyScreeze` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`PyScreeze`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/PyScreeze. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `078c6a095892e12c94462889fa43c2c2dc8280ad`.
+'''.lstrip()
+
+setup(name=name,
+      version="0.1.0.7",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/PyScreeze.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=['types-Pillow'],
+      packages=['pyscreeze-stubs'],
+      package_data={'pyscreeze-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
