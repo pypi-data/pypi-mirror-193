4ff213a73ee923a5318d5ec73ad9f0eb63adf100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:13 +0000

    Add package digi/0.2.8/digi-0.2.8.tar.gz

---
+++ packages/digi/digi-0.2.8.tar.gz/digi-0.2.8/digi/data/flow.py
@@ -0,0 +1,12 @@
+"""A collection of predefined dataflows."""
+
+refresh_ts = """switch ( 
+    case has(event_ts) => yield this | put ts := now()
+    case has(ts) => put event_ts := ts | put ts := now() 
+    default => put event_ts := now() | put ts := event_ts
+)"""
+
+patch_ts = "switch ( case has(ts) => yield this " \
+           "default => put ts := now() )"
+
+drop_meta = "not __meta"
