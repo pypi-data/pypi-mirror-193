e19b9e82941fd291191ce46d450e0dd564e6dc05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:58 +0000

    Add package passwordgenerator-uz/0.0.1/passwordgenerator_uz-0.0.1.tar.gz

---
+++ packages/passwordgenerator-uz/passwordgenerator_uz-0.0.1.tar.gz/passwordgenerator_uz-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="passwordgenerator_uz",
+    version="0.0.1",
+    description="Ushbu kutubxona yordamida parol generatsiya qilib olishingiz mumkin!",
+    author="Ozodbek Sobirjonovich",
+    author_email="ozodbeksobirjonovich@gmail.com"
+)
