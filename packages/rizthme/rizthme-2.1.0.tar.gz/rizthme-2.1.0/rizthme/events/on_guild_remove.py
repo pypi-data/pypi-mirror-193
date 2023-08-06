1442c8e0be069d0ec9a05289afecc02d0f1195bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:08 +0000

    Add package rizthme/2.1.0/rizthme-2.1.0.tar.gz

---
+++ packages/rizthme/rizthme-2.1.0.tar.gz/rizthme-2.1.0/rizthme/events/on_guild_remove.py
@@ -0,0 +1,17 @@
+import logging
+import discord
+from rizthme.models.threads import Player
+
+
+def init(client: "Client"):
+
+    @client.event
+    async def on_guild_remove(guild: discord.Guild):
+        """
+        When a guild is removed, remove the music players linked to it.
+
+        :param guild: The guild that was removed.
+        :type guild: discord.Guild
+        """
+        logging.info(f'{guild.name} has been removed')
+        Player.get(guild).delete_thread()
