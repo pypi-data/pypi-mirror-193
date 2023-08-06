355565fbce5c7beaae0318078cc50a8f03ce4dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:36 +0000

    Add package bme/1.3.2/bme-1.3.2.tar.gz

---
+++ packages/bme/bme-1.3.2.tar.gz/bme-1.3.2/bme/init.py
@@ -0,0 +1,9 @@
+from bme.config_mng import Config
+from bme.saved_types.bookmark import Bookmark
+from bme.saved_types.sequence import Sequence
+
+
+def init_all():
+    Bookmark.init()
+    Sequence.init()
+    Config.init()
